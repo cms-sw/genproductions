a3a0751a166cb758453e9207b49cdb7b7838cfc7
kalavase <sha1-37b724a660ad78b379b1c02b36b874291d39b117@cern.ch>
2008-07-02 16:30:28 +0000

    Configuration/GenProduction/python/PYTHIA6_TTbar_10TeV_cff_py__GEN_SIM_DIGI_L1_DIGI2RAW_HLT.py

--- python/PYTHIA6_MinBias_10TeV_cff.py
+++ python/PYTHIA6_MinBias_10TeV_cff.py
@@ -4,7 +4,7 @@ from Configuration.GenProduction.PythiaUESettings_cfi import *
 source = cms.Source("PythiaSource",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     maxEventsToPrint = cms.untracked.int32(0),
-    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
     filterEfficiency = cms.untracked.double(1.0),
     comEnergy = cms.untracked.double(10000.0),
     crossSection = cms.untracked.double(55000000000.),
@@ -28,8 +28,8 @@ source = cms.Source("PythiaSource",
 )
 
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
-    name = cms.untracked.string('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/PYTHIA6_MinBias_10TeV_cff.py,v $'),
+    version = cms.untracked.string('$Revision: 1.3 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_MinBias_10TeV_cff.py,v $'),
     annotation = cms.untracked.string('PYTHIA6-MinBias at 10TeV')
 )
 
