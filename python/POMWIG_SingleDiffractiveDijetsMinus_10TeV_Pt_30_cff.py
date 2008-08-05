c1359f63a7dd8b5a1ab55bc16da0421ff1273daf
Antonio Vilela Pereira <sha1-99b95d901683f11ac96ebd76fd2bbc9bee4e1aa0@cern.ch>
2008-08-05 18:41:00 +0000

    adding Pomwig files

---
+++ python/POMWIG_SingleDiffractiveDijetsMinus_10TeV_Pt_30_cff.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+
+# Pomwig DataCard
+source = cms.Source("PomwigSource",
+    herwigVerbosity = cms.untracked.int32(1),
+    herwigHepMCVerbosity = cms.untracked.bool(False),
+    herwigLhapdfVerbosity = cms.untracked.int32(0),
+    maxEventsToPrint = cms.untracked.int32(2),
+    printCards = cms.untracked.bool(True),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.untracked.double(10000.0),
+    crossSection = cms.untracked.double(-1.),
+    diffTopology = cms.int32(2),
+    h1fit = cms.int32(2),	
+    HerwigParameters = cms.PSet(	
+	parameterSets = cms.vstring('SD2InclusiveJets'),
+	SD2InclusiveJets = cms.vstring(
+		'NSTRU      = 14         ! H1 Pomeron Fit B',
+                'Q2WWMN     = 1E-6       ! Minimum |t|',
+                'Q2WWMX     = 4.0        ! Maximum |t|',
+                'YWWMIN     = 1E-6       ! Minimum xi',
+                'YWWMAX     = 0.2        ! Maximum xi',
+                'IPROC      = 11500      ! Process PomP -> jets',
+                'PTMIN      = 30         ! 2->2 PT min',
+                'MODPDF(1)  = 10150      ! Set MODPDF CTEQ61',
+                'MODPDF(2)  = -1         ! Set MODPDF'),
+    )
+)	
+
+pomwigfilter = cms.EDFilter("PomwigFilter")
+
+ProductionFilterSequence = cms.Sequence(pomwigfilter)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: $'),
+    name = cms.untracked.string('$Source: $'),
+    annotation = cms.untracked.string('POMWIG SD minus Di-jets ptmin 30 GeV at 10TeV')
+)
+
