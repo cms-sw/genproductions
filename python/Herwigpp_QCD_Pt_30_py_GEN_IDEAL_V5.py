d3df8cbe71b66a615aca3af5e69cd87fb5f83bee
cmsbuild <>
2008-08-14 13:26:12 +0000

    This commit was manufactured by cvs2git to create tag 'V00-00-03'.
    Sprout from master 2008-08-14 13:26:08 UTC mthomas <sha1-c3ddb5d317a38815e0d99b0f7ef83c94092833ba@cern.ch> 'Configuration/GenProduction/python/PYTHIA6_MinBias_10TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL.py'
    Cherrypick from master 2008-07-22 08:35:28 UTC Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch> 'updated tags':
        python/Herwigpp_QCD_Pt_1400.py
        python/Herwigpp_QCD_Pt_1400_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_1400_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_15.py
        python/Herwigpp_QCD_Pt_15_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_15_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_170.py
        python/Herwigpp_QCD_Pt_170_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_170_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_2200.py
        python/Herwigpp_QCD_Pt_2200_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_2200_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_30.py
        python/Herwigpp_QCD_Pt_300.py
        python/Herwigpp_QCD_Pt_3000.py
        python/Herwigpp_QCD_Pt_3000_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_3000_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_300_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_300_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_30_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_30_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_470.py
        python/Herwigpp_QCD_Pt_470_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_470_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_80.py
        python/Herwigpp_QCD_Pt_800.py
        python/Herwigpp_QCD_Pt_800_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_800_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_80_py_GEN_IDEAL_V5.py
        python/Herwigpp_QCD_Pt_80_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_IDEAL_V5.py
        python/Herwigpp_mkrun.sh

---
+++ python/Herwigpp_QCD_Pt_30_py_GEN_IDEAL_V5.py
@@ -0,0 +1,176 @@
+# Auto generated configuration file
+# using: 
+# $Revision: 1.45 $
+# $Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v $
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('Configuration/StandardSequences/Geometry_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/Generator_cff')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/Generator_cff')
+process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.ReleaseValidation = cms.untracked.PSet(
+    primaryDatasetName = cms.untracked.string('RelValConfiguration/GenProduction/python/Herwigpp_QCD_Pt_30_py'),
+    totalNumberOfEvents = cms.untracked.int32(5000),
+    eventsPerJob = cms.untracked.int32(250)
+)
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.6 $'),
+    annotation = cms.untracked.string('Summer08 CSA: Herwig++ generation of QCD events, MRST2001 used, MinKT=30 GeV'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/Herwigpp_base_cff.py,v $')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(600000)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+# Input source
+process.source = cms.Source("ThePEGSource",
+    cm10TeV = cms.vstring('set /Herwig/Generators/LHCGenerator:EventHandler:LuminosityFunction:Energy 10000.0', 
+        'set /Herwig/Shower/Evolver:IntrinsicPtGaussian 2.1*GeV'),
+    run = cms.string('LHC'),
+    repository = cms.string('HerwigDefaults.rpo'),
+    QCDCSAParameters = cms.vstring('cd /Herwig/MatrixElements/', 
+        'insert SimpleQCD:MatrixElements[0] MEQCD2to2', 
+        'cd /', 
+        'set /Herwig/Cuts/JetKtCut:MinKT 30*GeV', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:Algorithm 1'),
+    dataLocation = cms.string('${HERWIGPATH}'),
+    filterEfficiency = cms.untracked.double(1.0),
+    configFiles = cms.vstring(),
+    disableCtau10mmDecays = cms.vstring('cd /Herwig/Particles', 
+        'set K-/K-->nu_ebar,e-;:OnOff Off', 
+        'set K+/K+->nu_e,e+;:OnOff Off', 
+        'set K-/K-->nu_mubar,mu-;:OnOff Off', 
+        'set K+/K+->nu_mu,mu+;:OnOff Off', 
+        'set K-/K-->pi0,nu_ebar,e-;:OnOff Off', 
+        'set K+/K+->pi0,nu_e,e+;:OnOff Off', 
+        'set K-/K-->pi0,nu_mubar,mu-;:OnOff Off', 
+        'set K+/K+->pi0,nu_mu,mu+;:OnOff Off', 
+        'set K-/K-->pi0,pi0,nu_ebar,e-;:OnOff Off', 
+        'set K+/K+->pi0,pi0,nu_e,e+;:OnOff Off', 
+        'set K-/K-->pi-,gamma,gamma;:OnOff Off', 
+        'set K+/K+->pi+,gamma,gamma;:OnOff Off', 
+        'set K-/K-->pi-,pi0;:OnOff Off', 
+        'set K+/K+->pi+,pi0;:OnOff Off', 
+        'set K-/K-->pi-,pi0,pi0;:OnOff Off', 
+        'set K+/K+->pi+,pi0,pi0;:OnOff Off', 
+        'set K-/K-->pi+,pi-,nu_ebar,e-;:OnOff Off', 
+        'set K+/K+->pi+,pi-,nu_e,e+;:OnOff Off', 
+        'set K-/K-->pi+,pi-,nu_mubar,mu-;:OnOff Off', 
+        'set K+/K+->pi+,pi-,nu_mu,mu+;:OnOff Off', 
+        'set K-/K-->pi+,pi-,pi-;:OnOff Off', 
+        'set K+/K+->pi+,pi+,pi-;:OnOff Off', 
+        'set K_L0/K_L0->gamma,e-,e+;:OnOff Off', 
+        'set K_L0/K_L0->gamma,gamma;:OnOff Off', 
+        'set K_L0/K_L0->pi0,gamma,gamma;:OnOff Off', 
+        'set K_L0/K_L0->pi0,pi0;:OnOff Off', 
+        'set K_L0/K_L0->pi0,pi0,pi0;:OnOff Off', 
+        'set K_L0/K_L0->pi+,nu_ebar,e-;:OnOff Off', 
+        'set K_L0/K_L0->pi-,nu_e,e+;:OnOff Off', 
+        'set K_L0/K_L0->pi+,nu_mubar,mu-;:OnOff Off', 
+        'set K_L0/K_L0->pi-,nu_mu,mu+;:OnOff Off', 
+        'set K_L0/K_L0->pi+,pi0,nu_ebar,e-;:OnOff Off', 
+        'set K_L0/K_L0->pi-,pi0,nu_e,e+;:OnOff Off', 
+        'set K_L0/K_L0->pi+,pi-;:OnOff Off', 
+        'set K_L0/K_L0->pi+,pi-,pi0;:OnOff Off', 
+        'set K_S0/K_S0->gamma,gamma;:OnOff Off', 
+        'set K_S0/K_S0->pi0,pi0;:OnOff Off', 
+        'set K_S0/K_S0->pi+,nu_ebar,e-;:OnOff Off', 
+        'set K_S0/K_S0->pi-,nu_e,e+;:OnOff Off', 
+        'set K_S0/K_S0->pi+,pi-,e-,e+;:OnOff Off', 
+        'set K_S0/K_S0->pi+,pi-;:OnOff Off', 
+        'set Lambda0/Lambda0->n0,gamma;:OnOff Off', 
+        'set Lambda0/Lambda0->n0,pi0;:OnOff Off', 
+        'set Lambda0/Lambda0->p+,nu_ebar,e-;:OnOff Off', 
+        'set Lambda0/Lambda0->p+,pi-,gamma;:OnOff Off', 
+        'set Lambda0/Lambda0->p+,pi-;:OnOff Off', 
+        'set Lambdabar0/Lambdabar0->nbar0,gamma;:OnOff Off', 
+        'set Lambdabar0/Lambdabar0->nbar0,pi0;:OnOff Off', 
+        'set Lambdabar0/Lambdabar0->pbar-,nu_e,e+;:OnOff Off', 
+        'set Lambdabar0/Lambdabar0->pbar-,pi+,gamma;:OnOff Off', 
+        'set Lambdabar0/Lambdabar0->pbar-,pi+;:OnOff Off', 
+        'set mu+/mu+->nu_mubar,nu_e,e+;:OnOff Off', 
+        'set mu-/mu-->nu_mu,nu_ebar,e-;:OnOff Off', 
+        'set Omegabar+/Omegabar+->Lambdabar0,K+;:OnOff Off', 
+        'set Omegabar+/Omegabar+->Xibar0,nu_e,e+;:OnOff Off', 
+        'set Omegabar+/Omegabar+->Xibar0,pi+;:OnOff Off', 
+        'set Omegabar+/Omegabar+->Xibar+,pi0;:OnOff Off', 
+        'set Omega-/Omega-->Lambda0,K-;:OnOff Off', 
+        'set Omega-/Omega-->Xi0,nu_ebar,e-;:OnOff Off', 
+        'set Omega-/Omega-->Xi0,pi-;:OnOff Off', 
+        'set Omega-/Omega-->Xi-,pi0;:OnOff Off', 
+        'set pi-/pi-->nu_ebar,e-;:OnOff Off', 
+        'set pi+/pi+->nu_e,e+;:OnOff Off', 
+        'set pi-/pi-->nu_mubar,mu-;:OnOff Offset pi-/pi-->nu_mubar,mu-;:OnOff Off', 
+        'set pi+/pi+->nu_mu,mu+;:OnOff Off', 
+        'set Sigmabar+/Sigmabar+->nbar0,nu_e,e+;:OnOff Off', 
+        'set Sigmabar+/Sigmabar+->nbar0,pi+,gamma;:OnOff Off', 
+        'set Sigmabar-/Sigmabar-->nbar0,pi-;:OnOff Off', 
+        'set Sigmabar+/Sigmabar+->nbar0,pi+;:OnOff Off', 
+        'set Sigmabar-/Sigmabar-->pbar-,gamma;:OnOff Off', 
+        'set Sigmabar-/Sigmabar-->pbar-,pi0;:OnOff Off', 
+        'set Sigma-/Sigma-->n0,nu_ebar,e-;:OnOff Off', 
+        'set Sigma-/Sigma-->n0,pi-,gamma;:OnOff Off', 
+        'set Sigma-/Sigma-->n0,pi-;:OnOff Off', 
+        'set Sigma+/Sigma+->n0,pi+;:OnOff Off', 
+        'set Sigma+/Sigma+->p+,gamma;:OnOff Off', 
+        'set Sigma+/Sigma+->p+,pi0;:OnOff Off', 
+        'set Xi0/Xi0->Lambda0,gamma;:OnOff Off', 
+        'set Xi0/Xi0->Lambda0,pi0;:OnOff Off', 
+        'set Xi0/Xi0->Sigma0,gamma;:OnOff Off', 
+        'set Xibar0/Xibar0->Lambdabar0,gamma;:OnOff Off', 
+        'set Xibar0/Xibar0->Lambdabar0,pi0;:OnOff Off', 
+        'set Xibar0/Xibar0->Sigmabar0,gamma;:OnOff Off', 
+        'set Xibar+/Xibar+->Lambdabar0,nu_e,e+;:OnOff Off', 
+        'set Xibar+/Xibar+->Lambdabar0,pi+;:OnOff Off', 
+        'set Xi-/Xi-->Lambda0,nu_ebar,e-;:OnOff Off', 
+        'set Xi-/Xi-->Lambda0,pi-;:OnOff Off', 
+        'cd /'),
+    generatorModule = cms.string('/Herwig/Generators/LHCGenerator'),
+    crossSection = cms.untracked.double(111.746),
+    eventHandlers = cms.string('/Herwig/EventHandlers'),
+    parameterSets = cms.vstring('cm10TeV', 
+        'QCDCSAParameters', 
+        'basicSetup', 
+        'disableCtau10mmDecays'),
+    basicSetup = cms.vstring('cd /Herwig/Generators', 
+        'set LHCGenerator:NumberOfEvents 10000000', 
+        'set LHCGenerator:DebugLevel 1', 
+        'set LHCGenerator:PrintEvent 0', 
+        'set LHCGenerator:MaxErrors 10000', 
+        'cd /')
+)
+
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('Herwigpp_QCD_Pt_30_py_GEN.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN'),
+        filterName = cms.untracked.string('IDEAL_V5')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Other statements
+process.GlobalTag.globaltag = 'IDEAL_V5::All'
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.out_step)
