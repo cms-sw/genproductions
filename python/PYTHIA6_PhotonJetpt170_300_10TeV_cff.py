0e93d97f07302858f746304437513401183879f5
Leonard Apanasevich <sha1-81bf5b6d8589229bad93440d7931007f605561fa@cern.ch>
2008-08-28 20:58:32 +0000

    for TSG production

---
+++ python/PYTHIA6_PhotonJetpt170_300_10TeV_cff.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+source = cms.Source("PythiaSource",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.untracked.double(10000.0),
+    crossSection = cms.untracked.double(47.01),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+processParameters = cms.vstring(
+            'MSEL=10',
+            'CKIN(3)=170  ! minimum pt hat for hard interactions',
+            'CKIN(4)=300  ! maximum pt hat for hard interactions'),
+   # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+
+       
+   
+
+
+    )
+)
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    name = cms.untracked.string
+('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_PhotonJetpt170_300_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PhotonJetpt170-300 at 10TeV')
+)
+
