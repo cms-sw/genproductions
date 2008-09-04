2a0d5a010ee4b3f157de3eda4b7d0ad8494873db
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2008-09-04 15:49:52 +0000

    add MB at 900GeV applications

---
+++ python/PYTHIA6_MinBias_900GeV_cff.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms
+
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+source = cms.Source("PythiaSource",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.untracked.double(900.0),
+    crossSection = cms.untracked.double(55000000000.),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0         ! User defined processes', 
+            'MSUB(11)=1     ! Min bias process', 
+            'MSUB(12)=1     ! Min bias process', 
+            'MSUB(13)=1     ! Min bias process', 
+            'MSUB(28)=1     ! Min bias process', 
+            'MSUB(53)=1     ! Min bias process', 
+            'MSUB(68)=1     ! Min bias process', 
+            'MSUB(92)=1     ! Min bias process, single diffractive', 
+            'MSUB(93)=1     ! Min bias process, single diffractive', 
+            'MSUB(94)=1     ! Min bias process, double diffractive', 
+            'MSUB(95)=1     ! Min bias process'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.7 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_MinBias_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6-MinBias at 900GeV')
+)
+
