ed8655e3b1b572615dbcb42188494d867dc136ad
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2008-09-05 09:47:33 +0000

    a bunch of di-pion production configs

---
+++ python/PARTICLEGUN_Dipion_e50_eta5_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_IDEAL.py
@@ -0,0 +1,79 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.71 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('DIGI2RAW')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/Generator_cff')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/Generator_cff')
+process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff')
+process.load('Configuration/StandardSequences/Sim_cff')
+process.load('Configuration/StandardSequences/Digi_cff')
+process.load('Configuration/StandardSequences/SimL1Emulator_cff')
+process.load('L1TriggerConfig/L1GtConfigProducers/Luminosity/lumi1030.L1Menu2008_2E30_Unprescaled_cff')
+process.load('Configuration/StandardSequences/DigiToRaw_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: $'),
+    annotation = cms.untracked.string('di-pion of 50 GeV randomly distributed in the entire Calorimetry'),
+    name = cms.untracked.string('$Source: $')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+# Input source
+process.source = cms.Source("FlatRandomEGunSource",
+    PGunParameters = cms.untracked.PSet(
+        PartID = cms.untracked.vint32(211),
+        MaxEta = cms.untracked.double(5.1),
+        MaxPhi = cms.untracked.double(3.14159265359),
+        MinEta = cms.untracked.double(-5.1),
+        MinE = cms.untracked.double(50.0),
+        MinPhi = cms.untracked.double(-3.14159265359),
+        MaxE = cms.untracked.double(50.0)
+    ),
+    AddAntiParticle = cms.untracked.bool(True),
+    firstRun = cms.untracked.uint32(1)
+)
+
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+    fileName = cms.untracked.string('PARTICLEGUN_Dipion_e50_eta5_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-RAW'),
+        filterName = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'IDEAL_V9::All'
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step,process.out_step)
---
+++ python/PARTICLEGUN_Dipion_e9_eta5_cff_py_RAW2DIGI_RECO_IDEAL.py
@@ -0,0 +1,58 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.71 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/Generator_cff')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/RawToDigi_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.71 $'),
+    annotation = cms.untracked.string('Configuration/GenProduction/python/PARTICLEGUN_Dipion_e9_eta5_cff.py nevts:1000'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:PARTICLEGUN_Dipion_e9_eta5_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW.root')
+)
+
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+    fileName = cms.untracked.string('PARTICLEGUN_Dipion_e9_eta5_cff_py_RAW2DIGI_RECO.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-RECO'),
+        filterName = cms.untracked.string('')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'IDEAL_V9::All'
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.reconstruction_step,process.out_step)
