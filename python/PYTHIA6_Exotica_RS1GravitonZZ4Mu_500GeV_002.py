9c7265301d78a16fd93c2deb1c5afdb0efd8f95a
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2008-09-23 03:06:24 +0000

    updated with HLT changes

---
+++ python/PYTHIA6_Exotica_RS1GravitonZZ4Mu_500GeV_002.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+source = cms.Source("PythiaSource",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.untracked.double(10000.0),
+    crossSection = cms.untracked.double(0.003304),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('PMAS(347,1)=500.0        !mass of RS Graviton', 
+            'MSEL=0                  !(D=1)', 
+            'PARP(50) = 0.108  ! Dimensionless coupling constant for RS G*', 
+            'MSTJ(41)=1              !Switch off Pythia QED bremsshtrahlung', 
+            'MSUB(391)=1             !', 
+            'MSUB(392)=1             !', 
+            '5000039:ALLOFF           ', 
+            '5000039:ONIFMATCH 23 23  ', 
+            '23:ALLOFF                ', 
+            '23:ONIFANY 13 15         '),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    annotation = cms.untracked.string('RS1GravitonZZ4Mu_500GeV_002'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/Configuration/GenProduction/python/PYTHIA6_Exotica_RS1GravitonZZ4Mu_500GeV_002.py,v $')
+)
+
