954c45b341fc1557456f285bfd565d6bdc155de0
Gaelle Boudoul <sha1-a82f96c922d75aadc2b17eace2ae9543e16ce583@cern.ch>
2008-10-08 06:44:11 +0000

    config file updated

--- python/TKDPG_21X_TkCosmic_B0T_GenSimRaw_cfg.py
+++ python/TKDPG_21X_TkCosmic_B0T_GenSimRaw_cfg.py
@@ -1,52 +1,96 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("GenSimRaw")
-process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
-process.load("GeneratorInterface.CosmicMuonGenerator.CMSCGENsource_cfi")
-process.load("GeneratorInterface.GenFilters.CosmicGenFilterHelix_cff")
-process.load("Configuration.StandardSequences.Simulation_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.load("SimTracker.SiPixelDigitizer.PixelDigi_cfi")
-process.load("Configuration.StandardSequences.MagneticField_0T_cff")
-process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
-process.load("Configuration.StandardSequences.VtxSmearedNoSmear_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.load("Configuration.StandardSequences.DigiToRaw_cff")
-process.load("Configuration.StandardSequences.L1Emulator_cff")
-process.load("Configuration.EventContent.EventContent_cff")
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("L1Trigger.DTTrackFinder.dttfDigis_cfi")
+
+
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1000)
 )
-process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True)
-)
+
+#--------- Standard sequences
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.StandardSequences.GeometryPilot2_cff")
+process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
+process.load("Configuration.StandardSequences.VtxSmearedNoSmear_cff")
+process.load("Configuration.StandardSequences.Simulation_cff")
+process.load("Configuration.StandardSequences.DigiToRaw_cff")
+process.load("Configuration.EventContent.EventContentCosmics_cff")
+
+#--- random number generator (new in 21x) 
+
+process.load('Configuration/StandardSequences/Services_cff')
+process.RandomNumberGeneratorService.theSource.initialSeed = cms.untracked.uint32(12345688)
+
+
+#--- non-collision events
 
 process.g4SimHits.NonBeamEvent = True
 process.g4SimHits.Generator.ApplyEtaCuts = False
-
-process.FEVT = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('TkCosmicGenSimRaw0T.root'),
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('c')
-    ),
-    outputCommands = cms.untracked.vstring('keep *', 
-        'drop FEDRawDataCollection_*_*_*', 
-        'keep FEDRawDataCollection_rawDataCollector__GenSimRaw')
-)
+#process.psim = cms.Sequence(cms.SequencePlaceholder("randomEngineStateProducer")*process.g4SimHits)
 
 
-process.GlobalTag.globaltag = 'STARTUP_V7::All'
 
-process.c = cms.Path(process.cosmicInTracker*process.g4SimHits*process.pdigi*process.L1Emulator*process.DigiToRaw)
-process.outpath = cms.EndPath(process.FEVT)
-
+#--- Cosmic Generator
+process.load("GeneratorInterface.CosmicMuonGenerator.CMSCGENsource_cfi")
+process.load("GeneratorInterface.GenFilters.CosmicGenFilterHelix_cff")
 process.CosMuoGenSource.MinP = 4.
+
+
+#--- Conditions
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag =  'COSMMC_21X::All'
+
+#--------- Magnetic field Field should be OFF for this sample
+process.load("Configuration.StandardSequences.MagneticField_0T_cff")
+
+
+#--------- Include L1 emulator
+process.load("Configuration.StandardSequences.SimL1Emulator_cff")
+process.load("L1Trigger.DTTrackFinder.dttfDigis_cfi")
+process.dttfDigis.Open_LUTs = True
+
+#-------- SPECIAL stuff for subsystems
+
+#Pixel digitization
+process.load("SimTracker.SiPixelDigitizer.PixelDigi_cfi")
 process.simSiPixelDigis.TofLowerCut = 18.5
 process.simSiPixelDigis.TofUpperCut = 43.5
 process.simSiStripDigis.CosmicDelayShift = 31.
-process.dttfDigis.Open_LUTs = True
+
+
+# Ecal digitization: change thresholds for selective readout and zero suppression
+
+process.simEcalUnsuppressedDigis.cosmicsPhase = True
+process.simEcalUnsuppressedDigis.cosmicsShift = 1.
+process.simEcalDigis.ebDccAdcToGeV = cms.double(0.00875)
+process.simEcalDigis.srpBarrelLowInterestChannelZS = cms.double(0.0153125)
+process.EcalTrigPrimESProducer.DatabaseFile = cms.untracked.string('TPG_cosmics_ZS.txt.gz')
+
+# Remove HCAL zero suppression
+
+process.simHcalDigis.hbhe.level = -10000
+process.simHcalDigis.ho.level   = -10000
+process.simHcalDigis.hf.level   = -10000
+
+
+
+process.FEVT = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('TkCosmicGenSimRaw0T.root'),
+    dataset = cms.untracked.PSet(
+    dataTier = cms.untracked.string('GEN-SIM-RAW'),
+     filterName = cms.untracked.string('')
+    ),    
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('c')
+	)
+    )
+
+
+#---------------------------------------
+# paths 
+#---------------------------------------
+process.c = cms.Path(process.cosmicInTracker*process.g4SimHits*process.pdigi*process.SimL1Emulator*process.DigiToRaw)
+process.outpath = cms.EndPath(process.FEVT)
 
