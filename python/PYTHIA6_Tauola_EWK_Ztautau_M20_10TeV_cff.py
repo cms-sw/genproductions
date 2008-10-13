6801b6dceb7211daf932588e24504a332c7bd5b9
Martina Malberti <sha1-2b004fb57608a448d74be744ecf92d42b9b3ffaa@cern.ch>
2008-10-13 12:01:52 +0000

    Ztautau, M(tautau)>20, no eta cuts

---
+++ python/PYTHIA6_Tauola_EWK_Ztautau_M20_10TeV_cff.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+from GeneratorInterface.Pythia6Interface.TauolaSettings_cff import *
+source = cms.Source("PythiaSource",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.),
+    comEnergy = cms.untracked.double(10000.0),
+    crossSection = cms.untracked.double(1940.),
+    ExternalGenerators = cms.PSet(
+        Tauola = cms.untracked.PSet(
+            TauolaPolar,
+            TauolaDefaultInputCards
+        ),
+        parameterSets = cms.vstring('Tauola')
+    ),
+    UseExternalGenerators = cms.untracked.bool(True),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0            !User defined processes',
+                                        'MSUB(1)=1         !Incl Z0/gamma* production',
+                                        'MSTP(43)=3        !Both Z0 and gamma*',
+                                        'MDME(174,1)=0     !Z decay into d dbar', 
+                                        'MDME(175,1)=0     !Z decay into u ubar', 
+                                        'MDME(176,1)=0     !Z decay into s sbar', 
+                                        'MDME(177,1)=0     !Z decay into c cbar', 
+                                        'MDME(178,1)=0     !Z decay into b bbar', 
+                                        'MDME(179,1)=0     !Z decay into t tbar', 
+                                        'MDME(182,1)=0     !Z decay into e- e+', 
+                                        'MDME(183,1)=0     !Z decay into nu_e nu_ebar', 
+                                        'MDME(184,1)=0     !Z decay into mu- mu+', 
+                                        'MDME(185,1)=0     !Z decay into nu_mu nu_mubar', 
+                                        'MDME(186,1)=1     !Z decay into tau- tau+', 
+                                        'MDME(187,1)=0     !Z decay into nu_tau nu_taubar', 
+                                        'CKIN(1)=20.       !lower cut on M_ll mass'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+        
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_Tauola_EWK_Ztautau_M20_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('Z/gamma* to tautau sample, M(tautau) > 20 GeV -- sqrt(s) = 10TeV')
+)
