b85a903edd6f8f3cd3e4cab51bf0cab55cbe2e94
Gaelle Boudoul <sha1-a82f96c922d75aadc2b17eace2ae9543e16ce583@cern.ch>
2008-10-28 17:01:54 +0000

    TkCosmic 3.8T Reco

---
+++ python/TKDPG_21X_TkCosmic_B38T_Reco_cfg.py
@@ -0,0 +1,62 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Rec")
+
+#--------- Standard sequences
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+process.load("Configuration.StandardSequences.GeometryPilot2_cff")
+
+process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
+
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+
+
+#--- Conditions
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'COSMMC_21X::All'
+
+#--------- Magnetic field Field should be ON for this sample
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+
+process.source = cms.Source("PoolSource",
+    firstFreeID = cms.untracked.uint32(131072),
+ fileNames = cms.untracked.vstring(
+'file:TkCosmicGenSimRaw38T.root',
+ )
+)
+#--- output module
+process.load("Configuration.EventContent.EventContentCosmics_cff")
+
+process.FEVT = cms.OutputModule("PoolOutputModule",
+    process.RECOSIMEventContent,
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO')
+    ),
+    fileName = cms.untracked.string('Reco_TKCosmicMC_BON_21110.root')
+)
+
+
+#--- Metadata
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Examples/data/RecoCosmicsMC__RECOALCARECO.cfg,v $'),
+    annotation = cms.untracked.string('MC Cosmic Reco at 3.8 tesla')
+)
+
+
+#---------------------------------------
+# paths 
+#---------------------------------------
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstructionCosmics)
+
+process.outpath = cms.EndPath(process.FEVT)
+process.schedule = cms.Schedule(process.raw2digi_step,
+                                process.reconstruction_step,
+                                process.outpath)
+
+
