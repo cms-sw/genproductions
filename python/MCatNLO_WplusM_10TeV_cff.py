c20bd479a1485d83bffe203ed5328347351dd84c
Martina Malberti <sha1-2b004fb57608a448d74be744ecf92d42b9b3ffaa@cern.ch>
2008-12-01 09:07:03 +0000

    adding MCatNLO W+, muon channel

---
+++ python/MCatNLO_WplusM_10TeV_cff.py
@@ -0,0 +1,92 @@
+import FWCore.ParameterSet.Config as cms
+
+
+#
+# NOTE: it's a MANDATORY that the 2 parameters match, ECM and comEnergy
+#       a mismatch will result in job abort on inconsistent init
+#
+source = cms.Source("MCatNLOSource",
+    HerwigParameters = cms.PSet(
+        defaultHerwig = cms.vstring(),
+        parameterSets = cms.vstring('defaultHerwig')
+    ),
+    doMPInteraction = cms.untracked.bool(True),
+    processNumber = cms.untracked.int32(-1462), #W+->mu+nu
+    herwigHepMCVerbosity = cms.untracked.bool(False),
+    doHardEvents = cms.untracked.bool(True),
+    herwigVerbosity = cms.untracked.int32(0),
+    comEnergy = cms.untracked.double(10000.),
+    MCatNLOParameters = cms.PSet(
+        mcatnloReadin = cms.vstring('TWIDTH=1.4', 
+            'ECM=10000', 
+            'FREN=1', 
+            'FFACT=1', 
+            'HVQMASS=172.4', 
+            'WMASS=80.419', 
+            'WWIDTH=2.124', 
+            'ZMASS=91.188', 
+            'ZWIDTH=2.495', 
+            'HGGMASS=165', 
+            'HGGWIDTH=1.04', 
+            'IBORNHGG=1', 
+            'V1GAMMAX=-1', 
+            'V1MASSINF=1', 
+            'V1MASSSUP=10000', 
+            'V2GAMMAX=30', 
+            'V2MASSINF=0', 
+            'V2MASSSUP=0', 
+            'HGAMMAX=30', 
+            'HMASSINF=0', 
+            'HMASSSUP=0', 
+            'UMASS=0.32', 
+            'DMASS=0.32', 
+            'SMASS=0.5', 
+            'CMASS=1.55', 
+            'BMASS=4.95', 
+            'GMASS=0.75', 
+            'VUD=0.9748', 
+            'VUS=0.2225', 
+            'VUB=0.0036', 
+            'VCD=0.2225', 
+            'VCS=0.9740', 
+            'VCB=0.041 ', 
+            'VTD=0.009 ', 
+            'VTS=0.0405', 
+            'VTB=0.9992', 
+            'AEMRUN=YES', 
+            'IVCODE=1', 
+            'IL1CODE=1', 
+            'IL2CODE=1', 
+            'PDFGROUP=LHAPDF', 
+            'PDFSET=10050', #CTEQ6M PDFs
+            'LAMBDAFIVE=1', 
+            'SCHEMEOFPDF=MS', 
+            'LAMBDAHERW=-1', 
+            'FPREFIX=wplusM', 
+            'EVPREFIX=wplusM', 
+            'WGTTYPE=1', 
+            'PDFLIBRARY=HWLHAPDF', 
+            'HERPDF=EXTPDF', 
+            'LHAPATH="/home/xv/fabstoec/LHAPDF"', 
+            'LHAOFL=FREEZE'),
+        parameterSets = cms.vstring('mcatnloReadin')
+    ),
+    mcatnloVerbosity = cms.untracked.int32(0),
+    lhapdfSetPath = cms.untracked.string(''),
+    printCards = cms.untracked.bool(False),
+    numHardEvents = cms.untracked.int32(1),
+    maxEventsToPrint = cms.untracked.int32(0),
+    stringFileName = cms.untracked.string('stringInput.txt'),
+    useJimmy = cms.untracked.bool(True)
+)
+
+
+mcatnlofilter = cms.EDFilter("MCatNLOFilter")
+
+ProductionFilterSequence = cms.Sequence(mcatnlofilter)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/MCatNLO_WplusM_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('MCatNLO W+ -> munu  at 10TeV')
+)
---
+++ python/MCatNLO_WplusM_10TeV_cff_py_GEN_IDEAL.py
@@ -0,0 +1,136 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.77 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/MCatNLO_WplusM_10TeV_cff.py -s GEN:ProductionFilterSequence --eventcontent RAWSIM --datatier GEN --conditions FrontierConditions_GlobalTag,IDEAL_V9::All -n 1000 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/Generator_cff')
+process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    annotation = cms.untracked.string('MCatNLO W+ -> munu  at 10TeV'),
+    name = cms.untracked.string('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/MCatNLO_WplusM_10TeV_cff.py,v $')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+# Input source
+process.source = cms.Source("MCatNLOSource",
+    HerwigParameters = cms.PSet(
+        defaultHerwig = cms.vstring(),
+        parameterSets = cms.vstring('defaultHerwig')
+    ),
+    doMPInteraction = cms.untracked.bool(True),
+    useJimmy = cms.untracked.bool(True),
+    stringFileName = cms.untracked.string('stringInput.txt'),
+    mcatnloVerbosity = cms.untracked.int32(0),
+    doHardEvents = cms.untracked.bool(True),
+    herwigVerbosity = cms.untracked.int32(0),
+    MCatNLOParameters = cms.PSet(
+        mcatnloReadin = cms.vstring('TWIDTH=1.4', 
+            'ECM=10000', 
+            'FREN=1', 
+            'FFACT=1', 
+            'HVQMASS=172.4', 
+            'WMASS=80.419', 
+            'WWIDTH=2.124', 
+            'ZMASS=91.188', 
+            'ZWIDTH=2.495', 
+            'HGGMASS=165', 
+            'HGGWIDTH=1.04', 
+            'IBORNHGG=1', 
+            'V1GAMMAX=-1', 
+            'V1MASSINF=1', 
+            'V1MASSSUP=10000', 
+            'V2GAMMAX=30', 
+            'V2MASSINF=0', 
+            'V2MASSSUP=0', 
+            'HGAMMAX=30', 
+            'HMASSINF=0', 
+            'HMASSSUP=0', 
+            'UMASS=0.32', 
+            'DMASS=0.32', 
+            'SMASS=0.5', 
+            'CMASS=1.55', 
+            'BMASS=4.95', 
+            'GMASS=0.75', 
+            'VUD=0.9748', 
+            'VUS=0.2225', 
+            'VUB=0.0036', 
+            'VCD=0.2225', 
+            'VCS=0.9740', 
+            'VCB=0.041 ', 
+            'VTD=0.009 ', 
+            'VTS=0.0405', 
+            'VTB=0.9992', 
+            'AEMRUN=YES', 
+            'IVCODE=1', 
+            'IL1CODE=1', 
+            'IL2CODE=1', 
+            'PDFGROUP=LHAPDF', 
+            'PDFSET=10050', 
+            'LAMBDAFIVE=1', 
+            'SCHEMEOFPDF=MS', 
+            'LAMBDAHERW=-1', 
+            'FPREFIX=wplusM', 
+            'EVPREFIX=wplusM', 
+            'WGTTYPE=1', 
+            'PDFLIBRARY=HWLHAPDF', 
+            'HERPDF=EXTPDF', 
+            'LHAPATH="/home/xv/fabstoec/LHAPDF"', 
+            'LHAOFL=FREEZE'),
+        parameterSets = cms.vstring('mcatnloReadin')
+    ),
+    comEnergy = cms.untracked.double(10000.0),
+    lhapdfSetPath = cms.untracked.string(''),
+    printCards = cms.untracked.bool(False),
+    numHardEvents = cms.untracked.int32(1),
+    maxEventsToPrint = cms.untracked.int32(0),
+    herwigHepMCVerbosity = cms.untracked.bool(False),
+    processNumber = cms.untracked.int32(-1462)
+)
+
+process.source.numHardEvents = process.maxEvents.input
+process.genParticles.abortOnUnknownPDGCode = False
+
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('MCatNLO_WplusM_10TeV_cff_py_GEN.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN'),
+        filterName = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'IDEAL_V9::All'
+process.mcatnlofilter = cms.EDFilter("MCatNLOFilter")
+process.ProductionFilterSequence = cms.Sequence(process.mcatnlofilter)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.ProductionFilterSequence*process.pgen)
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.out_step)
