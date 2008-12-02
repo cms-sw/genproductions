d0b785ae80f04d3a23a6c4446ff4e6e3bc1f4389
Martina Malberti <sha1-2b004fb57608a448d74be744ecf92d42b9b3ffaa@cern.ch>
2008-12-02 10:42:44 +0000

    ZZ inclusive production

---
+++ python/PYTHIA6_EWK_ZZ_10TeV_cff.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+source = cms.Source("PythiaSource",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.),
+    comEnergy = cms.untracked.double(10000.0),
+    crossSection = cms.untracked.double(11960.),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL        = 0    !User defined processes', 
+                                        'MSUB(22) = 1 !ZZ production'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_EWK_ZZ_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6-EWK WW at 10TeV')
+)
+
