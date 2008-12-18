ebe7d75ea1a9a498e39fba3d0dee753e040b9e48
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2008-12-18 10:58:50 +0000

    Added GEN and GEN-HLT Sherpa files at request of Marco Cardaci

---
+++ python/SHERPA112_ADD_Monophoton_MD1d5_10TeV_cff_py_GEN_IDEAL.py
@@ -0,0 +1,65 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.99.2.3 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/SHERPA112_ADD_Monophoton_MD1d5_10TeV_cff.py -s GEN --eventcontent RAWSIM --datatier GEN --conditions FrontierConditions_GlobalTag,IDEAL_V9::All -n 10000 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/Generator_cff')
+process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff')
+process.load('Configuration/StandardSequences/EndOfProcess_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.99.2.3 $'),
+    annotation = cms.untracked.string('Configuration/GenProduction/python/SHERPA112_ADD_Monophoton_MD1d5_10TeV_cff.py nevts:10000'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10000)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+# Input source
+process.source = cms.Source("SherpaSource",
+    resultDir = cms.untracked.string('Result'),
+    libDir = cms.untracked.string('SherpaRun'),
+    firstRun = cms.untracked.uint32(1)
+)
+
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('SHERPA112_ADD_Monophoton_MD1d5_10TeV_cff_py_GEN.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN'),
+        filterName = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'IDEAL_V9::All'
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.endjob_step = cms.Path(process.endOfProcess)
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.endjob_step,process.out_step)
+process.genParticles.abortOnUnknownPDGCode = False
