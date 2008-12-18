b04cdae97ba855f6a5de950743d2a3e1fb920307
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2008-12-18 10:46:49 +0000

    Added SHERPA generator fragment files from Marco Cardaci for Fastsim production

---
+++ python/SHERPA112_ADD_Monophoton_MD3d3_10TeV_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+source=cms.Source("SherpaSource",
+  firstRun  = cms.untracked.uint32(1),
+  libDir    = cms.untracked.string('SherpaRun'),
+  resultDir = cms.untracked.string('Result')
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision 1.1. %'),
+    annotation = cms.untracked.string('default documentation string for SHERPA112_ADD_Monophoton_MD3d3_10TeV_cff.py'),
+    name = cms.untracked.string('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/SHERPA112_ADD_Monophoton_MD3d3_10TeV_cff.py,v $')
+)
