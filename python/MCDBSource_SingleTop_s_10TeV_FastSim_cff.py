2d23b2a15f933db6f52c93c21050fbfd6094facc
weinelt <sha1-ab77e35a8a4450bd3fc34c24d11c731f192603f4@cern.ch>
2009-01-09 15:50:42 +0000

    MadGraph MCDB/LHEInterface config files for FastSim production (different MCDB article ID)

---
+++ python/MCDBSource_SingleTop_s_10TeV_FastSim_cff.py
@@ -0,0 +1,51 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('LHE')
+
+# import of standard configurations
+process.load('FWCore/MessageService/MessageLogger_cfi')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    annotation = cms.untracked.string('MadGraph single top s channel at 10TeV'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/MCDBSource_SingleTop_s_10TeV_cff.py,v $')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+
+# Input source
+
+process.source = cms.Source("MCDBSource",
+    articleID = cms.uint32(214),
+    supportedProtocols = cms.vstring('rfio')
+)
+
+# direct CASTOR path
+#process.source = cms.Source("LHESource",
+#    fileNames = cms.untracked.vstring("rfio:///castor/cern.ch/sft/mcdb/storage/187/events_12k_10TeV_STsch.lhe")
+#)    
+
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('MCDBSource_SingleTop_s_10TeV_cff_py_LHE.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('USER'),
+        filterName = cms.untracked.string('')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+
+# Path and EndPath definitions
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.out_step)
