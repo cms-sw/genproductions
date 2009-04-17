ef1c4fd62309b7572422483fc1e4826bc16a0c08
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2009-04-17 17:15:23 +0000

    switch to the new infrastructure

--- python/PYTHIA6_MinBias_900GeV_cff.py
+++ python/PYTHIA6_MinBias_900GeV_cff.py
@@ -1,14 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
+source = cms.Source("EmptySource")
 
 from Configuration.GenProduction.PythiaUESettings_cfi import *
-source = cms.Source("PythiaSource",
+generator = cms.EDFilter("Pythia6GeneratorFilter",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     maxEventsToPrint = cms.untracked.int32(0),
     pythiaPylistVerbosity = cms.untracked.int32(1),
     filterEfficiency = cms.untracked.double(1.0),
-    comEnergy = cms.untracked.double(900.0),
     crossSection = cms.untracked.double(52410000000.),
+    comEnergy = cms.double(900.0),
     PythiaParameters = cms.PSet(
         pythiaUESettingsBlock,
         processParameters = cms.vstring('MSEL=0         ! User defined processes', 
@@ -29,8 +30,9 @@ source = cms.Source("PythiaSource",
 )
 
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
+    version = cms.untracked.string('$Revision: 1.2 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_MinBias_900GeV_cff.py,v $'),
     annotation = cms.untracked.string('PYTHIA6-MinBias at 900GeV')
 )
 
+ProductionFilterSequence = cms.Sequence(generator)
