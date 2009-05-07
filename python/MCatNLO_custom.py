7bbc77fb0974aa3fc1ed743e3ef2970c84b1e033
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2009-05-07 15:16:51 +0000

    move obsolete configs to attic

--- python/MCatNLO_custom.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-def customise(process):
-
-	process.genParticles.abortOnUnknownPDGCode = False
-	process.source.numHardEvents = process.maxEvents.input
-
-	return(process)
--- python/sherpa_Zll_0j3incl_10TeV_OEW2_10m50_opt_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-source=cms.Source("SherpaSource",
-  firstRun  = cms.untracked.uint32(1),
-  libDir    = cms.untracked.string('SherpaRun'),
-  resultDir = cms.untracked.string('Result')
-)
