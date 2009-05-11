43e48fb88a21de6c4e584f01d7ea7b81b90f2e78
Silvano Tosi <sha1-bb53fd53afc09750c06bae65f87fecc20afee31b@cern.ch>
2009-05-11 09:37:04 +0000

    Readded fragments for TTbar and Dibosons for 31X in PYTHIA6

---
+++ python/PYTHIA6_EWK_Wgamma_10TeV_cff.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.),
+    comEnergy = cms.double(10000.0),
+    crossSection = cms.untracked.double(11960.),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL        = 0    !User defined processes',
+                'MSUB(20)=1 !Wgamma production',
+                'CKIN(3)=10.        !minimum pt hat for hard interactions',
+		'MDME(190,1)=0      !W decay into dbar u',
+		'MDME(191,1)=0      !W decay into dbar c',
+		'MDME(192,1)=0      !W decay into dbar t',
+		'MDME(194,1)=0      !W decay into sbar u',
+		'MDME(195,1)=0      !W decay into sbar c',
+		'MDME(196,1)=0      !W decay into sbar t',
+		'MDME(198,1)=0      !W decay into bbar u',
+		'MDME(199,1)=0      !W decay into bbar c',
+		'MDME(200,1)=0      !W decay into bbar t',
+		'MDME(206,1)=1      !W decay into e+ nu_e',
+		'MDME(207,1)=1      !W decay into mu+ nu_mu',
+		'MDME(208,1)=1      !W decay into tau+ nu_tau'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_EWK_Wgamma_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6-EWK Wgamma at 10TeV')
+)
+
