d5d0d49ebbb25a7f2266262314d6d1e16cf917d8
Joanna Weng <sha1-e1d5fb97f74876fe25d59750eed1d5611df817d0@cern.ch>
2009-05-25 13:28:39 +0000

    First commit

---
+++ python/PYTHIA6_QCDpt_80_120_10TeV_cff.py
@@ -0,0 +1,38 @@
+
+
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0000),
+    comEnergy = cms.double(10000.0),
+    crossSection = cms.untracked.double(1617240),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+	processParameters = cms.vstring(
+             'MSEL=1   ! QCD hight pT processes',
+             'CKIN(3)=80  ! minimum pt hat for hard interactions',
+             'CKIN(4)=120  ! maximum pt hat for hard interactions'),
+     # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+
+ 
+   
+
+
+    )
+)
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string
+('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/PYTHIA6_QCDpt_80_120_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('QCDpt-80-120 at 10TeV')
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
