7809977126e315be4ebce1d84c65ada11555c042
Silvano Tosi <sha1-bb53fd53afc09750c06bae65f87fecc20afee31b@cern.ch>
2009-07-23 15:23:01 +0000

    Dibosons cfg files with Tauola

---
+++ python/PYTHIA6_EWK_Tauola_WZ_10TeV_cff.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.),
+    comEnergy = cms.double(10000.0),
+    crossSection = cms.untracked.double(17.4),
+    ExternalDecays = cms.PSet(
+        Tauola = cms.untracked.PSet(
+            TauolaPolar,
+            TauolaDefaultInputCards
+        ),
+        parameterSets = cms.vstring('Tauola')
+    ),
+    UseExternalGenerators = cms.untracked.bool(True),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL        = 0    !User defined processes', 
+                                        'MSUB(23) = 1 ! WZ production'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.4 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/Attic/PYTHIA6_EWK_WZ_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6-EWK WZ with Tauola at 10TeV')
+)
+
