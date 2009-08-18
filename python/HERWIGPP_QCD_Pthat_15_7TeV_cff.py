c894e4a4f7bf12bca23360d63fc7442a047290c5
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2009-08-18 08:18:22 +0000

    7TeV config files for Herwig++

---
+++ python/HERWIGPP_QCD_Pthat_15_7TeV_cff.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('$Revision$'),
+	name = cms.untracked.string('$Source$'),
+	annotation = cms.untracked.string('Summer09: Herwig++ generation of QCD events, 7TeV, MRST2001, pthat > 15 GeV')
+)
+
+from Configuration.GenProduction.HerwigppDefaults_cfi import *
+
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("ThePEGGeneratorFilter",
+	herwigDefaultsBlock,
+
+	configFiles = cms.vstring(),
+	parameterSets = cms.vstring(
+		'cm7TeV',
+		'pdfMRST2001',
+		'Summer09QCDParameters',
+		'basicSetup',
+		'setParticlesStableForDetector',
+	),
+
+	Summer09QCDParameters = cms.vstring(
+		'cd /Herwig/MatrixElements/',
+		'insert SimpleQCD:MatrixElements[0] MEQCD2to2',
+
+		'cd /',
+		'set /Herwig/Cuts/JetKtCut:MinKT 15*GeV',
+		'set /Herwig/Cuts/QCDCuts:MHatMin 0.0*GeV',
+		'set /Herwig/UnderlyingEvent/MPIHandler:IdenticalToUE 0',
+	),
+
+	crossSection = cms.untracked.double(8.93283e+08),
+	filterEfficiency = cms.untracked.double(1.0),
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
