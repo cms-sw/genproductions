f5fb49d357fd452c48761664b264c064d251e78e
Zongchang Yang <sha1-85650d06579ccfb553aa23182d43548a8f251eb7@cern.ch>
2009-08-20 09:03:00 +0000

    First commit for 7 TeV Onia and inclusive muon files

---
+++ python/PYTHIA6_JPsiWithFSR_7TeV_cff.py
@@ -0,0 +1,103 @@
+import FWCore.ParameterSet.Config as cms
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('$Revision: 1.1 $'),
+	name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_JPsiWithFSR_10TeV_cff.py,v $'),
+	annotation = cms.untracked.string('Summer09: Pythia6 generation of prompt JPsi, 10TeV, D6T tune')
+)
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(212000000.0),
+    filterEfficiency = cms.untracked.double(0.0739),
+    maxEventsToPrint = cms.untracked.int32(0),
+    ExternalDecays = cms.PSet(
+        EvtGen = cms.untracked.PSet(
+             operates_on_particles = cms.vint32( 0 ), # 0 (zero) means default list (hardcoded)
+                                                      # you can put here the list of particles (PDG IDs)
+                                                      # that you want decayed by EvtGen
+             use_default_decay = cms.untracked.bool(False),
+             decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
+             # decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY.DEC'),
+             particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evt.pdl'),
+             user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/Onia_mumu.dec'),
+             # user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/incl_BtoJpsi_mumu.dec'),
+             list_forced_decays = cms.vstring('MyJ/psi'),
+             ),
+        parameterSets = cms.vstring('EvtGen')
+    ),                             
+    PythiaParameters = cms.PSet(
+	pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=61          ! Quarkonia', 
+            'MDME(858,1) = 0  ! 0.060200    e-    e+', 
+            'MDME(859,1) = 1  ! 0.060100    mu-  mu+', 
+            'MDME(860,1) = 0  ! 0.879700    rndmflav        rndmflavbar', 
+            'MSTP(142)=2      ! turns on the PYEVWT Pt re-weighting routine', 
+            'PARJ(13)=0.750   ! probability that a c or b meson has S=1', 
+            'PARJ(14)=0.162   ! probability that a meson with S=0 is produced with L=1, J=1', 
+            'PARJ(15)=0.018   ! probability that a meson with S=1 is produced with L=1, J=0', 
+            'PARJ(16)=0.054   ! probability that a meson with S=1 is produced with L=1, J=1', 
+            'MSTP(145)=0      ! choice of polarization', 
+            'MSTP(146)=0      ! choice of polarization frame ONLY when mstp(145)=1', 
+            'MSTP(147)=0      ! particular helicity or density matrix component when mstp(145)=1', 
+            'MSTP(148)=1      ! possibility to allow for final-state shower evolution, extreme case !', 
+            'MSTP(149)=1      ! if mstp(148)=1, it determines the kinematics of the QQ~3S1(8)->QQ~3S1(8)+g branching', 
+            'PARP(141)=1.16   ! New values for COM matrix elements', 
+            'PARP(142)=0.0119 ! New values for COM matrix elements', 
+            'PARP(143)=0.01   ! New values for COM matrix elements', 
+            'PARP(144)=0.01   ! New values for COM matrix elements', 
+            'PARP(145)=0.05   ! New values for COM matrix elements', 
+            'PARP(146)=9.28   ! New values for COM matrix elements', 
+            'PARP(147)=0.15   ! New values for COM matrix elements', 
+            'PARP(148)=0.02   ! New values for COM matrix elements', 
+            'PARP(149)=0.02   ! New values for COM matrix elements', 
+            'PARP(150)=0.085  ! New values for COM matrix elements', 
+            'BRAT(861)=0.202  ! chi_2c->J/psi gamma', 
+            'BRAT(862)=0.798  ! chi_2c->rndmflav rndmflavbar', 
+            'BRAT(1501)=0.013 ! chi_0c->J/psi gamma', 
+            'BRAT(1502)=0.987 ! chi_0c->rndmflav rndmflavbar', 
+            'BRAT(1555)=0.356 ! chi_1c->J/psi gamma', 
+            'BRAT(1556)=0.644 ! chi_1c->rndmflav rndmflavbar'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters', 
+            'CSAParameters'),
+        CSAParameters = cms.vstring('CSAMODE = 6     ! cross-section reweighted quarkonia')
+    )
+)
+
+oniafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(2),
+    MaxEta = cms.untracked.double(1000.0),
+    MinEta = cms.untracked.double(-1000.0),
+    MinPt = cms.untracked.double(0.0),
+    ParticleID = cms.untracked.int32(443)
+)
+
+mumugenfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MinPt = cms.untracked.vdouble(0.0, 0.0),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    ParticleCharge = cms.untracked.int32(-1),
+    ParticleID1 = cms.untracked.vint32(13),
+    ParticleID2 = cms.untracked.vint32(13)
+)
+
+mugenfilter = cms.EDFilter("MCSmartSingleParticleFilter",
+    MaxDecayRadius = cms.untracked.vdouble(1500.0, 1500.0),
+    Status = cms.untracked.vint32(1, 1),
+    MinPt = cms.untracked.vdouble(2.5, 2.5),
+    ParticleID = cms.untracked.vint32(13, -13),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    MaxDecayZ = cms.untracked.vdouble(3000.0, 3000.0),
+    MinDecayZ = cms.untracked.vdouble(-3000.0, -3000.0)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*oniafilter*mumugenfilter*mugenfilter)
+
---
+++ python/PYTHIA6_ppMuX_7TeV_cff.py
@@ -0,0 +1,45 @@
+import FWCore.ParameterSet.Config as cms
+
+configurationMetadata = cms.untracked.PSet(
+        version = cms.untracked.string('$Revision: 1.6 $'),
+        name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_ppMuX_10TeV_cff.py,v $'),
+        annotation = cms.untracked.string('Summer09: Pythia6-MinBias at 10TeV with Muon preselection (Loose cut), +decay-inflight, 10TeV, D6T tune')
+)
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(48440000000.0),
+    filterEfficiency = cms.untracked.double(0.00176),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=1           ! User defined processes', 
+            'MSTJ(22)=4       ! Decay unstable particles in a cylinder', 
+            'PARJ(73)=1500.   ! max. radius for MSTJ(22)=4', 
+            'PARJ(74)=3000.   ! max. Z for MSTJ(22)=4', 
+            'MDCY(C130,1)=1   ! decay k0-longs', 
+            'MDCY(C211,1)=1   ! decay pions', 
+            'MDCY(C321,1)=1   ! decay kaons'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings',
+            'processParameters')
+    )
+)
+
+mugenfilter = cms.EDFilter("MCSmartSingleParticleFilter",
+    Status = cms.untracked.vint32(     1,    1),
+    ParticleID = cms.untracked.vint32(13,  -13),
+    MinPt = cms.untracked.vdouble(    2.5, 2.5),
+    MaxEta = cms.untracked.vdouble(   2.5, 2.5),
+    MinEta = cms.untracked.vdouble(  -2.5, -2.5),
+    MaxDecayRadius = cms.untracked.vdouble(2000.0, 2000.0),
+    MaxDecayZ = cms.untracked.vdouble(4000.0, 4000.0),
+    MinDecayZ = cms.untracked.vdouble(-4000.0, -4000.0)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*mugenfilter)
