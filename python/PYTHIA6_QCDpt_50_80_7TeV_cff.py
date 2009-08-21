a2efcbc2c67f908bf39c18b4d05f836c756d682e
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2009-08-21 05:27:23 +0000

    7TeV Pythia JES

---
+++ python/PYTHIA6_QCDpt_50_80_7TeV_cff.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('$Revision$'),
+	name = cms.untracked.string('$Source$'),
+	annotation = cms.untracked.string('Summer09: Pythia6 generation of QCD events, 7TeV, D6T tune, pthat = 50 .. 80 GeV')
+)
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+	comEnergy = cms.double(7000.0),
+	crossSection = cms.untracked.double(6.35821e+06),
+	filterEfficiency = cms.untracked.double(1.0000),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
+	PythiaParameters = cms.PSet(
+		pythiaUESettingsBlock,
+		processParameters = cms.vstring(
+			'MSEL=1   ! QCD hight pT processes',
+			'CKIN(3)=50  ! minimum pt hat for hard interactions',
+            'CKIN(4)=80  ! minimum pt hat for hard interactions',
+		),
+		parameterSets = cms.vstring(
+			'pythiaUESettings',
+			'processParameters',
+		)
+	)
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
