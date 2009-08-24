be57321f9a53c53e0638529fd35cb6f6a2f46ce7
Silvano Tosi <sha1-bb53fd53afc09750c06bae65f87fecc20afee31b@cern.ch>
2009-08-24 08:29:27 +0000

    Pythia fragments for diboson samples at 7 TeV

---
+++ python/PYTHIA6_EWK_ZZ_7TeV_cff.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(0.43),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL        = 0    !User defined processes', 
+                                        'MSUB(22) = 1 !ZZ production'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.4 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/Attic/PYTHIA6_EWK_ZZ_7TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6-EWK ZZ at 7TeV')
+)
+
