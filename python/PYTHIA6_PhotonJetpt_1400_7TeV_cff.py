574386e7ad7cb5d9ddc5ed57f3e9ee7f815e0baf
Joanna Weng <sha1-e1d5fb97f74876fe25d59750eed1d5611df817d0@cern.ch>
2009-09-01 16:47:46 +0000

    first committ @ 7 TeV

---
+++ python/PYTHIA6_PhotonJetpt_1400_7TeV_cff.py
@@ -0,0 +1,37 @@
+
+
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(1.286e-05),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+processParameters = cms.vstring(
+            'MSEL=10',
+            'CKIN(3)=1400  ! minimum pt hat for hard interactions'),
+   # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+
+       
+   
+
+
+    )
+)
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.4 $'),
+    name = cms.untracked.string
+('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_PhotonJetpt_1400_7TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PhotonJetpt-1400 at 7TeV')
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
