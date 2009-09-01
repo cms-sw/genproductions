f9de2821c60b3f018634c7ccd37550bc41213f20
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-09-01 15:49:32 +0000

    first commit config files for L1 trigger muon LUT tuning production

---
+++ python/SingleMuPlusOneOverPt_NewField_Chimney.py
@@ -0,0 +1,93 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.138 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: SingleMuPt100_cfi -s GEN:ProductionFilterSequence,SIM,DIGI,L1,DIGI2RAW,RAW2DIGI,RECO --number 1000 --eventcontent FEVTSIM --conditions FrontierConditions_GlobalTag,MC_31X_V5::All --datatier GEN-SIM-DIGI-RAW-RECO --no_exec --magField=MagneticField_38T_ToscaMap090322_cff
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_ToscaMap090322_cff')
+process.load('Configuration/StandardSequences/Generator_cff')
+process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff')
+process.load('Configuration/StandardSequences/Sim_cff')
+process.load('Configuration/StandardSequences/Digi_cff')
+process.load('Configuration/StandardSequences/SimL1Emulator_cff')
+process.load('Configuration/StandardSequences/DigiToRaw_cff')
+process.load('Configuration/StandardSequences/RawToDigi_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('Configuration/StandardSequences/EndOfProcess_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.138 $'),
+    annotation = cms.untracked.string('SingleMuPt100_cfi nevts:1000'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+# Input source
+process.source = cms.Source("EmptySource")
+
+# Output definition
+process.FEVTDEBUGEventContent.outputCommands.extend(cms.untracked.vstring("drop FEDRawDataCollection_*_*_*"))
+process.output = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    fileName = cms.untracked.string('SingleMuPlusOneOverPt_NewField_Chimney_GEN_SIM_DIGI_L1_DIGI2RAW_RAW2DIGI_RECO_MC.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RAW-RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'MC_31X_V5::All'
+process.generator = cms.EDProducer("FlatRandomOneOverPtGunProducer",
+                                   PGunParameters = cms.PSet(
+    PartID = cms.vint32(-13),
+    MinEta = cms.double(-0.75),
+    MaxEta = cms.double(0.75),
+    MinPhi = cms.double(-0.25),
+    MaxPhi = cms.double(0.8),
+    MinOneOverPt = cms.double(0.005),
+    MaxOneOverPt = cms.double(0.5)
+    ),
+                                   Verbosity       = cms.untracked.int32(0),
+                                   AddAntiParticle = cms.bool(False),
+                                   firstRun = cms.untracked.uint32(1)
+)
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.Path(process.endOfProcess)
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step,process.raw2digi_step,process.reconstruction_step,process.endjob_step,process.out_step)
+
+# special treatment in case of production filter sequence  
+for path in process.paths: 
+    getattr(process,path)._seq = process.ProductionFilterSequence*getattr(process,path)._seq
