b4e768a4f1a189efacd4755dff06da8405489e57
Sezen Sekmen <sha1-2fa96d9c9cf6fea71aa1195b59ce00a6ace72959@cern.ch>
2009-09-03 13:55:16 +0000

    Summer09 10TeV SUSY GM1c GEN fragment

---
+++ python/PYTHIA6_SUSY_GM1c_sftsht_10TeV_cff.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.double(10000.0),
+    crossSection = cms.untracked.double(0.8896),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0         ! User defined processes',
+            'MSEL=39                  ! All SUSY processes',
+            'IMSS(1) = 11             ! Spectrum from external SLHA file',
+            'IMSS(11) = 1             ! Gravitino LSP',
+            'IMSS(21) = 33            ! LUN number for SLHA File (must be 33)',
+            'IMSS(22) = 33            ! Read-in SLHA decay table'),
+        SLHAParameters = cms.vstring(
+            'SLHAFILE = Configuration/Generator/data/SUSY_GM1c_sftsht.slha'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings',
+            'processParameters', 'SLHAParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.3 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_SUSY_GM1c_sftsht_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('SUSY-GM1c-sftsht at 10TeV')
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
