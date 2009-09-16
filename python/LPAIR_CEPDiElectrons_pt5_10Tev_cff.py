58d819530460f177b40cc720c877b28991900470
Benoit Roland <sha1-2200346e613b7795d3e973c9eb4d9c0d63733b86@cern.ch>
2009-09-16 13:17:24 +0000

    Summer09 FAMC 10 TeV LHE

--- python/LPAIR_CEPDiElectrons_pt5_10Tev_cff.py
+++ python/LPAIR_CEPDiElectrons_pt5_10Tev_cff.py
@@ -1,21 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-source = cms.Source("MCDBSource",
-        articleID = cms.uint32(310),
-        supportedProtocols = cms.untracked.vstring('rfio')
-)
-
 generator = cms.EDProducer("LHEProducer",
         hadronisation = cms.PSet(
                 generator = cms.string('None')
         )
 )
-
-configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 0.0 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/LPAIR_CEPDiElectrons_pt5_10TeV_cff.py,v $'),
-    annotation = cms.untracked.string('LPAIR GammaGamma to e+e-, pt(e) > 5 GeV at sqrt(s) = 10TeV')
-)
-
-ProductionFilterSequence = cms.Sequence(generator)
-
