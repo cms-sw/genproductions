330b417db0e858c2c1acba50f1fa39b63188125e
Guillelmo Gomez Ceballos Retuerto <sha1-45f56802e0a674235d88fd4afb21f0d04767a408@cern.ch>
2009-09-24 07:49:17 +0000

    Small change for H+ requests

--- python/PYTHIA6_Tauola_TTbar_H160_taunu_10TeV_cff.py
+++ python/PYTHIA6_Tauola_TTbar_H160_taunu_10TeV_cff.py
@@ -1,20 +1,23 @@
+# General Import
 import FWCore.ParameterSet.Config as cms
-
+# Define the source (empty for Multipurpose Generators)
 source = cms.Source("EmptySource")
-
-from Configuration.GenProduction.PythiaUESettings_cfi import *
+# Import settings for modules
+from Configuration.Generator.PythiaUESettings_cfi import *
 from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+
+# Define the generator module
 generator = cms.EDFilter("Pythia6GeneratorFilter",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
-    maxEventsToPrint = cms.untracked.int32(3),
+    maxEventsToPrint = cms.untracked.int32(0),
     pythiaPylistVerbosity = cms.untracked.int32(1),
     filterEfficiency = cms.untracked.double(1.0),
     comEnergy = cms.double(10000.0),
-    crossSection = cms.untracked.double(242.8),
+    crossSection = cms.untracked.double(1.1),
     ExternalDecays = cms.PSet(
         Tauola = cms.untracked.PSet(
-            TauolaPolar,
-            TauolaDefaultInputCards
+            TauolaPolar, # tau polarisation switch on
+            TauolaDefaultInputCards #set default TAUOLA input card
         ),
         parameterSets = cms.vstring('Tauola')
     ),
@@ -25,20 +28,53 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
             'MSUB(81)  = 1     ! qqbar to QQbar',
             'MSUB(82)  = 1     ! gg to QQbar',
             'MSTP(7)   = 6     ! flavor = top',
-            'PMAS(6,1) = 172.5  ! top quark mass',
-            'PMAS(37,1)=160.0       !mass of charged Higgs', 
-            'MDME(44,1)=4           !top decay into Wd', 
-            'MDME(45,1)=4           !top decay into Ws', 
-            'MDME(46,1)=4           !top decay into Wb', 
-            'MDME(49,1)=5           !top decay into H+b', 
-            'MDME(503,1)=0 !H+ decay into dbar u', 
-            'MDME(504,1)=0 !H+ decay into sbar c', 
-            'MDME(505,1)=0 !H+ decay into bbar t', 
-            'MDME(507,1)=0 !H+ decay into e+   nu_e', 
-            'MDME(508,1)=0 !H+ decay into mu+  nu_mu', 
-            'MDME(509,1)=1 !H+ decay into tau+ nu_tau', 
-            'MDME(511,1)=0 !H+ decay into W+   h0' 
-	    ),
+            # Set the masses of the higgs
+            'PMAS(6,1)  = 172.5  ! top quark mass',                                        
+            'PMAS(37,1) = 160  ! charged Higgs mass ',
+            # Switch off / on desirable channels for top quark
+            'MDME(41,1) = 0   ! t decay into g t', 
+            'MDME(42,1) = 0   ! t decay into gamma t', 
+            'MDME(43,1) = 0   ! t decay into Z0 t', 
+            'MDME(44,1) = 0   ! t decay into W d', 
+            'MDME(45,1) = 0   ! t decay into W s', 
+            'MDME(46,1) = 3   ! t decay into W and b (channel is ON for tbar, OFF for t)', 
+            'MDME(47,1) = 0   ! t decay into W b` ', 
+            'MDME(48,1) = 0   ! t decay into h0 t', 
+            'MDME(49,1) = 2   ! t decay into H and b (channel is ON for t, OFF for tbar)', 
+            'MDME(50,1) = 0   ! t decay into ~chi_10 ~t_1', 
+            'MDME(51,1) = 0   ! t decay into ~chi_20 ~t_1', 
+            'MDME(52,1) = 0   ! t decay into ~chi_30 ~t_1', 
+            'MDME(53,1) = 0   ! t decay into ~chi_40 ~t_1', 
+            'MDME(54,1) = 0   ! t decay into ~g ~t_1', 
+            'MDME(55,1) = 0   ! t decay into ~Gravitino ~t_1', 
+            # Switch off / on desirable channels for H+
+            'MDME(503,1) = 0   ! H+ decay into dbar u', 
+            'MDME(504,1) = 0   ! H+ decay into sbar c', 
+            'MDME(505,1) = 0   ! H+ decay into bbar t', 
+            'MDME(506,1) = 0   ! H+ decay into b`bar t`', 
+            'MDME(507,1) = 0   ! H+ decay into e+ nu_e', 
+            'MDME(508,1) = 0   ! H+ decay into mu+ nu_mu', 
+            'MDME(509,1) = 1   ! H+ decay into tau+ nu_tau (channel is switched on)', 
+            'MDME(510,1) = 0   ! H+ decay into tau`+ nu`_tau', 
+            'MDME(511,1) = 0   ! H+ decay into W+ h0', 
+            'MDME(512,1) = 0   ! H+ decay into ~chi_10 ~chi_1+', 
+            'MDME(513,1) = 0   ! H+ decay into ~chi_10 ~chi_2+', 
+            'MDME(514,1) = 0   ! H+ decay into ~chi_20 ~chi_1+', 
+            'MDME(515,1) = 0   ! H+ decay into ~chi_20 ~chi_2+', 
+            'MDME(516,1) = 0   ! H+ decay into ~chi_30 ~chi_1+', 
+            'MDME(517,1) = 0   ! H+ decay into ~chi_30 ~chi_2+', 
+            'MDME(518,1) = 0   ! H+ decay into ~chi_40 ~chi_1+', 
+            'MDME(519,1) = 0   ! H+ decay into ~chi_40 ~chi_2+', 
+            'MDME(520,1) = 0   ! H+ decay into ~t_1 ~b_1bar   ', 
+            'MDME(521,1) = 0   ! H+ decay into ~t_2 ~b_1bar   ', 
+            'MDME(522,1) = 0   ! H+ decay into ~t_1 ~b_2bar   ', 
+            'MDME(523,1) = 0   ! H+ decay into ~t_2 ~b_2bar   ', 
+            'MDME(524,1) = 0   ! H+ decay into ~d_Lbar ~u_L   ', 
+            'MDME(525,1) = 0   ! H+ decay into ~s_Lbar ~c_L   ', 
+            'MDME(526,1) = 0   ! H+ decay into ~e_L+ ~nu_muL  ', 
+            'MDME(527,1) = 0   ! H+ decay into ~mu_L+ ~u_L    ', 
+            'MDME(528,1) = 0   ! H+ decay into ~tau_1+ ~nu_tauL', 
+            'MDME(529,1) = 0   ! H+ decay into ~tau_2+ ~nu_tauL'),                            
         # This is a vector of ParameterSet names to be read, in this order
         parameterSets = cms.vstring('pythiaUESettings', 
             'processParameters')
@@ -48,7 +84,8 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
 ProductionFilterSequence = cms.Sequence(generator)
 
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.8 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_Tauola_TTbar_10TeV_cff.py,v $'),
-    annotation = cms.untracked.string('tt->H+(160)bt at 10TeV')
-)
+    version = cms.untracked.string('$Revision: 1.0 $'),
+    name = cms.untracked.string('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/PYTHIA6_Tauola_TTbar_Hplus80_TauNu_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6-TTbar Hplus80 To TauNu, with Tauola at 10TeV')
+    )
+
