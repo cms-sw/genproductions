7e1656754ebea4691f150fc8b0433f1b9661b20c
Roberta Arcidiacono <sha1-0e85798e0b441edc9b9523f2b35aadd193df9052@cern.ch>
2009-09-25 20:07:34 +0000

    first version

---
+++ python/PYTHIA6_EWK_DYee_M1to10_10TeV_cff.py
@@ -0,0 +1,57 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(0.0101),
+    crossSection = cms.untracked.double(111800.),
+    comEnergy = cms.double(10000.0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0            !User defined processes', 
+                                        'MSUB(1)=1         !Incl Z0/gamma* production', 
+                                        'MSTP(43)=3        !Both Z0 and gamma*', 
+                                        'MDME(174,1)=0     !Z decay into d dbar', 
+                                        'MDME(175,1)=0     !Z decay into u ubar', 
+                                        'MDME(176,1)=0     !Z decay into s sbar', 
+                                        'MDME(177,1)=0     !Z decay into c cbar', 
+                                        'MDME(178,1)=0     !Z decay into b bbar', 
+                                        'MDME(179,1)=0     !Z decay into t tbar', 
+                                        'MDME(182,1)=1     !Z decay into e- e+', 
+                                        'MDME(183,1)=0     !Z decay into nu_e nu_ebar', 
+                                        'MDME(184,1)=0     !Z decay into mu- mu+', 
+                                        'MDME(185,1)=0     !Z decay into nu_mu nu_mubar', 
+                                        'MDME(186,1)=0     !Z decay into tau- tau+', 
+                                        'MDME(187,1)=0     !Z decay into nu_tau nu_taubar', 
+                                        'CKIN(1)=1.       !Minimum sqrt(s_hat) value (=Z mass)',
+                                        'CKIN(2)=10.       !Maximum sqrt(s_hat) value (=Z mass)',
+                                        'CKIN(7)=-3.5',
+                                        'CKIN(8)=3.5'
+                                        ),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+eegenfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MinPt = cms.untracked.vdouble(3.0, 3.0),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    ParticleCharge = cms.untracked.int32(0),
+    ParticleID1 = cms.untracked.vint32(11),
+    ParticleID2 = cms.untracked.vint32(11)
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_EWK_DYee_M1to10_10TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6 Z/gamma* to ee, 1 GeV < M(e+e-) < 10 GeV at sqrt(s) = 10TeV')
+)
+
+ProductionFilterSequence = cms.Sequence(generator * eegenfilter)
