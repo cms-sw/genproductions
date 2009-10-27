f3fc973d5f5d013c110750d31f28475d4946b682
David Alexander Mason <sha1-5f005eb123c4a7f7844ce7fdbe94e9557fac5664@cern.ch>
2009-10-27 01:41:29 +0000

    10TeV configs from Vladimir Litvine

---
+++ python/PYTHIA6_box_CKIN3_10_25_10TeV.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+                             pythiaHepMCVerbosity = cms.untracked.bool(False),
+                             maxEventsToPrint = cms.untracked.int32(0),
+                             pythiaPylistVerbosity = cms.untracked.int32(1),
+                             filterEfficiency = cms.untracked.double(1.0),
+                             comEnergy = cms.double(10000.0),
+                             crossSection = cms.untracked.double(0.004285),
+                             PythiaParameters = cms.PSet(
+            pythiaUESettingsBlock,
+                    processParameters = cms.vstring(
+                    'MSEL=0 ',
+                    'MSUB(114)=1       ',
+                    'CKIN(3)=10.          ! minimum pt hat for hard interactions',
+                    'CKIN(4)=25.          ! maximum pt hat for hard interactions'),
+                    parameterSets = cms.vstring('pythiaUESettings',
+                                   'processParameters')
+                )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
+
+
