0af14f7d3aa5576cc7b9ca18a3ba82d5658e04d4
Fabian Stoeckli <sha1-43e75525c3e6e41805da67101e406c6d881e76cd@cern.ch>
2009-11-05 17:48:33 +0000

    *** empty log message ***

---
+++ python/GG2WW_PYTHIA6_WW4leptons_7TeV_cff.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+
+generator = cms.EDFilter("Pythia6HadronizerFilter",
+            pythiaHepMCVerbosity = cms.untracked.bool(False),
+            maxEventsToPrint = cms.untracked.int32(0),
+            pythiaPylistVerbosity = cms.untracked.int32(0),
+            filterEfficiency = cms.untracked.double(1.),
+            comEnergy = cms.double(7000.0),
+            PythiaParameters = cms.PSet( pythiaUESettingsBlock,
+                           processParameters = cms.vstring(
+                                    'MSEL        = 0    !User defined processes', 
+                                    'PMAS(5,1)=4.4   ! b quark mass',
+                                    'PMAS(6,1)=172.5 ! t quark mass'),
+                                    # This is a vector of ParameterSet names to be read, in this order
+                                    parameterSets = cms.vstring('pythiaUESettings', 
+                                                                'processParameters')
+                                         )
+)
