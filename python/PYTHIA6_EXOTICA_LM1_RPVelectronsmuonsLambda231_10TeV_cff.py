0992553e9557d94444912df42336fad961a0cf85
Alexander Belyaev <sha1-45889d0b422ced4b08fb223eae59c585c331ccec@cern.ch>
2009-11-13 13:06:06 +0000

    *** empty log message ***

---
+++ python/PYTHIA6_EXOTICA_LM1_RPVelectronsmuonsLambda231_10TeV_cff.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter", 
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.double(10000.0),
+    crossSection = cms.untracked.double(14.0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring( 
+            'MSEL = 39                !',    
+            'IMSS(1) = 2              !  mSUGRA', 
+            'IMSS(51) = 3             !  R-parity violation',
+            'RMSS(8)  =   60.         !  m_0',
+            'RMSS(1)  =   250.        !  m_1/2',
+            'RMSS(4)  =   1.D0        !  Sign(mu)', 
+            'RMSS(5)  =   10.D0       !  tan(beta)',
+            'RMSS(16) =   0.D0        !  A_top',
+            'RVLAM(1,2,1) = 0.                ',         
+            'RVLAM(1,2,2) = 0.                ', 
+            'RVLAM(1,2,3) = 0.                ', 
+            'RVLAM(1,3,1) = 0.                ',
+            'RVLAM(1,3,2) = 0.                ',
+            'RVLAM(1,3,3) = 0.                ',
+            'RVLAM(2,1,1) = 0.                ',
+            'RVLAM(2,1,2) = 0.                ',
+            'RVLAM(2,1,3) = 0.                ',
+            'RVLAM(3,1,1) = 0.                ',
+            'RVLAM(3,1,2) = 0.                ',
+            'RVLAM(3,1,3) = 0.                ',
+            'RVLAM(3,2,1) = 0.                ',
+            'RVLAM(3,2,2) = 0.                ', 
+            'RVLAM(3,2,3) = 0.                ',
+            'RVLAM(2,3,1) = 0.0001            ',
+            'RVLAM(2,3,2) = 0.                ',  
+            'RVLAM(2,3,3) = 0.                '),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+# the definition of the Filter Sequence
+ProductionFilterSequence = cms.Sequence(generator)
+
---
+++ python/PYTHIA6_Exotica_LQ_1stgen_500_10TeV_cff.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.double(10000.0),
+    crossSection  = cms.untracked.double(0.432000),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0                  !(D=1) to select between full user control (0,\\ then use MSUB) and some preprogrammed alternative', 
+            'PMAS(42,1)=500.0         !LQ mass', 
+            'KFDP(539,1)=2           !LQ-> e+u', 
+            'KFDP(539,2)=11          !e', 
+            'MSUB(163)=1             !g+g->LQ+LQbar', 
+            'MSUB(164)=1             !q+qbar->LQ+LQbar'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+ProductionFilterSequence = cms.Sequence(generator)
