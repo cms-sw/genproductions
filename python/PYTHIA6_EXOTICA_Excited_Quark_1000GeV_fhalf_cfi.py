c468065545daed5f7b3c8f218d6d54970fb0e221
Alexander Belyaev <sha1-45889d0b422ced4b08fb223eae59c585c331ccec@cern.ch>
2009-11-13 01:43:49 +0000

    *** empty log message ***

---
+++ python/PYTHIA6_EXOTICA_Excited_Quark_1000GeV_fhalf_cfi.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(0.6924),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0  !User selected',
+            'MSTP(6)=1              ! excited quark', 
+            'MSUB(147)=1            ! dg--> d*',
+            'MSUB(148)=1            ! ug--> u*',
+            'PMAS(343,1) = 1000.     ! d* mass',
+            'PMAS(344,1) = 1000.     ! u* mass', 
+            'RTCM(41) = 1000.        ! Scale parameter Lambda',
+            #Below the couplings
+            'RTCM(43)=0.5              ! f=1   SM coupling', 
+            'RTCM(44)=0.5              ! fprime=1  SM coupling', 
+            'RTCM(45)=0.5              ! f_s=1 SM coupling',
+            '4000001:ALLOFF            !Turn off all u* decays',
+            '4000001:ONIFMATCH 1 22    !Turn on u*->u Photon',
+            '4000002:ALLOFF            !Turn off all d* decays',
+            '4000002:ONIFMATCH 2 22    !Turn on d*->d Photon'),
+       # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+ProductionFilterSequence = cms.Sequence(generator)
---
+++ python/PYTHIA6_EXOTICA_Excited_Quark_1200GeV_cfi.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(0.948),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0  !User selected',
+            'MSTP(6)=1              ! excited quark', 
+            'MSUB(147)=1            ! dg--> d*',
+            'MSUB(148)=1            ! ug--> u*',
+            'PMAS(343,1) = 1200.     ! d* mass',
+            'PMAS(344,1) = 1200.     ! u* mass', 
+            'RTCM(41) = 1200.        ! Scale parameter Lambda',
+            #below are the couplingss
+            'RTCM(43)=1.0              ! f=1   SM coupling', 
+            'RTCM(44)=1.0              ! fprime=1  SM coupling', 
+            'RTCM(45)=1.0              ! f_s=1 SM coupling',
+            '4000001:ALLOFF            !Turn off all u* decays',
+            '4000001:ONIFMATCH 1 22    !Turn on u*->u Photon',
+	    '4000002:ALLOFF            !Turn off all d* decays',
+            '4000002:ONIFMATCH 2 22    !Turn on d*->d Photon'),
+       # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+ProductionFilterSequence = cms.Sequence(generator)
