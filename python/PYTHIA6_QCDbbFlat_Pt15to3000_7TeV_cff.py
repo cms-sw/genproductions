d6cb9cb4d1c5821e47bce89a6d0a3b87fbcc522b
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2009-11-24 18:17:14 +0000

    add configuration files (7&10TeV) for corresponding jetmet requests.

---
+++ python/PYTHIA6_QCDbbFlat_Pt15to3000_7TeV_cff.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    comEnergy = cms.double(7000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring(
+            'MSEL=0           ! user defined processes',
+            'MSUB(81)=1       ! qq to QQ massive',
+            'MSUB(82)=1       ! gg to QQ massive',
+            'MSTP(7)=5        ! 5 for bbbar',
+            'CKIN(3)=15.      ! minimum pt hat for hard interactions', 
+            'CKIN(4)=3000.    ! maximum pt hat for hard interactions',
+            'MSTP(142)=2      ! Turns on the PYWEVT Pt reweighting routine' 
+        ),
+        CSAParameters = cms.vstring(
+            'CSAMODE = 7     ! towards a "flat" QCD spectrum',
+            'PTPOWER = 4.5   ! reweighting of the pt spectrum'
+        ),
+        parameterSets = cms.vstring(
+            'pythiaUESettings', 
+            'processParameters', 
+            'CSAParameters'
+        )
+    )
+)
+ProductionFilterSequence = cms.Sequence(generator)
---
+++ python/PYTHIA6_ZeeJetsFlat_Pt15to3000_7TeV_cff.py
@@ -0,0 +1,45 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    comEnergy = cms.double(7000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring(
+            'MSEL=0            ! user defined processes',
+            'MSUB(15)    = 1   ! ff -> Z0 f',
+            'MSUB(30)    = 1   ! ff -> Z0 g',
+            "MDME(174,1) = 0   ! Z decay into d dbar",        
+            "MDME(175,1) = 0   ! Z decay into u ubar",
+            'MDME(176,1) = 0   ! Z decay into s sbar',
+            'MDME(177,1) = 0   ! Z decay into c cbar',
+            'MDME(178,1) = 0   ! Z decay into b bbar',
+            'MDME(179,1) = 0   ! Z decay into t tbar',
+            'MDME(182,1) = 1   ! Z decay into e- e+',
+            'MDME(183,1) = 0   ! Z decay into nu_e nu_ebar',
+            'MDME(184,1) = 0   ! Z decay into mu- mu+',
+            'MDME(185,1) = 0   ! Z decay into nu_mu nu_mubar',
+            'MDME(186,1) = 0   ! Z decay into tau- tau+',
+            'MDME(187,1) = 0   ! Z decay into nu_tau nu_taubar' ,
+            'CKIN(3)=15.       ! minimum pt hat for hard interactions', 
+            'CKIN(4)=3000.     ! maximum pt hat for hard interactions',
+            'MSTP(142)=2       ! Turns on the PYWEVT Pt reweighting routine' 
+        ),
+        CSAParameters = cms.vstring(
+            'CSAMODE = 7     ! towards a "flat" QCD spectrum',
+            'PTPOWER = 4.5   ! reweighting of the pt spectrum'
+        ),
+        parameterSets = cms.vstring(
+            'pythiaUESettings', 
+            'processParameters', 
+            'CSAParameters'
+        )
+    )
+)
+ProductionFilterSequence = cms.Sequence(generator)
