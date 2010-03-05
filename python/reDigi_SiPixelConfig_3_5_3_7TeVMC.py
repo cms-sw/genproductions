6135325b33c9f6af5849df8627c80d3914c9046b
Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch>
2010-03-05 22:15:46 +0000

    Merge new reDigi_SiPixelConfig_3_5_3_7TeVMC with Configuration/StandardSequences/python/DigiToRecoNoPU.py

--- python/reDigi_SiPixelConfig_3_5_3_7TeVMC.py
+++ python/reDigi_SiPixelConfig_3_5_3_7TeVMC.py
@@ -8,6 +8,37 @@ def customise(process):
     process.simSiPixelDigis.ThresholdSmearing_FPix = 210.0
     process.simSiPixelDigis.ThresholdSmearing_BPix = 245.0
 
+    REDIGIInputEventSkimming= cms.PSet(
+        inputCommands=cms.untracked.vstring('drop *')
+        )
 
-    return(process)
+    HLTCleaning= cms.PSet(
+        inputCommands=cms.untracked.vstring('drop FEDRawDataCollection_*_*_*')
+        )
+
+    REDIGIInputEventSkimming.inputCommands.extend(process.RecoGenJetsFEVT.outputCommands)
+    REDIGIInputEventSkimming.inputCommands.extend(process.RecoGenMETFEVT.outputCommands)
+    REDIGIInputEventSkimming.inputCommands.extend(process.SimG4CoreRAW.outputCommands) 
+    REDIGIInputEventSkimming.inputCommands.extend(process.GeneratorInterfaceRAW.outputCommands) 
+    REDIGIInputEventSkimming.inputCommands.extend(process.IOMCRAW.outputCommands) 
+    REDIGIInputEventSkimming.inputCommands.extend(process.HLTriggerRAW.outputCommands) 
+    REDIGIInputEventSkimming.inputCommands.extend(HLTCleaning.inputCommands)
     
+    process.source.inputCommands = REDIGIInputEventSkimming.inputCommands
+    process.source.dropDescendantsOfDroppedBranches=cms.untracked.bool(False)
+    
+    process.RandomNumberGeneratorService.restoreStateLabel = cms.untracked.string('randomEngineStateProducer')
+    
+    # Output definition for RAW
+    #process.outputRaw = cms.OutputModule("PoolOutputModule",
+    #                                     outputCommands = process.RAWSIMEventContent.outputCommands,
+    #                                     fileName = cms.untracked.string('New_RAWSIM.root'),
+    #                                     dataset = cms.untracked.PSet(dataTier = cms.untracked.string(''),
+    #                                                                  filterName = cms.untracked.string('')
+    #                                                                  )
+    #                                     )
+    
+    #process.out_step_raw = cms.EndPath(process.outputRaw)
+    #process.schedule.append(process.out_step_raw)
+                
+    return(process)
