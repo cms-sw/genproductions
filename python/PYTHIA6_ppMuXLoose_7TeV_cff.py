50cf5287d2b216b5aac688110ee652a8107771f9
Roberto Covarelli <sha1-195c8adc73af39d9dbe4407aec6ff1a79a6f51ef@cern.ch>
2010-03-08 17:22:17 +0000

    New ppMuXLoose

--- python/PYTHIA6_ppMuXLoose_7TeV_cff.py
+++ python/PYTHIA6_ppMuXLoose_7TeV_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 configurationMetadata = cms.untracked.PSet(
-        version = cms.untracked.string('$Revision: 1.2 $'),
-        name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_ppMuXLoose_10TeV_cff.py,v $'),
+        version = cms.untracked.string('$Revision: 1.1 $'),
+        name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_ppMuXLoose_7TeV_cff.py,v $'),
         annotation = cms.untracked.string('Summer09: Pythia6-MinBias at 10TeV with Muon preselection (Loose cut), +decay-inflight, 10TeV, D6T tune')
 )
 
@@ -14,7 +14,7 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     comEnergy = cms.double(7000.0),
     crossSection = cms.untracked.double(48440000000.0),
-    filterEfficiency = cms.untracked.double(0.0423),
+    filterEfficiency = cms.untracked.double(0.0355),
     maxEventsToPrint = cms.untracked.int32(0),
     PythiaParameters = cms.PSet(
         pythiaUESettingsBlock,
@@ -32,14 +32,15 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
 )
 
 mugenfilter = cms.EDFilter("MCSmartSingleParticleFilter",
-    Status = cms.untracked.vint32(     1,    1,   1,   1,   1,    1,     1,    1,    1,    1),
-    ParticleID = cms.untracked.vint32(13,  -13,  13,  -13,  13,  -13,   13,  -13,   13,  -13),
-    MinPt = cms.untracked.vdouble(    0.6, 0.6,  0.6,  0.6, 1.0, 1.0,  1.0,  1.0,  1.5,  1.5),
-    MaxEta = cms.untracked.vdouble(   2.5, 2.5, -1.7, -1.7, 1.7, 1.7, -1.2, -1.2,  1.2,  1.2),
-    MinEta = cms.untracked.vdouble(   1.7, 1.7, -2.5, -2.5, 1.2, 1.2, -1.7, -1.7, -1.2, -1.2),
-    MaxDecayRadius = cms.untracked.vdouble(2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0),
-    MaxDecayZ = cms.untracked.vdouble(4000.0, 4000.0, 4000.0, 4000.0, 4000.0, 4000.0, 4000.0, 4000.0, 4000.0, 4000.0, 4000.0, 4000.0),
-    MinDecayZ = cms.untracked.vdouble(-4000.0, -4000.0, -4000.0, -4000.0, -4000.0, -4000.0, -4000.0, -4000.0, -4000.0, -4000.0, -4000.0, -4000.0)
+    Status = cms.untracked.vint32(     1,    1,   1,   1,   1,    1),
+    ParticleID = cms.untracked.vint32(13,  -13,  13,  -13,  13,  -13),
+    MinPt = cms.untracked.vdouble(    0.0, 0.0,  3.0,  3.0, 0.0, 0.0),
+    MinP  = cms.untracked.vdouble(    2.5, 2.5,  2.5,  2.5, 2.5, 2.5),
+    MaxEta = cms.untracked.vdouble(   2.5, 2.5, 1.0, 1.0, -1.0, -1.0),
+    MinEta = cms.untracked.vdouble(   1.0, 1.0, -1.0, -1.0, -2.5, -2.5),
+    MaxDecayRadius = cms.untracked.vdouble(2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0),
+    MaxDecayZ = cms.untracked.vdouble(4000.0, 4000.0, 4000.0, 4000.0, 4000.0, 4000.0),
+    MinDecayZ = cms.untracked.vdouble(-4000.0, -4000.0, -4000.0, -4000.0, -4000.0, -4000.0)
 )
 
 ProductionFilterSequence = cms.Sequence(generator*mugenfilter)
