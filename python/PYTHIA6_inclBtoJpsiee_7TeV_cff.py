f3ccf62bf8619fc5ed9416f47d434e0fdf78c5a1
Simone Paoletti <sha1-f56c279ace5d35d60e60b72b7ca0178aafd29bcf@cern.ch>
2010-03-10 15:28:03 +0000

    7 TeV simulation of B -> jpsi -> ee

---
+++ python/PYTHIA6_inclBtoJpsiee_7TeV_cff.py
@@ -0,0 +1,76 @@
+import FWCore.ParameterSet.Config as cms
+
+configurationMetadata = cms.untracked.PSet(
+        version = cms.untracked.string('$Revision: 1.2 $'),
+        name = cms.untracked.string('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/PYTHIA6_inclBtoJpsiee_7TeV_cff.py,v $'),
+        annotation = cms.untracked.string('Summer09: Pythia6+EvtGen generation of B->Jpsi->ee, 10TeV')
+)
+
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(66640000.0),
+    filterEfficiency = cms.untracked.double(0.000466),
+    maxEventsToPrint = cms.untracked.int32(0),
+    ExternalDecays = cms.PSet(
+        EvtGen = cms.untracked.PSet(
+             operates_on_particles = cms.vint32( 0 ), # 0 (zero) means default list (hardcoded)
+                                                      # you can put here the list of particles (PDG IDs)
+                                                      # that you want decayed by EvtGen
+             use_default_decay = cms.untracked.bool(False),
+             decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
+             #decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY.DEC'),
+             particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evt.pdl'),
+             #user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/Validation.dec'),
+             user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/incl_BtoJpsi_ee.dec'),
+             list_forced_decays = cms.vstring('MyB0',
+                                              'Myanti-B0',
+                                              'MyB+',
+                                              'MyB-',
+                                              'MyB_s0',
+                                              'Myanti-B_s0',
+                                              'MyLambda_b0',
+                                              'Myanti-Lambda_b0'),
+        ),
+        parameterSets = cms.vstring('EvtGen')
+    ),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        bbbarSettings = cms.vstring('MSEL = 1'), 
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring(
+             'pythiaUESettings',
+             'bbbarSettings')
+    )
+)
+
+bfilter = cms.EDFilter("PythiaFilter",
+    ParticleID = cms.untracked.int32(5)
+)
+
+oniafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(2),
+    MaxEta = cms.untracked.double(1000.0),
+    MinEta = cms.untracked.double(-1000.0),
+    MinPt = cms.untracked.double(0.0),
+    ParticleID = cms.untracked.int32(443)
+)
+
+eegenfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MinPt = cms.untracked.vdouble(3.0, 3.0),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    ParticleCharge = cms.untracked.int32(-1),
+    ParticleID1 = cms.untracked.vint32(11),
+    ParticleID2 = cms.untracked.vint32(11)
+)
+
+
+ProductionFilterSequence = cms.Sequence(generator*bfilter*oniafilter*eegenfilter)
+
+
