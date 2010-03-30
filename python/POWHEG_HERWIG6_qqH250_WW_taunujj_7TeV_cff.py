69741aa38f43829d9fe42281605718cc714bf326
Andrea Davide Benaglia <sha1-73fb43758fe99043d0577882b844e9bc3891f6c0@cern.ch>
2010-03-30 21:58:08 +0000

    first import

---
+++ python/POWHEG_HERWIG6_qqH250_WW_taunujj_7TeV_cff.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDFilter(
+    "Herwig6HadronizerFilter",
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(0.0227),
+    doMPInteraction = cms.bool(True),
+    emulatePythiaStatusCodes = cms.untracked.bool(True),
+    filterEfficiency = cms.untracked.double(1.0),
+    herwigHepMCVerbosity = cms.untracked.bool(False),
+    herwigVerbosity = cms.untracked.int32(0),
+    lhapdfSetPath = cms.untracked.string(''),
+    maxEventsToPrint = cms.untracked.int32(5),
+    printCards = cms.untracked.bool(False),
+    useJimmy = cms.bool(True),
+    UseExternalGenerators = cms.untracked.bool(True),
+    HerwigParameters = cms.PSet(
+        herwigUEsettings = cms.vstring(
+            'JMUEO     = 1         ! multiparton interaction model',
+            'PTJIM     = 4.449     ! 2.8x(sqrt(s)/1.8TeV)^0.27 @ 10 TeV',
+            'JMRAD(73) = 1.8       ! inverse proton radius squared',
+            'PRSOF     = 0.0       ! prob. of a soft underlying event',
+            'MAXER     = 1000000   ! max error',
+            'MODPDF(1) = 10050     ! PDF set according to LHAPDF',
+            'MODPDF(2) = 10050     ! CTEQ6m',
+            'MODBOS(1) = 4         ! first  W -> tau',
+            'MODBOS(2) = 1         ! second W -> qq ',
+            ),
+        herwigPowheg = cms.vstring(
+            'PTMIN      = 0.5    ! minimum pt in hadronic jet',
+            'IPROC      = -1910     ! - = dont generate hard event, 1000 = qqH   10 = H->WW'
+            ),
+        parameterSets = cms.vstring(
+            'herwigUEsettings',
+            'herwigPowheg'
+            )
+        )
+    )
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/POWHEG_HERWIG6_H250_WW_taunujj_7TeV_cff.py,v $'),
+    annotation = cms.untracked.string('POWHEG + HERWIG6 - VBF Higgs -> WW -> taunujj at 7TeV')
+    )
