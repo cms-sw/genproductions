d459622a0c451a0c236794d4925ff102d3bcde72
Zoltan Gecse <sha1-a0fcc5ded3c110ede739e8012ee7070ef7d0aea5@cern.ch>
2010-04-15 22:25:49 +0000

    setup the environment, auto GlobalTag

--- test/calculateXSectionAndFilterEfficiency.sh
+++ test/calculateXSectionAndFilterEfficiency.sh
@@ -1,8 +1,8 @@
 # Use this command to obtain the cross section and filter efficiency
 
-# Adjust the MySample_cff.py file, number of events to generate and GlobalTag
+# Adjust the MySample_cff.py file and the number of events to generate
 
-# To calculate filter efficiency, in the output look for:
+# To calculate the filter efficiency, in the output look for:
 
 # TrigReport ---------- Modules in Path: generation_step ------------
 # TrigReport  Trig Bit#    Visited     Passed     Failed      Error Name
@@ -10,5 +10,6 @@
 # TrigReport     1    0     100000      25834      74166          0 oniafilter
 # TrigReport     1    0      25834      14286      11548          0 mumugenfilter
 
-cmsDriver.py Configuration/GenProduction/python/MySample_cff.py -s GEN -n 100000 --conditions FrontierConditions_GlobalTag,STARTUP3X_V8H::All --customise Configuration/GenProduction/calculateXSectionAndFilterEfficiency.py
+cmsenv
+cmsDriver.py Configuration/Generator/python/MySample_cff.py -s GEN -n 10000 --conditions=auto:mc --customise Configuration/GenProduction/calculateXSectionAndFilterEfficiency.py
 
