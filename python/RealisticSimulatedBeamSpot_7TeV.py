3d7fc8e8aedfc198200b9713559b537ec1e6fede
Fabian Stoeckli <sha1-43e75525c3e6e41805da67101e406c6d881e76cd@cern.ch>
2010-04-29 17:37:56 +0000

    update BeamSpot April 29, 2010

--- python/RealisticSimulatedBeamSpot_7TeV.py
+++ python/RealisticSimulatedBeamSpot_7TeV.py
@@ -3,13 +3,13 @@ import FWCore.ParameterSet.Config as cms
 def customise(process):
 
         process.VtxSmeared.Phi = cms.double(0.0)
-        process.VtxSmeared.BetaStar = cms.double(1100.0)
+        process.VtxSmeared.BetaStar = cms.double(200.0)
         process.VtxSmeared.Emittance = cms.double(1.0e-07)
         process.VtxSmeared.Alpha = cms.double(0.0)
-        process.VtxSmeared.SigmaZ = cms.double(4.5)
+        process.VtxSmeared.SigmaZ = cms.double(2.87)
         process.VtxSmeared.TimeOffset = cms.double(0.0)
-        process.VtxSmeared.X0 = cms.double(0.2542)
-        process.VtxSmeared.Y0 = cms.double(0.4082)
-        process.VtxSmeared.Z0 = cms.double(-0.6569)         
+        process.VtxSmeared.X0 = cms.double(0.2434)
+        process.VtxSmeared.Y0 = cms.double(0.3834)
+        process.VtxSmeared.Z0 = cms.double(0.7552)         
                                                                 
         return(process)                                     
