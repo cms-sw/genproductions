59888e94baa693c29d44fe2e13a338cece2c624e
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2010-05-21 11:08:43 +0000

    Pythia 8: QCD JetMET request

---
+++ python/QCD_Pt_120to170_7TeV_pythia8_cff.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('$Revision$'),
+	name = cms.untracked.string('$Source$'),
+	annotation = cms.untracked.string('Spring10: Pythia8 generation of QCD events, 7TeV, pthat = 120 .. 170 GeV')
+)
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+	crossSection = cms.untracked.double(1.120e+05),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaPylistVerbosity = cms.untracked.int32(1),
+	filterEfficiency = cms.untracked.double(1.0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	comEnergy = cms.double(7000.0),
+	PythiaParameters = cms.PSet(
+		processParameters = cms.vstring(
+			'Main:timesAllowErrors    = 10000',
+			'ParticleDecays:limitTau0 = on',
+			'ParticleDecays:tauMax = 10',
+			'HardQCD:all = on',
+			'PhaseSpace:pTHatMin = 120',
+			'PhaseSpace:pTHatMax = 170',
+			'Tune:pp 2',
+			'Tune:ee 3'),
+		parameterSets = cms.vstring('processParameters')
+	)
+)
