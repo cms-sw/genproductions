48bbd2b71abd1bf6b940c30f2117ea3f6133fa00
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2010-06-28 12:15:16 +0000

    Fixed swap of external cross sections

--- python/QCD_Pt_80to120_7TeV_pythia8_cff.py
+++ python/QCD_Pt_80to120_7TeV_pythia8_cff.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 configurationMetadata = cms.untracked.PSet(
-	version = cms.untracked.string('$Revision$'),
-	name = cms.untracked.string('$Source$'),
+	version = cms.untracked.string('$Revision: 1.1 $'),
+	name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/QCD_Pt_80to120_7TeV_pythia8_cff.py,v $'),
 	annotation = cms.untracked.string('Spring10: Pythia8 generation of QCD events, 7TeV, pthat = 80 .. 120 GeV')
 )
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
-	crossSection = cms.untracked.double(6.035e+06),
+	crossSection = cms.untracked.double(7.519e+05),
 	maxEventsToPrint = cms.untracked.int32(0),
 	pythiaPylistVerbosity = cms.untracked.int32(1),
 	filterEfficiency = cms.untracked.double(1.0),
