1ee51ae0600589bbe121a289f39712fb6c0f2c8e
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2010-07-22 14:01:01 +0000

    Pythia6 QCD samples in pthat bins

---
+++ python/QCD_Pt_20_30_TuneD6T_7TeV_pythia6_cff.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    crossSection = cms.untracked.double(236000000.),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.double(7000.0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=1               ! QCD hight pT processes', 
+            'CKIN(3)=20.          ! minimum pt hat for hard interactions', 
+            'CKIN(4)=30.          ! maximum pt hat for hard interactions'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision:$'),
+    name = cms.untracked.string('$Source:$'),
+    annotation = cms.untracked.string('QCD Pt [20,30] at 7 TeV')
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
