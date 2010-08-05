5795fa96baa8e76a83890613edf6fc51b31b43df
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-08-05 10:32:01 +0000

    heavy-ion embedding configurations

---
+++ python/Pyquen_UnquenchedDiJet_Pt80_cfi.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PyquenDefaultSettings_cff import *
+
+hiSignal = cms.EDFilter("PyquenGeneratorFilter",
+                        collisionParameters,
+                        qgpParameters,
+                        pyquenParameters,
+                        doQuench = cms.bool(False),
+                        bFixed = cms.double(0.0), ## fixed impact param (fm); valid only if cflag_=0
+                        PythiaParameters = cms.PSet(pyquenPythiaDefaultBlock,
+                                                    parameterSets = cms.vstring('pythiaUESettings',
+                                                                                'ppJets',
+                                                                                'kinematics'),
+                                                    kinematics = cms.vstring ("CKIN(3)=80",  #min pthat
+                                                                              "CKIN(4)=9999" #max pthat
+                                                                              )
+                                                    ),
+                        cFlag = cms.int32(0), ## centrality flag
+                        bMin = cms.double(0.0), ## min impact param (fm); valid only if cflag_!=0
+                        bMax = cms.double(0.0) ## max impact param (fm); valid only if cflag_!=0
+                        )
+
+hiSignal.embeddingMode = True
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/edwenger/GenProduction/Pyquen_UnquenchedDiJet_Pt80_cfi.py,v $'),
+    annotation = cms.untracked.string('PYQUEN un-quenched dijets (pt-hat > 80 GeV) at sqrt(s) = 2.76TeV')
+    )
+
+ProductionFilterSequence = cms.Sequence(hiSignal)
