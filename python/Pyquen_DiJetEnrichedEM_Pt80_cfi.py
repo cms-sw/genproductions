972592d6859e08f0009fc4f7a93b70f4e8a77091
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-08-12 13:50:14 +0000

    fixed Pyquen configs to compile with 381

---
+++ python/Pyquen_DiJetEnrichedEM_Pt80_cfi.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PyquenDefaultSettings_cff import *
+from GeneratorInterface.HiGenCommon.ecalTrigSettings_cff import *
+
+hiSignal = cms.EDFilter("PyquenGeneratorFilter",
+                        #ecalTrigPt80,             # ecal gen event selector
+                        ecalTrigPt20,
+			collisionParameters,
+                        qgpParameters,
+                        pyquenParameters,
+                        doQuench = cms.bool(True),
+                        bFixed = cms.double(0.0), ## fixed impact param (fm); valid only if cflag_=0
+                        PythiaParameters = cms.PSet(pyquenPythiaDefaultBlock,
+                                                    parameterSets = cms.vstring('pythiaUESettings',
+                                                                                'ppJets',
+                                                                                'kinematics'),
+                                                    kinematics = cms.vstring ("CKIN(3)=80",  #min pthat
+                                                                              "CKIN(4)=9999" #max pthat
+                                                                              )
+                                                    ),
+                        cFlag = cms.int32(0), ## centrality flag
+                        bMin = cms.double(0.0), ## min impact param (fm); valid only if cflag_!=0
+                        bMax = cms.double(0.0) ## max impact param (fm); valid only if cflag_!=0
+                        )
+
+hiSignal.embeddingMode = True
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/Pyquen_DiJetEnrichedEM_Pt80_cfi.py,v $'),
+    annotation = cms.untracked.string('PYQUEN quenched EM-enriched dijets (pt-hat > 80 GeV) at sqrt(s) = 2.76TeV')
+    )
+
+ProductionFilterSequence = cms.Sequence(hiSignal)
