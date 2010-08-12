d853b46cabc7d05b2de87480b5e3daf2e43b703a
Fabian Stoeckli <sha1-43e75525c3e6e41805da67101e406c6d881e76cd@cern.ch>
2010-08-12 10:00:38 +0000

    38X MC Production

---
+++ python/DYToEE_M_10To20_TuneD6T_7TeV_pythia6_cff.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    crossSection = cms.untracked.double(2659.),
+    comEnergy = cms.double(7000.0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0            !User defined processes', 
+                                        'MSUB(1)=1         !Incl Z0/gamma* production', 
+                                        'MSTP(43)=3        !Both Z0 and gamma*', 
+                                        'MDME(174,1)=0     !Z decay into d dbar', 
+                                        'MDME(175,1)=0     !Z decay into u ubar', 
+                                        'MDME(176,1)=0     !Z decay into s sbar', 
+                                        'MDME(177,1)=0     !Z decay into c cbar', 
+                                        'MDME(178,1)=0     !Z decay into b bbar', 
+                                        'MDME(179,1)=0     !Z decay into t tbar', 
+                                        'MDME(182,1)=1     !Z decay into e- e+', 
+                                        'MDME(183,1)=0     !Z decay into nu_e nu_ebar', 
+                                        'MDME(184,1)=0     !Z decay into mu- mu+', 
+                                        'MDME(185,1)=0     !Z decay into nu_mu nu_mubar', 
+                                        'MDME(186,1)=0     !Z decay into tau- tau+', 
+                                        'MDME(187,1)=0     !Z decay into nu_tau nu_taubar', 
+                                        'CKIN(1)=10.       !Minimum sqrt(s_hat) value (=Z mass)',
+                                        'CKIN(2)=20.       !Maximum sqrt(s_hat) value (=Z mass)'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/Attic/PYTHIA6_EWK_Zmumu_7TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6 Z/gamma* to ee, 10 < M(e+e-) < 20 GeV at sqrt(s) = 7TeV')
+)
