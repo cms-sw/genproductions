9cae29b46df2f2b7968a60ba625374b6209af7c9
Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch>
2010-08-19 19:44:44 +0000

    b-enriched QCD using EVTGEN

---
+++ python/QCD_Pt_150_bEnriched_TuneD6T_7TeV_pythia6_cff.py
@@ -0,0 +1,48 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    crossSection = cms.untracked.double(47760),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(0.1030),
+    comEnergy = cms.double(7000.0),
+    ExternalDecays = cms.PSet(
+        EvtGen = cms.untracked.PSet(
+             operates_on_particles = cms.vint32( 0 ), # 0 (zero) means default list (hardcoded)
+                                                      # you can put here the list of particles (PDG IDs)
+                                                      # that you want decayed by EvtGen
+             use_default_decay = cms.untracked.bool(True),
+             decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
+             particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evt.pdl'),
+             user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
+             list_forced_decays = cms.vstring(),
+        ),
+        parameterSets = cms.vstring('EvtGen')
+    ),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=1               ! QCD hight pT processes', 
+            'CKIN(3)=150.          ! minimum pt hat for hard interactions'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.3 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/QCD_Pt_30to50_TuneD6T_7TeV_pythia6_cff.py,v $'),
+    annotation = cms.untracked.string('b-enriched QCD Pt [150,inf] at 7 TeV')
+)
+
+
+bbFilter = cms.EDFilter("MCSingleParticleFilter",
+                        ParticleID = cms.untracked.vint32(5,-5),
+                        Status = cms.untracked.vint32(2,2)
+                       )  
+
+ProductionFilterSequence = cms.Sequence(generator*bbFilter)
