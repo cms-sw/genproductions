857603ce1d8a954f0cd38061b24360930904860a
Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch>
2010-08-19 13:06:03 +0000

    first version of new mu enriched QCD cfgs

---
+++ python/QCD_Pt_20to30_MuPt5Enriched_TuneD6T_7TeV_pythia6_cff.py
@@ -0,0 +1,48 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUESettings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    crossSection = cms.untracked.double(236300000.),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(0.00518),
+    comEnergy = cms.double(7000.0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=1               ! QCD hight pT processes', 
+            'CKIN(3)=20.          ! minimum pt hat for hard interactions', 
+            'CKIN(4)=30.          ! maximum pt hat for hard interactions',
+            'MSTJ(22)=4       ! Decay unstable particles inside a cylinder',
+            'PARJ(73)=2000.   ! max. radius for MSTJ(22)=4',
+            'PARJ(74)=4000.   ! max. Z for MSTJ(22)=4',
+            'MDCY(C130,1)=1   ! decay k0-longs',
+            'MDCY(C211,1)=1   ! decay pions',
+            'MDCY(C321,1)=1   ! decay kaons'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.3 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/QCD_Pt_15to20_TuneD6T_7TeV_pythia6_cff.py,v $'),
+    annotation = cms.untracked.string('mu-enriched QCD Pt [20,30] at 7 TeV')
+)
+
+mugenfilter = cms.EDFilter("MCSmartSingleParticleFilter",
+                           MinPt = cms.untracked.vdouble(5.,5.),
+                           MinEta = cms.untracked.vdouble(-2.5,-2.5),
+                           MaxEta = cms.untracked.vdouble(2.5,2.5),
+                           ParticleID = cms.untracked.vint32(13,-13),
+                           Status = cms.untracked.vint32(1,1),
+                           # Decay cuts are in mm
+                           MaxDecayRadius = cms.untracked.vdouble(2000.,2000.),
+                           MinDecayZ = cms.untracked.vdouble(-4000.,-4000.),
+                           MaxDecayZ = cms.untracked.vdouble(4000.,4000.)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*mugenfilter)
