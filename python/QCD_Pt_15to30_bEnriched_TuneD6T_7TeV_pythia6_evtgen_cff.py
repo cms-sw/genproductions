d3659af454e2741e98fce5a332e141d0044639ef
Fabian Stoeckli <sha1-43e75525c3e6e41805da67101e406c6d881e76cd@cern.ch>
2010-08-20 08:23:54 +0000

    Fall10 38X MC production

--- python/QCD_Pt_15to30_bEnriched_TuneD6T_7TeV_pythia6_evtgen_cff.py
+++ python/QCD_Pt_15to30_bEnriched_TuneD6T_7TeV_pythia6_evtgen_cff.py
@@ -1,14 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-source = cms.Source("EmptySource")
+from Configuration.Generator.PythiaUESettings_cfi import *
 
-from Configuration.Generator.PythiaUEZ2Settings_cfi import *
 generator = cms.EDFilter("Pythia6GeneratorFilter",
-    crossSection = cms.untracked.double(7224000),
+    crossSection = cms.untracked.double(812800000),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     maxEventsToPrint = cms.untracked.int32(0),
     pythiaPylistVerbosity = cms.untracked.int32(0),
-    filterEfficiency = cms.untracked.double(0.08297),
+    filterEfficiency = cms.untracked.double(0.04760),
     comEnergy = cms.double(7000.0),
     ExternalDecays = cms.PSet(
         EvtGen = cms.untracked.PSet(
@@ -26,8 +25,8 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
     PythiaParameters = cms.PSet(
         pythiaUESettingsBlock,
         processParameters = cms.vstring('MSEL=1               ! QCD hight pT processes', 
-            'CKIN(3)=50.          ! minimum pt hat for hard interactions', 
-            'CKIN(4)=150.          ! maximum pt hat for hard interactions'),
+            'CKIN(3)=15.          ! minimum pt hat for hard interactions', 
+            'CKIN(4)=30.          ! maximum pt hat for hard interactions'),
         # This is a vector of ParameterSet names to be read, in this order
         parameterSets = cms.vstring('pythiaUESettings', 
             'processParameters')
@@ -35,9 +34,9 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
 )
 
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.3 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/QCD_Pt_30to50_TuneD6T_7TeV_pythia6_cff.py,v $'),
-    annotation = cms.untracked.string('b-enriched QCD Pt [50,150] at 7 TeV')
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/QCD_Pt_15to30_bEnriched_TuneD6T_7TeV_pythia6_cff.py,v $'),
+    annotation = cms.untracked.string('b-enriched QCD Pt [15,30] at 7 TeV')
 )
 
 
--- python/QCD_Pt_50to150_bEnriched_TuneD6T_7TeV_pythia6_evtgen_cff.py
+++ python/QCD_Pt_50to150_bEnriched_TuneD6T_7TeV_pythia6_evtgen_cff.py
@@ -1,14 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-source = cms.Source("EmptySource")
+from Configuration.Generator.PythiaUESettings_cfi import *
 
-from Configuration.Generator.PythiaUEZ2Settings_cfi import *
 generator = cms.EDFilter("Pythia6GeneratorFilter",
-    crossSection = cms.untracked.double(53100000),
+    crossSection = cms.untracked.double(7224000),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     maxEventsToPrint = cms.untracked.int32(0),
     pythiaPylistVerbosity = cms.untracked.int32(0),
-    filterEfficiency = cms.untracked.double(0.06720),
+    filterEfficiency = cms.untracked.double(0.08297),
     comEnergy = cms.double(7000.0),
     ExternalDecays = cms.PSet(
         EvtGen = cms.untracked.PSet(
@@ -26,8 +25,8 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
     PythiaParameters = cms.PSet(
         pythiaUESettingsBlock,
         processParameters = cms.vstring('MSEL=1               ! QCD hight pT processes', 
-            'CKIN(3)=30.          ! minimum pt hat for hard interactions', 
-            'CKIN(4)=50.          ! maximum pt hat for hard interactions'),
+            'CKIN(3)=50.          ! minimum pt hat for hard interactions', 
+            'CKIN(4)=150.          ! maximum pt hat for hard interactions'),
         # This is a vector of ParameterSet names to be read, in this order
         parameterSets = cms.vstring('pythiaUESettings', 
             'processParameters')
@@ -35,9 +34,9 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
 )
 
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.3 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/QCD_Pt_30to50_TuneD6T_7TeV_pythia6_cff.py,v $'),
-    annotation = cms.untracked.string('b-enriched QCD Pt [30,50] at 7 TeV')
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/QCD_Pt_50to150_bEnriched_TuneD6T_7TeV_pythia6_cff.py,v $'),
+    annotation = cms.untracked.string('b-enriched QCD Pt [50,150] at 7 TeV')
 )
 
 
