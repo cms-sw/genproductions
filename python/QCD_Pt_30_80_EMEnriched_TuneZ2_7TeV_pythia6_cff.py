2bf958c80ce7c32995e18c334d3029dea773388d
Piergiulio Lenzi <sha1-50f2e876925e1e2ac1aca9d0f1bdf5c962ec9f65@cern.ch>
2010-08-22 14:28:42 +0000

    qcd em enriched  30-80

---
+++ python/QCD_Pt_30_80_EMEnriched_TuneZ2_7TeV_pythia6_cff.py
@@ -0,0 +1,61 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(0.065),
+    crossSection = cms.untracked.double(59480000.),                         
+    comEnergy = cms.double(7000.0),  # center of mass energy in GeV
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=1               ! QCD high pT processes',
+                                        'CKIN(3)=30.          ! minimum pt hat for hard interactions',
+                                        'CKIN(4)=80.          ! maximum pt hat for hard interactions'
+                                        ),
+        parameterSets = cms.vstring('pythiaUESettings', 
+                                    'processParameters')
+        )
+)
+
+
+genParticlesForFilter = cms.EDProducer("GenParticleProducer",
+    saveBarCodes = cms.untracked.bool(True),
+    src = cms.InputTag("generator"),
+    abortOnUnknownPDGCode = cms.untracked.bool(True)
+)
+
+bctoefilter = cms.EDFilter("BCToEFilter",
+                           filterAlgoPSet = cms.PSet(eTThreshold = cms.double(10),
+                                                     genParSource = cms.InputTag("genParticlesForFilter")
+                                                     )
+                           )
+
+emenrichingfilter = cms.EDFilter("EMEnrichingFilter",
+                                 filterAlgoPSet = cms.PSet(isoGenParETMin=cms.double(20.),
+                                                           isoGenParConeSize=cms.double(0.1),
+                                                           clusterThreshold=cms.double(20.),
+                                                           isoConeSize=cms.double(0.2),
+                                                           hOverEMax=cms.double(0.5),
+                                                           tkIsoMax=cms.double(5.),
+                                                           caloIsoMax=cms.double(10.),
+                                                           requireTrackMatch=cms.bool(False),
+                                                           genParSource = cms.InputTag("genParticlesForFilter")
+                                                           )
+                                 )
+    
+
+
+# enter below the configuration metadata (only a description is needed, the rest is filled in by cvs)
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    name = cms.untracked.string('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/Attic/PYTHIA6_QCD_Pt_30_80_7TeV_EMEnrichingFilter_cff.py,v $'),
+    annotation = cms.untracked.string('em-enriching filtered QCD pthat 30-80, 7 TeV')
+)
+
+# add your filters to this sequence
+ProductionFilterSequence = cms.Sequence(generator * (genParticlesForFilter + ~bctoefilter + emenrichingfilter))
+
