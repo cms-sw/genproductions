103b9e6267938728cd59e0ab878179fe75898eff
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2010-08-25 08:56:07 +0000

    QCD dijet

---
+++ python/QCD_Pt_80to120_TuneZ2_7TeV_pythia6_cff.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter('Pythia6GeneratorFilter',
+	comEnergy = cms.double(7000.0),
+	crossSection = cms.untracked.double(7.842652e+05),
+	filterEfficiency = cms.untracked.double(1),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
+	PythiaParameters = cms.PSet(
+		pythiaUESettingsBlock,
+		processParameters = cms.vstring(
+			'MSEL = 1        ! QCD hight pT processes',
+			'CKIN(3) = 80    ! minimum pt hat for hard interactions',
+			'CKIN(4) = 120   ! maximum pt hat for hard interactions',
+		),
+		parameterSets = cms.vstring(
+			'pythiaUESettings',
+			'processParameters',
+		)
+	)
+)
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('\$Revision$'),
+	name = cms.untracked.string('\$Source$'),
+	annotation = cms.untracked.string('Fall2010 sample with PYTHIA6: QCD dijet production, pThat = 80 .. 120 GeV, TuneZ2')
+)
