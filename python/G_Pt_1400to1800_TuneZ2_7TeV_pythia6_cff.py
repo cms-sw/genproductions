084db1e9e07cc9e1f476f85e932e5b7b10c34013
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2010-08-25 08:28:26 +0000

    Gamma + Jet

---
+++ python/G_Pt_1400to1800_TuneZ2_7TeV_pythia6_cff.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter('Pythia6GeneratorFilter',
+	comEnergy = cms.double(7000.0),
+	crossSection = cms.untracked.double(1.269863e-05),
+	filterEfficiency = cms.untracked.double(1),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
+	PythiaParameters = cms.PSet(
+		pythiaUESettingsBlock,
+		processParameters = cms.vstring(
+			'MSEL = 10       ! Photon+Jets production',
+			'CKIN(3) = 1400  ! minimum pt hat for hard interactions',
+			'CKIN(4) = 1800  ! maximum pt hat for hard interactions',
+		),
+		parameterSets = cms.vstring(
+			'pythiaUESettings',
+			'processParameters',
+		)
+	)
+)
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('\$Revision$'),
+	name = cms.untracked.string('\$Source$'),
+	annotation = cms.untracked.string('Fall2010 sample with PYTHIA6: Prompt photon production, pThat = 1400 .. 1800 GeV, TuneZ2')
+)
