af54c4183078e4f36d43bd76ea04d34ea0d2aa66
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2010-09-01 14:29:18 +0000

    38X MC production

---
+++ python/PYTHIA6_Tauola_Exotica_LQ_btau_500_7TeV_tautaubb_cff.py
@@ -0,0 +1,46 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(2.340e-2),
+    ExternalDecays = cms.PSet(
+        Tauola = cms.untracked.PSet(
+            TauolaPolar,
+            TauolaDefaultInputCards
+        ),
+        parameterSets = cms.vstring('Tauola')
+    ),
+    UseExternalGenerators = cms.untracked.bool(True),        
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('PMAS(42,1)=500.0        ! LQ mass', 
+            'IMSS(21)=33             ! LUN number for SLHA File (must be 33)',             
+            'IMSS(22)=33             ! Read-in SLHA decay table',
+            'KCHG(42,1)=2            ! LQ charge x 3',  
+            'MSEL=0                  ! (D=1) to select between full user control (0, then use MSUB) and some preprogrammed alternative', 
+            'MSUB(163)=1             ! g+g->LQ+LQbar', 
+            'MSUB(164)=1             ! q+qbar->LQ+LQbar'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters',
+            'SLHAParameters'),
+        SLHAParameters = cms.vstring('SLHAFILE = Configuration/Generator/data/LQ_btau_beta1.0.out')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+        version = cms.untracked.string('$Revision: 1.2 $'),
+        name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/Configuration/GenProduction/python/PYTHIA6_Tauola_Exotica_LQ_btau_500_7TeV_tautaubb_cff.py,v $'),
+        annotation = cms.untracked.string('default documentation string for PYTHIA6_Tauola_Exotica_LQ_btau_500_7TeV_tautaubb_cff.py')
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
