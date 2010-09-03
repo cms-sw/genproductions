3f1949c4bb8f8403fd99b339f12e4ea3920448d5
Benoit Roland <sha1-2200346e613b7795d3e973c9eb4d9c0d63733b86@cern.ch>
2010-09-03 10:01:15 +0000

    Fall 2010

---
+++ python/Pizero_Pt2to20_gun_cff.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDProducer('FlatRandomPtGunProducer',
+	PGunParameters = cms.PSet(
+		PartID = cms.vint32(111),  ## pizero
+		MinPhi = cms.double(-3.14159265359), ## in radians
+		MaxPhi = cms.double(3.14159265359),
+		MinEta = cms.double(-3),
+		MaxEta = cms.double(3),
+		MinPt = cms.double(2),
+		MaxPt = cms.double(20),
+	),
+	Verbosity = cms.untracked.int32(0),
+	psethack = cms.string('single pizero 2<Pt<20 -3<Eta<3'),
+	AddAntiParticle = cms.bool(False),  ## only pizero
+)
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('\$Revision: 1.1 $'),
+        name = cms.untracked.string('\$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/Pizero_Pt2to20_gun_cff.py,v $'),
+	annotation = cms.untracked.string('Fall 2010: Flat Random Pt Gun: pizero, 2<Pt<20, -3<Eta<3')
+)
