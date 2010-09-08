a9d7daf4ceb66396c6060d1fe2d28eb872122903
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2010-09-08 16:15:24 +0000

    EXO 38X

---
+++ python/PYTHIA6_EXOTICA_RSGravWW_kMpl005_M750_7TeV_enujj_cff.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(0.15),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(7000.0),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         crossSection  = cms.untracked.double(0.36),
+                         PythiaParameters = cms.PSet(pythiaUESettingsBlock,
+                                                     processParameters = cms.vstring('PMAS(5,1)= 4.8         ! b quark mass',
+                                                                                     'PMAS(6,1)= 175.0       ! t quark mass',
+                                                                                     'PMAS(347,1)= 750.0     ! mass of RS Graviton',
+                                                                                     'PARP(50) = 0.27        ! 0.054 == c=0.01 (k/M_PL=0.01)',
+                                                                                     'MSEL=0                 ! (D=1) 0 to select full user control',
+                                                                                     'MSUB(391)=1               ! q qbar -> G* ',
+                                                                                     'MSUB(392)=1               ! g g -> G*',
+                                                                                     '5000039:ALLOFF            ! Turn off all decays of G*',
+                                                                                     '5000039:ONIFANY 24        ! Turn on the decays WW'),
+                                                     parameterSets = cms.vstring('pythiaUESettings',
+                                                                                 'processParameters')
+                                                     )
+                         )
+
+enujjFilter  = cms.EDFilter("TwoVBGenFilter",
+    src      = cms.untracked.InputTag("generator"),
+    eejj     = cms.bool(False),
+    enujj    = cms.bool(True),
+    mumujj   = cms.bool(False),
+    munujj   = cms.bool(False),
+    tautaujj = cms.bool(False),
+    taunujj  = cms.bool(False),
+    nunujj   = cms.bool(False)
+)
+
+configurationMetadata = cms.untracked.PSet(
+        version = cms.untracked.string('$Revision: 1.3 $'),
+        name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_EXOTICA_RSGravWW_kMpl005_M750_7TeV_enujj_cff.py,v $'),
+        annotation = cms.untracked.string('default documentation string for PYTHIA6_EXOTICA_RSGravWW_kMpl005_M750_7TeV_enujj_cff.py')
+)
+
+ProductionFilterSequence = cms.Sequence(generator*enujjFilter)
