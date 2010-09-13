3c613d748031b9bc4feb92f9ab595b8357cf9ab3
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2010-09-13 15:47:08 +0000

    EXO 38X

---
+++ python/PYTHIA8_Exotica_UNPARTmonojet_S1_d1_1_7TeV_cfi.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                                        maxEventsToPrint = cms.untracked.int32(1),
+                                        pythiaPylistVerbosity = cms.untracked.int32(1),
+                                        filterEfficiency = cms.untracked.double(1.0),
+                                        pythiaHepMCVerbosity = cms.untracked.bool(False),
+                                        comEnergy = cms.double(7000.),
+                                        PythiaParameters = cms.PSet(
+                                        pythia8_unparticle = cms.vstring( ## see details on http://home.thep.lu.se/~torbjorn/php8135/ExtraDimensionalProcesses.php?filepath=files/
+                                               'ExtraDimensionsUnpart:monojet = on',
+                                               'ExtraDimensionsUnpart:gg2Ug = off',
+                                               'ExtraDimensionsUnpart:qg2Uq = on',
+                                               'ExtraDimensionsUnpart:qqbar2Ug = on',
+					       'ExtraDimensionsUnpart:spinU = 1',
+					       'ExtraDimensionsUnpart:dU = 1.1',
+					       'ExtraDimensionsUnpart:LambdaU = 1000.',
+					       'ExtraDimensionsUnpart:lambda = 1.0',
+					       'ExtraDimensionsUnpart:CutOffmode = 1',
+					       '5000039:m0 = 300.',
+					       '5000039:mWidth = 300.',
+					       '5000039:mMin = 1.',
+					       '5000039:mMax = 13990.',
+					       'PhaseSpace:pTHatMin = 50.',
+                                               'PartonLevel:MI = off',
+                                               'PartonLevel:ISR = on',
+                                               'PartonLevel:FSR = on'
+                                        ), 
+                                        parameterSets = cms.vstring('pythia8_unparticle')
+                                 ) 
+                         )
+
