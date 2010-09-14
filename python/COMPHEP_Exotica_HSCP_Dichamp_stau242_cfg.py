43327a0deec9eb1d0b1bd90751a6370392b3bf57
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2010-09-14 18:41:58 +0000

    EXO 38X

---
+++ python/COMPHEP_Exotica_HSCP_Dichamp_stau242_cfg.py
@@ -0,0 +1,43 @@
+FLAVOR = 'stau'
+COM_ENERGY = 7000. 
+MASS_POINT = 242   # GeV
+PROCESS_FILE = 'SimG4Core/CustomPhysics/data/RhadronProcessList.txt'
+PARTICLE_FILE = 'SimG4Core/CustomPhysics/data/particles_Dichamp_%s_%d_GeV.txt' % (FLAVOR, MASS_POINT)
+SLHA_FILE = 'file:/uscms_data/d2/jchen/dichamp/Dichamp_7TeV_Dichamp%dGeV.lhe' % MASS_POINT
+
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+#source = cms.Source("LHESource",
+#                                       fileNames = cms.untracked.vstring('%s' %
+# SLHA_FILE)
+#                                       )
+generator = cms.EDFilter("Pythia6HadronizerFilter",
+    filterEfficiency = cms.untracked.double(1.),
+    comEnergy = cms.double(COM_ENERGY),
+    crossSection = cms.untracked.double(0.023),
+    maxEventsToPrint = cms.untracked.int32(0),
+    stopHadrons = cms.bool(False),
+    gluinoHadrons =  cms.bool(False),  
+                         
+	PythiaParameters = cms.PSet(
+	    pythiaUESettingsBlock,
+	 	processParameters = cms.vstring(
+ 	      'MSEL=0          ! User defined processes',
+		  'MDCY(C1000015,1)=0       ! set the stau stable.'
+		  ),
+    parameterSets = cms.vstring(
+
+    'pythiaUESettings', 'processParameters'),
+    
+    )
+ )
+                         
+generator.hscpFlavor = cms.untracked.string(FLAVOR)
+generator.massPoint = cms.untracked.int32(MASS_POINT)
+generator.slhaFile = cms.untracked.string(SLHA_FILE)
+generator.processFile = cms.untracked.string(PROCESS_FILE)
+generator.particleFile = cms.untracked.string(PARTICLE_FILE)
+
+ProductionFilterSequence = cms.Sequence(generator)
