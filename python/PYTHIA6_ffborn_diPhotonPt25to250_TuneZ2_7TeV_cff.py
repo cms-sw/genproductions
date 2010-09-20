7f4a6e569f1ddd670ff390cd616e378782e749b7
Sanjay Padhi <sha1-021a4c85a7c4bf0b49df6e15b28b55f4b491bbf8@cern.ch>
2010-09-20 08:13:18 +0000

    added configs for diphoton and Z/gamma to invisible

---
+++ python/PYTHIA6_ffborn_diPhotonPt25to250_TuneZ2_7TeV_cff.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter",   
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    crossSection  = cms.untracked.double(22.37),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring(
+            'MSEL=0 ',
+            'MSUB(18)=1       ',
+            'CKIN(3)=25.          ! minimum pt hat for hard interactions', 
+            'CKIN(4)=250.          ! maximum pt hat for hard interactions'),
+
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_ffborn_diPhotonPt25to250_TuneZ2_7TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6 ff to diphotons born with Pt25-250 at sqrt(s) = 7TeV')
+)
+ProductionFilterSequence = cms.Sequence(generator)
+
+
---
+++ python/PYTHIA6_ggbox_diPhotonPt250_TuneZ2_7TeV_cff.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter",   
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    crossSection  = cms.untracked.double(0.000208),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring(
+            'MSEL=0 ',
+            'MSUB(114)=1       ',
+            'CKIN(3)=250.          ! minimum pt hat for hard interactions', 
+            'CKIN(4)=-1.          ! maximum pt hat for hard interactions'),
+
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_ggbox_diPhotonPt250_TuneZ2_7TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6 gg to diphotons box with Pt250 at sqrt(s) = 7TeV')
+)
+ProductionFilterSequence = cms.Sequence(generator)
+
+
