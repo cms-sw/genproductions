7ac889f0278b2e09ecf91b183cb374dc6250f3dc
Guillelmo Gomez Ceballos Retuerto <sha1-45f56802e0a674235d88fd4afb21f0d04767a408@cern.ch>
2010-12-21 08:05:24 +0000

    new 8TeV energy

---
+++ python/PYTHIA6_EWK_Zll_2lfilter_8TeV_cff.py
@@ -0,0 +1,53 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(0.1087),
+    crossSection = cms.untracked.double(16082.2),
+    comEnergy = cms.double(8000.0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0            !User defined processes', 
+                                        'MSUB(1)=1         !Incl Z0/gamma* production', 
+                                        'MSTP(43)=3        !Both Z0 and gamma*', 
+                                        'MDME(174,1)=0     !Z decay into d dbar', 
+                                        'MDME(175,1)=0     !Z decay into u ubar', 
+                                        'MDME(176,1)=0     !Z decay into s sbar', 
+                                        'MDME(177,1)=0     !Z decay into c cbar', 
+                                        'MDME(178,1)=0     !Z decay into b bbar', 
+                                        'MDME(179,1)=0     !Z decay into t tbar', 
+                                        'MDME(182,1)=1     !Z decay into e- e+', 
+                                        'MDME(183,1)=0     !Z decay into nu_e nu_ebar', 
+                                        'MDME(184,1)=1     !Z decay into mu- mu+', 
+                                        'MDME(185,1)=0     !Z decay into nu_mu nu_mubar', 
+                                        'MDME(186,1)=1     !Z decay into tau- tau+', 
+                                        'MDME(187,1)=0     !Z decay into nu_tau nu_taubar', 
+                                        'CKIN(1)=10.       !Minimum sqrt(s_hat) value (=Z mass)'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.4 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_EWK_Zll_2lfilter_7TeV_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6 Z/gamma* to ll, M(l+l-) > 10 GeV at sqrt(s) = 7TeV, 2lepton filter')
+)
+
+leplepgenfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MinPt = cms.untracked.vdouble(7.0, 7.0),
+    MinEta = cms.untracked.vdouble(-2.6, -2.6),
+    MaxEta = cms.untracked.vdouble(2.6, 2.6),
+    ParticleCharge = cms.untracked.int32(0),
+    ParticleID1 = cms.untracked.vint32(11,13),
+    ParticleID2 = cms.untracked.vint32(11,13)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*leplepgenfilter)
