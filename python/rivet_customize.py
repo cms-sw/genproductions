4590fe6ec1482458c3ddb8853de5bfa8570b4cf9
Piergiulio Lenzi <sha1-50f2e876925e1e2ac1aca9d0f1bdf5c962ec9f65@cern.ch>
2011-01-31 17:10:52 +0000

    rivet analyzer customize function

--- python/rivet_customize.py
+++ python/rivet_customize.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 def customise(process):
         process.load('GeneratorInterface.RivetInterface.rivetAnalyzer_cfi')
-        process.rivetAnalyzer.AnalysisNames = cms.vstring('ATLAS_2010_S8591806', 'CMS_2010_S8547297', 'CMS_2010_S8656010', 'CMS_QCD_10_002')
+        process.rivetAnalyzer.AnalysisNames = cms.vstring('ATLAS_2010_S8591806', 'CMS_QCD_10_002', 'CMS_QCD_10_006')
         process.generation_step+=process.rivetAnalyzer
         process.schedule.remove(process.RAWSIMoutput_step)
         return(process)
