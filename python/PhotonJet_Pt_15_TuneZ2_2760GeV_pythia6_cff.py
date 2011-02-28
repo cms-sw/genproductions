becf89b7da39f9c0dd353fa5c63908519f54300c
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2011-02-28 05:19:02 +0000

    add 2.76 TeV Photon Jet sequences

---
+++ python/PhotonJet_Pt_15_TuneZ2_2760GeV_pythia6_cff.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.GenProduction.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    comEnergy = cms.double(2760.0),
+    crossSection = cms.untracked.double(5.900e-05),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring(
+            'MSEL=10',
+            'CKIN(3)=15  ! minimum pt hat for hard interactions'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    name = cms.untracked.string
+('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/Attic/PhotonJet_Pt_15_TuneZ2_2760GeV_pythia6_cff.py,v $'),
+    annotation = cms.untracked.string('PhotonJetpt-15 at 2.36TeV')
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
