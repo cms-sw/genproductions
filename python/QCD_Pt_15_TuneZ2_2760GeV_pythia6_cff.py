066b74fe38d2737a423ed707f8125fbfb547200f
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2011-03-01 15:28:49 +0000

    Fixed xsec numbers

--- python/QCD_Pt_15_TuneZ2_2760GeV_pythia6_cff.py
+++ python/QCD_Pt_15_TuneZ2_2760GeV_pythia6_cff.py
@@ -4,7 +4,7 @@ from Configuration.Generator.PythiaUEZ2Settings_cfi import *
 
 generator = cms.EDFilter('Pythia6GeneratorFilter',
 	comEnergy = cms.double(2760.0),
-	crossSection = cms.untracked.double(1.087214e-02),
+	crossSection = cms.untracked.double(2.035786e+08),
 	filterEfficiency = cms.untracked.double(1),
 	maxEventsToPrint = cms.untracked.int32(0),
 	pythiaHepMCVerbosity = cms.untracked.bool(False),
@@ -14,7 +14,7 @@ generator = cms.EDFilter('Pythia6GeneratorFilter',
 		pythiaUESettingsBlock,
 		processParameters = cms.vstring(
 			'MSEL = 1        ! QCD hight pT processes',
-			'CKIN(3) = 15  ! minimum pt hat for hard interactions'
+			'CKIN(3) = 15    ! minimum pt hat for hard interactions',
 		),
 		parameterSets = cms.vstring(
 			'pythiaUESettings',
@@ -24,9 +24,7 @@ generator = cms.EDFilter('Pythia6GeneratorFilter',
 )
 
 configurationMetadata = cms.untracked.PSet(
-	version = cms.untracked.string('\$Revision: 1.2 $'),
-	name = cms.untracked.string('\$Source: /cvs/CMSSW/CMSSW/Configuration/GenProduction/python/QCD_Pt_15_TuneZ2_2760GeV_pythia6_cff.py,v $'),
-	annotation = cms.untracked.string('Sprint2011 sample with PYTHIA6: QCD dijet production, pThat > 15 GeV, TuneZ2')
+	version = cms.untracked.string('\$Revision$'),
+	name = cms.untracked.string('\$Source$'),
+	annotation = cms.untracked.string('Spring11 sample with PYTHIA6: QCD dijet production, pThat > 15 GeV, TuneZ2')
 )
-
-ProductionFilterSequence = cms.Sequence(generator)
