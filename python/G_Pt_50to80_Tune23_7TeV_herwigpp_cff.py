d2dc61a1261fb8df72db87cf8a91a125b721be63
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2011-03-05 23:25:11 +0000

    Summer 2011 config files

---
+++ python/G_Pt_50to80_Tune23_7TeV_herwigpp_cff.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.HerwigppDefaults_cfi import *
+
+generator = cms.EDFilter("ThePEGGeneratorFilter",
+	herwigDefaultsBlock,
+	crossSection = cms.untracked.double(3.322233e+03),
+	filterEfficiency = cms.untracked.double(1),
+
+	configFiles = cms.vstring(),
+	parameterSets = cms.vstring(
+		'cm7TeV',
+		'pdfMRST2001',
+		'productionParameters',
+		'basicSetup',
+		'setParticlesStableForDetector',
+	),
+	productionParameters = cms.vstring(
+		'cd /Herwig/MatrixElements/',
+		'insert SimpleQCD:MatrixElements[0] MEGammaJet',
+
+		'cd /',
+		'set /Herwig/Cuts/JetKtCut:MinKT 50  *GeV',
+		'set /Herwig/Cuts/JetKtCut:MaxKT 80  *GeV',
+		'set /Herwig/Cuts/QCDCuts:MHatMin 0.0*GeV',
+		'set /Herwig/Cuts/PhotonKtCut:MinKT 0.0*GeV',
+		'set /Herwig/Cuts/PhotonKtCut:MinEta -5',
+		'set /Herwig/Cuts/PhotonKtCut:MaxEta 5',
+	),
+)
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('\$Revision$'),
+	name = cms.untracked.string('\$Source$'),
+	annotation = cms.untracked.string('Summer2011 sample with HERWIGPP: Prompt photon production, pThat = 50 .. 80 GeV, Tune23')
+)
---
+++ python/QCD_Pt_15to3000_Tune1_Flat_7TeV_pythia8_cff.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+	comEnergy = cms.double(7000.0),
+	crossSection = cms.untracked.double(2.162410e+10),
+	filterEfficiency = cms.untracked.double(1),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+	useUserHook = cms.bool(True),
+
+	PythiaParameters = cms.PSet(
+		processParameters = cms.vstring(
+			'Main:timesAllowErrors    = 10000',
+			'ParticleDecays:limitTau0 = on',
+			'ParticleDecays:tauMax = 10',
+			'HardQCD:all = on',
+			'PhaseSpace:pTHatMin = 15',
+			'PhaseSpace:pTHatMax = 3000',
+			'Tune:pp 2',
+			'Tune:ee 3',
+
+		),
+		parameterSets = cms.vstring('processParameters')
+	)
+)
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('\$Revision$'),
+	name = cms.untracked.string('\$Source$'),
+	annotation = cms.untracked.string('Summer2011 sample with PYTHIA8: QCD dijet production, pThat = 15 .. 3000 GeV, weighted, Tune1')
+)
