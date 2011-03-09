594f727191fdd44ca7ee64e3a45f99a25996c592
mucib <sha1-35ba7bd263ec6d22dda87866d459d201992d1820@cern.ch>
2011-03-09 15:37:40 +0000

    DY to ll M>20 Tune4C 7TeV pythia8 for Summer11 production

---
+++ python/DYToMuMu_M_20_Tune4C_7TeV_pythia8_cff.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+   crossSection = cms.untracked.double(1300),
+   maxEventsToPrint = cms.untracked.int32(0),
+   pythiaPylistVerbosity = cms.untracked.int32(1),
+   filterEfficiency = cms.untracked.double(1.0),
+   pythiaHepMCVerbosity = cms.untracked.bool(False),
+   comEnergy = cms.double(7000.0),
+   PythiaParameters = cms.PSet(
+       processParameters = cms.vstring(
+
+           'Main:timesAllowErrors = 10000',
+           'WeakSingleBoson:ffbar2gmZ = on', # turn on Z production
+           'ParticleDecays:limitTau0 = on',
+           'ParticleDecays:tau0Max = 10.',
+
+           'PartonLevel:MI = on',
+           'SecondHard:generate = off',
+           'SecondHard:TwoJets = off',
+           'PhaseSpace:sameForSecond = off',
+
+           'PhaseSpace:mHatMin = 20',
+           '23:onMode = off',  #turn off all Z decays
+           '23:onIfAny = 13',   # turn ON Z->mumu
+
+           'Tune:pp 5'
+       ),
+       parameterSets = cms.vstring('processParameters')
+   )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs/CMSSW/CMSSW/Configuration/GenProduction/python/DYToMuMu_M_20_Tune4C_7TeV_pythia8_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA8 Z/gamma* to mumu, M(mu+mu-) > 20 GeV at sqrt(s) = 7TeV, Tune 4c')
+)
