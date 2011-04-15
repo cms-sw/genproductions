e68ec54f2a637a4dd523164df99f326a3702b451
Chuanzhe Lin <sha1-6e45990c2294df64408e5aadd05b5764802a1c59@cern.ch>
2011-04-15 10:22:43 +0000

    *** empty log message ***

---
+++ python/GJet_PT_300toInf_TuneZ2_cJet_7TeV_pythia6.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter('Pythia6GeneratorFilter',
+	comEnergy = cms.double(7000.0),
+	crossSection = cms.untracked.double(3.321052e-01),
+	filterEfficiency = cms.untracked.double(1),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
+	PythiaParameters = cms.PSet(
+		pythiaUESettingsBlock,
+		processParameters = cms.vstring(
+			'MSEL = 10        ! prompt photon processes',
+			'CKIN(3) = 300     ! minimum pt hat for hard interactions',
+#			'CKIN(4) = 1400  ! maximum pt hat for hard interactions',
+		),
+		parameterSets = cms.vstring(
+			'pythiaUESettings',
+			'processParameters',
+		)
+	)
+)
+
+heavyflavorjetfilter = cms.EDFilter("MCSingleParticleFilter",
+    ParticleID = cms.untracked.vint32(4, -4)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*heavyflavorjetfilter)
+
+
