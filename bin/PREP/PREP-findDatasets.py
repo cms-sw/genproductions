16892b48221a7410f84a43a07482b6b1489dd914
Piergiulio Lenzi <sha1-50f2e876925e1e2ac1aca9d0f1bdf5c962ec9f65@cern.ch>
2011-04-20 08:35:26 +0000

    factorized submission script, and add preliminary version of script to do DAS query

---
+++ bin/PREP/PREP-findDatasets.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python
+
+from PREPtools import *
+
+from optparse import OptionParser, Option
+
+import urllib, random, json
+
+
+#
+# main
+#
+if __name__ == '__main__':
+
+  usage = 'usage: %prog [options]\n'
+  parser = OptionParser(usage=usage,option_class=MyOption)
+  parser.add_option("-p", "--prepids", action="extend", help="comma separated list of PREP-IDs, REQUIRED", default=None)
+  parser.add_option('-t', '--data-tier', action="store", help='which data tier, REQUIRED', default=None)
+  parser.add_option('-d', "--dbfilename", action="store", help="dbfilename", default='ID-DatasetName_Mapping.txt' )
+  parser.add_option("-f", "--force", action="store_true", help="overwrite old results (default=False)", default=False)
+  parser.add_option('--www-cli', action="store", help='location of the DAS cli, (default https://cmsweb.cern.ch/das/cli)', default='https://cmsweb.cern.ch/das/cli')
+
+
+  (options, args) = parser.parse_args()
+
+  if (options.prepids is None):
+    print "\n[Config error] option -p or --prepids is required\n"
+    parser.print_help()
+    sys.exit(1)
+
+  if options.data_tier is None:
+    print "\n[Config error] option -t or --data-tier is required\n"
+    parser.print_help()
+    sys.exit(1)
+
+  if (len(options.prepids) == 0):
+     parser.print_help()
+     sys.exit(1)
+
+
+  # create logger
+  logger = logging.getLogger("logger")
+  logger.setLevel(logging.DEBUG)
+  # create console handler and set level to info
+  ch = logging.StreamHandler()
+  ch.setLevel(logging.INFO)
+  # create console handler and set level to debug
+  logfilename='datasets.log'
+  fh = logging.FileHandler(logfilename)
+  fh.setLevel(logging.DEBUG)
+  # create formatter
+  formatter = logging.Formatter("%(levelname)s - %(asctime)s - %(message)s")
+  # add formatter to ch and fh
+  ch.setFormatter(formatter)
+  fh.setFormatter(formatter)
+  # add ch and fh to logger
+  logger.addHandler(ch)
+  logger.addHandler(fh)
+
+  #get the latest DAS cli
+  try:
+    urllib.urlretrieve(options.www_cli, filename='DAScli.py')
+    from DAScli import get_data, get_value 
+  except Exception, e:
+    logger.error("error downloading %s: %s" % (options.www_cli, e))
+    sys.exit(1)
+
+
+  key = 0 
+  host = 'https://cmsweb.cern.ch'
+  for requestId in options.prepids:
+    reqInfo = requestInfo(requestId, str(key))
+    query='dataset=/'
+    query+=reqInfo.dataset+'/'
+    query+=reqInfo.campaign+'-' 
+    query+=reqInfo.gt+'-v*/'
+    query+=options.data_tier
+    logger.info('Executing query: '+query)
+    data = get_data(host, query, 1, 999999, False)
+    jsondict = json.loads(data)
+    logger.debug(json.dumps(jsondict, sort_keys=True, indent=4))
+    mongo_query = jsondict['mongo_query']
+    rows = []
+    if  mongo_query.has_key('fields'):
+      filters = mongo_query['fields']
+      data = jsondict['data']
+      if isinstance(data, list):
+        for row in data:
+          rows = [r for r in get_value(row, filters)]
+  
+    matches = []
+    for row in rows:
+      rowsplit = row.split(',')  
+      datasetname = rowsplit[3].lstrip('u\'name\': u\'').rstrip('\',')
+      matches.append(datasetname)
+
+    if len(matches) > 0:
+      logger.info('matches found for '+requestId+': '+' '.join(matches)) 
+    else: 
+      logger.info('no dataset found for id '+requestId)
+      continue
+  
+    #if more than one match look for the latest version
+    largestversion = 0 
+    choice = 0
+    for matchindex in range(len(matches)):
+      #take the second part of the dataset name
+      datasetpart2 = matches[matchindex].split('/')[2]
+      version = int(datasetpart2.split('-')[2].lstrip('v')) 
+      if version > largestversion:
+        choice = matchindex
+
+    logger.info(requestId+' '+matches[choice])     
+
+    key += 1
+
