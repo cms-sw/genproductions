7c835cb4b7189b8b2b2cd9ba2023092b60669159
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2011-07-12 16:35:15 +0000

    EXO Summer11

---
+++ python/ADDmonoJet_MD_5_d_2_7TeV_Tune4C_pythia8_cfi.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                                        maxEventsToPrint = cms.untracked.int32(1),
+                                        pythiaPylistVerbosity = cms.untracked.int32(1),
+                                        filterEfficiency = cms.untracked.double(1.0),
+                                        pythiaHepMCVerbosity = cms.untracked.bool(False),
+                                        comEnergy = cms.double(7000.),
+                                        PythiaParameters = cms.PSet(
+                                        pythia8_monojet = cms.vstring( ## see details on http://home.thep.lu.se/~torbjorn/php8135/ExtraDimensionalProcesses.php?filepath=files/
+'Tune:pp 5',
+'PDF:pSet = 5',
+                                               'ExtraDimensionsLED:monojet = on',
+                                               'ExtraDimensionsLED:CutOffmode = 1',
+					       'ExtraDimensionsLED:t = 0.5',
+                                               'ExtraDimensionsLED:n = 2',
+                                               'ExtraDimensionsLED:MD = 5000.',
+                                               '5000039:m0 = 1200.',
+                                               '5000039:mWidth = 1000.',
+                                               '5000039:mMin = 1.',
+                                               '5000039:mMax = 13990.',
+                                               'PhaseSpace:pTHatMin = 80.',
+                                               'PartonLevel:MI = on',
+                                               'PartonLevel:ISR = on',
+                                               'PartonLevel:FSR = on'					       
+                                        ), 
+                                        parameterSets = cms.vstring('pythia8_monojet')
+                                 ) 
+                         )
+
+
+
+        
