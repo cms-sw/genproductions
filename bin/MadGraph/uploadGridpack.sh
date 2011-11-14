251947112899f622cf4c83e8029f002af2c94e5a
Piergiulio Lenzi <sha1-50f2e876925e1e2ac1aca9d0f1bdf5c962ec9f65@cern.ch>
2011-11-14 16:43:51 +0000

    script to upload gridpaks with versioning system

---
+++ bin/MadGraph/uploadGridpack.sh
@@ -0,0 +1,85 @@
+#! /bin/bash
+
+rootdir=/afs/cern.ch/user/l/lenzip/scratch0/madgraph/repo
+
+function preparepath() {
+  local PROCESS=${rootdir}/${1}
+  local lastv=-1
+  local newv=-1
+  if [ -d $PROCESS ]; then
+    local listversions=`ls $PROCESS | grep "^v[0-9]\+"`
+    if [[ ${listversions} != "" ]]; then
+      lastv=`echo ${listversions}|tr " " "\n" | sed -e "s#v\([0-9]\+\)#\1#g" | sort -n | tail -n1`
+    fi
+  fi  
+  if [[ ${lastv} == -1 ]]; then 
+    newv=1
+  else 
+    newv=$(($lastv+1))
+  fi
+
+  echo ${PROCESS}/v${newv}
+
+}
+
+# parse command line arguments and options
+NAME=$(basename $0)
+OPTS=$(getopt -n "$NAME" -o "np:" -l "dryrun,process:" -- "$@")
+
+# check for invalid options
+if [ $? != 0 ]; then 
+  exit 1
+fi
+
+# reload the parsed options into the environment
+eval set -- "$OPTS"
+
+# parse options
+PROCESS=""
+DRYRUN=""
+while true; do
+  case "$1" in 
+    "-p" | "--process" )
+      PROCESS="$2"
+      shift 2
+      ;;
+    "-n" | "--dryrun" )
+      DRYRUN="1"
+      shift
+      ;;
+    "--" )
+      # inserted by getopt to singal the end of options
+      shift
+      break
+      ;;
+  esac
+done
+
+if [ ! "$PROCESS" ]; then
+  echo "$NAME: error: the --process option is required"
+  exit 1
+fi
+
+if [ ! "$@" ]; then
+  echo "$NAME: error: no files provided for upload"
+  exit 1
+fi  
+
+for file in "$@"; do
+  if [ ! -f ${file} ]; then
+    echo "$NAME: error: file $file does not exist"
+    exit 1
+  fi
+done
+
+
+path="$(preparepath "$PROCESS")"
+echo "new path ${path}"
+
+if [ ! "$DRYRUN" ]; then
+  mkdir -p ${path} 
+  for file in "$@"; do
+    cp ${file} ${path}  
+  done
+fi  
+exit 0
