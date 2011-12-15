db137056e08cd409b933bd99992971f3a3cde924
Vitaliano Ciulli <sha1-f399038891d8c8f386a0797887c942b0a7f8000a@cern.ch>
2011-12-15 23:42:56 +0000

    *** empty log message ***

---
+++ python/WJetsToLNu_8TeV_madgraph_cfg.py
@@ -0,0 +1,43 @@
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ScriptExample")
+
+process.load("IOMC.RandomEngine.IOMC_cff")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.EventContent.EventContent_cff')
+process.load("GeneratorInterface.LHEInterface.ExternalLHEAsciiDumper_cfi")
+#the following two parameters need to be changed on a job by job basis
+process.externalLHEAsciiDumper.lheFileName = cms.string('output.lhe') 
+process.RandomNumberGeneratorService.externalLHEProducer.initialSeed = 1111111
+
+process.source = cms.Source("EmptySource",
+    firstLuminosityBlock = cms.untracked.uint32(23456),
+    numberEventsInLuminosityBlock = cms.untracked.uint32(10)
+)                            
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50000) )
+
+process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
+    scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
+    outputFile = cms.string("events_final.lhe"),
+    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/WJetsToLNu_8TeV-madgraph','false','true','wjets','5','20','true','0','4'),
+    nEvents = cms.uint32(process.maxEvents.input.value())
+)
+
+
+process.out = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.LHEEventContent.outputCommands,
+    fileName = cms.untracked.string('myOutputFile.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('LHE')
+    )
+)
+
+process.p = cms.Path(process.externalLHEProducer)
+
+process.e = cms.EndPath(process.externalLHEAsciiDumper)
+#+process.out)
