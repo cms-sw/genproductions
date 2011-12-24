bed5579aa0a4f7cbe07b5996d2d3665b41ccdc5f
Sanjay Padhi <sha1-021a4c85a7c4bf0b49df6e15b28b55f4b491bbf8@cern.ch>
2011-12-24 13:14:08 +0000

    fixed typo in the config

--- python/DYJetsToLL_M_50_8TeV_madgraph_cfg.py
+++ python/DYJetsToLL_M_50_8TeV_madgraph_cfg.py
@@ -21,8 +21,8 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50000) )
 process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("events_final.lhe"),
-    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DYJetsToLL_M-50_7TeV-madgraph/v2',
-    'DYJetsToLL_M-50_7TeV-madgraph','false','true','zjets','5','20','true','0','4'),
+    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DYJetsToLL_M-50_8TeV-madgraph/v2',
+    'DYJetsToLL_M-50_8TeV-madgraph','false','true','zjets','5','20','true','0','4'),
     nEvents = cms.uint32(process.maxEvents.input.value())
 )
 
