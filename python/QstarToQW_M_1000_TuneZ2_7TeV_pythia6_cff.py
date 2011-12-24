1600378ad9f9316e2086ab11204890df5d31d180
Alexander Belyaev <sha1-45889d0b422ced4b08fb223eae59c585c331ccec@cern.ch>
2011-12-24 01:26:25 +0000

    *** empty log message ***

--- python/QstarToQW_M_1000_TuneZ2_7TeV_pythia6_cff.py
+++ python/QstarToQW_M_1000_TuneZ2_7TeV_pythia6_cff.py
@@ -35,7 +35,7 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
 )
 
 configurationMetadata = cms.untracked.PSet(
-	version = cms.untracked.string('\$Revision: 1.1 $'),
-	name = cms.untracked.string('\$Source: /cvs/CMSSW/CMSSW/Configuration/GenProduction/python/Attic/QstarToQW_M_1000_TuneZ2_7TeV_pythia6_cff.py,v $'),
+	version = cms.untracked.string('\$Revision: 1.2 $'),
+	name = cms.untracked.string('\$Source: /cvs/CMSSW/UserCode/hinzmann/production/QstarToQW_M_1000_TuneZ2_7TeV_pythia6_cff.py,v $'),
 	annotation = cms.untracked.string('Fall2011 sample with PYTHIA6: Qstar -> qW, TuneZ2')
 )
