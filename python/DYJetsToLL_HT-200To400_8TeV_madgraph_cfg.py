96ce2556088c690b3d96e4ee35c9a9511b43ae31
Sanjay Padhi <sha1-021a4c85a7c4bf0b49df6e15b28b55f4b491bbf8@cern.ch>
2011-12-24 10:38:15 +0000

    8TeV madgraph v2

--- python/DYJetsToLL_HT-200To400_8TeV_madgraph_cfg.py
+++ python/DYJetsToLL_HT-200To400_8TeV_madgraph_cfg.py
@@ -33,7 +33,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(30000) )
 process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("events_final.lhe"),
-    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DYJetsToLL_HT-200To400_8TeV-madgraph/v1', 
+    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DYJetsToLL_HT-200To400_8TeV-madgraph/v2', 
       'DYJetsToLL_HT-200To400_8TeV-madgraph', 'false','true','zjets','5','20','true','0','4'),
     nEvents = cms.uint32(process.maxEvents.input.value())
 )
--- python/QCD_HT_100To250_8TeV_madgraph_cfg.py
+++ python/QCD_HT_100To250_8TeV_madgraph_cfg.py
@@ -33,7 +33,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100000) )
 process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("events_final.lhe"),
-    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.27/8TeV_Summer12/QCD_HT-100To250_8TeV-madgraph/v1',
+    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/QCD_HT-100To250_8TeV-madgraph/v2',
     'QCD_HT-100To250_8TeV-madgraph','false','false','qcd','5','21','true','2','4'),
     nEvents = cms.uint32(process.maxEvents.input.value())
 )
