de22a7b6a18efeec3e087d6d1b2f44f49576a7f0
Sanjay Padhi <sha1-021a4c85a7c4bf0b49df6e15b28b55f4b491bbf8@cern.ch>
2011-12-24 10:30:03 +0000

    v2 added

--- python/DY3JetsToLL_M_50_8TeV_madgraph_cfg.py
+++ python/DY3JetsToLL_M_50_8TeV_madgraph_cfg.py
@@ -33,7 +33,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
 process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("events_final.lhe"),
-    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DY3JetsToLL_M-50_8TeV-madgraph/v1',
+    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DY3JetsToLL_M-50_8TeV-madgraph/v2',
     'DY3JetsToLL_M-50_8TeV-madgraph','false','true','zjets','5','20','true','3','99'),
     nEvents = cms.uint32(process.maxEvents.input.value())
 )
