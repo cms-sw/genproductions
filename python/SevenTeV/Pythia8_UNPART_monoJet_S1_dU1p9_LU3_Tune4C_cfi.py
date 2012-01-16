1202edfe55dd449192722dcb9945ccd152a4900d
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2012-01-16 15:35:14 +0000

    EXO

---
+++ python/SevenTeV/Pythia8_UNPART_monoJet_S1_dU1p9_LU3_Tune4C_cfi.py
@@ -0,0 +1,31 @@
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythia8_unparticle = cms.vstring(
+	'Tune:pp 5',
+	'PDF:pSet = 5',
+	'ExtraDimensionsUnpart:monojet = on', 
+	'ExtraDimensionsUnpart:gg2Ug = off', 
+	'ExtraDimensionsUnpart:qg2Uq = on', 
+	'ExtraDimensionsUnpart:qqbar2Ug = on', 
+	'ExtraDimensionsUnpart:spinU = 1', 
+	'ExtraDimensionsUnpart:dU = 1.9', 
+	'ExtraDimensionsUnpart:LambdaU = 3000.', 
+	'ExtraDimensionsUnpart:lambda = 1.0', 
+	'ExtraDimensionsUnpart:CutOffmode = 1', 
+	'5000039:m0 = 300.', 
+	'5000039:mWidth = 300.', 
+	'5000039:mMin = 1.', 
+	'5000039:mMax = 13990.', 
+	'PhaseSpace:pTHatMin = 50.', 
+	'PartonLevel:MI = on', 
+	'PartonLevel:ISR = on', 
+	'PartonLevel:FSR = on'
+	),
+        parameterSets = cms.vstring('pythia8_unparticle')
+    )
+)
