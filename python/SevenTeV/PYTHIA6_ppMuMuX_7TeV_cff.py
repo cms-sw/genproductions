07d8e86643ab661616008890edb62f98a27e2fe7
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2012-01-17 16:32:56 +0000

    *** empty log message ***

--- python/SevenTeV/PYTHIA6_ppMuMuX_7TeV_cff.py
+++ python/SevenTeV/PYTHIA6_ppMuMuX_7TeV_cff.py
@@ -2,8 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 configurationMetadata = cms.untracked.PSet(
         version = cms.untracked.string('$Revision: 1.5 $'),
-        name = cms.untracked.string('$Source: /afs/cern.ch/project/cvs/reps/CMSSW/CMSSW/Configuration/GenProduction/python/Attic/PYTHIA6_ppMuMuX_7TeV_cff.py,v $'),
-        annotation = cms.untracked.string('Summer09: Pythia6-MinBias at 10TeV with Muon preselection (Loose cut), +decay-inflight, 10TeV, D6T tune')
+        name = cms.untracked.string('$Source: /cvs/CMSSW/CMSSW/Configuration/GenProduction/python/Attic/PYTHIA6_ppMuMuX_7TeV_c
+ff.py,v $'),
+        annotation = cms.untracked.string('Summer09: Pythia6-MinBias at 10TeV with Muon preselection (Loose cut), +decay-infli
+ght, 10TeV, D6T tune')
 )
 
 from Configuration.Generator.PythiaUEZ2Settings_cfi import *
@@ -12,8 +14,6 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
     pythiaPylistVerbosity = cms.untracked.int32(0),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     comEnergy = cms.double(7000.0),
-    crossSection = cms.untracked.double(48440000000.0),
-    filterEfficiency = cms.untracked.double(0.00084),
     maxEventsToPrint = cms.untracked.int32(0),
     PythiaParameters = cms.PSet(
         pythiaUESettingsBlock,
@@ -32,7 +32,7 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
 
 mumugenfilter = cms.EDFilter("MCParticlePairFilter",
     Status = cms.untracked.vint32(1, 1),
-    MinP = cms.untracked.vdouble(2.5, 2.5),
+    MinPt = cms.untracked.vdouble(5, 5),
     MaxEta = cms.untracked.vdouble(2.5, 2.5),
     MinEta = cms.untracked.vdouble(-2.5, -2.5),
     ParticleCharge = cms.untracked.int32(1),
