b3a284934791a4a82c73c6e97a2cc752eafd195c
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2012-01-31 15:13:17 +0000

    config files for particle guns

---
+++ python/EightTeV/P_E_1to100_gun_cff.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDProducer('FlatRandomEGunProducer',
+	PGunParameters = cms.PSet(
+		PartID = cms.vint32(2212),
+		MinPhi = cms.double(-3.14159265359), ## in radians
+		MaxPhi = cms.double(3.14159265359),
+		MinEta = cms.double(-2.5),
+		MaxEta = cms.double(2.5),
+		MinE = cms.double(1   ),
+		MaxE = cms.double(100 ),
+	),
+	Verbosity = cms.untracked.int32(0),
+	psethack = cms.string('single p E 1   -100 '),
+	AddAntiParticle = cms.bool(False),
+)
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('\$Revision$'),
+	name = cms.untracked.string('\$Source$'),
+	annotation = cms.untracked.string('Summer2012 sample with GUN: Flat random proton gun, E = 1 .. 100 GeV, no tune')
+)
