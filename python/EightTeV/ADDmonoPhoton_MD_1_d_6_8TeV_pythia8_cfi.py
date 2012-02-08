8a8511a36fb1327b24d31c6b79f596f75fde8a25
Alexander Belyaev <sha1-45889d0b422ced4b08fb223eae59c585c331ccec@cern.ch>
2012-02-08 14:37:52 +0000

    Summer12 EXO 8 TeV

---
+++ python/EightTeV/ADDmonoPhoton_MD_1_d_6_8TeV_pythia8_cfi.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(8000.),
+                         crossSection = cms.untracked.double(3.32e-1),
+                         PythiaParameters = cms.PSet(
+    pythia8_monophoton = cms.vstring( ## see details on http://home.thep.lu.se/~torbjorn/php8135/ExtraDimensionalProcesses.php?filepath=files/
+    'Tune:pp 5',
+    'Tune:ee 3',
+    'ExtraDimensionsLED:ffbar2Ggamma = on',
+    'ExtraDimensionsLED:CutOffmode = 1',
+    'ExtraDimensionsLED:t = 0.5',
+    'ExtraDimensionsLED:n = 6',
+    'ExtraDimensionsLED:MD = 1000.',
+    'ExtraDimensionsLED:LambdaT = 1000.',
+    '5000039:m0 = 1200.',
+    '5000039:mWidth = 1000.',
+    '5000039:mMin = 1.',
+    '5000039:mMax = 13990.',
+    'PhaseSpace:pTHatMin = 125.',
+    'PartonLevel:MI = on',
+    'PartonLevel:ISR = on',
+    'PartonLevel:FSR = on'					       
+    ), 
+    parameterSets = cms.vstring('pythia8_monophoton')
+    ) 
+                         )
+
+
+
+        
