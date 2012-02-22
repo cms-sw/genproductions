8e210f4eb1cb5062f0f1ebac27b3d9a1642fdbf4
Thiago Tomei Fernandez <sha1-796b2659f539c78d0b46c6e2488664232960deff@cern.ch>
2012-02-22 13:55:39 +0000

    Adding WPrime files.

---
+++ python/EightTeV/WprimeToWZToLLLNu_M_500_TuneZ2star_8TeV_pythia6_tauola_cff.py
@@ -0,0 +1,78 @@
+## Wprime to WZ, Z2star tune, Summer12 production
+
+
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
+from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.),
+    comEnergy = cms.double(8000.0),
+    crossSection = cms.untracked.double(0.1013),
+    ExternalDecays = cms.PSet(
+        Tauola = cms.untracked.PSet(
+            TauolaPolar,
+            TauolaDefaultInputCards
+        ),
+        parameterSets = cms.vstring('Tauola')
+    ),
+    UseExternalGenerators = cms.untracked.bool(True),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0', 
+                                        "MSUB(142)=1 !qq->Wprime", 
+                                        'PMAS(34,1)=500.0  !Wprime Mass', 
+                                        'MDME(311,1)=    0   !Wprime to d          ubar', 
+                                        'MDME(312,1)=    0   !Wprime to d          cbar', 
+                                        'MDME(313,1)=    0   !Wprime to d          tbar', 
+                                        'MDME(315,1)=    0   !Wprime to s          ubar', 
+                                        'MDME(316,1)=    0   !Wprime to s          cbar', 
+                                        'MDME(317,1)=    0   !Wprime to s          tbar', 
+                                        'MDME(319,1)=    0   !Wprime to b          ubar', 
+                                        'MDME(320,1)=    0   !Wprime to b          cbar', 
+                                        'MDME(321,1)=    0   !Wprime to b          tbar', 
+                                        'MDME(327,1)=    0   !Wprime to e          nu_e', 
+                                        'MDME(328,1)=    0   !Wprime to mu         nu_mu', 
+                                        'MDME(329,1)=    0   !Wprime to tau        nu_tau', 
+                                        'MDME(331,1)=    1   !Wprime to W          Z', 
+                                        'MDME(332,1)=    0   !Wprime to W          gamma', 
+                                        'MDME(333,1)=    0   !Wprime to W          h0', 
+                                        'MDME(190,1)=    0   !W to d               ubar', 
+                                        'MDME(191,1)=    0   !W to d               cbar', 
+                                        'MDME(192,1)=    0   !W to d               tbar', 
+                                        'MDME(194,1)=    0   !W to s               ubar', 
+                                        'MDME(195,1)=    0   !W to s               cbar', 
+                                        'MDME(196,1)=    0   !W to s               tbar', 
+                                        'MDME(198,1)=    0   !W to b               ubar', 
+                                        'MDME(199,1)=    0   !W to b               cbar', 
+                                        'MDME(200,1)=    0   !W to b               tbar', 
+                                        'MDME(206,1)=    1   !W to e               nu_e', 
+                                        'MDME(207,1)=    1   !W to mu              nu_mu', 
+                                        'MDME(208,1)=    1   !W to tau             nu_tau', 
+                                        'MDME(174,1)=    0   !Z to d               dbar', 
+                                        'MDME(175,1)=    0   !Z to u               ubar', 
+                                        'MDME(176,1)=    0   !Z to s               sbar', 
+                                        'MDME(177,1)=    0   !Z to c               cbar', 
+                                        'MDME(178,1)=    0   !Z to b               bbar', 
+                                        'MDME(179,1)=    0   !Z to t               tbar', 
+                                        'MDME(182,1)=    1   !Z to e-              e+', 
+                                        'MDME(183,1)=    0   !Z to nu_e            nu_ebar', 
+                                        'MDME(184,1)=    1   !Z to mu-             mu+', 
+                                        'MDME(185,1)=    0   !Z to nu_mu           nu_mubar', 
+                                        'MDME(186,1)=    1   !Z to tau-            tau+', 
+                                        'MDME(187,1)=    0   !Z to nu_tau          nu_taubar'), 
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+
+
+
+ProductionFilterSequence = cms.Sequence(generator)
