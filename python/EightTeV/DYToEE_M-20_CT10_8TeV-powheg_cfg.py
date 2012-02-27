8d55a97308674a622d07528b55ed507cbe2d06de
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2012-02-27 21:44:43 +0000

    W/Z powheg Summer12 lhe prod

---
+++ python/EightTeV/DYToEE_M-20_CT10_8TeV-powheg_cfg.py
@@ -0,0 +1,46 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("LHE")
+
+process.load("IOMC.RandomEngine.IOMC_cff")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.EventContent.EventContent_cff')
+
+process.source = cms.Source("EmptySource",
+    firstLuminosityBlock = cms.untracked.uint32(23456),
+    numberEventsInLuminosityBlock = cms.untracked.uint32(10)
+)                            
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+#############
+# LHE
+#############
+process.load("GeneratorInterface/LHEInterface/ExternalLHEProducer_cfi")
+process.externalLHEProducer.nEvents = process.maxEvents.input.value()
+#process.outputFile = cms.string("/tmp/ochando/events.lhe"),
+
+process.externalLHEProducer.scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/create_lhe_powheg.sh")
+process.externalLHEProducer.args = cms.vstring('slc5_ia32_gcc434/powheg/V1.0/src',
+                                               'powhegboxv1.1_Feb2012',
+                                               'Z',
+                                               'slc5_ia32_gcc434/powheg/V1.0/8TeV_Summer12/DYToEE_M-20_CT10_8TeV-powheg/v1/DYToEE_M-20_CT10_8TeV-powheg.input')
+
+
+#############
+# Output
+#############
+process.out = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.LHEEventContent.outputCommands,
+    fileName = cms.untracked.string('lheOutputFile.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('LHE')
+    )
+)
+
+process.p = cms.Path(process.externalLHEProducer)
+
+process.e = cms.EndPath(process.out)
