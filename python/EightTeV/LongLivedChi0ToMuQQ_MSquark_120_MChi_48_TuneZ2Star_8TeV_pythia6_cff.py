46091e496eb6e08b6ef5237709fc3ffc40402354
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2012-03-02 17:18:09 +0000

    *** empty log message ***

---
+++ python/EightTeV/LongLivedChi0ToMuQQ_MSquark_120_MChi_48_TuneZ2Star_8TeV_pythia6_cff.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
+from Configuration.GenProduction.EightTeV.LongLivedChi0_TuneZ2Star_8TeV_pythia6_cff import *
+
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia6GeneratorFilter", 
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    crossSection = cms.untracked.double(284.),
+    comEnergy = cms.double(8000.0),
+    UseExternalGenerators = cms.untracked.bool(False),
+    PythiaParameters = cms.PSet(
+            pythiaUESettingsBlock,
+            pythiaStandardRPVBlock,
+            pythiaParameters = cms.vstring(         
+            'IMSS(52)=3               ! Turn on Lepton number violating LQD decay channels with all couplings set to zero',
+            'RVLAMP(2,1,1)=0.000026   ! Set lambda Prime(2,1,1)',
+            'MDME(2241,1)=0           ! Turn off LQD decays to neutrinos',
+            'MDME(2242,1)=0           ! Turn off LQD decays to neutrinos',        
+            'RMSS(1)=50              ! M1 mass',
+            'RMSS(8)=120              ! Left squark mass',
+            'RMSS(9)=120              ! Right squark mass'
+        ),
+        parameterSets = cms.vstring('pythiaUESettings',
+            'pythiaStandardRPVSettings',
+            'pythiaParameters')
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator) 
---
+++ python/EightTeV/LongLivedChi0ToNuLL_MSquark_120_MChi_48_TuneZ2Star_8TeV_pythia6_cff.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
+from Configuration.GenProduction.EightTeV.LongLivedChi0_TuneZ2Star_8TeV_pythia6_cff import *
+
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia6GeneratorFilter", 
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    crossSection = cms.untracked.double(1092),
+    comEnergy = cms.double(8000.0),
+    UseExternalGenerators = cms.untracked.bool(False),
+    PythiaParameters = cms.PSet(
+            pythiaUESettingsBlock,
+            pythiaStandardRPVBlock,
+            pythiaParameters = cms.vstring(         
+            'IMSS(51)=3               ! Turn on Lepton number violating LLE decay channels with all couplings set to zero',
+            'RVLAM(1,2,2)=0.011    ! Set lambda (1,2,2)',
+            'RVLAM(1,2,1)=0.011    ! Set lambda (2,1,1)',
+            'RMSS(1)=50              ! M1 mass',
+            'RMSS(8)=120              ! Left squark mass',
+            'RMSS(9)=120              ! Right squark mass'
+        ),
+        parameterSets = cms.vstring('pythiaUESettings',
+            'pythiaStandardRPVSettings',
+            'pythiaParameters')
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator) 
