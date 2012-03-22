9cb32d9e05bd8dbcc0801541b1afb793a8f7ea58
Alexey Ferapontov <sha1-481a5a2ac9973b7cab990140e7a395466f50a31e@cern.ch>
2012-03-22 13:36:47 +0000

    EXO. removed import

--- python/EightTeV/LongLivedChi0ToNuLL_MSquark_120_MChi_48_TuneZ2Star_8TeV_pythia6_cff.py
+++ python/EightTeV/LongLivedChi0ToNuLL_MSquark_120_MChi_48_TuneZ2Star_8TeV_pythia6_cff.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
-from Configuration.GenProduction.EightTeV.LongLivedChi0_TuneZ2Star_8TeV_pythia6_cff import *
 
 source = cms.Source("EmptySource")
 generator = cms.EDFilter("Pythia6GeneratorFilter", 
@@ -14,8 +13,34 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
     UseExternalGenerators = cms.untracked.bool(False),
     PythiaParameters = cms.PSet(
             pythiaUESettingsBlock,
-            pythiaStandardRPVBlock,
             pythiaParameters = cms.vstring(         
+            'MSTJ(22)=1             ! Decay all unstable particles',
+            'MSTP(95)=0            ! Disable colour reconnection, since it can put colour strings between widely separated partons', 
+            'MSEL=0',
+            'MSUB(271)=1            ! Squark pair production',
+            'MSUB(272)=1',
+            'MSUB(273)=1',
+            'MSUB(274)=1',
+            'MSUB(275)=1',
+            'MSUB(276)=1',
+            'MSUB(277)=1',
+            'MSUB(278)=1',
+            'MSUB(279)=1',
+            'MSUB(280)=1',
+            'IMSS(1)=1                ! General MSSM simultaion',
+
+            'RMSS(2)=5000.                ! M2 mass',
+            'RMSS(3)=5000.                ! M3 mass',
+            'RMSS(4)=800.                 ! mu parameter',
+            'RMSS(5)=2.                   ! tan Beta',
+            'RMSS(6)=5000.                ! Left slepton mass',
+            'RMSS(7)=5000.                ! Right slepton mass',
+            'RMSS(10)=5000.               ! Left squark mass for third generation',
+            'RMSS(11)=5000.               ! Right sbottom mass',
+            'RMSS(12)=5000.               ! Right stop mass',
+            'RMSS(13)=5000.               ! Left stau mass',
+            'RMSS(14)=5000.               ! Right stau mass',
+            
             'IMSS(51)=3               ! Turn on Lepton number violating LLE decay channels with all couplings set to zero',
             'RVLAM(1,2,2)=0.011    ! Set lambda (1,2,2)',
             'RVLAM(1,2,1)=0.011    ! Set lambda (2,1,1)',
@@ -24,7 +49,6 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
             'RMSS(9)=120              ! Right squark mass'
         ),
         parameterSets = cms.vstring('pythiaUESettings',
-            'pythiaStandardRPVSettings',
             'pythiaParameters')
     )
 )
