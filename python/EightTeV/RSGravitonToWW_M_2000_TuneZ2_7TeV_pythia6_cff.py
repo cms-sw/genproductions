9b506c06cc0a52325c4b82ae0a084dd377a86ea5
Alexander Belyaev <sha1-45889d0b422ced4b08fb223eae59c585c331ccec@cern.ch>
2012-04-17 14:28:51 +0000

    Summer12 EXO 8 TeV

---
+++ python/EightTeV/RSGravitonToWW_M_2000_TuneZ2_7TeV_pythia6_cff.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+	filterEfficiency = cms.untracked.double(1),
+	comEnergy = cms.double(7000.0),
+	crossSection = cms.untracked.double(1.021e-1),
+	
+	PythiaParameters = cms.PSet(
+	        pythiaUESettingsBlock,
+                processParameters = cms.vstring(
+		        'MSEL = 0',
+		        'MSUB(391) = 1',
+		        'MSUB(392) = 1',
+		        'PMAS(347,1) = 2000',
+		        'PARP(50) = 0.54', #0.54
+		        '5000039:ALLOFF',
+		        '5000039:ONIFANY 24',
+        ),
+		parameterSets = cms.vstring(
+		        'pythiaUESettings',
+			'processParameters')
+	)
+)
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('\$Revision: 1.2 $'),
+	name = cms.untracked.string('\$Source: /cvs/CMSSW/UserCode/hinzmann/production/RSGravitonToWW_M_2000_TuneZ2_7TeV_pythia6_cff.py,v $'),
+	annotation = cms.untracked.string('Fall2011 sample with PYTHIA6: RSG -> WW, TuneZ2')
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
