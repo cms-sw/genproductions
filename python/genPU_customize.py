6b0d9e81677d328576bf1ae3e2d953e89d0808b8
Piergiulio Lenzi <sha1-50f2e876925e1e2ac1aca9d0f1bdf5c962ec9f65@cern.ch>
2012-08-01 16:34:14 +0000

    added area calculation

--- python/genPU_customize.py
+++ python/genPU_customize.py
@@ -1,5 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
+def initJetAreaSequence(process):
+  jamedian_pt_jets_value=0.
+  eta_max_ghosts=6.
+  from RecoJets.JetProducers.AnomalousCellParameters_cfi import AnomalousCellParameters
+
+  from RecoJets.JetProducers.GenJetParameters_cfi import GenJetParameters
+  GenJetParameters.doPUFastjet = cms.bool(False)
+  GenJetParameters.doAreaFastjet = cms.bool(True)
+  GenJetParameters.doRhoFastjet = cms.bool(True)
+  GenJetParameters.Active_Area_Repeats = cms.int32(1)
+  GenJetParameters.jetPtMin = cms.double(jamedian_pt_jets_value)
+  GenJetParameters.Ghost_EtaMax= cms.double(eta_max_ghosts)
+
+
+  process.kt2GenJetsWithArea = cms.EDProducer("FastjetJetProducer",
+      GenJetParameters,
+      AnomalousCellParameters,
+      jetAlgorithm = cms.string("Kt"),
+      rParam       = cms.double(0.2)
+  )
+
+  process.kt4GenJetsWithArea=process.kt2GenJetsWithArea.clone(rParam=0.4)
+  process.kt8GenJetsWithArea=process.kt2GenJetsWithArea.clone(rParam=0.8)
+  process.kt12GenJetsWithArea=process.kt2GenJetsWithArea.clone(rParam=1.2)
+
+  process.GenJetsWithAreaSeq=cms.Sequence(process.kt2GenJetsWithArea+
+                                process.kt4GenJetsWithArea+
+                                process.kt8GenJetsWithArea+
+                                process.kt12GenJetsWithArea) 
+
 def cloneWithSuffix(sequence, process, suffix):
   newSequence = cms.Sequence()
   #poor main way to iterate over a Sequence
@@ -23,11 +53,15 @@ def customiseGenPU(process):
         process.genParticlesWithPU_step = cms.Path(process.genParticlesWithPU)
         #insert the genParticles with PU in the schedule right after the mixinf has been done
         process.schedule.insert( process.schedule.index(process.digi2raw_step)+1, process.genParticlesWithPU_step )
+        #add the area calculation
+        initJetAreaSequence(process)
+        process.genJetMET = cms.Sequence(process.genJetMET + process.GenJetsWithAreaSeq)
         #clone the whole gen jet part
         newsequence = cloneWithSuffix(process.genJetMET, process, "WithPU")
         process.genJetMETWithPU = cms.Path(newsequence)
         process.schedule.insert( process.schedule.index(process.genParticlesWithPU_step)+1, process.genJetMETWithPU)
         # add the collections done with PU to teh output
         process.RAWSIMoutput.outputCommands.extend(['keep *_*WithPU*_*_*'])
+        process.RAWSIMoutput.outputCommands.extend(['keep *_*WithArea*_*_*'])
         return(process)
 
