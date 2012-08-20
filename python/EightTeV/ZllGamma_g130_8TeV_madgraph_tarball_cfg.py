3c77ed4c67308ba4c11477a86867fa98f58ffba4
Simon De Visscher <sha1-aafd21e13d4932b6488764d072a6d70a5bb7d09b@cern.ch>
2012-08-20 14:59:23 +0000

    points to V3

--- python/EightTeV/ZllGamma_g130_8TeV_madgraph_tarball_cfg.py
+++ python/EightTeV/ZllGamma_g130_8TeV_madgraph_tarball_cfg.py
@@ -32,7 +32,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(200) )
 process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_tarball.sh"),
     outputFile = cms.string("ZllG130_8TeV-madgraph_unweighted_events_final.lhe"),
-    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/ZllG130_8TeV_madgraph/v2',
+    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/ZllG130_8TeV_madgraph/v3',
     'ZllG130_8TeV_madgraph','false','true','zjets','5','20','true','0','3'),
     nEvents = cms.uint32(process.maxEvents.input.value())
 )
