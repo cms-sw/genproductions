da2aba0dd19fda950e7bb747c67695e0c2cb6a64
cmsbuild <>
2012-09-13 14:13:27 +0000

    This commit was manufactured by cvs2git to create tag 'V02-02-38'.
    Sprout from master 2012-09-13 14:13:26 UTC Tom Gustaaf M Cornelis <sha1-4873e11ea5f75708bcf7bcc5333acad6b2865652@cern.ch> 'making file self-consistent for 2010-like FSQ production'
    Cherrypick from master 2011-07-25 10:14:54 UTC Alexis Kalogeropoulos <sha1-91110b754141b4ea1416a095611be3c9b61e195f@cern.ch> 'Commented out the Number of events in the banner block':
        bin/MadGraph/mgPostProcv2.py
    Delete:
        bin/MadGraph/bsubmit_template_gridpacks_mg5v1.4.8.sh
        bin/MadGraph/create_gridpack_148.sh
        bin/MadGraph/gridpacks_mg_148.sh

--- bin/MadGraph/gridpacks_mg_148.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sed 's/template/'$1'/g' bsubmit_template_gridpacks_mg5v1.4.8.sh > bsubmit_${1}_gridpacks.sh
-echo done...you can now send the jobs
-
-#source  bsubmit_${1}_gridpacks.sh
-
-
-
