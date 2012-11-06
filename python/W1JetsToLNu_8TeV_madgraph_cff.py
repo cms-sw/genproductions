b6675425551bf54b524fe5d68e19fe1be7738f92
Piergiulio Lenzi <sha1-50f2e876925e1e2ac1aca9d0f1bdf5c962ec9f65@cern.ch>
2012-11-06 09:49:12 +0000

    fix config

--- python/W1JetsToLNu_8TeV_madgraph_cff.py
+++ python/W1JetsToLNu_8TeV_madgraph_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
-    outputFile = cms.string("events_final.lhe"),
+    outputFile = cms.string("W1JetsToLNu_8TeV-madgraph_final.lhe"),
     args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/W1JetsToLNu_8TeV-madgraph/v2','W1JetsToLNu_8TeV-madgraph','false','true','wjets','5','20','true','1','99'),
     nEvents = cms.uint32(10000)
 )
