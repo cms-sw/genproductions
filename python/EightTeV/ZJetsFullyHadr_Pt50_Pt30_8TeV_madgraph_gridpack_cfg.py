1966671c36d99ac95dec806a965f9cdfbe3d488f
Simon De Visscher <sha1-aafd21e13d4932b6488764d072a6d70a5bb7d09b@cern.ch>
2012-11-13 19:06:53 +0000

    comlient with 536

--- python/EightTeV/ZJetsFullyHadr_Pt50_Pt30_8TeV_madgraph_gridpack_cfg.py
+++ python/EightTeV/ZJetsFullyHadr_Pt50_Pt30_8TeV_madgraph_gridpack_cfg.py
@@ -32,6 +32,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100000) )
 process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("ZJetsFullyHard_8TeV-madgraph_final.lhe"),
+    numberOfParameters = cms.uint32(10),	
     args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/ZJetsFullyHard_8TeV-madgraph/v4', 'ZJetsFullyHard_8TeV-madgraph', 'false','false','zjets','5','17','false','1','3'),
     nEvents = cms.uint32(process.maxEvents.input.value())
 )
