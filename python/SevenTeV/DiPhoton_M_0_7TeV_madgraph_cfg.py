e17781f588d6f992cdc1d1ced3d8514bbfc694f3
Simon De Visscher <sha1-aafd21e13d4932b6488764d072a6d70a5bb7d09b@cern.ch>
2012-12-05 08:11:07 +0000

    diphoton

--- python/SevenTeV/DiPhoton_M_0_7TeV_madgraph_cfg.py
+++ python/SevenTeV/DiPhoton_M_0_7TeV_madgraph_cfg.py
@@ -6,5 +6,5 @@ externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     numberOfParameters = cms.uint32(10),
     outputFile = cms.string("diPhoton_M0_7TeV_madgraph_final.lhe"),
     args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.4.48/8TeV_Summer12/diPhoton_M0_7TeV_madgraph/v1','diPhoton_M0_7TeV_madgraph','false','false','zjets','5','6','false','0','2'),
-    nEvents = cms.uint32(50000)
+    nEvents = cms.uint32(100000)
 )
