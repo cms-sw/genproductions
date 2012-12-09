f0b76d20ebcd304695b63ea805e6fec9c531334c
Simon De Visscher <sha1-aafd21e13d4932b6488764d072a6d70a5bb7d09b@cern.ch>
2012-12-09 16:42:09 +0000

    new config file

--- python/EightTeV/ggToHbb_8TeV_madgraph_cfg.py
+++ python/EightTeV/ggToHbb_8TeV_madgraph_cfg.py
@@ -5,6 +5,6 @@ externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     numberOfParameters = cms.uint32(10),
     outputFile = cms.string("ggToHbb_8TeV_madgraph_final.lhe"),
-    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.4.8/8TeV_Summer12/ggToHbb_8TeV_madgraph/v2','ggToHbb_8TeV_madgraph','false','false','qcd','5','25','false','0','3'),
+    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.4.8/8TeV_Summer12/ggToHbb_8TeV_madgraph/v3','ggToHbb_8TeV_madgraph','false','false','qcd','5','25','false','0','3'),
     nEvents = cms.uint32(50000)
 )
