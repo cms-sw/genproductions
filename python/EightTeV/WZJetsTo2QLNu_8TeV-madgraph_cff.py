e18c28b1f001b8e30864aff7037bade978de8073
Christophe Ochando <sha1-c8b28d70dd1f4235246c4a027e80dcdcf397db6f@cern.ch>
2012-12-30 21:47:31 +0000

    wz fix

--- python/EightTeV/WZJetsTo2QLNu_8TeV-madgraph_cff.py
+++ python/EightTeV/WZJetsTo2QLNu_8TeV-madgraph_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
                                              scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
-                                             outputFile = cms.string("ZJetsToNuNu_PtZ-70To100_8TeV-madgraph_final.lhe"),
+                                             outputFile = cms.string("WZJetsTo2QLNu_8TeV-madgraph_final.lhe"),
                                              numberOfParameters = cms.uint32(10),
                                              args = cms.vstring('/slc5_ia32_gcc434/madgraph/V5_1.4.8/8TeV_Summer12/WZJetsTo2QLNu_8TeV-madgraph/v2/', #gridpack path
                                                                 'WZJetsTo2QLNu_8TeV-madgraph', #gridpack name, without _gridpack.tar.gz
