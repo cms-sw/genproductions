715ea705d244f34095d91e24ddc797fc92e16cd1
Xin Shi <sha1-88f7fb16b51e2081802b30c98eb0c555316609f3@cern.ch>
2013-01-08 10:58:22 +0000

    Add for BsMuMu Group

---
+++ python/EightTeV/PYTHIA6_Bd2D0starPi_EtaPtFilter_TuneZ2star_8TeV_cff.py
@@ -0,0 +1,73 @@
+import FWCore.ParameterSet.Config as cms
+
+#source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
+
+generator = cms.EDFilter(
+    "Pythia6GeneratorFilter",
+    comEnergy = cms.double(8000.0),
+    crossSection = cms.untracked.double(48440000000.),
+    filterEfficiency = cms.untracked.double(1.38e-3),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    ExternalDecays = cms.PSet(
+        EvtGen = cms.untracked.PSet(
+             operates_on_particles = cms.vint32( 0 ), # 0 (zero) means default list (hardcoded)
+                                                      # you can put here the list of particles (PDG IDs)
+                                                      # that you want decayed by EvtGen
+             use_default_decay = cms.untracked.bool(False),
+             decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
+             particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evt.pdl'),
+             user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/Bd_D0starPi_KPiPiPi.dec'),
+             list_forced_decays = cms.vstring('MyB0',
+                                              'Myanti-B0'),
+        ),
+        parameterSets = cms.vstring('EvtGen')
+    ),
+
+    
+    PythiaParameters = cms.PSet(
+    pythiaUESettingsBlock,
+         bbbarSettings = cms.vstring('MSEL = 1'), 
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring(
+             'pythiaUESettings',
+             'bbbarSettings')
+       
+    )
+    )
+
+ # -- Require Bd presence
+bfilter = cms.EDFilter(
+        "PythiaFilter",
+        MaxEta = cms.untracked.double(9999.),
+        MinEta = cms.untracked.double(-9999.),
+        ParticleID = cms.untracked.int32(511)
+        )
+
+D0filter = cms.EDFilter(
+        "PythiaDauVFilter",
+        verbose         = cms.untracked.int32(1), 
+        NumberDaughters = cms.untracked.int32(2), 
+        MotherID        = cms.untracked.int32(413),  
+        ParticleID      = cms.untracked.int32(421),  
+        DaughterIDs     = cms.untracked.vint32(-321, 211),
+        MinPt           = cms.untracked.vdouble(1.0, 1.0), 
+        MinEta          = cms.untracked.vdouble(-2.5, -2.5), 
+        MaxEta          = cms.untracked.vdouble( 2.5,  2.5)
+        )
+ 
+
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string
+    ('$Source: /local/projects/CMSSW/rep/CMSSW/Configuration/GenProduction/python/PYTHIA6_Bd2D0starPi_EtaPtFilter_TuneZ2star_8TeV_cff.py,v $'),
+    annotation = cms.untracked.string('B0 -> D0*+ pi- -> K pi pi pi at 8TeV')
+    )
+
+ProductionFilterSequence = cms.Sequence(generator*bfilter*D0filter)
+
+
