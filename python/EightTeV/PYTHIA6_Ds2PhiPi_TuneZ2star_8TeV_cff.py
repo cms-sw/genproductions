dd5d31496f03b38264a0d87c6bd0d3cc95daffd4
Xin Shi <sha1-88f7fb16b51e2081802b30c98eb0c555316609f3@cern.ch>
2013-01-08 15:27:11 +0000

    Merge update from Francesco Fiori

--- python/EightTeV/PYTHIA6_Ds2PhiPi_TuneZ2star_8TeV_cff.py
+++ python/EightTeV/PYTHIA6_Ds2PhiPi_TuneZ2star_8TeV_cff.py
@@ -12,7 +12,29 @@ generator = cms.EDFilter(
     maxEventsToPrint = cms.untracked.int32(0),
     
     PythiaParameters = cms.PSet(
-        pythiaUESettingsBlock,
+        #pythiaUESettingsBlock,
+        pythiaUESettings = cms.vstring(
+	    'MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.832 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.275 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model'),
         bbbarSettings = cms.vstring('MSEL=1'),
         parameterSets = cms.vstring(
             'pythiaUESettings', 
@@ -34,11 +56,12 @@ generator = cms.EDFilter(
 
 
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.3 $'),
+    version = cms.untracked.string('$Revision: 1.1 $'),
     annotation = cms.untracked.string('Ds -> Phi Pi at 8 TeV'),
     name = cms.untracked.string
-    ('$Source: /local/reps/CMSSW/CMSSW/Configuration/GenProduction/python/EightTeV/PYTHIA6_Ds2PhiPi_TuneZ2star_8TeV_cff.py,v $')
+    ('$Source: /afs/cern.ch/project/cvs/reps/CMSSW/CMSSW/Configuration/GenProduction/python/EightTeV/PYTHIA6_Ds2PhiPi_TuneZ2star_8TeV_cff.py,v $')
     )
+
 
 # filter to select events with a Ds
 Dfilter = cms.EDFilter(
@@ -50,5 +73,25 @@ Dfilter = cms.EDFilter(
     ParticleID = cms.untracked.int32(431)  #D_s 
     )
 
-ProductionFilterSequence = cms.Sequence(generator*Dfilter)
+#ProductionFilterSequence = cms.Sequence(generator*Dfilter)
+
+genParticlesForFilter = cms.EDProducer("GenParticleProducer",
+    saveBarCodes = cms.untracked.bool(True),
+    src = cms.InputTag("generator"),
+    abortOnUnknownPDGCode = cms.untracked.bool(False)
+)
+
+muonParticlesInAcc = cms.EDFilter("GenParticleSelector",
+    filter = cms.bool(False),
+    src = cms.InputTag("genParticlesForFilter"),
+    cut = cms.string('pt > 1. && abs(pdgId) == 13 && abs(eta) < 2.4'),
+    stableOnly = cms.bool(True)
+)
+
+twoMuonFilter = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("muonParticlesInAcc"),
+    minNumber = cms.uint32(2)
+)
+
+ProductionFilterSequence = cms.Sequence(generator+Dfilter+genParticlesForFilter+muonParticlesInAcc+twoMuonFilter)
 
