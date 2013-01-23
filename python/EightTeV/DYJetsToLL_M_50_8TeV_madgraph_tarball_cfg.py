de1c605519df29f9bdee55d26ace051ed68f7011
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2013-01-23 10:25:09 +0000

    Change to Summer12_WMLHE fragment format

--- python/EightTeV/DYJetsToLL_M_50_8TeV_madgraph_tarball_cfg.py
+++ python/EightTeV/DYJetsToLL_M_50_8TeV_madgraph_tarball_cfg.py
@@ -1,10 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
-    scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_tarball.sh"),
-    outputFile = cms.string("DYJetsToLL_M-50_8TeV-madgraph_unweighted_events_final.lhe"),
-    args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DYJetsToLL_M-50_8TeV-madgraph/v2',
+externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
+   scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_tarball.sh"),
+   outputFile = cms.string("DYJetsToLL_M-50_8TeV-madgraph_unweighted_events_final.lhe"),
+   numberOfParameters = cms.uint32(10), 
+   args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DYJetsToLL_M-50_8TeV-madgraph/v2',
     'DYJetsToLL_M-50_8TeV-madgraph','false','true','zjets','5','20','true','0','4'),
-    nEvents = cms.uint32(process.maxEvents.input.value())
+   nEvents = cms.uint32(50000)
 )
 
