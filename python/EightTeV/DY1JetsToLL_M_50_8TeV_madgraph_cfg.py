854a5f5ca7929d4e0ab2b3c684f7b96da5dc9feb
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2013-01-23 13:02:16 +0000

    For Summer12_LHE extenstion

--- python/EightTeV/DY1JetsToLL_M_50_8TeV_madgraph_cfg.py
+++ python/EightTeV/DY1JetsToLL_M_50_8TeV_madgraph_cfg.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("events_final.lhe"),
-    numberOfParameters = cms.integer(10),
+    numberOfParameters = cms.uint32(10),
     args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/DY1JetsToLL_M-50_8TeV-madgraph/v2',
     'DY1JetsToLL_M-50_8TeV-madgraph','false','true','zjets','5','20','true','1','99'),
     nEvents = cms.uint32(10000)
