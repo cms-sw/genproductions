887c4f69db274ab88946b78a2bd9e54c1899a224
Vladimir Ivantchenko <sha1-05628bbcf33eb7e0ec319f684b9c387a833d2578@cern.ch>
2013-02-06 14:48:48 +0000

    add RusRoul

---
+++ python/RussianRoulette_ngamma_0.3_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+def customiseGeant4(process):
+
+    process.g4SimHits.Physics.RusRoEcalGamma = cms.double(0.3)
+    process.g4SimHits.Physics.RusRoEcalGammaLimit = cms.double(10.)
+    process.g4SimHits.Physics.RusRoHcalGamma = cms.double(0.3)
+    process.g4SimHits.Physics.RusRoHcalGammaLimit = cms.double(10.)
+
+    process.g4SimHits.StackingAction.RusRoEcalNeutron = cms.double(0.3)
+    process.g4SimHits.StackingAction.RusRoEcalNeutronLimit = cms.double(10.)
+    process.g4SimHits.StackingAction.RusRoHcalNeutron = cms.double(0.3)
+    process.g4SimHits.StackingAction.RusRoHcalNeutronLimit = cms.double(10.)
+
+    return(process)
