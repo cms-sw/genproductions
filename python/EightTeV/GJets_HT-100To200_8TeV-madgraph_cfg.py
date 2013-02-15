7e41a32598ea9f9fd0590fd1cf554cb38ba35924
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2013-02-15 11:41:41 +0000

    reduced number of events

--- python/EightTeV/GJets_HT-100To200_8TeV-madgraph_cfg.py
+++ python/EightTeV/GJets_HT-100To200_8TeV-madgraph_cfg.py
@@ -7,7 +7,7 @@ externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.30/8TeV_Summer12/GJets_HT-100To200_8TeV-madgraph/v1',
                        'GJets_HT-100To200_8TeV-madgraph',
                        'false','false','qcd','5','15','true','1','4'),
-    nEvents = cms.uint32(50000)
+    nEvents = cms.uint32(15000)
 )
 
 
