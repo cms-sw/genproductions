042088db65d199a7e1e447680611e162b4b98331
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2013-04-18 12:03:16 +0000

    outputFile name fix

--- python/EightTeV/LminusNubarVBF_Mqq-120_8TeV-madgraph_cfg.py
+++ python/EightTeV/LminusNubarVBF_Mqq-120_8TeV-madgraph_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     numberOfParameters = cms.uint32(10),
-    outputFile = cms.string("W_VBF_8TeV_madgraph_final.lhe"),
+    outputFile = cms.string("Wminus_Lept_VBF_8TeV_madgraph_final.lhe"),
     args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.4.8/8TeV_Summer12/Wminus_Lept_VBF_8TeV_madgraph/v1',
                        'Wminus_Lept_VBF_8TeV_madgraph','false','false','wjets','5','20','false','0','3'),
     nEvents = cms.uint32(50000)
