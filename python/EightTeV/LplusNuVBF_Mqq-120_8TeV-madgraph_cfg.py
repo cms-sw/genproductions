a0858994bd661bfebc10064ade96e5426592e7c3
Simon De Visscher <sha1-aafd21e13d4932b6488764d072a6d70a5bb7d09b@cern.ch>
2013-04-19 08:48:58 +0000

    W and W- VBF at 120 GeV

--- python/EightTeV/LplusNuVBF_Mqq-120_8TeV-madgraph_cfg.py
+++ python/EightTeV/LplusNuVBF_Mqq-120_8TeV-madgraph_cfg.py
@@ -6,6 +6,6 @@ externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     numberOfParameters = cms.uint32(10),
     outputFile = cms.string("Wplus_VBF_Lept_mqq_120_inf_noaddpart_8TeV_madgraph_final.lhe"),
     args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.4.8/8TeV_Summer12/Wplus_VBF_Lept_mqq_120_inf_noaddpart_8TeV_madgraph/v1',
-                       'Wplus_VBF_Lept_mqq_120_inf_noaddpart_8TeV_madgraph','false','true','wjets','5','20','false','0','3'),
+                       'Wplus_VBF_Lept_mqq_120_inf_noaddpart_8TeV_madgraph','false','true','wjets','5','20','false','0','0'),
     nEvents = cms.uint32(50000)
 )
