aa1066d65c30edeb3d2ac2f0145394e6507df3bd
David Nash <sha1-f4b79ea59bb0e246e80f98f01394574fe9201dca@cern.ch>
2013-05-03 17:10:11 +0000

    Summer12 EXO 8 TeV

---
+++ python/EightTeV/RSGravitonToZZ_kMpl01_M_2200_Tune23_8TeV_herwiggpp_cff.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.HerwigppDefaults_cfi import *
+
+generator = cms.EDFilter("ThePEGGeneratorFilter",
+	herwigDefaultsBlock,
+#	crossSection = cms.untracked.double(1.301),
+	filterEfficiency = cms.untracked.double(1),
+
+	configFiles = cms.vstring('RS.model'),
+	parameterSets = cms.vstring(
+		'cm8TeV',
+		'pdfMRST2001',
+		'productionParameters',
+		'basicSetup',
+		'setParticlesStableForDetector',
+	),
+	productionParameters = cms.vstring(
+                'cd /Herwig/NewPhysics',
+                'insert ResConstructor:Incoming 0 /Herwig/Particles/g',
+                'insert ResConstructor:Incoming 1 /Herwig/Particles/u',
+                'insert ResConstructor:Incoming 2 /Herwig/Particles/ubar',
+                'insert ResConstructor:Incoming 3 /Herwig/Particles/d',
+                'insert ResConstructor:Incoming 4 /Herwig/Particles/dbar',
+                'insert ResConstructor:Intermediates 0 /Herwig/Particles/Graviton',
+                'insert ResConstructor:Outgoing 0 /Herwig/Particles/Z0',
+                'set RS/Model:Lambda_pi 5744*GeV',
+                'set /Herwig/Particles/Graviton:NominalMass 2200*GeV',
+	),
+)
+
+
+ProductionFilterSequence = cms.Sequence(generator)
