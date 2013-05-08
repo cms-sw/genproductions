aefc01de670435e2b57fdf7aeff42e0ec57e6ec2
Roberto Covarelli <sha1-195c8adc73af39d9dbe4407aec6ff1a79a6f51ef@cern.ch>
2013-05-08 15:37:35 +0000

    Adding LHEProducers for SM ZZ

---
+++ python/EightTeV/POWHEG_ZZ_2e2tau_mll8_8TeV_cff.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
+                                     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/create_lhe_powheg.sh"),
+                                     outputFile = cms.string("events_final.lhe"),
+                                     numberOfParameters = cms.uint32(4),
+                                     args = cms.vstring('slc5_ia32_gcc434/powheg/V1.0/src',
+                                     'powhegboxv1.5_Feb2013',
+                                     'ZZ',
+                                     '/slc5_ia32_gcc434/powheg/V1.0/8TeV_Summer12/ZZ_2e2tau_mll8_8TeV-powheg/v1/ZZ_2e2tau_mll8_8TeV-powheg.input',
+                                     ),
+    nEvents = cms.uint32(18375000)
+                                            
+                                             )
+
