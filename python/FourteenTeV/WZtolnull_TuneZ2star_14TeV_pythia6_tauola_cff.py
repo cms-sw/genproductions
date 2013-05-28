44524a5648c78426bea3edbd1ffe7bac590aacce
Roberto Covarelli <sha1-195c8adc73af39d9dbe4407aec6ff1a79a6f51ef@cern.ch>
2013-05-28 13:52:57 +0000

    new files for upgarde

---
+++ python/FourteenTeV/WZtolnull_TuneZ2star_14TeV_pythia6_tauola_cff.py
@@ -0,0 +1,69 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
+from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.),
+    comEnergy = cms.double(14000.0),
+    crossSection = cms.untracked.double(12.6),
+    ExternalDecays = cms.PSet(
+        Tauola = cms.untracked.PSet(
+            TauolaPolar,
+            TauolaDefaultInputCards
+        ),
+        parameterSets = cms.vstring('Tauola')
+    ),
+    UseExternalGenerators = cms.untracked.bool(True),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL        = 0    !User defined processes', 
+                                        'MSUB(23) = 1 ! WZ production',
+            'MDME(190,1)=0    ! W decay into dbar u', 
+            'MDME(191,1)=0    ! W decay into dbar c',   
+            'MDME(192,1)=0    ! W decay into dbar t',
+            'MDME(194,1)=0    ! W decay into sbar u',
+            'MDME(195,1)=0    ! W decay into sbar c', 
+            'MDME(196,1)=0    ! W decay into sbar t',
+            'MDME(198,1)=0    ! W decay into bbar u',   
+            'MDME(199,1)=0    ! W decay into bbar c', 
+            'MDME(200,1)=0    ! W decay into bbar t', 
+            'MDME(206,1)=1    ! W decay into e+ nu_e',
+            'MDME(207,1)=1    ! W decay into mu+ nu_mu',
+            'MDME(208,1)=1    ! W decay into tau+ nu_tau',
+
+            'MDME(174,1)=0           !Z decay into d dbar',
+            'MDME(175,1)=0           !Z decay into u ubar',
+            'MDME(176,1)=0           !Z decay into s sbar',
+            'MDME(177,1)=0           !Z decay into c cbar',
+            'MDME(178,1)=0           !Z decay into b bbar',
+            'MDME(179,1)=0           !Z decay into t tbar',
+            'MDME(182,1)=1           !Z decay into e- e+',
+            'MDME(183,1)=0           !Z decay into nu_e nu_ebar',
+            'MDME(184,1)=1           !Z decay into mu- mu+',
+            'MDME(185,1)=0           !Z decay into nu_mu nu_mubar',
+            'MDME(186,1)=0           !Z decay into tau- tau+',
+            'MDME(187,1)=0           !Z decay into nu_tau nu_taubar'
+
+
+),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /local/reps/CMSSW/CMSSW/Configuration/GenProduction/python/FourteenTeV/WZtoAnything_TuneZ2star_14TeV_pythia6_tauola_cff.py,v $'),
+    annotation = cms.untracked.string('PYTHIA6-EWK WZ at 8 TeV')
+)
+
+
