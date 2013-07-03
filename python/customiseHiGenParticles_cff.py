27fd7ea7a2d1f4135989368c1d66bcef9f15a90a
cmsbuild <>
2013-07-03 08:36:34 +0000

    This commit was manufactured by cvs2git to create tag 'V02-05-07'.
    Sprout from master 2013-07-03 08:36:33 UTC Nadja Strobbe <sha1-e755448bf5459bdcc02fae4b5a70f58a38fc7416@cern.ch> 'Hadronizer for 14 TeV SUSY SMS'
    Delete:
        python/QGSP_FTFP_BERT_EML_New_cff.py
        python/customiseHiGenParticles_cff.py

--- python/customiseHiGenParticles_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-def customiseHiGenParticles(process):
-
-    process.hiGenParticles.abortOnUnknownPDGCode = cms.untracked.bool(False)
-
-    return(process)
