c3eda04fe0465fbf4c0fffcd2a004f5345c32451
Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch>
2013-07-10 21:23:02 +0000

    b-jets

---
+++ python/HI/Pyquen_BJet_Pt120_TuneZ2_Unquenched_2760GeV_cfi.py
@@ -0,0 +1,79 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.GenProduction.HI.PyquenTuneZ2Settings_cff import *
+
+bjetTrigCommon = cms.PSet(filterType = cms.untracked.string("EcalGenEvtSelector"),
+                          etaMax     = cms.double(3),
+                          partons    = cms.vint32(5),
+                          partonPt   = cms.vdouble(10),
+                          partonStatus = cms.vint32(2),
+                          particleStatus = cms.vint32(2, 2, 
+                                                      2, 2, 
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2,                                                      
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2,                                                      
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2,
+                                                      2, 2
+                                                      ),
+                          particles = cms.vint32(511, -511, # B0
+                                                 513, -513, # B*0
+                                                 521, -521, # B+
+                                                 523, -523, # B*+
+                                                 531, -531, # Bs0
+                                                 533, -533, # Bs*0
+                                                 541, -541, # Bc0
+                                                 543, -543, # Bc*0
+                                                 551, -551, # etab
+                                                 553, -553, # Upsilon
+                                                 5112, -5112, # Sigma_b-
+                                                 5114, -5114, # Sigma*_b-
+                                                 5122, -5122, # Lambda_b0
+                                                 5212, -5212, # Sigma_b0
+                                                 5214, -5214, # Sigma*_b0
+                                                 5222, -5222, # Sigma_b+
+                                                 5224, -5224 # Sigma*_b+
+                                                 ),
+                          particlePt = cms.vdouble(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+                                                   )
+                          )
+
+hiSignal = cms.EDFilter("PyquenGeneratorFilter",
+                        bjetTrigCommon,
+                        collisionParameters,
+                        qgpParameters,
+                        pyquenParameters,
+                        doQuench = cms.bool(False),
+                        bFixed = cms.double(0.0), ## fixed impact param (fm); valid only if cflag_=0
+                        cFlag = cms.int32(0), ## centrality flag
+                        bMin = cms.double(0.0), ## min impact param (fm); valid only if cflag_!=0
+                        bMax = cms.double(0.0), ## max impact param (fm); valid only if cflag_!=0
+                        PythiaParameters = cms.PSet(pyquenPythiaDefaultBlock,
+                                                    parameterSets = cms.vstring('pythiaUESettings',
+                                                                                'pythiaDijet',
+                                                                                'kinematics'
+                                                                                ),
+                                                    kinematics = cms.vstring ("CKIN(7)=-3.",  #min rapidity
+                                                                              "CKIN(8)=3.",    #max rapidity
+                                                                              'CKIN(3) = 120.       !(D=0 GeV) lower lim pT_hat',
+                                                                              'CKIN(4) = 9999.       !(D=-1 GeV) upper lim pT_hat, if < 0 innactive',
+                                                                              ),
+                                                    pythiaDijet = cms.vstring ("MSEL=1" # dijets
+                                                                               ),
+                                                    
+                                                    )
+                       
+                        )
+
+hiSignal.embeddingMode = True
+
+ProductionFilterSequence = cms.Sequence(hiSignal)
