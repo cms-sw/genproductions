ceab6044116394c9f6ab61caf48b6a001a629b01
UAEDF-tomc <tom.cornelis@ua.ac.be>
2013-10-12 00:24:57 +0200

    Add 13TeV pythia QCD samples (still need to update xsec and filter)

---
+++ python/ThirteenTeV/QCD_Pt_5to10_TuneZ2star_13TeV_pythia6_cff.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
+
+
+generator = cms.EDFilter('Pythia6GeneratorFilter',
+	comEnergy = cms.double(13000.0),
+	crossSection = cms.untracked.double(0),
+	filterEfficiency = cms.untracked.double(1),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
+	PythiaParameters = cms.PSet(
+		pythiaUESettingsBlock,
+		processParameters = cms.vstring(
+			'MSEL = 1        ! QCD hight pT processes',
+			'CKIN(3) = 5     ! minimum pt hat for hard interactions',
+			'CKIN(4) = 10    ! maximum pt hat for hard interactions',
+		),
+		parameterSets = cms.vstring(
+			'pythiaUESettings',
+			'processParameters',
+		)
+	)
+)
+
+pthat_filter = cms.EDFilter('MCProcessFilter',
+	MaxPthat = cms.untracked.vdouble(10., 10.0, 10.0, 10.0, 10.0, 10.0),
+	ProcessID = cms.untracked.vint32(11, 12, 13, 68, 28, 53),
+	MinPthat = cms.untracked.vdouble(0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+)
+
+ProductionFilterSequence = cms.Sequence(generator * pthat_filter)
