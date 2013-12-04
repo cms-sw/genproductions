19f149c1a109181fae83742715b3df4f076e6d60
David <dnash@cern.ch>
2013-12-04 20:42:21 +0100

    Cards made by Klaas Padeken

---
+++ python/ThirteenTeV/WprimeToENu_M_3800_Tune4C_13TeV_pythia8_cfi.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+        comEnergy = cms.double(13000.0),
+        crossSection = cms.untracked.double(2.683e-03),
+        filterEfficiency = cms.untracked.double(1),
+        maxEventsToPrint = cms.untracked.int32(1),
+        pythiaHepMCVerbosity = cms.untracked.bool(False),
+        pythiaPylistVerbosity = cms.untracked.int32(1),
+        PythiaParameters = cms.PSet(
+                processParameters = cms.vstring(
+                        'Main:timesAllowErrors = 10000',
+                        'ParticleDecays:limitTau0 = on',
+                        'ParticleDecays:tauMax = 10',
+                        'Tune:ee 3',
+                        'Tune:pp 5',
+
+                        'NewGaugeBoson:ffbar2Wprime = on',
+                        '34:m0 = 3800',
+                        '34:onMode = off',
+                        '34:onIfAny = 11,12',
+
+                ),
+                parameterSets = cms.vstring('processParameters')
+        )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
