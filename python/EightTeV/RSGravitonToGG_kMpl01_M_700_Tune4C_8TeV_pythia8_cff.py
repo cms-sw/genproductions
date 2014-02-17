8879012c6f81c428d832908c87cca0d8e2ef01f0
David <dnash@cern.ch>
2014-02-18 00:02:56 +0100

    fragments from Francesco

---
+++ python/EightTeV/RSGravitonToGG_kMpl01_M_700_Tune4C_8TeV_pythia8_cff.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+	comEnergy = cms.double(8000.0),
+        crossSection = cms.untracked.double(8.755),
+	filterEfficiency = cms.untracked.double(1.0),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
+	PythiaParameters = cms.PSet(
+		processParameters = cms.vstring(
+                'Main:timesAllowErrors    = 10000',
+	        'ParticleDecays:limitTau0 = on',
+		'ParticleDecays:tauMax = 10',
+                'ExtraDimensionsG*:gg2G* = on',
+                '5100039:m0 = 700.',
+                '5100039:onMode = off',
+                '5100039:onIfAny = 21',
+                'ExtraDimensionsG*:kappaMG = 0.54',
+                'Tune:pp 5',
+		'Tune:ee 3',
+		),
+
+		parameterSets = cms.vstring('processParameters')
+	)
+)
