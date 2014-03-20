340494ace6a5bf992d983c67b6c985d59b1df2a0
Philippe Gras <philippe.gras@cern.ch>
2014-03-20 17:13:45 +0100

    enabled Powheg pdfreweight option

--- python/ThirteenTeV/WplusToENu_CT10_13TeV-powheg_cff.py
+++ python/ThirteenTeV/WplusToENu_CT10_13TeV-powheg_cff.py
@@ -7,7 +7,7 @@ externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
                                      args = cms.vstring('slc5_ia32_gcc434/powheg/V1.0/src', #repository of powheg source code
                                                         'powhegboxv1.0_Oct2013', # version of powheg source code, not used for precompiled mode
                                                         'W',     # process name
-                                                        'slc5_amd64_gcc472/13TeV/powheg/WplusToENu_CT10_13TeV-powheg/v1/WplusToENu_CT10_13TeV-powheg.input', # location of input data card  
+                                                        'slc5_amd64_gcc472/13TeV/powheg/WplusToENu_CT10_13TeV-powheg/v2/WplusToENu_CT10_13TeV-powheg.input', # location of input data card  
                                                         'true',  #use a precompiled tar ball or not 
                                                         'false',  #if compiling during the run, create a tar ball or not
                                                         'slc5_amd64_gcc472/8TeV/powheg/precompiled_set_v1', # repository of precompiled tar ball
