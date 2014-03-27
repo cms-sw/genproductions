1e49a449655c0b84bc3ea43d500a4493dea29598
Philippe Gras <philippe.gras@cern.ch>
2014-03-27 11:25:46 +0100

    fixed LHE file name

--- python/ThirteenTeV/WJetsToLNu_13TeV-madgraph_cff.py
+++ python/ThirteenTeV/WJetsToLNu_13TeV-madgraph_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
                                      scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
-                                     outputFile = cms.string("DYJetsToLL_M-50_13TeV-madgraph_final.lhe"),
+                                     outputFile = cms.string("WJetsToLNu_13TeV_final.lhe"),
                                      numberOfParameters = cms.uint32(10),
                                      args = cms.vstring('slc5_amd64_gcc472/13TeV/madgraph/V5_1.5.11/WJetsToLNu_13TeV/v1', #gridpack path
                                                         'WJetsToLNu_13TeV', #gridpack name, without _gridpack.tar.gz
