c675f84f735ffc1a82a4c5c0c4a6b47a16ef109a
Roberto Covarelli <roberto.covarelli@cern.ch>
2014-04-23 16:37:19 +0200

    higgs generic decay fragments

---
+++ python/ThirteenTeV/HTauTau_Tune4C_13TeV_LHE_pythia8_Tauola_cff.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         ExternalDecays = cms.PSet(
+    Tauola = cms.untracked.PSet(
+    TauolaPolar,
+    TauolaDefaultInputCards
+    ),
+    parameterSets = cms.vstring('Tauola')
+    ),
+    UseExternalGenerators = cms.untracked.bool(True),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        processParameters = cms.vstring(
+	'Main:timesAllowErrors    = 10000', 
+        'ParticleDecays:limitTau0 = on',
+	'ParticleDecays:tau0Max = 10',
+        'Tune:ee 3',
+        'Tune:pp 5',
+        '25:onMode = off',    # turn OFF all H decays
+        '25:onIfAny = 15',    # turn ON H->tautau
+        ),
+        parameterSets = cms.vstring('processParameters')
+    )
+)
