9cac4b52888a5280c352a0f0901cb857944649c9
davidsheffield <sheffield@physics.rutgers.edu>
2014-06-17 11:53:39 -0400

    Create BStarToGJ_M_1200_f0p1_Tune4C_8TeV_Pythia8_cfi.py

---
+++ python/EightTeV/BStarToGJ_M_1200_f0p1_Tune4C_8TeV_Pythia8_cfi.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                  pythiaPylistVerbosity = cms.untracked.int32(0),
+                  filterEfficiency = cms.untracked.double(1.0),
+                  pythiaHepMCVerbosity = cms.untracked.bool(False),
+                  comEnergy = cms.double(8000.0),
+                  maxEventsToPrint = cms.untracked.int32(0),
+                  crossSection = cms.untracked.double(0.00006691),
+                  PythiaParameters = cms.PSet(
+                        processParameters = cms.vstring(
+
+                            'Main:timesAllowErrors = 10000',
+                            'ParticleDecays:limitTau0 = on',
+                            'ParticleDecays:tau0Max = 10',
+                            'ExcitedFermion:bg2bStar = on',
+                            '4000005:m0 = 1200.',
+                            '4000005:onMode = off',
+                            '4000005:onIfMatch = 22 5',
+                            'ExcitedFermion:Lambda = 1200.',
+                            'ExcitedFermion:coupFprime = 0.1',
+                            'ExcitedFermion:coupF = 0.1',
+                            'ExcitedFermion:coupFcol = 0.1',
+                            'Tune:pp 5',
+                            'Tune:ee 3'
+                            ),
+                        parameterSets = cms.vstring('processParameters')
+                  )
+)
