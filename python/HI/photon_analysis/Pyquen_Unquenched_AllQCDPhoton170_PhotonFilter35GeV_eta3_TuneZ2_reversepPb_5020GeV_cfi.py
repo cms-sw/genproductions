f69fde590718c67c1b4d14159b7e8c85da542377
R. Alex Barbieri <luck@mit.edu>
2014-07-11 17:41:06 +0200

    move photon analysis configs to subfolder, make new reverse beam direction pPb

---
+++ python/HI/photon_analysis/Pyquen_Unquenched_AllQCDPhoton170_PhotonFilter35GeV_eta3_TuneZ2_reversepPb_5020GeV_cfi.py
@@ -0,0 +1,57 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PyquenDefaultSettings_cff import *
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+hiSignal = cms.EDFilter("PyquenGeneratorFilter",
+                        qgpParameters,
+                        pyquenParameters,
+                        aBeamTarget = cms.double(208.0), ## beam/target atomic number
+                        comEnergy = cms.double(5020.0),
+                        doQuench = cms.bool(False),
+                        bFixed = cms.double(0.0), ## fixed impact param (fm); valid only if cflag_=0
+                        cFlag = cms.int32(0), ## centrality flag
+                        bMin = cms.double(0.0), ## min impact param (fm); valid only if cflag_!=0
+                        bMax = cms.double(0.0), ## max impact param (fm); valid only if cflag_!=0
+
+                        filterType     = cms.untracked.string('EcalGenEvtSelectorFrag'),
+                        particlePt     = cms.vdouble(35),
+                        particleStatus = cms.vint32(1),
+                        particles      = cms.vint32(22),
+                        etaMax         = cms.double(3),   # Photon eta cut
+                        partonStatus = cms.vint32(2, 2, 2, 2, 2, 2, 2, 1),
+                        partonPt = cms.vdouble(0,0,0,0,0,0,0,0),
+                        partons = cms.vint32(1, 2, 3, 4, 5, 6, 21, 22), # parton cut is not functioning
+                        maxTries = cms.untracked.int32(5000),
+                        protonSide = cms.untracked.int32(1), #reverse pPb direction
+
+                        PythiaParameters = cms.PSet(
+                            pythiaUESettingsBlock,
+                            customProcesses = cms.vstring('MSEL=0   ! User processes'),
+                            allQCDPhotonChannel = cms.vstring(    'MSUB(11)=1', # q+q->q+q
+                                                                  'MSUB(12)=1', # q+qbar->q+qbar
+                                                                  'MSUB(13)=1', # q+qbar->g+g
+                                                                  'MSUB(28)=1', # q+g->q+g
+                                                                  'MSUB(53)=1', # g+g->q+qbar
+                                                                  'MSUB(68)=1', # g+g->g+g
+                                                                  # Leading order photons
+                                                                  'MSUB(14)=1', # q+qbar->g+gamma
+                                                                  'MSUB(18)=1', # q+qbar->gamma+gamma
+                                                                  'MSUB(29)=1', # q+g->q+gamma
+                                                                  'MSUB(114)=1', # g+g->gamma+gamma
+                                                                  'MSUB(115)=1' # g+g->g+gamma
+                                                              ),
+                            parameterSets = cms.vstring('pythiaUESettings',
+                                                        'customProcesses',
+                                                        'allQCDPhotonChannel',
+                                                        'kinematics'),
+                            kinematics = cms.vstring('CKIN(3)=170',
+                                                     'CKIN(4)=9999'
+                                                 )
+                        )
+)
+
+
+hiSignal.embeddingMode = True
+
+ProductionFilterSequence = cms.Sequence(hiSignal)
---
+++ python/HI/photon_analysis/Pyquen_Unquenched_EmEnrichedDijet120_ParticleFilter35GeV_eta3_TuneZ2_reversepPb_5020GeV_cfi.py
@@ -0,0 +1,53 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PyquenDefaultSettings_cff import *
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+hiSignal = cms.EDFilter("PyquenGeneratorFilter",
+                        qgpParameters,
+                        pyquenParameters,
+                        aBeamTarget = cms.double(208.0), ## beam/target atomic number
+                        comEnergy = cms.double(5020.0),
+                        doQuench = cms.bool(False),
+                        cFlag = cms.int32(0), ## centrality flag
+                        bMin = cms.double(0.0), ## min impact param (fm); valid only if cflag_!=0
+                        bMax = cms.double(0.0), ## max impact param (fm); valid only if cflag_!=0
+                        bFixed = cms.double(0.0), ## fixed impact param (fm); valid only if cflag_=0
+
+
+                        filterType = cms.untracked.string('EcalGenEvtSelector'),
+                        partons = cms.vint32(1, 2, 3, 4, 5, 6, #quarks
+                                             21, 22), #gluon, photon
+                        partonPt = cms.vdouble(0, 0, 0, 0, 0, 0,
+                                               0, 0),
+                        partonStatus = cms.vint32(2, 2, 2, 2, 2, 2,
+                                                  2, 1),
+                        particles = cms.vint32(221, #eta
+                                               331, #eta'
+                                               223, #omega
+                                               111), #pi0
+                        particlePt = cms.vdouble(35, 35, 35, 35),
+                        particleStatus = cms.vint32(2, #eta
+                                                    2, #eta'
+                                                    2, #omega
+                                                    2), #pi0
+                        etaMax = cms.double(3.0),   # Photon eta cut
+                        maxTries = cms.untracked.int32(5000),
+                        protonSide = cms.untracked.int32(1), #reverse pPb direction
+
+                        PythiaParameters = cms.PSet(
+                            pythiaUESettingsBlock,
+                            ppJets = cms.vstring('MSEL=1   ! QCD hight pT processes'),
+                            parameterSets = cms.vstring('pythiaUESettings',
+                                                        'ppJets',
+                                                        'kinematics'),
+
+                            kinematics = cms.vstring('CKIN(3)=120',
+                                                     'CKIN(4)=9999'
+                                                 )
+                        )
+)
+
+
+hiSignal.embeddingMode = True
+ProductionFilterSequence = cms.Sequence(hiSignal)
