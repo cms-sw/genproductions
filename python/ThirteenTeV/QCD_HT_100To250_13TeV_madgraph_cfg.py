039c41070e3ab9f7ebe280281adfcd2561fab74e
Clemens Lange <clemens.lange@cern.ch>
2014-07-31 01:00:57 +0200

    add missing number of parameters line

--- python/ThirteenTeV/QCD_HT_100To250_13TeV_madgraph_cfg.py
+++ python/ThirteenTeV/QCD_HT_100To250_13TeV_madgraph_cfg.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("events_final.lhe"),
+    numberOfParameters = cms.uint32(10),
     args = cms.vstring('slc5_amd64_gcc472/13TeV/madgraph/V5_1.5.11/QCD_HT-100To250_13TeV-madgraph/v1',
     'QCD_HT-100To250_13TeV-madgraph','false','false','qcd','5','15','true','2','4'),
     nEvents = cms.uint32(100000)
--- python/ThirteenTeV/QCD_HT_500To1000_13TeV_madgraph_cfg.py
+++ python/ThirteenTeV/QCD_HT_500To1000_13TeV_madgraph_cfg.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("events_final.lhe"),
+    numberOfParameters = cms.uint32(10),
     args = cms.vstring('slc5_amd64_gcc472/13TeV/madgraph/V5_1.5.11/QCD_HT-500To1000_13TeV-madgraph/v1',
-    'QCD_HT-500To1000_13TeV-madgraph','false','false','qcd','5','54','true','2','4'),
+    'QCD_HT-500To1000','false','false','qcd','5','54','true','2','4'),
     nEvents = cms.uint32(100000)
 )
