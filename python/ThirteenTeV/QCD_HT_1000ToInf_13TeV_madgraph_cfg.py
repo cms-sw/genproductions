ff0c2a7844eb92210d193577d9533268631cd0b8
Clemens Lange <clemens.lange@cern.ch>
2014-07-30 13:36:26 +0200

    add QCD HT-binned configs for 13 TeV

---
+++ python/ThirteenTeV/QCD_HT_1000ToInf_13TeV_madgraph_cfg.py
@@ -0,0 +1,58 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ScriptExample")
+
+process.load("IOMC.RandomEngine.IOMC_cff")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.categories=cms.untracked.vstring('FwkJob'
+                                                           ,'FwkReport'
+                                                           ,'FwkSummary'
+                                                           ,'Root_NoDictionary'
+                                                           ,'Generator'
+                                                           ,'LHEInterface'
+                                                           )
+process.MessageLogger.cerr.INFO = cms.untracked.PSet(limit = cms.untracked.int32(-1))
+process.MessageLogger.cerr.Generator = cms.untracked.PSet(limit = cms.untracked.int32(0))
+process.MessageLogger.cerr.LHEInterface = cms.untracked.PSet(limit = cms.untracked.int32(0))
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10000)
+
+process.load('Configuration.EventContent.EventContent_cff')
+process.load("GeneratorInterface.LHEInterface.ExternalLHEAsciiDumper_cfi")
+#the following two parameters need to be changed on a job by job basis
+process.externalLHEAsciiDumper.lheFileName = cms.string('output.lhe') 
+process.RandomNumberGeneratorService.externalLHEProducer.initialSeed = 1111111
+
+process.source = cms.Source("EmptySource",
+    firstLuminosityBlock = cms.untracked.uint32(23456),
+    numberEventsInLuminosityBlock = cms.untracked.uint32(10)
+)                            
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100000) )
+
+process.externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
+    scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
+    outputFile = cms.string("events_final.lhe"),
+    args = cms.vstring('slc5_amd64_gcc472/13TeV/madgraph/V5_1.5.11/QCD_HT-1000ToInf_13TeV-madgraph/v1',
+    'QCD_HT-1000ToInf_13TeV-madgraph','false','false','qcd','5','75','true','2','4'),
+    nEvents = cms.uint32(process.maxEvents.input.value())
+)
+
+
+process.out = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.LHEEventContent.outputCommands,
+    fileName = cms.untracked.string('myOutputFile.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('LHE')
+    )
+)
+
+process.p = cms.Path(process.externalLHEProducer)
+
+process.e = cms.EndPath(process.externalLHEAsciiDumper)
+#+process.out)
+
+
+
