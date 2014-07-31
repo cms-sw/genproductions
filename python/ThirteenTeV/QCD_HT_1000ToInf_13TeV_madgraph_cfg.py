f72ffd5d5b64c934eccf5d201e006e867f127e5c
Clemens Lange <clemens.lange@cern.ch>
2014-07-31 11:21:48 +0200

    individual LHE output names for each fragment needed

--- python/ThirteenTeV/QCD_HT_1000ToInf_13TeV_madgraph_cfg.py
+++ python/ThirteenTeV/QCD_HT_1000ToInf_13TeV_madgraph_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
-    outputFile = cms.string("events.lhe"),
+    outputFile = cms.string("QCD_HT-1000ToInf_final.lhe"),
     numberOfParameters = cms.uint32(10),
     args = cms.vstring('slc5_amd64_gcc472/13TeV/madgraph/V5_1.5.11/QCD_HT-1000ToInf/v1',
     'QCD_HT-1000ToInf','false','false','qcd','5','75','true','2','4'),
--- python/ThirteenTeV/QCD_HT_100To250_13TeV_madgraph_cfg.py
+++ python/ThirteenTeV/QCD_HT_100To250_13TeV_madgraph_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
-    outputFile = cms.string("events.lhe"),
+    outputFile = cms.string("QCD_HT-100To250_final.lhe"),
     numberOfParameters = cms.uint32(10),
     args = cms.vstring('slc5_amd64_gcc472/13TeV/madgraph/V5_1.5.11/QCD_HT-100To250/v1',
     'QCD_HT-100To250','false','false','qcd','5','15','true','2','4'),
