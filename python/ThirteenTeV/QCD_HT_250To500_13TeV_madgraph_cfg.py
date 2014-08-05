f3df36294161aa47cdecd4c7ccdba59eb56c878a
Clemens Lange <clemens.lange@cern.ch>
2014-08-05 14:14:53 +0200

    update to v3 of gridpack (confirmed to work)

--- python/ThirteenTeV/QCD_HT_250To500_13TeV_madgraph_cfg.py
+++ python/ThirteenTeV/QCD_HT_250To500_13TeV_madgraph_cfg.py
@@ -4,7 +4,7 @@ externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
     outputFile = cms.string("QCD_HT-250To500_final.lhe"),
     numberOfParameters = cms.uint32(10),
-    args = cms.vstring('slc5_amd64_gcc472/13TeV/madgraph/V5_1.5.11/QCD_HT-250To500/v2',
+    args = cms.vstring('slc5_amd64_gcc472/13TeV/madgraph/V5_1.5.11/QCD_HT-250To500/v3',
     'QCD_HT-250To500','false','false','qcd','5','32','true','2','4'),
     nEvents = cms.uint32(100000)
 )
