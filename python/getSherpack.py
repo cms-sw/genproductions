5217bb73af0746be8b3ab6556746574fc9aeb218
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-09-03 23:58:11 +0200

    Fix python line breaks

--- python/getSherpack.py
+++ python/getSherpack.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 import os
 
 def customise(process):
-  os.system("echo 'Start directory'; pwd;
-             cd $CMSSW_BASE/..;
-             fn-fileget -c `cmsGetFnConnect frontier://smallfiles` slc5_ia32_gcc434/sherpa/1.3.0-cms/sherpa_DYToLL_10to50_MASTER_v2.tgz;
-             tar -xzf sherpa_DYToLL_10to50_MASTER_v2.tgz;
-             echo 'CmsRun directory:'; pwd;
-             ls *;
-             cd -;
+  os.system("echo 'Start directory'; pwd;\
+             cd $CMSSW_BASE/..;\
+             fn-fileget -c `cmsGetFnConnect frontier://smallfiles` slc5_ia32_gcc434/sherpa/1.3.0-cms/sherpa_DYToLL_10to50_MASTER_v2.tgz;\
+             tar -xzf sherpa_DYToLL_10to50_MASTER_v2.tgz;\
+             echo 'CmsRun directory:'; pwd;\
+             ls *;\
+             cd -;\
              echo 'Back to original directory:'; pwd")
   return(process)
