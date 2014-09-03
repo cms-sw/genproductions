d044328f2915d26c3426918c3f4dec06acd6cf4d
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-09-03 23:53:18 +0200

    Keep cd /.. and cd - inside the same os.system command

--- python/getSherpack.py
+++ python/getSherpack.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 import os
 
 def customise(process):
-  os.system("echo 'Start directory'; pwd")
-  os.system("cd $CMSSW_BASE/..")
-  os.system("fn-fileget -c `cmsGetFnConnect frontier://smallfiles` slc5_ia32_gcc434/sherpa/1.3.0-cms/sherpa_DYToLL_10to50_MASTER_v2.tgz")
-  os.system("tar -xzf sherpa_DYToLL_10to50_MASTER_v2.tgz")
-  os.system("echo 'CmsRun directory:'; pwd")
-  os.system("ls *")
-  os.system("cd -")
-  os.system("echo 'Back to original directory:'; pwd")
+  os.system("echo 'Start directory'; pwd;
+             cd $CMSSW_BASE/..;
+             fn-fileget -c `cmsGetFnConnect frontier://smallfiles` slc5_ia32_gcc434/sherpa/1.3.0-cms/sherpa_DYToLL_10to50_MASTER_v2.tgz;
+             tar -xzf sherpa_DYToLL_10to50_MASTER_v2.tgz;
+             echo 'CmsRun directory:'; pwd;
+             ls *;
+             cd -;
+             echo 'Back to original directory:'; pwd")
   return(process)
