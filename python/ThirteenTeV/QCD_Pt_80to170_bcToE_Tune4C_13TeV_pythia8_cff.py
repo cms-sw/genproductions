f1e79f0d3e4cdae3b77851862a36f07eba684d29
Roberto <roberto.covarelli@cern.ch>
2014-09-10 14:43:14 +0200

    test fragments for Phys14

---
+++ python/ThirteenTeV/QCD_Pt_80to170_bcToE_Tune4C_13TeV_pythia8_cff.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+	comEnergy = cms.double(13000.0),
+	crossSection = cms.untracked.double(3.529e+06),
+	filterEfficiency = cms.untracked.double(0.0112),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
+	PythiaParameters = cms.PSet(
+		processParameters = cms.vstring(
+			'Main:timesAllowErrors    = 10000',
+			'ParticleDecays:limitTau0 = on',
+			'ParticleDecays:tauMax = 10',
+			'HardQCD:all = on',
+			'PhaseSpace:pTHatMin = 80',
+			'PhaseSpace:pTHatMax = 170',
+			'Tune:pp 5',
+			'Tune:ee 3',
+
+		),
+		parameterSets = cms.vstring('processParameters')
+	)
+)
+
+genParticlesForFilter = cms.EDProducer("GenParticleProducer",
+    saveBarCodes = cms.untracked.bool(True),
+    src = cms.InputTag("generator"),
+    abortOnUnknownPDGCode = cms.untracked.bool(False)
+)
+
+bctoefilter = cms.EDFilter("BCToEFilter",
+                           filterAlgoPSet = cms.PSet(eTThreshold = cms.double(10),
+                                                     genParSource = cms.InputTag("genParticlesForFilter")
+                                                     )
+                           )
+    
+
+# add your filters to this sequence
+ProductionFilterSequence = cms.Sequence(generator * (genParticlesForFilter + bctoefilter ))
+
