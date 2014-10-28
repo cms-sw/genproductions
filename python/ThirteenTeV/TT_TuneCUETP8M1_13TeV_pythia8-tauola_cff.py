cd64d60b9e0326197574ebda883b78000b033255
JesusManuel <vizan@cern.ch>
2014-10-28 10:51:30 +0100

    tt pythia8 fragment intended for RECODEBUG BTV studies

---
+++ python/ThirteenTeV/TT_TuneCUETP8M1_13TeV_pythia8-tauola_cff.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+	maxEventsToPrint = cms.untracked.int32(1),
+	pythiaPylistVerbosity = cms.untracked.int32(1),
+	filterEfficiency = cms.untracked.double(1.0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	comEnergy = cms.double(13000.0),
+
+	crossSection = cms.untracked.double(518.3),
+
+        ExternalDecays = cms.PSet(
+            Tauola = cms.untracked.PSet(
+               UseTauolaPolarization = cms.bool(True),
+               InputCards = cms.PSet
+               (
+                  pjak1 = cms.int32(0),
+                  pjak2 = cms.int32(0),
+                  mdtau = cms.int32(0)
+               )
+            ),
+            parameterSets = cms.vstring('Tauola')
+        ),
+	 
+	UseExternalGenerators = cms.untracked.bool(True),
+	
+	PythiaParameters = cms.PSet(
+            pythia8CommonSettingsBlock,
+            pythia8CUEP8M1SettingsBlock,
+	    processParameters = cms.vstring(
+                        'Top:gg2ttbar    = on',
+                        'Top:qqbar2ttbar = on',
+                        '6:m0 = 172.5',    # top mass',
+	    ),
+            parameterSets = cms.vstring('pythia8CommonSettings',
+                                        'pythia8CUEP8M1Settings',
+                                        'processParameters',
+                                        )
+	)
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('\$Revision$'),
+    name = cms.untracked.string('\$Source$'),
+    annotation = cms.untracked.string('ttbar, 13 TeV, TuneCUETP8M1')
+)
