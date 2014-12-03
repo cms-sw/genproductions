97640a4dcf2117cf6ef5511d9031144344b5c35c
Steven Kaplan <skaplan@physics.rutgers.edu>
2014-12-03 10:14:44 -0600

    initial commit of ADD GEN fragments

---
+++ python/ThirteenTeV/ADDdiPhoton_LambdaT-6000_PtHat-150to500_Tune4C_13TeV-pythia8_cfi.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.0),
+    crossSection = cms.untracked.double(1),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        processParameters = cms.vstring(
+            'Main:timesAllowErrors    = 10000', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tauMax = 10', 
+            'Tune:pp 5', # Tune 4C
+            'Tune:ee 3', 
+            'ExtraDimensionsLED:ffbar2gammagamma = on', 
+            'ExtraDimensionsLED:CutOffmode = 0',
+            'ExtraDimensionsLED:LambdaT = 6000.',
+            'PhaseSpace:pTHatMin = 150.',
+            'PhaseSpace:pTHatMax = 500.',
+            'PartonLevel:MI = on', 
+            'PartonLevel:ISR = on', 
+            'PartonLevel:FSR = on'
+        ),
+        parameterSets = cms.vstring('processParameters')
+    )
+)
