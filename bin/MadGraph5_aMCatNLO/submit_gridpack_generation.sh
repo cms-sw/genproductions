43d1c6d7d99485b4d8da33d77ff70b2a839457fb
Josh Bendavid <Josh.Bendavid@cern.ch>
2015-01-26 02:48:30 +0100

    script cleanup

--- bin/MadGraph5_aMCatNLO/submit_gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/submit_gridpack_generation.sh
@@ -7,10 +7,6 @@ name=${4}
 carddir=${5}
 workqueue=${6}
 
-lsfselect="select[!noturbo &! fullsmt &! smt &! plus &! spacecharge && type==SLC6_64 &&!(model==a6_48_1091h17_1333 || model==i6_8_61a5h23_1066 || model==ai_intel_8) ]"
-
-#lsfselect="select[qa && type==SLC6_64]"
-
-#bsub -q ${masterqueue} -C 0  -R "${lsfselect} rusage[mem=${memory}:pool=${diskspace}]" "export PRODHOME=`pwd`; gridpack_generation.sh ${carddir} ${name} ${workqueue}"
+#n.b this is not actually working reliably pending some further fixes from CERN IT
 
 bsub -q ${masterqueue} -C 0  -R "rusage[mem=${memory}:pool=${diskspace}]" "export PRODHOME=`pwd`; gridpack_generation.sh ${name} ${carddir} ${workqueue}"
