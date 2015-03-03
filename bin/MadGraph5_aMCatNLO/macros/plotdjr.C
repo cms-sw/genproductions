c703b97bdc9b1741ed063ede77fd9699be4238ca
Javier <Javier.Fernandez@cern.ch>
2015-03-03 22:23:51 +0100

    Missing branch alias for FxFx plot

--- bin/MadGraph5_aMCatNLO/macros/plotdjr.C
+++ bin/MadGraph5_aMCatNLO/macros/plotdjr.C
@@ -129,7 +129,8 @@ void plotdjr(const TString & infile, const TString & outfile) {
   tree->Add(infile);
   
   tree->SetAlias("GenEvent","GenEventInfoProduct_generator__GEN.obj");
-  
+  tree->SetAlias("LHEEvent","LHEEventProduct_externalLHEProducer__GEN.obj");
+ 
   TCut weight = "GenEvent.weight()";
   int nbins = 50.;
   double djrmin = -0.5;
