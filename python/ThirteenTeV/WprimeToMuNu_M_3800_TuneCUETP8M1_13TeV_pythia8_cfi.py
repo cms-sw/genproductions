97b4b36431416e601c6144b3ef1b0c2291a0b3c3
David Sheffield <sheffield@physics.rutgers.edu>
2015-03-17 21:17:36 +0100

    Fragments for EXO-RunIIWinter15GS WprimteToLNu and heavy WToLNu

---
+++ python/ThirteenTeV/WprimeToMuNu_M_3800_TuneCUETP8M1_13TeV_pythia8_cfi.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import * 
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import * 
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         comEnergy = cms.double(13000.0),
+                         crossSection = cms.untracked.double(0.002683),
+                         filterEfficiency = cms.untracked.double(1),
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'Main:timesAllowErrors = 10000',
+            'ParticleDecays:limitTau0 = on',
+            'ParticleDecays:tauMax = 10',
+            'Tune:ee 3',
+            'Tune:pp 5',
+            'NewGaugeBoson:ffbar2Wprime = on',
+            '34:m0 = 3800',
+            '34:onMode = off',
+            '34:onIfAny = 13,14',
+            ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters')
+        )
+                         )
+ProductionFilterSequence = cms.Sequence(generator)
---
+++ python/ThirteenTeV/WprimeToTauNu_M_3600_TuneCUETP8M1_tauola_13TeV_pythia8_cfi.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import * 
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         comEnergy = cms.double(13000.0),
+                         crossSection = cms.untracked.double(0.003904),
+                         filterEfficiency = cms.untracked.double(1),
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         ExternalDecays = cms.PSet(Tauola = cms.untracked.PSet(TauolaPolar, TauolaDefaultInputCards ),
+                                                   parameterSets = cms.vstring('Tauola')
+                                                   ),
+                         PythiaParameters = cms.PSet(        
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'Main:timesAllowErrors = 10000',
+            #'ParticleDecays:limitTau0 = on',
+            #'ParticleDecays:tauMax = 10',
+            'Tune:ee 3',
+            'Tune:pp 5',
+            'NewGaugeBoson:ffbar2Wprime = on',
+            '34:m0 = 3600',
+            '34:onMode = off',
+            '34:onIfAny = 15,16',
+            ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters')       
+        )
+                         )
+#ProductionFilterSequence = cms.Sequence(generator)
