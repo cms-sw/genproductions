8842ed017f01aa2cc796c5ef5fdace13ea259516
Philip <violatingcp@gmail.com>
2015-04-21 03:08:42 +0200

    fixed LHAPDF

--- bin/MCFM/runcmsgrid_template.sh
+++ bin/MCFM/runcmsgrid_template.sh
@@ -13,13 +13,25 @@ LHEWORKDIR=`pwd`
 basename=BASENAME
 
 cd $LHEWORKDIR/$basename
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/lhapdf/6.1.5/lib/
-export LHAPATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/lhapdf/6.1.5/share/LHAPDF/
+
+LHAPDFCONFIG=`echo "$LHAPDF_DATA_PATH/../../bin/lhapdf-config"`
+#if lhapdf6 external is available then above points to lhapdf5 and needs to be overridden
+LHAPDF6TOOLFILE=$CMSSW_BASE/config/toolbox/$SCRAM_ARCH/tools/available/lhapdf6.xml
+if [ -e $LHAPDF6TOOLFILE ]; then
+  LHAPDFCONFIG=`cat $LHAPDF6TOOLFILE | grep "<environment name=\"LHAPDF6_BASE\"" | cut -d \" -f 4`/bin/lhapdf-config
+fi
+#make sure env variable for pdfsets points to the right place
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`$LHAPDFCONFIG --libdir`
+export LHAPDF_DATA_PATH=`$LHAPDFCONFIG --datadir`
+export LHAPATH=$LHADPDF_DATA_PATH
+
+
 sed "s@EVTS@$nevt@g" input_gen.DAT > input_g0.DAT
 sed "s@XRAN@$rnum@g" input_g0.DAT  > input.DAT
 ./mcfm
 #Replace the negative so pythia will work
-sed "s@-1000022@1000022@g" FILENAME > $LHEWORKDIR/cmsgrid_final.lhe
+#sed "s@-1000022@1000022@g" FILENAME > $LHEWORKDIR/cmsgrid_final.lhe
+mv FILENAME $LHEWORKDIR/cmsgrid_final.lhe
 cd $LHEWORKDIR
 
 ls -l
