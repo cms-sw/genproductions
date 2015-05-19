bda20787b603f0a5327b8dd4f4224cc59a23c3b4
David Sheffield <sheffield@physics.rutgers.edu>
2015-05-20 01:37:54 +0200

    Removed unecessary lines

--- python/ThirteenTeV/WToTauNu_M_500_TuneCUETP8M1_13TeV_pythia8_cfi.py
+++ python/ThirteenTeV/WToTauNu_M_500_TuneCUETP8M1_13TeV_pythia8_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
 from Configuration.Generator.Pythia8CommonSettings_cfi import * 
 from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import * 
 
@@ -11,22 +10,10 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
                          maxEventsToPrint = cms.untracked.int32(0),
                          pythiaHepMCVerbosity = cms.untracked.bool(False),
                          pythiaPylistVerbosity = cms.untracked.int32(1),
-#                         ExternalDecays = cms.PSet(
-#        Tauola = cms.untracked.PSet(
-#            TauolaPolar,
-#            TauolaDefaultInputCards
-#            ),
-#        parameterSets = cms.vstring('Tauola')
-#        ),
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
         pythia8CUEP8M1SettingsBlock,
         processParameters = cms.vstring(
-            'Main:timesAllowErrors = 10000',
-            'ParticleDecays:limitTau0 = on',
-            'ParticleDecays:tauMax = 10',
-            'Tune:ee 3',
-            'Tune:pp 5',
             'WeakSingleBoson:ffbar2W = on',
             '24:onMode = off',
             '24:onIfAny = 15,16',
