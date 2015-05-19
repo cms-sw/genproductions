2196c0d6309522d86cc8f77604d1f58d9fc7355a
David Sheffield <sheffield@physics.rutgers.edu>
2015-05-19 20:29:29 +0200

    Fragments for EXO-RunIIWinter15GS Heavy WToTauNu without tauola From @bdelacruz

---
+++ python/ThirteenTeV/WToTauNu_M_500_TuneCUETP8M1_13TeV_pythia8_cfi.py
@@ -0,0 +1,41 @@
+import FWCore.ParameterSet.Config as cms
+
+#from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+from Configuration.Generator.Pythia8CommonSettings_cfi import * 
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import * 
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         comEnergy = cms.double(13000.0),
+                         crossSection = cms.untracked.double(2.2254e-1),
+                         filterEfficiency = cms.untracked.double(1),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+#                         ExternalDecays = cms.PSet(
+#        Tauola = cms.untracked.PSet(
+#            TauolaPolar,
+#            TauolaDefaultInputCards
+#            ),
+#        parameterSets = cms.vstring('Tauola')
+#        ),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'Main:timesAllowErrors = 10000',
+            'ParticleDecays:limitTau0 = on',
+            'ParticleDecays:tauMax = 10',
+            'Tune:ee 3',
+            'Tune:pp 5',
+            'WeakSingleBoson:ffbar2W = on',
+            '24:onMode = off',
+            '24:onIfAny = 15,16',
+            '24:mMin = 500',
+            ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters')
+        )
+                         )
+ProductionFilterSequence = cms.Sequence(generator)
