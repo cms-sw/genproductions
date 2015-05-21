7bc80553ee4fa4481503ecae45f878363736fea7
alessio <alessio.magitteri@cern.ch>
2015-05-21 15:40:26 +0200

    Hadronizer for H to ZZ->2l2nu, the decay of the Higgs is done by Pythia8, Higgs mass 400 GeV

---
+++ python/ThirteenTeV/Higgs/Hadronizer_TuneCUETP8M1_13TeV_powhegEmissionVeto_3p_H_ZZ_2l2nu_VBF_M400GeV_LepDECAY_LHE_pythia8_cff.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(13000.),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        pythia8PowhegEmissionVetoSettingsBlock,
+        processParameters = cms.vstring(
+            'POWHEG:nFinal = 3',   ## Number of final state particles
+                                   ## (BEFORE THE DECAYS) in the LHE
+                                   ## other than emitted extra parton
+            '25:m0 = 400.0'         # Higgs Mass
+            '25:onMode = off',      # turn OFF all h2 decays
+            '25:onIfMatch = 23 23',      # turn ON h2->ZZ
+            '23:onMode = off',      # turn OFF all Z decays
+            '23:onIfAny = 11 13 12 14 16', # turn ON Z->ll
+          ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'pythia8PowhegEmissionVetoSettings',
+                                    'processParameters'
+                                    )
+        )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
