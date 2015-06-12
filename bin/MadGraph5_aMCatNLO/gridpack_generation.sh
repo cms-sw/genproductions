cms-sw/genproductions

commit 6189f1d362e17b06933691b4287ba26c21f05dde

update gridpack_generation.sh in order to get EffDM_222_restricted.tar model

bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -140,6 +140,10 @@ ZPTOVLQSOURCE=https://cms-project-generators.web.cern.ch/cms-project-generators/
 TGAMMAMODEL=tqAandG_UFO.zip
 TGAMMASOURCE=https://cms-project-generators.web.cern.ch/cms-project-generators/${TGAMMAMODEL}
 
+#ttDM EFT model, needed for ttDM production
+EFFDM=EffDM_222_restricted.tar
+EFFDMSOURCE= /afs/cern.ch/cms/generators/www/
+
 MGBASEDIRORIG=MG5_aMC_v2_2_2
 
 isscratchspace=0

commit 7f17502a83576eec64db490d8fcf816e3f576039

fix bug introduced in 6189f1d362e17b06933691b4287ba26c21f05dde

bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -142,7 +142,7 @@ TGAMMASOURCE=https://cms-project-generators.web.cern.ch/cms-project-generators/$
 
 #ttDM EFT model, needed for ttDM production
 EFFDM=EffDM_222_restricted.tar
-EFFDMSOURCE= /afs/cern.ch/cms/generators/www/
+EFFDMSOURCE=/afs/cern.ch/cms/generators/www/
 
 MGBASEDIRORIG=MG5_aMC_v2_2_2
 