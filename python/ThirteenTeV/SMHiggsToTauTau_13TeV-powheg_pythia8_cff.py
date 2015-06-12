7c47a67229f185eb3affd21d1518bebdd536de71
Yuta <Yuta.Takahashi@cern.ch>
2015-06-12 20:44:56 +0200

    add SM H->tautau hadronization fragment

---
+++ python/ThirteenTeV/SMHiggsToTauTau_13TeV-powheg_pythia8_cff.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(13000.),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        pythia8PowhegEmissionVetoSettingsBlock,      
+        processParameters = cms.vstring(
+            'POWHEG:nFinal = 1',
+            'Main:timesAllowErrors = 10000',
+            'ParticleDecays:limitTau0 = on',
+            'ParticleDecays:tau0Max = 10',
+            '25:onMode = off', # turn OFF all H decays
+            '25:onIfAny = 15', # turn ON H->tautau      
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'pythia8PowhegEmissionVetoSettings',
+                                    'processParameters'
+                                    )
+        )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
