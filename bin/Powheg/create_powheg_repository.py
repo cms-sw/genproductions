ff9f2f9d7640a35ce2d524dd3d4db6c7ea087391
perrozzi <perrozzi@cern.ch>
2015-06-29 12:45:00 +0200

    comply with standard naming and modify create_powheg_tarball.sh to cope with compressed process folders

--- bin/Powheg/create_powheg_repository.py
+++ bin/Powheg/create_powheg_repository.py
@@ -4,16 +4,17 @@ process_list = ["HZJ","HWJ"] # add as many processes as required
 
 print "downloading POWHEG-BOX-V2"
 os.system("svn checkout --username anonymous --password anonymous svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX-V2")
+os.system("mv POWHEG-BOX-V2 POWHEG-BOX")
 
-os.chdir("POWHEG-BOX-V2")
+os.chdir("POWHEG-BOX")
 
 for process in process_list:
   
   print "downloading "+str(process)
   os.system("svn co --username anonymous --password anonymous svn://powhegbox.mib.infn.it/trunk/User-Processes-V2/"+str(process))
-  os.system("tar cvzf "+str(process)+".tgz "+str(process))
+  os.system("tar cvzf "+str(process)+".tar.gz "+str(process))
   os.system("rm -rf "+str(process))
   
 os.chdir("../")
 print "compressing the repository"
-os.system("tar cvzf POWHEG-BOX-V2.tgz POWHEG-BOX-V2")
+os.system("tar cvzf POWHEG-BOX.tar.gz POWHEG-BOX")
