9b0a1eaec2c0f93916880fead69ed0e054ddcfbd
chayanit <chayanit.asawatangtrakuldee@cern.ch>
2015-07-08 17:13:14 +0200

    hadronizer for LFV Higgs to mutau,etau,emu

---
+++ python/ThirteenTeV/Higgs/Hadronizer_TuneCUETP8M1_13TeV_powhegEmissionVeto_3p_LFV_HToETau_M-125_LHE_pythia8_cff.py
@@ -0,0 +1,46 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                       	comEnergy = cms.double(13000.0),
+                       	filterEfficiency = cms.untracked.double(1),
+                       	maxEventsToPrint = cms.untracked.int32(1),
+                       	pythiaHepMCVerbosity = cms.untracked.bool(False),
+                       	pythiaPylistVerbosity = cms.untracked.int32(1),
+                       	PythiaParameters = cms.PSet(
+      					pythia8CommonSettingsBlock,
+      					pythia8CUEP8M1SettingsBlock,
+      					pythia8PowhegEmissionVetoSettings = cms.vstring(
+         					'POWHEG:veto = 1',
+         					'POWHEG:pTdef = 1',
+         					'POWHEG:emitted = 0',
+         					'POWHEG:pTemt = 0',
+         					'POWHEG:pThard = 0',
+         					'POWHEG:vetoCount = 100',
+         					'SpaceShower:pTmaxMatch = 2',
+         					'TimeShower:pTmaxMatch = 2',
+      						),
+      					processParameters = cms.vstring(
+                             			'POWHEG:nFinal = 3',                            
+          					'TauDecays:mode = 2',
+          					'TauDecays:tauPolarization = 0',
+          					'TauDecays:tauMother = 25',
+          					'6:m0 = 172.04',
+          					'25:m0 = 125.0',
+          					'25:addChannel 1 0.1 100 15 -11',
+          					'25:addChannel 1 0.1 100 11 -15',
+          					'25:onMode = off',
+          					'25:onIfMatch 15 11'
+          					),
+      					parameterSets = cms.vstring(
+                           			'pythia8PowhegEmissionVetoSettings',
+                            			'pythia8CommonSettings',
+                            			'pythia8CUEP8M1Settings',
+                            			'processParameters',
+                            			)
+      					)
+			)
+
+ProductionFilterSequence = cms.Sequence(generator)
