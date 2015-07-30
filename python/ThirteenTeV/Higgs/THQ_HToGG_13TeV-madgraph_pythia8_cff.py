de71a46ab62fefd05d97b38021393c68db7080d0
Pietro <pietro.govoni@cern.ch>
2015-07-30 10:55:38 +0200

    first try

--- python/ThirteenTeV/Higgs/THQ_HToGG_13TeV-madgraph_pythia8_cff.py
+++ python/ThirteenTeV/Higgs/THQ_HToGG_13TeV-madgraph_pythia8_cff.py
@@ -1,22 +1,22 @@
 generator = cms.EDFilter("Pythia8HadronizerFilter",
-	maxEventsToPrint = cms.untracked.int32(1),
-	pythiaPylistVerbosity = cms.untracked.int32(1),
-	filterEfficiency = cms.untracked.double(1.0),
-	pythiaHepMCVerbosity = cms.untracked.bool(False),
-	comEnergy = cms.double(13000.),
-	PythiaParameters = cms.PSet(
-		pythia8CommonSettingsBlock,
-		pythia8CUEP8M1SettingsBlock,
-		parameterSets = cms.vstring(
-			'pythia8CommonSettings',
-			'pythia8CUEP8M1Settings'
-			'25:m0 = 125.0',
-			'23:mMin = 0.05',       # Solve problem with mZ cut
-			'24:mMin = 0.05',       # Solve problem with mW cut
-			'25:onMode = off', 
-			'25:onIfAny = 22 22',    # Decay only higgs to gamma gamma
-		)
-	)
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings'
+            '25:m0 = 125.0',
+            '23:mMin = 0.05',       # Solve problem with mZ cut
+            '24:mMin = 0.05',       # Solve problem with mW cut
+            '25:onMode = off', 
+            '25:onIfAny = 22 22',    # Decay only higgs to gamma gamma
+        )
+    )
 )
 
 
