01413c205e913e7bfa9c9844fad193096289a257
Roberto <roberto.covarelli@cern.ch>
2015-08-24 14:36:15 +0200

    up to 2 bsm models and keep local running possibility

--- bin/MadGraph5_aMCatNLO/submit_gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/submit_gridpack_generation.sh
@@ -7,5 +7,6 @@ name=${4}
 carddir=${5}
 workqueue=${6}
 model=${7}
+model2=${8}
 
-bsub -q ${masterqueue} -C 0  -R "rusage[mem=${memory}:pool=${diskspace}]" "export PRODHOME=`pwd`; gridpack_generation.sh ${name} ${carddir} ${workqueue} ${model}"
+bsub -q ${masterqueue} -C 0  -R "rusage[mem=${memory}:pool=${diskspace}]" "export PRODHOME=`pwd`; gridpack_generation.sh ${name} ${carddir} ${workqueue} ${model} ${model2}"
