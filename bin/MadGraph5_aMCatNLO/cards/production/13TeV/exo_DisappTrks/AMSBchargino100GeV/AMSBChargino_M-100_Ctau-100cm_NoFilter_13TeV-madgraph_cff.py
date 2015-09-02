4deb3d57719391de54a3ef519f019a2b076ffe51
Wells Wulsin <wulsin.1@osu.edu>
2015-09-02 14:44:39 +0200

    Define slha file inline

--- bin/MadGraph5_aMCatNLO/cards/production/13TeV/exo_DisappTrks/AMSBchargino100GeV/AMSBChargino_M-100_Ctau-100cm_NoFilter_13TeV-madgraph_cff.py
+++ bin/MadGraph5_aMCatNLO/cards/production/13TeV/exo_DisappTrks/AMSBchargino100GeV/AMSBChargino_M-100_Ctau-100cm_NoFilter_13TeV-madgraph_cff.py
@@ -6,6 +6,45 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
 
+SLHA_TABLE = """# This file is read by SimG4Core/CustomPhysics/src/CustomParticleFactory.cc  
+# The strings "decay", "pdg code", and "block", with correct capitalization, are used 
+# to control the data input, so do not use these in any comments.  
+# 
+# 
+# Get values for chargino and neutralino masses from:  
+# ../data/AMSB_chargino_100GeV_Isajet780.slha
+BLOCK MASS   
+#  PDG code   mass   particle 
+   1000022   9.98443985E+01   # ~neutralino(1) 
+   1000024   1.00032974E+02   # ~chargino(1)+  
+  -1000024   1.00032974E+02   # ~chargino(1)-  
+Block 
+
+
+
+# Set chargino lifetime 
+# and decay:  chargino -> neutralino + pion 
+# chargino ctau  = 100 cm 
+# chargino  tau  = 3.33564604793 ns 
+# chargino width = 1.97326691904e-16 GeV 
+#       PDG       Width               #
+DECAY  1000024  1.97326691904e-16 # +chargino decay  
+#   BR       NDA      ID1      ID2  
+   1.0000    2     1000022    211   
+Block 
+
+
+#       PDG       Width               #
+DECAY  -1000024  1.97326691904e-16 # -chargino decay  
+#   BR       NDA      ID1      ID2  
+   1.0000    2     1000022    -211  
+Block 
+
+
+EOF 
+"""
+
+
 generator = cms.EDFilter("Pythia8HadronizerFilter",
     maxEventsToPrint = cms.untracked.int32(1),
     pythiaPylistVerbosity = cms.untracked.int32(1),
@@ -40,7 +79,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
     useregge = cms.bool(False), 
     hscpFlavor = cms.untracked.string('stau'),          
     massPoint = cms.untracked.int32(100),  # value not used 
-    particleFile = cms.untracked.string('geant4_AMSB_chargino_100GeV_ctau100cm.slha')
+    particleFile = cms.untracked.string('%s' % SLHA_TABLE), 
 )
 
 
