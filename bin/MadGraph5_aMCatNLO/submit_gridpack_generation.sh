059cafa459c40211aab2f5f3fcf43e60b8e2c99e
Josh Bendavid <Josh.Bendavid@cern.ch>
2015-10-04 01:40:32 +0200

    cleanup unused parameters related to extra models

--- bin/MadGraph5_aMCatNLO/submit_gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/submit_gridpack_generation.sh
@@ -6,7 +6,5 @@ masterqueue=${3}
 name=${4}
 carddir=${5}
 workqueue=${6}
-model=${7}
-model2=${8}
 
-bsub -q ${masterqueue} -C 0  -R "rusage[mem=${memory}:pool=${diskspace}]" "export PRODHOME=`pwd`; gridpack_generation.sh ${name} ${carddir} ${workqueue} ${model} ${model2}"
+bsub -q ${masterqueue} -C 0  -R "rusage[mem=${memory}:pool=${diskspace}]" "export PRODHOME=`pwd`; gridpack_generation.sh ${name} ${carddir} ${workqueue}"
--- bin/MadGraph5_aMCatNLO/submit_gridpack_generation_local.sh
+++ bin/MadGraph5_aMCatNLO/submit_gridpack_generation_local.sh
@@ -5,7 +5,5 @@ masterqueue=${2}
 name=${3}
 carddir=${4}
 workqueue=${5}
-model=${6}
-model2=${7}
 
-bsub -q ${masterqueue} -C 0  -R "rusage[mem=${memory}]" "cd `pwd`; gridpack_generation.sh ${name} ${carddir} ${workqueue} ${model} ${model2}"
+bsub -q ${masterqueue} -C 0  -R "rusage[mem=${memory}]" "cd `pwd`; gridpack_generation.sh ${name} ${carddir} ${workqueue}"
