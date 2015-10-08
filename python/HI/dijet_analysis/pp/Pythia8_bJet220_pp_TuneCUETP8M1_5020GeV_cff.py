36b2e3462634d8db9ca62746f286e4a803d339b1
Yetkin <yetkin.yilmaz@cern.ch>
2015-10-08 17:58:03 +0200

    b-jets in Pythia8

---
+++ python/HI/dijet_analysis/pp/Pythia8_bJet220_pp_TuneCUETP8M1_5020GeV_cff.py
@@ -0,0 +1,47 @@
+import FWCore.ParameterSet.Config as cms
+
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CUEP8M1Settings', 
+            'processParameters'),
+        processParameters = cms.vstring('HardQCD:all = on', 
+            'PhaseSpace:pTHatMin = 220.', 
+            'PhaseSpace:pTHatMax = 9999.'),
+        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:pT0Ref=2.4024', 
+            'MultipartonInteractions:ecmPow=0.25208', 
+            'MultipartonInteractions:expPow=1.6'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on')
+    ),
+    comEnergy = cms.double(5020.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0)
+)
+
+configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('PYTHIA 8 (unquenched) dijets in NN (pt-hat > 220 GeV) at sqrt(s) = 5.02 TeV')
+    )
+
+bfilter = cms.EDFilter("MCSingleParticleFilter",
+                       MaxEta     = cms.untracked.vdouble(3.0, 3.0),
+                       MinEta     = cms.untracked.vdouble(-3.0, -3.0),
+                       MinPt      = cms.untracked.vdouble(10.0, 10.0),
+                       ParticleID = cms.untracked.vint32(5, -5)
+                       )
+
+ProductionFilterSequence = cms.Sequence(generator*bfilter)
+
+
--- python/HI/dijet_analysis/pp/generateDijets.sh
+++ python/HI/dijet_analysis/pp/generateDijets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for template in Pythia6_bJetTemplate_pp_TuneZ2_5020GeV_cff Pythia6_DijetTemplate_pp_TuneZ2_5020GeV_cff Pythia8_DijetTemplate_pp_TuneCUETP8M1_5020GeV_cff
+for template in Pythia6_bJetTemplate_pp_TuneZ2_5020GeV_cff Pythia6_DijetTemplate_pp_TuneZ2_5020GeV_cff Pythia8_DijetTemplate_pp_TuneCUETP8M1_5020GeV_cff Pythia8_bJetTemplate_pp_TuneCUETP8M1_5020GeV_cff
 do
     for pthat in 15 30 50 80 90 100 110 120 170 200 220 250 280 300 370 460 540
     do
