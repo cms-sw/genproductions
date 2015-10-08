f21097eb329ffc02af141dad9451803719767308
Yetkin <yetkin.yilmaz@cern.ch>
2015-10-08 17:52:23 +0200

    Pythia8 dijet cffs at 5.02 TeV

---
+++ python/HI/dijet_analysis/pp/Pythia8_Dijet100_pp_TuneCUETP8M1_5020GeV_cff.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CUEP8M1Settings', 
+            'processParameters'),
+        processParameters = cms.vstring('HardQCD:all = on', 
+            'PhaseSpace:pTHatMin = 100.', 
+            'PhaseSpace:pTHatMax = 9999.'),
+        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:pT0Ref=2.4024', 
+            'MultipartonInteractions:ecmPow=0.25208', 
+            'MultipartonInteractions:expPow=1.6'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on')
+    ),
+    comEnergy = cms.double(5020.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0)
+)
+
+configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('PYTHIA 8 (unquenched) dijets in NN (pt-hat > 100 GeV) at sqrt(s) = 5.02 TeV')
+    )
+
+ProductionFilterSequence = cms.Sequence(generator)
