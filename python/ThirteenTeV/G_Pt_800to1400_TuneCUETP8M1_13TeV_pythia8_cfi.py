31bf3c3375284ca459af73f9cec93cf8f0cf6518
David <dnash@cern.ch>
2015-10-09 13:57:07 +0200

    Cards from Varun

---
+++ python/ThirteenTeV/G_Pt_800to1400_TuneCUETP8M1_13TeV_pythia8_cfi.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                  comEnergy = cms.double(13000.0),
+                  crossSection = cms.untracked.double(6.075e-02),
+                  filterEfficiency = cms.untracked.double(1.0),
+                  maxEventsToPrint = cms.untracked.int32(0),
+                  pythiaHepMCVerbosity = cms.untracked.bool(False),
+                  pythiaPylistVerbosity = cms.untracked.int32(0),
+
+		  PythiaParameters = cms.PSet(
+		        pythia8CommonSettingsBlock,
+			pythia8CUEP8M1SettingsBlock,
+                        processParameters = cms.vstring(
+                            'PromptPhoton:all = on',
+			    'PhaseSpace:pTHatMin = 800',
+			    'PhaseSpace:pTHatMax = 1400',
+			    ),
+                        parameterSets = cms.vstring(
+			    'pythia8CommonSettings',
+                            'pythia8CUEP8M1Settings',
+			    'processParameters',
+			    )
+                  )
+)
+ProductionFilterSequence = cms.Sequence(generator)
+
+configurationMetadata = cms.untracked.PSet(
+        version = cms.untracked.string('$Revision$'),
+	name = cms.untracked.string('$Source$'),
+	annotation = cms.untracked.string('QCD GJ, pthat 800to1400 GeV, 13 TeV, TuneCUETP8M1')
+)
