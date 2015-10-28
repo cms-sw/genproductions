d751bab7d102fe8cecb58538faf8cdab639c3b6a
Yetkin <yetkin.yilmaz@cern.ch>
2015-10-28 17:45:32 +0100

    z events with no pthat cut

---
+++ python/HI/zjet_analysis/pp/Pythia8_Zee_eta2p5_TuneCUETP8M1_5020GeV_cff.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         #pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         # cross section: 20-30 == 326.7, 30-50 == 227.0, 50-80 == 93.17,
+                         # 80-120 == 31.48, 120-170 == 9.63, 170-230 == 2.92, 230-300 == 0.8852
+                         # crossSection = cms.untracked.double(691.7852),
+                         #
+                         # at 10 TeV it scales down to 426
+                         #
+                         crossSection = cms.untracked.double(425.6),
+                         comEnergy = cms.double(5020.0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'WeakSingleBoson:ffbar2gmZ = on',
+            '23:onMode = off',
+            '23:onIfAny = 11',
+            'PhaseSpace:mHatMin = 75.',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+
+mumugenfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MinPt = cms.untracked.vdouble(10, 10),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    ParticleCharge = cms.untracked.int32(-1),
+    ParticleID1 = cms.untracked.vint32(11),
+    ParticleID2 = cms.untracked.vint32(11)
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
