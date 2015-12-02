9c755472b7a901c2860a67a19954e91bfe89ffbf
Yetkin <yetkin.yilmaz@cern.ch>
2015-12-02 12:38:44 +0100

    dijet cffs changed to properly include the common blocks for Pythia8CUEP8M1 settings

--- python/HI/dijet_analysis/pp/Pythia8_bJet110_pp_TuneCUETP8M1_5020GeV_cff.py
+++ python/HI/dijet_analysis/pp/Pythia8_bJet110_pp_TuneCUETP8M1_5020GeV_cff.py
@@ -1,28 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 
-process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
     PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
         parameterSets = cms.vstring('pythia8CommonSettings', 
             'pythia8CUEP8M1Settings', 
             'processParameters'),
         processParameters = cms.vstring('HardQCD:all = on', 
             'PhaseSpace:pTHatMin = 110.', 
             'PhaseSpace:pTHatMax = 9999.'),
-        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
-            'Tune:ee 7', 
-            'MultipartonInteractions:pT0Ref=2.4024', 
-            'MultipartonInteractions:ecmPow=0.25208', 
-            'MultipartonInteractions:expPow=1.6'),
-        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
-            'Main:timesAllowErrors = 10000', 
-            'Check:epTolErr = 0.01', 
-            'Beams:setProductionScalesFromLHEF = off', 
-            'SLHA:keepSM = on', 
-            'SLHA:minMassSM = 1000.', 
-            'ParticleDecays:limitTau0 = on', 
-            'ParticleDecays:tau0Max = 10', 
-            'ParticleDecays:allowPhotonRadiation = on')
     ),
     comEnergy = cms.double(5020.0),
     filterEfficiency = cms.untracked.double(1.0),
