1542eb6495e4fd6fc53c2588ceb43493d35a6a63
Olivier Bondu <olivier.bondu@gmail.com>
2015-12-08 14:31:26 +0100

    bbH hadronization fragment

---
+++ python/ThirteenTeV/Higgs/Hadronizer_TuneCUETP8M1_13TeV_aMCatNLO_bbHToBB_M-125_4FS_LHE_pythia8_cff.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(13000.),
+                         PythiaParameters = cms.PSet(
+                            pythia8CommonSettingsBlock,
+                            pythia8CUEP8M1SettingsBlock,
+                            pythia8aMCatNLOSettingsBlock,
+                            processParameters = cms.vstring(
+                                'TimeShower:nPartonsInBorn = 2', #number of coloured particles (before resonance decays) in born matrix element
+                                'SLHA:useDecayTable = off',
+                                'JetMatching:nQmatch = 5', #4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
+                                '25:m0 = 125.0',
+                                '25:onMode = off',
+                                '25:onIfMatch = 5 -5',
+                                ),
+                            parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'pythia8aMCatNLOSettings',
+                                    'processParameters',
+                                    )
+                            )
+                         )
+
+
+
+
+
+
