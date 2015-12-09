f214c9d7ef8125db27fc9691d40a51bda2c0e7b3
David Sheffield <sheffield@physics.rutgers.edu>
2015-12-09 02:01:56 +0100

    Fragments for WToLNu and WprimeToLNu for EXO-Summer15GS

---
+++ python/ThirteenTeV/WToMuNu_M_5000_TuneCUETP8M1_13TeV_pythia8_cff.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import * 
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         comEnergy = cms.double(13000.0),
+                         crossSection = cms.untracked.double(2.045e-07),
+                         filterEfficiency = cms.untracked.double(1),
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'Main:timesAllowErrors = 10000',
+            'ParticleDecays:limitTau0 = on',
+            'ParticleDecays:tauMax = 10',
+            'Tune:ee 3',
+            'Tune:pp 5',
+            'WeakSingleBoson:ffbar2W = on',
+            '24:onMode = off',
+            '24:onIfAny = 13,14',
+            '24:mMin = 5000',
+            ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters')
+        )
+                         )
+ProductionFilterSequence = cms.Sequence(generator)
