abf7d847dd0ab26e6bf8d05106ca28d1e1620c45
Roberto <roberto.covarelli@cern.ch>
2016-01-21 16:33:19 +0100

    remove lhe manipulation

--- bin/JHUGen/runcmsgrid_template.sh
+++ bin/JHUGen/runcmsgrid_template.sh
@@ -14,15 +14,6 @@ eval `scramv1 runtime -sh`
 cd BASEDIR/
 
 GENCOMMAND
-file=Out.lhe
 
-head=`cat   $file | grep -in init | sed "s@:@ @g" | awk '{print $1-2}' | tail -1`
-tail=`wc -l $file | awk -v tmp="$head" '{print $1-2-tmp}'`
-
-tail -${tail} $file                                     >  ${file}_tail_dec
-sed "s@\*\*\*@1.000@g"     ${file}_tail_dec              > ${file}_tail
-cat ${file}_tail                                       >> cmsgrid.lhe
-rm ${file}
-rm ${file}_tail
-mv cmsgrid.lhe ../cmsgrid_final.lhe
+mv Out.lhe ../cmsgrid_final.lhe
 cd ..
