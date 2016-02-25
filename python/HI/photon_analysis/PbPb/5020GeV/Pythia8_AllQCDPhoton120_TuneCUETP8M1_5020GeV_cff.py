fbd3b20a486062c91f870a0d6c951836cf7dc77f
R. Alex Barbieri <luck@mit.edu>
2016-02-25 15:12:07 -0500

    fix egregious error which bypassed photon signal filter

--- python/HI/photon_analysis/PbPb/5020GeV/Pythia8_AllQCDPhoton120_TuneCUETP8M1_5020GeV_cff.py
+++ python/HI/photon_analysis/PbPb/5020GeV/Pythia8_AllQCDPhoton120_TuneCUETP8M1_5020GeV_cff.py
@@ -30,7 +30,7 @@ photonFilter = cms.EDFilter("PythiaFilterMultiMother",
                             Status = cms.untracked.int32(1),
                             MinPt = cms.untracked.double(15.0),
                             ParticleID = cms.untracked.int32(22),
-                            motherIDs = cms.untracked.vint32(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1)
+                            MotherIDs = cms.untracked.vint32(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1)
 )
 
 
