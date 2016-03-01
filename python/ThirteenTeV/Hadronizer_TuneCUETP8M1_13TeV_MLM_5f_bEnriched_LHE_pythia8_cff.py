c8328a6f8fe13a447b20b18873c62bbdcb2f2a6d
Clemens Lange <clemens.lange@cern.ch>
2016-03-01 11:39:30 +0100

    remove time shower max gamma cut

--- python/ThirteenTeV/Hadronizer_TuneCUETP8M1_13TeV_MLM_5f_bEnriched_LHE_pythia8_cff.py
+++ python/ThirteenTeV/Hadronizer_TuneCUETP8M1_13TeV_MLM_5f_bEnriched_LHE_pythia8_cff.py
@@ -24,7 +24,6 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
             'JetMatching:nQmatch = 5', #4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
             'JetMatching:nJetMax = 4', #number of partons in born matrix element for highest multiplicity
             'JetMatching:doShowerKt = off', #off for MLM matching, turn on for shower-kT matching
-            'TimeShower:mMaxGamma = 4.0',
         ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CUEP8M1Settings',
