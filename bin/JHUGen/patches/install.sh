f5dc3d31b238f2178bbd979d73040f400972f2e2
Heshy Roskes <jroskes1@jhu.edu>
2016-03-23 14:54:29 +0100

    JHUGen v6.9.5

--- bin/JHUGen/patches/install.sh
+++ bin/JHUGen/patches/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget --no-check-certificate http://cms-project-generators.web.cern.ch/cms-project-generators/slc6_amd64_gcc481/JHUGenerator.v6.8.5.tar.gz -O JHUGenerator.v6.8.5.tar.gz 
-tar xzvf JHUGenerator.v6.8.5.tar.gz 
+wget --no-check-certificate http://cms-project-generators.web.cern.ch/cms-project-generators/slc6_amd64_gcc481/JHUGenerator.v6.9.5.tar.gz -O JHUGenerator.v6.9.5.tar.gz
+tar xzvf JHUGenerator.v6.9.5.tar.gz
 rm -rf AnalyticMELA
 rm -rf JHUGenMELA
 cd JHUGenerator
 sed -i "s/UseLHAPDF=No/UseLHAPDF=Yes/" makefile
 make
-rm ../JHUGenerator.v6.8.5.tar.gz
+rm ../JHUGenerator.v6.9.5.tar.gz
 
