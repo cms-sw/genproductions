75fbc905d9fd2efb19721b8a74f99af40c132084
Andrey Pozdnyakov <andreypz@users.noreply.github.com>
2016-08-25 17:39:39 +0200

    Update HiggsToJPsiGammaToMuMuGamma_pythia8.py
    Removed duplicate line

--- python/ThirteenTeV/Higgs/HiggsToJPsiGammaToMuMuGamma_pythia8.py
+++ python/ThirteenTeV/Higgs/HiggsToJPsiGammaToMuMuGamma_pythia8.py
@@ -6,10 +6,9 @@ from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
 process.generator = cms.EDFilter("Pythia8GeneratorFilter",
                                  comEnergy = cms.double(13000.0),
                                  crossSection = cms.untracked.double(8.45E-6),
-                                 filterEfficiency = cms.untracked.double(1),
+                                 filterEfficiency = cms.untracked.double(1.0),
                                  maxEventsToPrint = cms.untracked.int32(1),
                                  pythiaPylistVerbosity = cms.untracked.int32(1),
-                                 filterEfficiency = cms.untracked.double(1.0),
                                  pythiaHepMCVerbosity = cms.untracked.bool(False),
                                  PythiaParameters = cms.PSet(
 
