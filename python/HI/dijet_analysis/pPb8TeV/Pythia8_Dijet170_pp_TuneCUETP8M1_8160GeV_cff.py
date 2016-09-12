024beac04323fbe5a03352d096812c718be56ed0
Marta <marta.verweij@cern.ch>
2016-09-12 16:54:19 +0200

    higher pt-hat bins for pPb 8 TeV dijet MC

---
+++ python/HI/dijet_analysis/pPb8TeV/Pythia8_Dijet170_pp_TuneCUETP8M1_8160GeV_cff.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+   	pythia8CUEP8M1SettingsBlock,
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CUEP8M1Settings', 
+            'processParameters'),
+        processParameters = cms.vstring('HardQCD:all = on', 
+            'PhaseSpace:pTHatMin = 170.', 
+            'PhaseSpace:pTHatMax = 9999.'),
+    ),
+    comEnergy = cms.double(8160.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0)
+)
+
+configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('PYTHIA 8 (unquenched) dijets in NN (pt-hat > 170 GeV) at sqrt(s) = 8.16 TeV')
+    )
+
+ProductionFilterSequence = cms.Sequence(generator)
