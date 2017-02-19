bf15fa02f02023f0cee9e3bdd4ad961b5665e7d8
Saptaparna Bhattacharya <saptaparna.bhattacharya@cern.ch>
2017-02-19 23:25:12 +0100

    Updated configs for heavy neutrino pair-production from Sungbin

---
+++ python/ThirteenTeV/WR5000_Zp2500_HN500_elel_cfg.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters'),
+        processParameters = cms.vstring('LeftRightSymmmetry:ffbar2ZR = on',
+            '9900024:m0 = 5000',
+            '9900023:m0 = 2500',
+            '9900012:m0 = 500',
+            '9900024:onMode = off',
+            '9900023:onMode = off',
+            '9900023:onIfAny = 9900012, 9900012',
+            '9900024:onIfAny = 11,9900012'),
+        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14',
+            'Tune:ee 7',
+            'MultipartonInteractions:pT0Ref=2.4024',
+            'MultipartonInteractions:ecmPow=0.25208',
+            'MultipartonInteractions:expPow=1.6'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2',
+            'Main:timesAllowErrors = 10000',
+            'Check:epTolErr = 0.01',
+            'Beams:setProductionScalesFromLHEF = off',
+            'SLHA:keepSM = on',
+            'SLHA:minMassSM = 1000.',
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on')
+    ),
+    comEnergy = cms.double(13000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False), 
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
