d19e2b10404d2bd937a09633481f7fe0ecab2c3d
perrozzi <perrozzi@cern.ch>
2017-03-13 08:51:55 +0100

    fix old version

--- test/calculateXSectionAndFilterEfficiency/calculateXSectionAndFilterEfficiency_non_existing_dataset.sh
+++ test/calculateXSectionAndFilterEfficiency/calculateXSectionAndFilterEfficiency_non_existing_dataset.sh
@@ -12,18 +12,4 @@
 
 cmsenv
 cmsDriver.py Configuration/Generator/python/MySample_cff.py -s GEN -n 10000 --conditions=auto:mc --customise Configuration/GenProduction/calculateXSectionAndFilterEfficiency.py
-e this command to obtain the cross section and filter efficiency
-
-# Adjust the MySample_cff.py file and the number of events to generate
-
-# To calculate the filter efficiency, in the output look for:
-
-# TrigReport ---------- Modules in Path: generation_step ------------
-# TrigReport  Trig Bit#    Visited     Passed     Failed      Error Name
-# TrigReport     1    0     100000     100000          0          0 generator
-# TrigReport     1    0     100000      25834      74166          0 oniafilter
-# TrigReport     1    0      25834      14286      11548          0 mumugenfilter
-
-cmsenv
-cmsDriver.py Configuration/Generator/python/MySample_cff.py -s GEN -n 10000 --conditions=auto:mc --customise Configuration/GenProduction/calculateXSectionAndFilterEfficiency.py
 
