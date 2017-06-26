74817d3e76d0edbb8f63867cd39867984f76d5d5
Markus Seidel <markus.seidel@cern.ch>
2017-06-26 15:17:35 +0200

    Randomized event order in multi gridpack, Powheg patch by authors

--- bin/Powheg/production/TT_NLO_dec_hdamp_Tune4_NNPDF30/multipack_ttb_nlo_dec.py
+++ bin/Powheg/production/TT_NLO_dec_hdamp_Tune4_NNPDF30/multipack_ttb_nlo_dec.py
@@ -10,7 +10,7 @@ def main():
     #configuration
     usage = 'usage: %prog [options]'
     parser = optparse.OptionParser(usage)
-    parser.add_option('--nevents', dest='nevents', help='Number of events [%default]', default=1,  type=int)
+    parser.add_option('--nevents', dest='nevents', help='Number of events [%default]', default=10,  type=int)
     parser.add_option('--seed', dest='seed', help='Random seed [%default]', default=1,  type=int)
     (opt, args) = parser.parse_args()
     
@@ -20,7 +20,7 @@ def main():
 
     samples = {}
     
-    gridpackpath = '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/powheg/V2/TT_NLO_dec_hdamp_Tune4_NNPDF30/'    
+    gridpackpath = '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/powheg/V2/TT_NLO_dec_hdamp_Tune4_NNPDF30/'
     samples['ttb_NLO_dec_e_e_NNPDF30_13TeV_ttb_NLO_dec.tgz']     = { 'br': lep*lep, 'enhance': 2.0 }
     samples['ttb_NLO_dec_e_mu_NNPDF30_13TeV_ttb_NLO_dec.tgz']    = { 'br': lep*lep, 'enhance': 2.0 }
     samples['ttb_NLO_dec_e_tau_NNPDF30_13TeV_ttb_NLO_dec.tgz']   = { 'br': lep*lep, 'enhance': 1.0 }
@@ -55,6 +55,8 @@ def main():
     
     print(samples)
     
+    events = []
+    
     with open('merged.lhe', 'w') as merged:
         firstSample = True
         for sample,data in samples.iteritems():
@@ -67,18 +69,30 @@ def main():
                 os.system('sed -i \'1,/<\/init>/d\' cmsgrid_final.lhe')
             with open('cmsgrid_final.lhe', 'r') as chunk:
                 hasWeights = False
+                event = []
                 for line in chunk:
-                    m = re.match('<wgt id=\'(\d*)\'>(.*)<\/wgt>', line)
-                    if m: #modify weight with 1/(enhancement factor)
-                        weight = float(m.group(2))/data['enhance']
-                        merged.write('<wgt id=\'%s\'>%s</wgt>\n'%(m.group(1), str(weight)))
-                        hasWeights = True
-                    elif '</event>' in line and hasWeights == False:
-                        merged.write('<rwgt>\n')
-                        merged.write('<wgt id=\'1001\'>%s</wgt>\n'%(str(1./data['enhance'])))
-                        merged.write('</rwgt>\n</event>\n')
+                    if '</event>' in line:
+                        if hasWeights == False:
+                            event.append('<rwgt>\n')
+                            event.append('<wgt id=\'1001\'>%s</wgt>\n'%(str(1./data['enhance'])))
+                            event.append('</rwgt>\n</event>\n')
+                        event.append(line)
+                        events.append(event)
+                        event = []
+                    elif '<event>' in line or len(event) > 0:
+                        m = re.match('<wgt id=\'(\d*)\'>(.*)<\/wgt>', line)
+                        if m: #modify weight with 1/(enhancement factor)
+                            weight = float(m.group(2))/data['enhance']
+                            event.append('<wgt id=\'%s\'>%s</wgt>\n'%(m.group(1), str(weight)))
+                            hasWeights = True
+                        else:
+                            event.append(line)
                     else:
                         merged.write(line)
+        random.shuffle(events)
+        for event in events:
+            for line in event:
+                merged.write(line)
     os.system('echo "</LesHouchesEvents>" >> merged.lhe')
     os.system('mv merged.lhe cmsgrid_final.lhe')
 
