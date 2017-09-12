af9476719462e3bcd8bfab705a3e912afbfc9f97
Kenneth Long <kdlong@hep.wisc.edu>
2017-09-12 12:17:11 +0200

    Use find rather than relying on git status

--- bin/MadGraph5_aMCatNLO/Utilities/scripts/copyOldCardsTo2017.sh
+++ bin/MadGraph5_aMCatNLO/Utilities/scripts/copyOldCardsTo2017.sh
@@ -8,36 +8,28 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-git diff-index --quiet HEAD -- cards/production
-if [ $? -ne 0 ]; then
-    echo "This script has to be run from a clean git area. "
-    echo "If you've made other changes, commit them. If you've "
-    echo "changed things but didn't mean to, run 'git reset HEAD' "
-    echo "to get back where you started"
-    exit 1
-fi
+base_folder=$(git rev-parse --show-toplevel)/bin/MadGraph5_aMCatNLO
 
-base_folder=$(git rev-parse --show-toplevel)
+old_cards_path=cards/production/13TeV/$1
+new_cards_path=cards/production/2017/$1
+git checkout tags/pre2017 -- $old_cards_path
 
-git checkout tags/pre2017 -- cards/production/13TeV/$1
-
-for old_path in $(git status --porcelain -uno cards/production); do 
-    old_card=$base_folder/$old_path
-    if [ -f $old_card ]; then 
-        new_card=${old_card/13TeV/2017}
-        card_dir=$(dirname $new_card)
-        if [ ! -d $card_dir ]; then
-            mkdir -p $card_dir
-        fi
-        mv $old_card $new_card
-        git add $new_card
-        git rm $old_card
-    fi 
+for old_card in $(find $old_cards_path -type f -follow -print); do 
+    old_card=$base_folder/$old_card
+    echo $old_card
+    new_card=${old_card/13TeV/2017}
+    card_dir=$(dirname $new_card)
+    if [ ! -d $card_dir ]; then
+        mkdir -p $card_dir
+    fi
+    mv $old_card $new_card
+    git add $new_card
+    git rm $old_card
 done
 
 git commit -m "Copying $1 cards from legacy production to modify for 2017"
 
-for run_card in cards/production/2017/$1/*run_card.dat; do
+for run_card in $(find $new_cards_path -type f -follow -print -name "*run_card*"); do 
     sed -i "s/^ [0-9]* *= *lhaid/\$DEFAULT_PDF_SETS = lhaid/g" $run_card
     sed -i "s/.*= *reweight_PDF/\$DEFAULT_PDF_MEMBERS = reweight_PDF/g" $run_card
     sed -i "s/.*= *PDF_set_min//g" $run_card
