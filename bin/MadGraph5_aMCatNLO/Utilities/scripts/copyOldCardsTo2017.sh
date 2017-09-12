91fce4de0da92fb4f1b7b736dd14520c3c28fc0f
Kenneth Long <kdlong@hep.wisc.edu>
2017-09-12 12:39:19 +0200

    Not all cards will have reweight_PDF argument

--- bin/MadGraph5_aMCatNLO/Utilities/scripts/copyOldCardsTo2017.sh
+++ bin/MadGraph5_aMCatNLO/Utilities/scripts/copyOldCardsTo2017.sh
@@ -8,6 +8,15 @@ if [ -z $1 ]; then
     exit 1
 fi
 
+git diff-index --quiet HEAD -- cards/production
+if [ $? -ne 0 ]; then
+    echo "This script has to be run from a clean git area. "
+    echo "If you've made other changes, commit them. If you've "
+    echo "changed things but didn't mean to, run 'git reset HEAD' "
+    echo "to get back where you started"
+    exit 1
+fi
+
 base_folder=$(git rev-parse --show-toplevel)/bin/MadGraph5_aMCatNLO
 
 old_cards_path=cards/production/13TeV/$1
@@ -27,14 +36,17 @@ for old_card in $(find $old_cards_path -type f -follow -print); do
     git rm $old_card
 done
 
-git commit -m "Copying $1 cards from legacy production to modify for 2017"
+#git commit -m "Copying $1 cards from legacy production to modify for 2017"
 
 for run_card in $(find $new_cards_path -type f -follow -print -name "*run_card*"); do 
-    sed -i "s/^ [0-9]* *= *lhaid/\$DEFAULT_PDF_SETS = lhaid/g" $run_card
-    sed -i "s/.*= *reweight_PDF/\$DEFAULT_PDF_MEMBERS = reweight_PDF/g" $run_card
-    sed -i "s/.*= *PDF_set_min//g" $run_card
-    sed -i "s/.*= *PDF_set_max//g" $run_card
+    # reweight_PDF may not be present in older cards
+    if grep -q -e ".*= *reweight_PDF" $run_card; then
+        sed -i "s/^ [0-9]* *= *lhaid/\$DEFAULT_PDF_SETS = lhaid/g" $run_card
+        sed -i "s/.*= *reweight_PDF/\$DEFAULT_PDF_MEMBERS = reweight_PDF/g" $run_card
+    else
+        sed -i "s/^ [0-9]* *= *lhaid/\$DEFAULT_PDF_SETS = lhaid\n\$DEFAULT_PDF_MEMBERS = reweight_PDF/g" $run_card
+    fi
     git add $run_card
 done
 
-git commit -m "Updating PDF sets to 2017 defaults for $1"
+#git commit -m "Updating PDF sets to 2017 defaults for $1"
