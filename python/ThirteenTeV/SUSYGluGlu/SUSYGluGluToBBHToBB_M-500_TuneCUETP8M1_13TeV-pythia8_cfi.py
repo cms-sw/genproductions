15b94f0fec50446c395c9ef7b4af2a31af9893b8
chayanit <chayanit.asawatangtrakuldee@cern.ch>
2017-09-25 21:04:43 +0200

    update bbH, H->bb with narrow width

--- python/ThirteenTeV/SUSYGluGlu/SUSYGluGluToBBHToBB_M-500_TuneCUETP8M1_13TeV-pythia8_cfi.py
+++ python/ThirteenTeV/SUSYGluGlu/SUSYGluGluToBBHToBB_M-500_TuneCUETP8M1_13TeV-pythia8_cfi.py
@@ -7,19 +7,22 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
     filterEfficiency = cms.untracked.double(1.0),                                                                                
     pythiaHepMCVerbosity = cms.untracked.bool(False),                                                                            
     comEnergy = cms.double(13000.0),                                                                                             
-    crossSection = cms.untracked.double(518.3),                                                                                  
+    crossSection = cms.untracked.double(1.0),                                                                                  
     maxEventsToPrint = cms.untracked.int32(1),                                                                                   
     PythiaParameters = cms.PSet(                                                                                                 
             pythia8CommonSettingsBlock,                                                                                          
             pythia8CUEP8M1SettingsBlock, 
-                    processParameters = cms.vstring('Higgs:useBSM = on',                                                                     
-                                                    'HiggsBSM:gg2A3bbbar = on',                                                                   
-                                                    '36:m0 = 500',                                                                           
-                                                    '36:onMode = off',                                                                       
-                                                    '36:onIfMatch = 5 -5'                                                                        
-                                                                                                                                 
-            ),                                                                                                                   
-                                                                                                                                 
+                    processParameters = cms.vstring(                                                                     
+                                'Higgs:useBSM = on',
+                                'HiggsBSM:gg2A3bbbar = on',
+                                'HiggsA3:coup2d  = 20.0',
+            			'HiggsA3:coup2l  = 20.0',
+            			'HiggsA3:coup2u  = 0.05',
+            			'HiggsA3:coup2H1Z = 0.0',
+            			'HiggsA3:coup2H2Z = 1.0',
+                                '36:m0 = 500',                                                                           
+                                '36:onMode = off',                                                    
+                                '36:onIfMatch = 5 -5',                                                                                            	         ),                                                                                                                    
         parameterSets = cms.vstring('pythia8CommonSettings',                                                                     
             'pythia8CUEP8M1Settings',                                                                                            
             'processParameters')                                                                                                 
@@ -31,3 +34,5 @@ configurationMetadata = cms.untracked.PSet(
     name = cms.untracked.string('\$Source$'),
     annotation = cms.untracked.string('gg->bbA, 13TeV, mA = 500GeV, A->bb, TuneCUETP8M1')
 )
+
+
