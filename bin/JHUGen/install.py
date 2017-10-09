e860087480e00c4cb27f3d4fac792d45a31cb055
Heshy Roskes <jroskes1@jhu.edu>
2017-10-09 22:33:10 +0200

    clean up JHUGen install script

--- bin/JHUGen/install.py
+++ bin/JHUGen/install.py
@@ -7,12 +7,14 @@ import shutil
 import subprocess
 
 aparser = argparse.ArgumentParser(description='Process benchmarks.')
-aparser.add_argument('-card'       ,'--card'      ,action='store' ,dest='card',default='JHUGen.input'      ,help='card')
-aparser.add_argument('-decay-card' ,'--decay-card',action='store' ,dest='card2'  ,default=None             ,help='second input card to run for decay')
-aparser.add_argument('-name'       ,'--name'      ,action='store' ,dest='name'   ,required=True            ,help='name')
-aparser.add_argument('-s'          ,'--seed'      ,action='store' ,dest='seed'   ,default='123456'         ,help='random seed for grid generation')
-aparser.add_argument('-n'          ,'--nevents'   ,action='store' ,dest='nevents',default='100'            ,help='number of events for the test run after grid generation')
-args1 = aparser.parse_args()
+aparser.add_argument('--card', '-c', action='store', required=True, help='card')
+aparser.add_argument('--decay-card', '-d', action='store', help='second input card to run for decay')
+aparser.add_argument('--name', action='store', required=True, help='name')
+aparser.add_argument('--seed', '-s', action='store', default='123456', help='random seed for grid generation')
+aparser.add_argument('--nevents', '-n', action='store', default='100', help='number of events for the test run after grid generation')
+aparser.add_argument('--force', '-f', action='store_true', help='overwrite existing folder')
+aparser.add_argument('--link-mela', action='store_true', help='indicate that the process requires MCFM libraries from MELA')
+args = aparser.parse_args()
 
 @contextlib.contextmanager
 def cd(newdir):
@@ -25,50 +27,55 @@ def cd(newdir):
         os.chdir(prevdir)
 
 basedir=os.getcwd()
-JHUbasedir = os.path.join(basedir, args1.name+"_JHUGen")
+JHUbasedir = os.path.join(basedir, args.name+"_JHUGen")
+if os.path.exists(JHUbasedir):
+    if args.force:
+        shutil.rmtree(JHUbasedir)
+    else:
+        raise OSError("{} already exists.  To overwrite, use the --force option".format(JHUbasedir))
+os.mkdir(JHUbasedir)
 #Start with the basics download MCFM and add the options we care  :
 with cd(JHUbasedir):
-    subprocess.check_call(["patches/install.sh"])
+    subprocess.check_call([os.path.join(basedir, "patches", "install.sh"), "true" if args.link_mela else "false"])
 
 ##Get the base files
-shutil.copy(os.path.join(basedir, args1.card), JHUbasedir)
+shutil.copy(os.path.join(basedir, args.card), JHUbasedir)
 
 command='./JHUGen '
-with open(os.path.join(basedir, args1.card), "rt") as flabel: 
+with open(os.path.join(basedir, args.card), "rt") as flabel: 
     for line in flabel:
-        command += line.rstrip('\n')
+        command += line.rstrip('\n')+" "
     command += " VegasNc2=${nevt} Seed=${rnum}"
-if args1.card2 is not None:
+if args.decay_card is not None:
     command += " DataFile=undecayed && ./JHUGen "
-    with open(os.path.join(basedir, args1.card2), "rt") as flabel:
+    with open(os.path.join(basedir, args.decay_card), "rt") as flabel:
         for line in flabel:
-            command += line.rstrip("\n").replace("ReadCSmax", "")  #remove CSmax so the same card can be used for generating ggH or decaying
+            command += line.rstrip("\n").replace("ReadCSmax", "")+" "  #remove CSmax so the same card can be used for generating ggH or decaying
     command += " ReadLHE=undecayed.lhe Seed=${rnum}"
 command += ' DataFile=Out'
 print command
 #Note the same seed is used twice.  This sounds bad but the JHUGen processes are completely independent and use the seed in different ways.
 
-runcmsgrid = os.path.join(basedir, args1.name+"_JHUGen", "runcmsgrid.sh")
+runcmsgrid = os.path.join(basedir, "runcmsgrid.sh")
 with open(os.path.join(basedir, "runcmsgrid_template.sh")) as f, open(runcmsgrid, "w") as newf:
     contents = (f.read()
                         .replace("GENCOMMAND", command)
-                        .replace("BASEDIR", args1.name+"_JHUGen")
+                        .replace("BASEDIR", args.name+"_JHUGen/JHUGenerator")
                         .replace("SCRAM_ARCH_VERSION_REPLACE", os.environ["SCRAM_ARCH"])
                         .replace("CMSSW_VERSION_REPLACE", os.environ["CMSSW_VERSION"])
                )
     newf.write(contents)
+os.chmod(runcmsgrid, 0777)
 
-with cd(os.path.join(basedir, args1.name+"_JHUGen")):
+with cd(os.path.join(JHUbasedir, "JHUGenerator")):
     if "ReadCSmax" in command:
         #set up the grid now so it can be read
         #but not the decay part (that is quick anyway)
-        runcommand = command.split("&&")[0].replace("ReadCSmax", "").replace("${nevt}", args1.nevents).replace("${rnum}", args1.seed)
+        runcommand = command.split("&&")[0].replace("ReadCSmax", "").replace("${nevt}", args.nevents).replace("${rnum}", args.seed)
         subprocess.check_call(runcommand.split())
         for _ in glob.glob("*.lhe"): os.remove(_)
         shutil.rmtree("data/")
     for _ in glob.glob("LSFJOB*"): shutil.rmtree(_)
-    shutil.move("runcmsgrid.sh", "..")
 
 with cd(basedir):
-    subprocess.check_call(["tar", "czvf", "JHUGen_%s_%s_%s.tgz" % (args1.name,os.environ["SCRAM_ARCH"],os.environ["CMSSW_VERSION"]), "%s_JHUGen runcmsgrid.sh" % args1.name])
-
+    subprocess.check_call(["tar", "czvf", "JHUGen_%s_%s_%s.tgz" % (args.name, os.environ["SCRAM_ARCH"], os.environ["CMSSW_VERSION"]), args.name+"_JHUGen", "runcmsgrid.sh"])
--- bin/JHUGen/patches/install.sh
+++ bin/JHUGen/patches/install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+sed -euo pipefail
+
+if [ $# -eq 0 ]; then
+    linkMELA=false
+else
+    linkMELA=$1
+fi
+if [ $linkMELA != true ] && [ $linkMELA != false ]; then
+    echo "Argument to $0 has to be true to link MELA or false to not link it"
+fi
+
 wget --no-check-certificate http://cms-project-generators.web.cern.ch/cms-project-generators/slc6_amd64_gcc481/JHUGenerator.v7.0.9.tar.gz -O JHUGenerator.v7.0.9.tar.gz
 tar xzvf JHUGenerator.v7.0.9.tar.gz
 rm -rf AnalyticMELA
+rm -f manJHUGenerator*.pdf
 cd JHUGenerator
 sed -i "s/UseLHAPDF=No/UseLHAPDF=Yes/" makefile
+if ! $linkMELA; then
+    sed -i "s/linkMELA = Yes/linkMELA = No/" makefile
+    rm -rf ../JHUGenMELAv2
+fi
 make
 rm ../JHUGenerator.v7.0.9.tar.gz
 
