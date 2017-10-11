dc2fe4132275d4e1afde779b0ee0db66c2da83db
Heshy Roskes <jroskes1@jhu.edu>
2017-10-11 20:00:37 +0200

    script to submit jobs

---
+++ bin/Powheg/submitjobs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from collections import namedtuple
+import subprocess
+
+Sample = namedtuple("Sample", "name powhegcard powhegmodel")
+
+parametersbase = {
+  "-p": "f",
+  "-g": "../JHUGen/cards/decay/ZZ4l_withtaus.input",
+  "-q": "1nw",
+  "-n": "10",
+  "-d": "1",
+}
+
+samples = [
+  Sample("ggH", "gg_H_quark-mass-effects_NNPDF30_14TeV", "gg_H_quark-mass-effects"),
+  Sample("WplusH", "HWplusJ_HanythingJ_NNPDF30_14TeV", "HWJ"),
+  Sample("WminusH", "HWminusJ_HanythingJ_NNPDF30_14TeV", "HWJ"),
+  Sample("minloHJJ", "HJJ_NNPDF30_14TeV", "HJJ"),
+]
+
+for name, powhegcard, powhegmodel in samples:
+  parameters = {
+    "-i": powhegcard+"/"+powhegcard+"_M125.input",
+    "-f": name,
+    "-m": powhegmodel,
+  }
+  parameters.update(parametersbase)
+
+  command = ["./run_pwg.py"]
+  for k, v in parameters.iteritems():
+    command += [k, v]
+
+subprocess.check_call(command)
