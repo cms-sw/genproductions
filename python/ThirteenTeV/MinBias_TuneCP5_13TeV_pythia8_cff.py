5769aa7f09e9ec8b69fc94e360ec435580512330
perrozzi <perrozzi@cern.ch>
2017-11-02 23:09:52 +0100

    Create MinBias_TuneCP5_13TeV_pythia8_cff.py

---
+++ python/ThirteenTeV/MinBias_TuneCP5_13TeV_pythia8_cff.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+              'SoftQCD:inelastic = on',
+     
+       ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+    )
+)
