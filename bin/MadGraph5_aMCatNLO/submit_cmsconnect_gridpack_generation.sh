a1e5e752af33df0cc05f6acff0075481f5dcdf13
Kenneth Long <kdlong@hep.wisc.edu>
2017-11-03 15:51:33 +0100

    Fix problems with folder structure and tar

--- bin/MadGraph5_aMCatNLO/submit_cmsconnect_gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/submit_cmsconnect_gridpack_generation.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-helpers_dir=$(git rev-parse --show-toplevel)/bin/MadGraph5_aMCatNLO/Utilities
-script_dir=$(git rev-parse --show-toplevel)/Utilities/scripts
-
-source ${helpers_dir}/cmsconnect_utils.sh
-source ${helpers_dir}/source_condor.sh
+source Utilities/cmsconnect_utils.sh
+source Utilities/source_condor.sh
 
 create_codegen_jdl(){
 cat<<-EOF
@@ -165,9 +162,10 @@ codegen_jdl="codegen_${card_name}.jdl"
 # Those will be input files for the condor CODEGEN step.
 input_files="input_${card_name}.tar.gz"
 patches_directory="./patches"
+utilities_dir="./Utilities"
 
 if [ -e "$input_files" ]; then rm "$input_files"; fi
-tar -zcf "$input_files" "$card_dir" "$patches_directory" $scripts_dir $helpers_dir
+tar -zchf "$input_files" "$card_dir" "$patches_directory" "$utitlies_dir}
 
 ## Create a submit file for a single job
 # create_codegen_exe arguments are:
