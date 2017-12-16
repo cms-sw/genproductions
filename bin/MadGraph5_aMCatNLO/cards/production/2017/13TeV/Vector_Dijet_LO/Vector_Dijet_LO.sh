7c0f145c4cbd1dd02c828cbb1beab1b2fcb86858
Cristina Mantilla <criss.ms7@gmail.com>
2017-12-16 21:59:30 +0100

    changing masses

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/Vector_Dijet_LO/Vector_Dijet_LO.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/Vector_Dijet_LO/Vector_Dijet_LO.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#masses=(500 750 1000 1250 1500 1750 2000 2500 3000 4000 5000)
-masses=(1250 1000)
+masses=(10 30 50 75 100 115 125 150 175 200 225 250 275 300 350 450 500 600)
 sample=Vector_Dijet_LO_Mphi
 
 postfix=(_run_card.dat _customizecards.dat _proc_card.dat _madspin_card.dat _reweight_card.dat _extramodels.dat)
@@ -11,7 +10,7 @@ echo ${masses[*]}
 for mass in ${masses[*]}; do
     echo generating cards for M = $mass GeV
     
-    mkdir ${sample}${mass}
+    mkdir -p ${sample}${mass}
            
     for i in {0..5}; do
 	sed "s/<MASS>/${mass}/g" ${sample}/${sample}${postfix[$i]} > ${sample}$mass/${sample}$mass${postfix[$i]}
