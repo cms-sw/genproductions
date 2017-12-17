01dfdc5a814cebecb0f255b290b4389753702f5c
Kenyi Hurtado <khurtado@nd.edu>
2017-12-17 05:55:31 -0500

    Update submit_cmsconnect_gridpack_generation.sh
    Change default maxwalltime values, to request up to 70 hours of MaxWallTime if needed.

--- bin/MadGraph5_aMCatNLO/submit_cmsconnect_gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/submit_cmsconnect_gridpack_generation.sh
@@ -130,7 +130,7 @@ fi
 # Set a list of maxwalltime in minutes
 # Pilots maximum life is 48h or 2880 minutes
 if [ -z "$CONDOR_SET_MAXWALLTIMES" ]; then
-  export CONDOR_SET_MAXWALLTIMES="500,960,2160,2820"
+  export CONDOR_SET_MAXWALLTIMES="500,960,2160,2820,4200"
 fi
 
 ##########################
