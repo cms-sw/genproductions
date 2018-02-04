d318002c38d074e1fde7db31c4759682e38e20cb
Kenneth Long <kdlong@hep.wisc.edu>
2018-02-04 20:34:49 +0100

    Update systematics call to so weights aren't duplicated

--- bin/MadGraph5_aMCatNLO/runcmsgrid_LO.sh
+++ bin/MadGraph5_aMCatNLO/runcmsgrid_LO.sh
@@ -84,7 +84,7 @@ pushd process/madevent
 pdfsets="PDF_SETS_REPLACE"
 scalevars="--mur=1,2,0.5 --muf=1,2,0.5 --together=muf,mur,dyn --dyn=-1,1,2,3,4"
 
-echo "systematics $runlabel --pdf=$pdfsets $scalevars" | ./bin/madevent
+echo "systematics $runlabel --remove_wgts=all --start_id=1001 --pdf=$pdfsets $scalevars" | ./bin/aMCatNLO
 popd
 
 mv process/madevent/Events/${runlabel}/events.lhe.gz cmsgrid_final.lhe.gz
