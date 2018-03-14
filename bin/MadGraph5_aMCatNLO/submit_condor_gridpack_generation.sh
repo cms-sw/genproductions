cac8691753f9b849f099a944118fcd3f327639a8
albert <albert@physik.rwth-aachen.de>
2018-03-14 15:17:46 +0100

    Fixup queue selection.

--- bin/MadGraph5_aMCatNLO/submit_condor_gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/submit_condor_gridpack_generation.sh
@@ -5,20 +5,20 @@ name=$1
 carddir=$2
 workqueue="condor"
 
-if [ "${HOSTNAME}" =~ "lxplus" ] && [ ! $(pwd) =~ "/afs/" ]; then
+if [[ "${HOSTNAME}" =~ "lxplus" ]] && [[ ! $(pwd) =~ "/afs/" ]]; then
     echo "You are submitting from lxplus and the local directory is not on AFS."
     echo "Automatically switch to condor spool mode."
     workqueue="condor_spool"
 fi
 
-if [ "${workqueue}" -eq "condor_spool" ] && [ $(pwd) =~ "/afs/" ]; then
+if [[ "${workqueue}" -eq "condor_spool" ]] && [[ $(pwd) =~ "/afs/" ]]; then
     echo "ERROR: You are using the condor SPOOL option while submitting from AFS." \
     "Spool only makes sense to allow condor submission from a file system unique to the submitter machine." \
     "Please either change directory to e.g. /tmp/user/ or simply do not use spool." \
 
     echo "Exiting."
     exit 1
-done
+fi
 
 scram_arch=$3
 cmssw_version=$4
