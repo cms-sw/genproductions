f9848acf82c91c535f1875bd2273bdc7f75e9ee2
Yutaro Iiyama <yiiyama@mit.edu>
2018-03-20 11:14:55 -0400

    dropping CP5 hard-code

--- python/ThirteenTeV/Higgs/Hadronizer_TuneCP5_13TeV_powhegEmissionVeto_3p_HToGDarkG_M-125_LHE_pythia8_cff.py
+++ python/ThirteenTeV/Higgs/Hadronizer_TuneCP5_13TeV_powhegEmissionVeto_3p_HToGDarkG_M-125_LHE_pythia8_cff.py
@@ -1,31 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-try:
-    from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
-except ImportError:
-    pythia8CP5SettingsBlock = cms.PSet(
-        pythia8CP5Settings = cms.vstring(
-        'Tune:pp 14',
-            'Tune:ee 7',
-            'MultipartonInteractions:ecmPow=0.03344',
-            'PDF:pSet=20',
-            'MultipartonInteractions:bProfile=2',
-            'MultipartonInteractions:pT0Ref=1.41',
-            'MultipartonInteractions:coreRadius=0.7634',
-            'MultipartonInteractions:coreFraction=0.63',
-            'ColourReconnection:range=5.176',
-            'SigmaTotal:zeroAXB=off',
-            'SpaceShower:alphaSorder=2',
-            'SpaceShower:alphaSvalue=0.118',
-            'SigmaProcess:alphaSvalue=0.118',
-            'SigmaProcess:alphaSorder=2',
-            'MultipartonInteractions:alphaSvalue=0.118',
-            'MultipartonInteractions:alphaSorder=2',
-            'TimeShower:alphaSorder=2',
-            'TimeShower:alphaSvalue=0.118',
-            )
-    )
-
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
