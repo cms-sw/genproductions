fff5e06811ebd438445f58601396e0a9a2f6369f
Heshy Roskes <jroskes1@jhu.edu>
2018-03-23 17:39:36 +0100

    update JHUGen version in POWHEG too

--- bin/Powheg/run_pwg.py
+++ bin/Powheg/run_pwg.py
@@ -286,7 +286,7 @@ def runGetSource(parstage, xgrid, folderName, powInputName, process, noPdfCheck,
 '''
 # Release to be used to define the environment and the compiler needed
 export RELEASE=${CMSSW_VERSION}
-export jhugenversion="v7.1.0"
+export jhugenversion="v7.1.4"
 
 cd $WORKDIR
 pwd
