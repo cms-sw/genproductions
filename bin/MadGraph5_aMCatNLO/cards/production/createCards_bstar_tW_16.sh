2f4f0534db15e42c22390811a18a33f164ca25fb
Alexander Froehlich <alexander.froehlich@desy.de>
2018-03-26 13:53:08 +0200

    added example card for 2017 and fixed scripts for creating other masspoints

--- bin/MadGraph5_aMCatNLO/cards/production/createCards_bstar_tW_16.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/createCards_bstar_tW_16.sh
@@ -2,20 +2,20 @@
 
 masspoints=( 700 800 900 1000 1100 )
 couplings=( "Left" "Right" )
-base_dir="/nfs/dust/cms/user/froehlia/BstarToTW/Run2_80X_v3/SignalProduction/genproductions/bin/MadGraph5_aMCatNLO/cards/production/pre2017/13TeV/"
-template_dir="/nfs/dust/cms/user/froehlia/BstarToTW/Run2_80X_v3/SignalProduction/bstar_tW_Left_M1200/"
+base_dir="pre2017/13TeV"
+template_dir="${base_dir}/bstar_tW_Left_M700/"
 
 # loop over all masspoints
 for handedness in "${couplings[@]}"
 do
     for mp in "${masspoints[@]}"
     do
-	dir="${base_dir}bstar_tW_${handedness}_M${mp}/"
+	dir="${base_dir}/bstar_tW_${handedness}_M${mp}/"
 	mkdir -p "$dir"
-	cp "${template_dir}bstar_tW_Left_M1200_extramodels.dat" "${dir}bstar_tW_${handedness}_M${mp}_extramodels.dat"
-	cp "${template_dir}bstar_tW_Left_M1200_proc_card.dat" "${dir}bstar_tW_${handedness}_M${mp}_proc_card.dat"
+	cp "${template_dir}bstar_tW_Left_M700_extramodels.dat" "${dir}bstar_tW_${handedness}_M${mp}_extramodels.dat"
+	cp "${template_dir}bstar_tW_Left_M700_proc_card.dat" "${dir}bstar_tW_${handedness}_M${mp}_proc_card.dat"
 	sed -i "s/Left_M1200/${handedness}_M${mp}/g" "${dir}bstar_tW_${handedness}_M${mp}_proc_card.dat"
-	cp "${template_dir}bstar_tW_Left_M1200_run_card.dat" "${dir}bstar_tW_${handedness}_M${mp}_run_card.dat"
+	cp "${template_dir}bstar_tW_Left_M700_run_card.dat" "${dir}bstar_tW_${handedness}_M${mp}_run_card.dat"
 	root -l -b -q "writeCards.C(${mp},\"${handedness}\",\"${dir}\")"
     done
 done
--- bin/MadGraph5_aMCatNLO/cards/production/createCards_bstar_tW_17.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/createCards_bstar_tW_17.sh
@@ -1,14 +1,16 @@
+# script to create production cards for 2016 bstar from example card (bstar_tW_Left_M700)
+
 masspoints=( 700 800 900 1000 1100 1200 1400 1600 1800 2000 2200 2400 2600 2800 3000)
 couplings=( "Left" "Right" )
 base_dir="2017/13TeV"
-template_dir="${base_dir}/bstar_tW_Left_M1200"
+template_dir="${base_dir}/bstar_tW_Left_M1200/"
 
 # loop over all masspoints
 for handedness in "${couplings[@]}"
 do
     for mp in "${masspoints[@]}"
     do
-	dir="${base_dir}bstar_tW_${handedness}_M${mp}/"
+	dir="${base_dir}/bstar_tW_${handedness}_M${mp}/"
 	mkdir -p "$dir"
 	cp "${template_dir}bstar_tW_Left_M1200_extramodels.dat" "${dir}bstar_tW_${handedness}_M${mp}_extramodels.dat"
 	cp "${template_dir}bstar_tW_Left_M1200_proc_card.dat" "${dir}bstar_tW_${handedness}_M${mp}_proc_card.dat"
