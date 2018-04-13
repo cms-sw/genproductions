97cffd79829f170a678e4b381828495e22d9845a
Kenneth Long <kdlong@hep.wisc.edu>
2018-04-13 10:20:55 +0200

    Boost include no longer needed in 2.6.1

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -128,7 +128,6 @@ make_gridpack () {
     
       LHAPDFINCLUDES=`$LHAPDFCONFIG --incdir`
       LHAPDFLIBS=`$LHAPDFCONFIG --libdir`
-      export BOOSTINCLUDES=`scram tool tag boost INCLUDE`
     
       echo "set auto_update 0" > mgconfigscript
       echo "set automatic_html_opening False" >> mgconfigscript
@@ -305,8 +304,6 @@ make_gridpack () {
       cd $WORKDIR
     
       eval `scram runtime -sh`
-      export BOOSTINCLUDES=`scram tool tag boost INCLUDE`
-    
       #if lhapdf6 external is available then above points to lhapdf5 and needs to be overridden
       LHAPDF6TOOLFILE=$CMSSW_BASE/config/toolbox/$SCRAM_ARCH/tools/available/lhapdf6.xml
       if [ -e $LHAPDF6TOOLFILE ]; then
