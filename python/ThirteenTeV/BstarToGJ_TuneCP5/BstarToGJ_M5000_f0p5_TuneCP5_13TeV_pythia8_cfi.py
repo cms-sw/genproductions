c1fea837451a8c459a766881b6c601d77a272019
rocky <rgarg@cern.ch>
2018-06-22 16:09:17 +0530

    Qstar and Bstar signal samples with new tune, TuneCP5

---
+++ python/ThirteenTeV/BstarToGJ_TuneCP5/BstarToGJ_M5000_f0p5_TuneCP5_13TeV_pythia8_cfi.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                  comEnergy = cms.double(13000.0),
+                  crossSection = cms.untracked.double(1.0),
+                  filterEfficiency = cms.untracked.double(1.0),
+                  maxEventsToPrint = cms.untracked.int32(0),
+                  pythiaHepMCVerbosity = cms.untracked.bool(False),
+                  pythiaPylistVerbosity = cms.untracked.int32(0),
+                  PythiaParameters = cms.PSet(
+		        pythia8CommonSettingsBlock,
+                        pythia8CP5SettingsBlock,
+                        processParameters = cms.vstring(
+                            'ExcitedFermion:bg2bStar = on',
+                            '4000005:m0 = 5000.0',
+                            '4000005:onMode = off',
+                            '4000005:onIfMatch = 22 5',
+                            'ExcitedFermion:Lambda = 5000.0',
+                            'ExcitedFermion:coupFprime = 0.5',
+                            'ExcitedFermion:coupF = 0.5',
+                            'ExcitedFermion:coupFcol = 0.5'
+                            ),
+                        parameterSets = cms.vstring(
+			    'pythia8CommonSettings',
+                            'pythia8CP5Settings',
+			    'processParameters',
+			    )
+                  )
+)
+ProductionFilterSequence = cms.Sequence(generator)
+
+
---
+++ python/ThirteenTeV/QstarToGJ_TuneCP5/QstarToGJ_M1000_f0p5_TuneCP5_13TeV_pythia8_cfi.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                  comEnergy = cms.double(13000.0),
+                  crossSection = cms.untracked.double(1.0),
+                  filterEfficiency = cms.untracked.double(1.0),
+                  maxEventsToPrint = cms.untracked.int32(0),
+                  pythiaHepMCVerbosity = cms.untracked.bool(False),
+                  pythiaPylistVerbosity = cms.untracked.int32(0),
+                  PythiaParameters = cms.PSet(
+		        pythia8CommonSettingsBlock,
+                        pythia8CP5SettingsBlock,
+                        processParameters = cms.vstring(
+                            'ExcitedFermion:ug2uStar = on',
+                            'ExcitedFermion:dg2dStar = on',
+                            '4000001:m0 = 1000.0',
+                            '4000001:onMode = off',
+                            '4000001:onIfMatch = 22 1',
+                            '4000002:m0 = 1000.0',
+                            '4000002:onMode = off',
+                            '4000002:onIfMatch = 22 2',
+                            'ExcitedFermion:Lambda = 1000.0',
+                            'ExcitedFermion:coupFprime = 0.5',
+                            'ExcitedFermion:coupF = 0.5',
+                            'ExcitedFermion:coupFcol = 0.5'
+                            ),
+                        parameterSets = cms.vstring(
+			    'pythia8CommonSettings',
+                            'pythia8CP5Settings',
+			    'processParameters',
+			    )
+                  )
+)
+ProductionFilterSequence = cms.Sequence(generator)
+
+
