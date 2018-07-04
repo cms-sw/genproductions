268da05906b82a8398f8a961ae17c96209060c72
Chad Freer <chad.freer@cern.ch>
2018-07-04 15:47:19 +0200

    monoZ+LLP fragements

---
+++ python/ThirteenTeV/DMSimp_DM_ZLL_LO_Vector_GQ0p25_GDM1p0_MY1-500_MXd-150/MX1-1_ctau-1000.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/afs/cern.ch/work/c/cfreer/public/MonoZ/CMSSW_9_4_8/src/DMSimp_DM_ZLL_LO_Vector_GQ0p25_GDM1p0_MY1-500_MXd-150/DMSimp_DM_ZLL_LO_Vector_GQ0p25_GDM1p0_MY1-500_MXd-150_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+# Link to cards:
+# https://github.com/AndreasAlbert/genproductions/tree/monoz_LO_forDMLL/bin/MadGraph5_aMCatNLO/cards/production/13TeV/MonoZ
+
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'ParticleDecays:tau0Max = 1000.1',
+            'LesHouches:setLifetime = 2',
+            'SLHA:useDecayTable = off', # use pythia8 decay mode instead of decays defined in LH accord
+            '5000522:new',
+            '5000522:m0 = 1',
+            '5000522:isResonance = false',
+            '5000522:onMode = off',
+            '5000522:mayDecay = off',
+            '52:mayDecay = on',
+            '52:mWidth = 0.01',  # needs to be non-zero for Pythia to decay it
+            '52:onMode = off',
+            '52:addChannel = 1 1 100 5000522 1 -1',
+            '52:onIfAny = 5000522 1 -1',
+            '52:tau0 = 1000'
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+    )
