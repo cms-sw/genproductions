b4472fa2d48adaf010bd01fa15e707b97ebcea6b
alexandra.oliveira@gmal.com <alexandra.oliveira@cern.ch>
2018-07-11 11:05:57 +0200

    fix decay pattern

--- python/ThirteenTeV/Higgs/HH/ResonanceDecayFilter_example_HHTo4V_powheg_pythia8_CP5_cff.py
+++ python/ThirteenTeV/Higgs/HH/ResonanceDecayFilter_example_HHTo4V_powheg_pythia8_CP5_cff.py
@@ -3,6 +3,9 @@ from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
 
+## Warning: it needs backport to this PR https://github.com/cms-sw/cmssw/pull/21132/files#diff-e6956e697ae15d477aa15e3af530f192
+## (valid from CMSSW_10X+)
+
 generator = cms.EDFilter("Pythia8HadronizerFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
                          pythiaPylistVerbosity = cms.untracked.int32(1),
@@ -19,7 +22,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
             '24:mMin = 0.05',
             '25:m0 = 125.0',
             '25:onMode = off',
-            '25:onIfMatch = 15 -15',
+            '25:onIfMatch = 24 -24',
             '25:onIfMatch = 23 23',
             'ResonanceDecayFilter:filter = on',
             'ResonanceDecayFilter:exclusive = on', #off: require at least the specified number of daughters, on: require exactly the specified number of daughters
@@ -27,7 +30,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
             'ResonanceDecayFilter:eMuTauAsEquivalent = on', #on: treat electrons, muons , and taus as equivalent
             'ResonanceDecayFilter:mothers = 25', #list of mothers not specified -> count all particles in hard process+resonance decays (better to avoid specifying mothers when including leptons from the lhe in counting, since intermediate resonances are not gauranteed to appear in general
             'ResonanceDecayFilter:wzAsEquivalent = on',
-            'ResonanceDecayFilter:daughters = 23,23,23,23,1,1,1,1',
+            'ResonanceDecayFilter:daughters = 23,23,23,23',
           ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CP5Settings',
