48712addf95fea99a77d2809d356bfd7af170da0
Chris <cfmcginn@mit.edu>
2018-08-26 06:01:00 +0200

    Added pthat extensions for 2017 pp ref MC

---
+++ python/HI/dijet_analysis/pp/Pythia6_Dijet720_pp_TuneZ2_5020GeV_cff.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+                         comEnergy = cms.double(5020.0),
+                         crossSection = cms.untracked.double(8.095e-10), #PYTHIA6 100 evtestimate, accuracy 10% level
+                         filterEfficiency = cms.untracked.double(1.),
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         pythiaPylistVerbosity = cms.untracked.int32(False),
+                         PythiaParameters = cms.PSet(pythiaUESettingsBlock,
+                                                     processParameters = cms.vstring('MSEL=1   ! Standard QCD processes',
+                                                                                     'CKIN(3)= 720  ! minimum pt hat for hard interactions',
+                                                                                     ),
+                                                     parameterSets = cms.vstring('pythiaUESettings',
+                                                                                 'processParameters',
+                                                                                 )
+                                                     )
+                         )
+
+configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('PYTHIA6 QCD, pt-hat > 720 GeV, at sqrt(s) = 5.02 TeV')
+    )
+
+ProductionFilterSequence = cms.Sequence(generator)
+
