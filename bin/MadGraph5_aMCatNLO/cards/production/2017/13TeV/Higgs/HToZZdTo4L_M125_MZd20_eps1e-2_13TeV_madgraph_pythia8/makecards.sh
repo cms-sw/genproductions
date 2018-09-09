964a4483e482e800b674a90ff81d27fff62c3d49
Jake <rosedj1@gmail.com>
2018-09-09 13:18:10 -0400

    Adding script to make cards for other mass points

---
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/Higgs/HToZZdTo4L_M125_MZd20_eps1e-2_13TeV_madgraph_pythia8/makecards.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+### PURPOSE: Quickly generate MG cards for various Zdark mass points.
+### SYNTAX: ./makecards.sh
+### NOTES: User should verify epsilon, lhaid used, and mass points below. 
+
+eps="1e-2"
+lhaid=263000
+
+for zdmass in 1 2 3 4 7 10 15 20 25 30 35
+do
+    cp -r HAHMcards_eps_MZD_lhaid_template/ HAHMcards_eps"$eps"_MZD"$zdmass"_lhaid"$lhaid"/ 
+    cd HAHMcards_eps"$eps"_MZD"$zdmass"_lhaid"$lhaid"/
+    sed -i "s/MASS/$zdmass/g" HAHM_variablesw_v3_customizecards.dat
+    sed -i "s/LHAID/$lhaid/g" HAHM_variablesw_v3_run_card.dat
+    cd ..
+done
