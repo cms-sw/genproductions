fdd6435d424f48b29e247e72ff9bd8abc2151654
Jake <rosedj1@gmail.com>
2018-09-12 12:15:17 -0400

    Deleting extraneous files

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/Higgs/HToZZdTo4L_M125_MZd20_eps1e-2_13TeV_madgraph_pythia8/mkgridpack.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/Higgs/HToZZdTo4L_M125_MZd20_eps1e-2_13TeV_madgraph_pythia8/mkgridpack.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-###################################
-##### SYNTAX TO MAKE GRIDPACK #####
-###################################
-## for the model use: HAHM_variablesw_v3
-## for the folder_path, point to the directory holding the cards
-## e.g. 
-## ./mkgridpack.sh HAHM_variablesw_v3 HAHMcards_MZD20_lhaid263000/ 
+### SYNTAX: ./mkgridpack.sh HAHM_variablesw_v3 <dir_holding_MG_cards>/ 
 
 model=${1}
 folder_path=${2}
