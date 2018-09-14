a168422cf6f2b6aec977e7476661c85e8f234a33
Andrew Levin <andrew.michael.levin@cern.ch>
2018-09-14 11:31:38 +0200

    remove unnecessary processParameters block

--- python/ThirteenTeV/Hadronizer_TuneCUEP8M1_13TeV_powhegEmissionVeto1_Wgamma_pythia8_cff.py
+++ python/ThirteenTeV/Hadronizer_TuneCUEP8M1_13TeV_powhegEmissionVeto1_Wgamma_pythia8_cff.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
-from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
@@ -24,33 +23,8 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
         pythia8CUEP8M1SettingsBlock,
-        processParameters = cms.vstring(
-            'POWHEG:nFinal = 2',   ## Number of final state particles
-                                   ## (BEFORE THE DECAYS) in the LHE
-                                   ## other than emitted extra parton
-            '23:mMin = 0.05',      
-	    	'24:mMin = 0.05',      
-            '25:m0 = 125.0',
-            '25:onMode = off',
-			'25:onIfMatch = 5 -5', #Higgs decays only to bb pair
-            #PSweights
-            'UncertaintyBands:doVariations = on',
-            # 3 sets of variations for ISR&FSR up/down
-            # Reduced sqrt(2)/(1/sqrt(2)), Default 2/0.5 and Conservative 4/0.25 variations
-                    'UncertaintyBands:List = {\
-            isrRedHi isr:muRfac=0.707,fsrRedHi fsr:muRfac=0.707,isrRedLo isr:muRfac=1.414,fsrRedLo fsr:muRfac=1.414,\
-            isrDefHi isr:muRfac=0.5, fsrDefHi fsr:muRfac=0.5,isrDefLo isr:muRfac=2.0,fsrDefLo fsr:muRfac=2.0,\
-            isrConHi isr:muRfac=0.25, fsrConHi fsr:muRfac=0.25,isrConLo isr:muRfac=4.0,fsrConLo fsr:muRfac=4.0}',
-
-            'UncertaintyBands:MPIshowers = on',
-            'UncertaintyBands:overSampleFSR = 10.0',
-            'UncertaintyBands:overSampleISR = 10.0',
-            'UncertaintyBands:FSRpTmin2Fac = 20',
-            'UncertaintyBands:ISRpTmin2Fac = 1'
-          ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CP5Settings',
-                                    'processParameters'
                                     )
         )
                          )
