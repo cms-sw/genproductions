ee4ce740875d0f12097b5902389adc691ccd589b
Andrew Levin <andrew.michael.levin@cern.ch>
2018-09-14 09:06:08 +0200

    correct the tune

--- python/ThirteenTeV/Hadronizer_TuneCUEP8M1_13TeV_powhegEmissionVeto1_Wgamma_pythia8_cff.py
+++ python/ThirteenTeV/Hadronizer_TuneCUEP8M1_13TeV_powhegEmissionVeto1_Wgamma_pythia8_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
 from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
