7f684496d649558624a5d4de6eb73261dc38cf5b
Efe <efe.yazgan@cern.ch>
2018-10-15 15:38:20 +0200

    updated help message

--- bin/utils/request_fragment_check.py
+++ bin/utils/request_fragment_check.py
@@ -4,7 +4,6 @@ from textwrap import dedent
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description=textwrap.dedent('''\
-            python request_fragment_check.py [prepid in mcm]
             ------------------------------------------------ 
                This script currently checks for the following to give an OK, WARNING, or ERROR
             
@@ -16,13 +15,28 @@ parser = argparse.ArgumentParser(
                               - warning to check whether it is a loop induced process
                   * [WARNING] if CP5 tune is used but campaign is not Fall18 or Fall17
                   * [WARNING] if Fall18 campaing but no parton shower weights configuration in the fragment
+                  * [WARNING] At least one of the MG5_aMC@NLO tmpdir patches is missing."
+                  *                    --> Please check that you use:"
+                  *                        >=  CMSSW_7_1_32_patch1 in 7_1_X or  
+                  *                        >= CMSSW_9_3_9_patch1 in 9_3_X or
+                  *                        >= 10_1_3 in 10_1_X or" 
+                  *                        >= CMSSW_10_2_0_pre2 in 10_2_X.
+                  *                        Your request uses "+cmssw+" :
+                  *                         If you are not using a proper CMSSW version, please switch to that or
+                  *                         re-create the gridpack using the updated genproductions area
 
                ERRORS:
+                  * [ERROR] Memory is not 2300 or 4000 GB"
+                  * [ERROR] Memory is 2300 GB while number of cores is XX but not = 1
+                  * [ERROR] Memory is 4000 GB while number of cores is 1 but not = 2,4 or 8
                   * [ERROR] Gridpack should have used cvmfs path instead of eos path
+                  * [ERROR] MG5_aMC@NLO multi-run patch missing in gridpack - please re-create a gridpack"
+                  *            using updated genproductions area
                   * [ERROR] May be wrong fragment: powheg/madgraph/mcatnlo in dataset name but settings in 
                             fragment not correct or vice versa"
                   * [ERROR] Tune configuration wrong in the fragment"
-                  * [ERROR] Parton shower weight configuretion not OK in the fragment"
+                  * [ERROR] PS weights in config but CMSSW version is not 10_2_3 - please check!"	
+                  * [ERROR] Parton shower weight configuration not OK in the fragment"
 
                The script also checks if there is no fragment there is a hadronizer used.'''))
 parser.add_argument('--prepid', type=str, help="check mcm requests using prepids", nargs='+')
