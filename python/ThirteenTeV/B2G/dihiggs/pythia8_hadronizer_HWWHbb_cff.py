41e15949b32cf9212b1ff82efae58a928ae3b8eb
Shin-Shan <syu@cern.ch>
2018-10-19 15:06:49 +0200

    indent hbbhbb python file, add hWWhbb

--- python/ThirteenTeV/B2G/dihiggs/pythia8_hadronizer_HWWHbb_cff.py
+++ python/ThirteenTeV/B2G/dihiggs/pythia8_hadronizer_HWWHbb_cff.py
@@ -13,9 +13,16 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
                              pythia8CP2SettingsBlock,
                              processParameters = cms.vstring(
                                  'SLHA:useDecayTable = off',  # Use pythia8s own decay mode instead of decays defined in LH accord
+                                 '24:mMin = 0.05',                  
+                                 '24:onMode = on',                 
                                  '25:m0 = 125.0',
                                  '25:onMode = off',
                                  '25:onIfMatch = 5 -5'
+                                 '25:onIfMatch = 24 -24',           # turn ON H->WW
+                                 'ResonanceDecayFilter:filter = on',
+                                 'ResonanceDecayFilter:exclusive = on', #off: require at least the specified number of daughters, on: require exactly the specified number of daughters
+                                 'ResonanceDecayFilter:mothers = 25', #list of mothers not specified -> count all particles in hard process+resonance decays (better to avoid specifying mothers when including leptons from the lhe in counting, since intermediate resonances are not gauranteed to appear in general
+                                 'ResonanceDecayFilter:daughters = 5,5,24,-24'
                                  ),
                              parameterSets = cms.vstring('pythia8CommonSettings',
                                                          'pythia8CP2Settings',
