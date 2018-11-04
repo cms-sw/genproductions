e4d214b57740dfaa8f600b5d6786c2d47f1ec9dd
Heshy Roskes <jroskes1@jhu.edu>
2018-11-04 20:30:24 +0100

    use the blocks from CMSSW, for when that's included in the relevant release

--- python/ThirteenTeV/Hadronizer/Hadronizer_TuneCP5Up_13TeV_powhegEmissionVeto_1p_LHE_pythia8_cff.py
+++ python/ThirteenTeV/Hadronizer/Hadronizer_TuneCP5Up_13TeV_powhegEmissionVeto_1p_LHE_pythia8_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5TuneUpSettings_cfi import *
 from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
 from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
 
@@ -12,35 +12,16 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
                          comEnergy = cms.double(13000.),
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
+        pythia8CP5TuneUpSettingsBlock,
         pythia8PowhegEmissionVetoSettingsBlock,
         pythia8PSweightsSettingsBlock,
-        pythia8CP5UpSettings = cms.vstring(
-            'Tune:pp 14',
-            'Tune:ee 7',
-            'MultipartonInteractions:ecmPow=0.03344',
-            'PDF:pSet=20',
-            'MultipartonInteractions:bProfile=2',
-            'MultipartonInteractions:pT0Ref=1.407',
-            'MultipartonInteractions:coreRadius=0.6671',
-            'MultipartonInteractions:coreFraction=0.4281',
-            'ColourReconnection:range=4.881',
-            'SigmaTotal:zeroAXB=off',
-            'SpaceShower:alphaSorder=2',
-            'SpaceShower:alphaSvalue=0.118',
-            'SigmaProcess:alphaSvalue=0.118',
-            'SigmaProcess:alphaSorder=2',
-            'MultipartonInteractions:alphaSvalue=0.118',
-            'MultipartonInteractions:alphaSorder=2',
-            'TimeShower:alphaSorder=2',
-            'TimeShower:alphaSvalue=0.118',
-        ),
         processParameters = cms.vstring(
             'POWHEG:nFinal = 1',   ## Number of final state particles
                                    ## (BEFORE THE DECAYS) in the LHE
                                    ## other than emitted extra parton
           ),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CP5UpSettings',
+                                    'pythia8CP5TuneUpSettings',
                                     'pythia8PowhegEmissionVetoSettings',
                                     'pythia8PSweightsSettings',
                                     'processParameters'
