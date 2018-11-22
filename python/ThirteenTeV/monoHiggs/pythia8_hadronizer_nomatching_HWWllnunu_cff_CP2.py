93000d08904c81d4f8a34519eefdfd2efa5702bc
fmanteca <fmanteca@ifca.unican.es>
2018-11-22 14:58:35 +0100

    _CP2 files added

--- python/ThirteenTeV/monoHiggs/pythia8_hadronizer_nomatching_HWWllnunu_cff_CP2.py
+++ python/ThirteenTeV/monoHiggs/pythia8_hadronizer_nomatching_HWWllnunu_cff_CP2.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
@@ -10,15 +10,19 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
                          comEnergy = cms.double(13000.),
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
-        pythia8CUEP8M1SettingsBlock,
+        pythia8CP2SettingsBlock,
         processParameters = cms.vstring(
             'SLHA:useDecayTable = off',  # Use pythia8s own decay mode instead of decays defined in LH accord
-            '25:m0 = 125.0',
+            '25:m0 = 125.0', 
             '25:onMode = off',
-            '25:onIfMatch = 22 22'
+            '25:onIfMatch = 24 -24',           # turn ON H->WW
+            '24:mMin = 0.05',                  #  
+            '24:onMode = off',                 # turn OFF all W decays
+            '24:onIfAny = 11 13 15 12 14 16'   # turn ON W->lnu
+
             ),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CUEP8M1Settings',
+                                    'pythia8CP2Settings',
                                     'processParameters'
                                     )
         )
--- python/ThirteenTeV/monoHiggs/pythia8_hadronizer_nomatching_HZZ4l_cff_CP2.py
+++ python/ThirteenTeV/monoHiggs/pythia8_hadronizer_nomatching_HZZ4l_cff_CP2.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
@@ -10,15 +10,19 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
                          comEnergy = cms.double(13000.),
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
-        pythia8CUEP8M1SettingsBlock,
+        pythia8CP2SettingsBlock,
         processParameters = cms.vstring(
             'SLHA:useDecayTable = off',  # Use pythia8s own decay mode instead of decays defined in LH accord
-            '25:m0 = 125.0',
+            '25:m0 = 125.0', 
+            '23:mMin = 0.05',                 # Solve problem with mZ cut
             '25:onMode = off',
-            '25:onIfMatch = 22 22'
+            '25:onIfAll = 23 23',           # turn ON H->ZZ
+            '23:onMode = off',                # turn OFF all Z decays
+            '23:onIfAny = 11 13 15'           # turn ON Z->ll
+
             ),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CUEP8M1Settings',
+                                    'pythia8CP2Settings',
                                     'processParameters'
                                     )
         )
