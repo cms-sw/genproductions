2cd9a7889a52f6c4ce7ce687e142ac6f5de5fe0c
Chris <cfmcginn@mit.edu>
2018-11-22 16:35:47 +0100

    Clones of CUETP8M1 configurations for photon + emenriched dijet, updated to tune CP5

---
+++ python/HI/photon_analysis/PbPb/5020GeV/Pythia8_AllQCDPhoton50_Filter30GeV_TuneCP5_5020GeV_cff.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        parameterSets = cms.vstring('pythia8CommonSettings',
+            'pythia8CP5Settings',
+            'processParameters'),
+        processParameters = cms.vstring('HardQCD:all = on',
+                                        'PromptPhoton:all = on',
+                                        'PhaseSpace:pTHatMin = 50.',
+                                        'PhaseSpace:pTHatMax = 9999.'),
+    ),
+    comEnergy = cms.double(5020.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0)
+)
+
+configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('PYTHIA 8, Tune CP5, (unquenched) photons in NN (pt-hat > 50 GeV) at sqrt(s) = 5.02 TeV')
+    )
+
+photonFilter = cms.EDFilter("PythiaFilterMultiMother",
+                            Status = cms.untracked.int32(1),
+                            MinPt = cms.untracked.double(30.0),
+                            ParticleID = cms.untracked.int32(22),
+                            MotherIDs = cms.untracked.vint32(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1)
+)
+
+
+ProductionFilterSequence = cms.Sequence(generator*photonFilter)
