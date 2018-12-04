c6e64d6914f939d76ba6e7e7d7c18707a06115ed
Kaya Tatar <tatark@mit.edu>
2018-12-04 17:52:15 +0100

    add Z30eeJet and Z30mumuJet with pythia8CP5Settings

---
+++ python/HI/zjet_analysis/pp/Pythia8_Z30mumuJet_pthat30Norm_TuneCP5_5020GeV_cff.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         crossSection = cms.untracked.double(83.2),
+                         comEnergy = cms.double(5020.0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            'WeakBosonAndParton:qqbar2gmZg = on',
+            'WeakBosonAndParton:qg2gmZq = on',
+            '23:onMode = off',
+            '23:onIfAny = 13',
+            'PhaseSpace:pTHatMin = 30.',
+            'PhaseSpace:bias2Selection = on',
+            'PhaseSpace:bias2SelectionPow = 1.5',
+            'PhaseSpace:bias2SelectionRef = 30',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+
+zgenfilter = cms.EDFilter("PythiaFilter",
+    MaxRapidity = cms.untracked.double(2.5),
+    MinRapidity = cms.untracked.double(-2.5),
+    MinPt = cms.untracked.double(30.),
+    ParticleID = cms.untracked.int32(23)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*zgenfilter)
