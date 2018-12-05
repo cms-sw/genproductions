d28f81ab3ee498c9f59eb1dfa1c5d632304904ef
pu-sheng <sam7k9621@gmail.com>
2018-12-05 14:22:45 +0100

    Update extramodel.dat

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/TT_CEDM/ProduceOtherdtG.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/TT_CEDM/ProduceOtherdtG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 firstdtG=1
-lastdtG=1
+lastdtG=5
 echo "You are creating the cards for the TT_CEDM process, starting from dtG=$firstdtG GeV to dtG=$lastdtG in steps of 1."
 
 index=0
@@ -9,6 +9,7 @@ for i in $(seq $firstdtG 1 $lastdtG)
 do 
     cp -r TTbar_LO_CEDM_dtG0 TTbar_LO_CEDM_dtG${i}
     mv TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG0_customizecards.dat TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG${i}_customizecards.dat
+    mv TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG0_extramodels.dat TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG${i}_extramodels.dat
     mv TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG0_proc_card.dat      TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG${i}_proc_card.dat
     mv TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG0_run_card.dat       TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG${i}_run_card.dat
     sed  -i "s/set\ dtG\ 0/set\ dtG\ ${i}/g" TTbar_LO_CEDM_dtG${i}/TTbar_LO_CEDM_dtG${i}_customizecards.dat
