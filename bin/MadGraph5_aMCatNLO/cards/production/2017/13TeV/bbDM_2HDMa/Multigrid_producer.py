23e432cae7ef25dc12da0b833dce88da78931bfe
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2018-12-06 13:21:23 +0530

    updated cards for bbDM analysis with 2HDM+a model

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/bbDM_2HDMa/Multigrid_producer.py
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/bbDM_2HDMa/Multigrid_producer.py
@@ -1,9 +1,17 @@
+# import ROOT
+# from math import pi, sqrt, copysign
+# from ROOT import TH1F, TFile, TTree, TString, gSystem, gROOT, AddressOf, TLorentzVector, TVector, TMath
 import sys
 import os
+# import numpy as np
 
 verbose = False
+#cardsPath = sys.argv[1]
+# model = sys.argv[1] # either 2hdm or zpb
 old_cardsPath = 'Template'
 
+#cardsPath = 'cards/examples/'
+# gridpoint = 'MH3_600_MH4_XX_Mchi_YY'
 prefix    = 'bbDM_2HDMa_MH3_AA_MH4_XX_Mchi_YY'
 os.system('mkdir '+ old_cardsPath+'/'+prefix)
 
@@ -11,17 +19,27 @@ os.system('cp '+old_cardsPath+'/'+'run_card.dat'+' '+  old_cardsPath+'/'+prefix+
 os.system('cp '+old_cardsPath+'/'+'proc_card.dat'+' '+  old_cardsPath+'/'+prefix+'/bbDM_2HDMa_MH3_AA_MH4_XX_Mchi_YY_proc_card.dat' )
 os.system('cp '+old_cardsPath+'/'+'extramodels.dat'+' '+  old_cardsPath+'/'+prefix+'/bbDM_2HDMa_MH3_AA_MH4_XX_Mchi_YY_extramodels.dat' )
 os.system('cp '+old_cardsPath+'/'+'customizecards.dat'+' '+  old_cardsPath+'/'+prefix+'/bbDM_2HDMa_MH3_AA_MH4_XX_Mchi_YY_customizecards.dat' )
-os.system('cp '+old_cardsPath+'/'+'cuts.f'+' '+  old_cardsPath+'/'+prefix+'/bbDM_2HDMa_MH3_AA_MH4_XX_Mchi_YY_cuts.f' )
+os.system('cp '+old_cardsPath+'/'+'cuts.f'+' '+ old_cardsPath+'/'+prefix+'/bbDM_2HDMa_MH3_AA_MH4_XX_Mchi_YY_cuts.f' )
+os.system('cp '+old_cardsPath+'/'+'setcuts.f'+' '+ old_cardsPath+'/'+prefix+'/bbDM_2HDMa_MH3_AA_MH4_XX_Mchi_YY_setcuts.f' )
 
 d_cardspath = os.path.join(old_cardsPath, prefix)
+
 if verbose: print (d_cardspath)
+
 d_run_card       = os.path.join(d_cardspath,prefix+'_run_card.dat')
 d_proc_card      = os.path.join(d_cardspath,prefix+'_proc_card.dat')
 d_extramodels    = os.path.join(d_cardspath,prefix+'_extramodels.dat')
 d_customizecards = os.path.join(d_cardspath,prefix+'_customizecards.dat')
 d_cutcards       = os.path.join(d_cardspath,prefix+'_cuts.f')
+d_setcutcards    = os.path.join(d_cardspath,prefix+'_setcuts.f')
 
-if verbose: print (d_run_card, d_proc_card, d_extramodels, d_customizecards)
+# print ()
+
+#
+if verbose: print (d_run_card, d_proc_card, d_extramodels, d_customizecards, d_cutcards, d_setcutcards)
+
+
+
 
 def change_cards(d_cardname, cardname,MH3, MH4, Mchi):
     f    = open(d_cardname, 'r')
@@ -34,6 +52,9 @@ def change_cards(d_cardname, cardname,MH3, MH4, Mchi):
     fout.close()
     print ("Cardname",cardname)
 
+
+
+
 def submitgrid(MH3, MH4, Mchi):
     cardspath = d_cardspath.replace("AA",str(MH3)).replace("XX",str(MH4)).replace("YY",str(Mchi))
     print ("cardpath",cardspath)
@@ -43,29 +64,34 @@ def submitgrid(MH3, MH4, Mchi):
     proc_card      = d_proc_card.replace("AA",str(MH3)).replace("XX",str(MH4)).replace("YY",str(Mchi))
     extramodels    = d_extramodels.replace("AA",str(MH3)).replace("XX",str(MH4)).replace("YY",str(Mchi))
     customizecards = d_customizecards.replace("AA",str(MH3)).replace("XX",str(MH4)).replace("YY",str(Mchi))
-    cutcards = d_cutcards.replace("AA",str(MH3)).replace("XX",str(MH4)).replace("YY",str(Mchi))
+    cutcards       = d_cutcards.replace("AA",str(MH3)).replace("XX",str(MH4)).replace("YY",str(Mchi))
+    setcutcards    = d_setcutcards.replace("AA",str(MH3)).replace("XX",str(MH4)).replace("YY",str(Mchi))
 
     change_cards(d_run_card, run_card, MH3, MH4, Mchi)
     change_cards(d_proc_card, proc_card,MH3 , MH4, Mchi)
     change_cards(d_extramodels, extramodels, MH3, MH4, Mchi)
     change_cards(d_customizecards, customizecards, MH3, MH4, Mchi)
     change_cards(d_cutcards, cutcards, MH3,MH4, Mchi)
+    change_cards(d_setcutcards, setcutcards, MH3,MH4, Mchi)
 
     outdir = prefix.replace("AA",str(MH3)).replace("XX",str(MH4)).replace("YY",str(Mchi))
     print ("output dir",outdir)
     os.system('nohup ./submit_cmsconnect_gridpack_generation.sh  '+ outdir +' '+ cardspath +'  4 "4 Gb" > mysubmit_'+outdir+'.debug 2>&1 &')
+    #os.system('./submit_gridpack_generation.sh 30000 30000 2nw '+ outdir +' '+ cardspath +'  2nw')
+    #os.system('nohup ./submit_cmsconnect_gridpack_generation.sh  '+ outdir +' '+ cardspath +'  4 "4 Gb" > mysubmit_'+outdir+'.debug 2>&1 &')
 
-Mchi = [10]
-MH3 = [600, 700, 800, 900, 1000, 1100, 1200]
+Mchi = [1]
+MH3 = [600,1200]
+MH4 = [10,50,100,150,200,250,300,350,400,450,500,700,750,1000]
 
 for k in MH3:
     for i in Mchi:
-        for j in range(100, 1200, 100):
+        for j in MH4:
             if k > 1000 and j > 1000:
                 break
             if j < k:
                 mh3 = k
                 mh4 = j
                 mchi = i
-                print("MH3=", mh3, "MH4= ", mh4)
+                print("MH3=", mh3, "MH4= ", mh4,"Mchi= ", mchi)
                 submitgrid(mh3, mh4, mchi)
