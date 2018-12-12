6fc0c6ebaefc72880b3d03e5bb912f0b977061e4
Simon <simon.regnard@cern.ch>
2018-12-12 18:47:43 +0100

    scriptify

---
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/exo_diboson/Spin-2/BulkGraviton_WW_WlepWhad/BulkGraviton_WW_WlepWhad_narrow.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+masses=(600 800 1000 1200 1400 1600 1800 2000 2500 3000 3500 4000 4500 5000 5500)
+sample=BulkGraviton_WW_WlepWhad_narrow_M
+
+postfix=(_run_card.dat _customizecards.dat _proc_card.dat _extramodels.dat)
+
+echo ${masses[*]}
+
+for mass in ${masses[*]}; do
+    echo generating cards for M = $mass GeV
+    
+    mkdir ${sample}${mass}
+           
+    for i in {0..3}; do
+	sed "s/<MASS>/${mass}/g" ${sample}/${sample}${postfix[$i]} > ${sample}$mass/${sample}$mass${postfix[$i]}
+    done    
+done
