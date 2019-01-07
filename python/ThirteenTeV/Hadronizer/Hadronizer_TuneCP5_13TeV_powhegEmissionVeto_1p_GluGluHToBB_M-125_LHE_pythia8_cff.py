3055b834dab6fd9c4ba4956f279b56ba0a703561
lata <panwarlsweet@gmail.com>
2019-01-07 19:22:30 +0100

    correcting cards and hadroniser according to comments

--- python/ThirteenTeV/Hadronizer/Hadronizer_TuneCP5_13TeV_powhegEmissionVeto_1p_GluGluHToBB_M-125_LHE_pythia8_cff.py
+++ python/ThirteenTeV/Hadronizer/Hadronizer_TuneCP5_13TeV_powhegEmissionVeto_1p_GluGluHToBB_M-125_LHE_pythia8_cff.py
@@ -14,7 +14,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
 	pythia8CP5SettingsBlock,
         pythia8PowhegEmissionVetoSettingsBlock,
         processParameters = cms.vstring(
-            #'POWHEG:nFinal = 1',   ## Number of final state particles
+            'POWHEG:nFinal = 2',   ## Number of final state particles
                                    ## (BEFORE THE DECAYS) in the LHE
                                    ## other than emitted extra parton
             '25:m0 = 125.0',
