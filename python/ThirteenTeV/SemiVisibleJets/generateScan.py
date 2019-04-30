8d6473024b966aa91bad594b18bfc647c526abcd
Kevin Pedro <kpedro88@gmail.com>
2019-04-30 09:25:38 -0500

    coarser mZprime grid, increase # events/sample based on acceptance

--- python/ThirteenTeV/SemiVisibleJets/generateScan.py
+++ python/ThirteenTeV/SemiVisibleJets/generateScan.py
@@ -5,6 +5,7 @@ from Configuration.GenProduction.ThirteenTeV.SemiVisibleJets.svjHelper import sv
 from collections import OrderedDict
 from copy import deepcopy
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import numpy as np
 
 # implementation of recursive loop over any number of dimensions
 # creates grid of all possible combinations of parameter values
@@ -21,6 +22,8 @@ def varyAll(pos,paramlist,sig,sigs):
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument("-y","--year", dest="year", type=int, default=2016, help="which year to simulate (specifies generator tune)")
+parser.add_argument("-n","--num", dest="num", type=int, default=20000, help="number of events for model point w/ weight 1.0 (before filter)")
+parser.add_argument("-a","--acc", dest="acc", type=float, default=0.0, help="increase number of events based on acceptance up to this maximum factor")
 args = parser.parse_args()
 
 # specification of tunes for each year
@@ -37,11 +40,39 @@ else:
 
 # complete set of parameter values
 params = OrderedDict([
-    ("mZprime", range(1000,5100,100)),
+    ("mZprime", range(1500,5200,200)),
     ("mDark", [1,5] + range(10,110,10)),
     ("rinv", [float(x)/10 for x in range(0,11,1)]),
     ("alpha", ["peak", "high", "low"]),
 ])
+
+# convert named alpha values to numerical
+alpha_vals = {
+    "peak": -2,
+    "high": -1,
+    "low": -3,
+}
+# acceptance values vs. each param
+acc = OrderedDict([
+    ("mZprime", ([500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,3100,3200,3300,3400,3500,3600,3700,3800,3900,4000,4100,4200,4300,4400,4500],[4.1e-05,0.00012,0.00012,4.1e-05,0.00027,0.0003,0.00035,0.00033,0.00053,0.0011,0.0014,0.0042,0.0089,0.015,0.023,0.031,0.037,0.047,0.051,0.057,0.061,0.067,0.07,0.074,0.079,0.08,0.081,0.084,0.088,0.089,0.09,0.093,0.093,0.092,0.095,0.098,0.099,0.097,0.098,0.1,0.1])),
+    ("mDark", ([1,5,10,20,30,40,50,60,70,80,90,100],[0.084,0.076,0.074,0.08,0.08,0.079,0.08,0.078,0.076,0.076,0.073,0.071])),
+    ("rinv", ([0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1],[0.00013,0.03,0.06,0.08,0.089,0.085,0.067,0.042,0.02,0.0054,0.0001])),
+    ("alpha", ([-2,-1,-3],[0.08,0.076,0.099])),
+])
+# acceptance w/ benchmark param values
+base_acc = 0.08
+# function to use pair of arrays as lookup table
+def find_nearest(val,xy):
+    x_array = np.asarray(xy[0])
+    idx = (np.abs(x_array - val)).argmin()
+    return xy[1][idx]
+# function to retrieve multiplied relative acceptance
+def get_acc(point):
+    this_acc = 1.0
+    for param,pval in point.iteritems():
+        pval = alpha_vals[pval] if param=="alpha" else pval
+        this_acc *= find_nearest(pval,acc[param])/base_acc
+    return this_acc
 
 # set to accumulate all scan points
 sigs = set()
@@ -64,9 +95,6 @@ params_alpha["rinv"] = [0.3]
 params_alpha["mDark"] = [20]
 varyAll(0,list(params_alpha.iteritems()),[],sigs)
 
-# some info on the scan
-print("This scan will contain "+str(len(sigs))+" model points")
-
 # format first part of output config
 first_part = """
 import FWCore.ParameterSet.Config as cms
@@ -87,16 +115,29 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
 # append process parameters for each model point
 helper = svjHelper()
 points = []
+numevents_before = 0
+numevents_after = 0
+base_filter_eff = 0.5
 for point in sorted(sigs):
     mZprime = point[0]
     mDark = point[1]
     rinv = point[2]
     alpha = point[3]
 
-    # down-weight rinv=0 b/c all events pass filter
     weight = 1.0
-    if rinv==0.0: weight = 0.5
+    filter_eff = base_filter_eff
+    # down-weight rinv=0 b/c all events pass filter
+    if rinv==0.0:
+        weight = 0.5
+        filter_eff = 1.0
     
+    # account for relative acceptance
+    if args.acc > 1:
+        this_acc = get_acc(OrderedDict([("mZprime",mZprime),("mDark",mDark),("rinv",rinv),("alpha",alpha)]))
+        min_weight = weight
+        max_weight = weight*args.acc
+        weight = np.clip(weight/this_acc,min_weight,max_weight)
+
     helper.setModel(mZprime,mDark,rinv,alpha)
     
     pdict = {
@@ -105,6 +146,11 @@ for point in sorted(sigs):
         'name': helper.getOutName(outpre="SVJ",outsuff=""),
     }
     points.append(pdict)
+    numevents_before += args.num*weight
+    numevents_after += args.num*weight*filter_eff
+
+# some info on the scan
+print("This scan will contain "+str(len(sigs))+" model points, "+str(int(numevents_before))+" events before filter, "+str(int(numevents_after))+" events after filter")
 
 # format last part of config (loop over all points)
 last_part = """
