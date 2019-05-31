750cedce375752004b20f5fc0b7df2189f263a02
Georgia <georgia.karapostoli@gmail.com>
2019-05-31 21:55:23 +0200

    One more GEN filter needed

--- python/ThirteenTeV/VH_M125_Toa01a01_Tobbbb_M12to60-LO_Pythia_cfg.py
+++ python/ThirteenTeV/VH_M125_Toa01a01_Tobbbb_M12to60-LO_Pythia_cfg.py
@@ -6,7 +6,6 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
     PythiaParameters = cms.PSet(
         parameterSets = cms.vstring('pythia8CommonSettings', 
             'pythia8CP5Settings', 
-            'processParameters',
             'pythia8PSweightsSettings'),
         pythia8CP5Settings = cms.vstring('Tune:pp 14', 
             'Tune:ee 7', 
@@ -35,7 +34,6 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
             'ParticleDecays:limitTau0 = on', 
             'ParticleDecays:tau0Max = 10', 
             'ParticleDecays:allowPhotonRadiation = on'),
-        processParameters = cms.vstring('15:onMode = off','15:onIfAny = 11 13'), 
         pythia8PSweightsSettings = cms.vstring('UncertaintyBands:doVariations = on', 
             'UncertaintyBands:List = {isrRedHi isr:muRfac=0.707,fsrRedHi fsr:muRfac=0.707,isrRedLo isr:muRfac=1.414,fsrRedLo fsr:muRfac=1.414,isrDefHi isr:muRfac=0.5,fsrDefHi fsr:muRfac=0.5,isrDefLo isr:muRfac=2.0,fsrDefLo fsr:muRfac=2.0,isrConHi isr:muRfac=0.25,fsrConHi fsr:muRfac=0.25,isrConLo isr:muRfac=4.0,fsrConLo fsr:muRfac=4.0,fsr_G2GG_muR_dn fsr:G2GG:muRfac=0.5,fsr_G2GG_muR_up fsr:G2GG:muRfac=2.0,fsr_G2QQ_muR_dn fsr:G2QQ:muRfac=0.5,fsr_G2QQ_muR_up fsr:G2QQ:muRfac=2.0,fsr_Q2QG_muR_dn fsr:Q2QG:muRfac=0.5,fsr_Q2QG_muR_up fsr:Q2QG:muRfac=2.0,fsr_X2XG_muR_dn fsr:X2XG:muRfac=0.5,fsr_X2XG_muR_up fsr:X2XG:muRfac=2.0,fsr_G2GG_cNS_dn fsr:G2GG:cNS=-2.0,fsr_G2GG_cNS_up fsr:G2GG:cNS=2.0,fsr_G2QQ_cNS_dn fsr:G2QQ:cNS=-2.0,fsr_G2QQ_cNS_up fsr:G2QQ:cNS=2.0,fsr_Q2QG_cNS_dn fsr:Q2QG:cNS=-2.0,fsr_Q2QG_cNS_up fsr:Q2QG:cNS=2.0,fsr_X2XG_cNS_dn fsr:X2XG:cNS=-2.0,fsr_X2XG_cNS_up fsr:X2XG:cNS=2.0,isr_G2GG_muR_dn isr:G2GG:muRfac=0.5,isr_G2GG_muR_up isr:G2GG:muRfac=2.0,isr_G2QQ_muR_dn isr:G2QQ:muRfac=0.5,isr_G2QQ_muR_up isr:G2QQ:muRfac=2.0,isr_Q2QG_muR_dn isr:Q2QG:muRfac=0.5,isr_Q2QG_muR_up isr:Q2QG:muRfac=2.0,isr_X2XG_muR_dn isr:X2XG:muRfac=0.5,isr_X2XG_muR_up isr:X2XG:muRfac=2.0,isr_G2GG_cNS_dn isr:G2GG:cNS=-2.0,isr_G2GG_cNS_up isr:G2GG:cNS=2.0,isr_G2QQ_cNS_dn isr:G2QQ:cNS=-2.0,isr_G2QQ_cNS_up isr:G2QQ:cNS=2.0,isr_Q2QG_cNS_dn isr:Q2QG:cNS=-2.0,isr_Q2QG_cNS_up isr:Q2QG:cNS=2.0,isr_X2XG_cNS_dn isr:X2XG:cNS=-2.0,isr_X2XG_cNS_up isr:X2XG:cNS=2.0}', 
             'UncertaintyBands:nFlavQ = 4', 
@@ -73,18 +71,18 @@ genSelectorLep = cms.EDFilter("GenParticleSelector",
   cut = cms.string('(abs(pdgId()) == 11 || abs(pdgId()) == 13 )')
 )
 
-selectedOneLepCandFilter = cms.EDFilter("CandViewCountFilter",
-   src = cms.InputTag('genSelectorLep'),
-   filter = cms.bool(True),
-   minNumber = cms.uint32(1)
-)
-
 bestLeps = cms.EDFilter("EtaPtMinCandViewSelector",
-    src = cms.InputTag("genSelectorLep"),
+    src = cms.InputTag('genSelectorLep'),
     filter = cms.bool(True),				
     ptMin   = cms.double(10),
     etaMin = cms.double(-2.5),
     etaMax = cms.double(2.5)
 )
 
-ProductionFilterSequence = cms.Sequence(generator * (genParticlesForFilter + genSelectorLep + selectedOneLepCandFilter + bestLeps))
+selectedOneLepCandFilter = cms.EDFilter("CandViewCountFilter", 
+   src = cms.InputTag('bestLeps'), 
+   filter = cms.bool(True), 
+   minNumber = cms.uint32(1) 
+)               
+
+ProductionFilterSequence = cms.Sequence(generator * (genParticlesForFilter + genSelectorLep + bestLeps + selectedOneLepCandFilter))
