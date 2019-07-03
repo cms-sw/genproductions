a4e934532473ac9f7b21ecddf5b3fe99ee81abb9
Roberto Covarelli <roberto.covarelli@cern.ch>
2019-07-03 13:35:14 +0200

    fix wrong tarball and max weights

--- bin/Powheg/run_pwg_condor.py
+++ bin/Powheg/run_pwg_condor.py
@@ -391,7 +391,7 @@ if [[ -s ./JHUGen.input ]]; then
 fi
 
 ### retrieve the powheg source tar ball
-export POWHEGSRC=powhegboxV2_rev3624_date20190201.tar.gz
+export POWHEGSRC=powhegboxV2_rev3624_date20190117.tar.gz
 
 if [ "$process" = "b_bbar_4l" ] || [ "$process" = "HWJ_ew" ] || [ "$process" = "HW_ew" ] || [ "$process" = "HZJ_ew" ] || [ "$process" = "HZ_ew" ]; then 
   export POWHEGSRC=powhegboxRES_rev3478_date20180122.tar.gz 
@@ -442,7 +442,7 @@ if [ "$process" = "Zj" ] || [ "$process" = "Wj" ]; then
 fi
 
 
-sed -i -e "s#500#1200#g"  POWHEG-BOX/include/pwhg_rwl.h
+sed -i -e "s#500#1350#g"  POWHEG-BOX/include/pwhg_rwl.h
 
 echo ${POWHEGSRC} > VERSION
 
