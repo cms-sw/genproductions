ed519aa5c7ae51d0633d11ab76ae0fc238a062a5
S. Lehti <sami.lehti@cern.ch>
2019-07-26 09:17:04 +0200

    Updated ChargedHiggs_TB: moved to 4FS, removing madspin, renaming, removing mass points above 3TeV

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ChargedHiggs_TB/ChargedHiggs_TB_NLO_cards.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ChargedHiggs_TB/ChargedHiggs_TB_NLO_cards.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-masses=(200 220 250 300 350 400 500 600 700 800 1000 1250 1500 1750 2000 2500 3000 4000 5000)
+masses=(200 220 250 300 350 400 500 600 700 800 1000 1250 1500 1750 2000 2500 3000)
 
-sample=ChargedHiggs_TB_madspin_NLO
-postfix=(_run_card.dat _customizecards.dat _proc_card.dat _extramodels.dat _madspin_card.dat)
+sample=ChargedHiggs_TB_NLO
+postfix=(_run_card.dat _customizecards.dat _proc_card.dat _extramodels.dat)
 
 #echo ${masses[*]}
 
