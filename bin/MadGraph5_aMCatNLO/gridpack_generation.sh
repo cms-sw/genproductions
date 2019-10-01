c8f9e9b98cd57c7f0cedb3093ff2419536bf728f
DryRun <david.renhwa.yu@gmail.com>
2019-10-01 14:25:59 -0500

    OS check: don't change whitespace

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -88,10 +88,10 @@ make_gridpack () {
     
     #   export SCRAM_ARCH=slc6_amd64_gcc472 #Here one should select the correct architechture corresponding with the CMSSW release
     #   export RELEASE=CMSSW_5_3_32_patch3
-
+    
       export SCRAM_ARCH=${scram_arch}
       export RELEASE=${cmssw_version}
-      
+    
       ############################
       #Create a workplace to work#
       ############################
