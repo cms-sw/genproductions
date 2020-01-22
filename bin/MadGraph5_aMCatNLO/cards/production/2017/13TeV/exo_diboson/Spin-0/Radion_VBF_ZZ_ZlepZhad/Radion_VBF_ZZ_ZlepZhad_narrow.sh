b9ba91ff27939d68f954557473e60866310f6f91
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2020-01-22 16:36:56 +0100

    Add more mass points at the lower end

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/exo_diboson/Spin-0/Radion_VBF_ZZ_ZlepZhad/Radion_VBF_ZZ_ZlepZhad_narrow.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/exo_diboson/Spin-0/Radion_VBF_ZZ_ZlepZhad/Radion_VBF_ZZ_ZlepZhad_narrow.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-masses=(600 800 1000 1200 1400 1600 1800 2000 2500 3000 3500 4000 4500 5000 6000 7000 8000)
+masses=(400 450 500 550 600 800 1000 1200 1400 1600 1800 2000 2500 3000 3500 4000 4500 5000 6000 7000 8000)
+
 sample=Radion_VBF_ZZ_ZlepZhad_narrow_M
 
 postfix=(_run_card.dat _customizecards.dat _proc_card.dat _extramodels.dat)
@@ -11,7 +12,8 @@ for mass in ${masses[*]}; do
     echo generating cards for M = $mass GeV
     
     mkdir ${sample}${mass}
-    for ((i=0;i<${#postfix[@]};i+=1)); do           
-	sed "s/<MASS>/${mass}/g" ${sample}/${sample}${postfix[$i]} > ${sample}$mass/${sample}$mass${postfix[$i]}
+    
+    for i in {0..3}; do           
+      sed "s/<MASS>/${mass}/g" ${sample}/${sample}${postfix[$i]} > ${sample}$mass/${sample}$mass${postfix[$i]}
     done    
 done
