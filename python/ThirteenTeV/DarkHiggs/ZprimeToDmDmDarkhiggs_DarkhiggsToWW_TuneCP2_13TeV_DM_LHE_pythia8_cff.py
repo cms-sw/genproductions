35f71621c02ed94aff22150a808b7f2739f30552
calderon <calderon@cern.ch>
2020-02-04 17:00:35 +0100

    update tune to CP5

--- python/ThirteenTeV/DarkHiggs/ZprimeToDmDmDarkhiggs_DarkhiggsToWW_TuneCP2_13TeV_DM_LHE_pythia8_cff.py
+++ python/ThirteenTeV/DarkHiggs/ZprimeToDmDmDarkhiggs_DarkhiggsToWW_TuneCP2_13TeV_DM_LHE_pythia8_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
@@ -24,7 +24,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
 
             ),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CP2Settings',
+                                    'pythia8CP5Settings',
                                     'processParameters',
                                     'pythia8PSweightsSettings'
                                     )
