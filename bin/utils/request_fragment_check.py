a9d3e30dbe3dbf5e2705efdd1c6622a72795f98c
Efe <efe.yazgan@cern.ch>
2020-03-02 10:10:25 +0100

    fix reweight_scale parse for NLO

--- bin/utils/request_fragment_check.py
+++ bin/utils/request_fragment_check.py
@@ -1054,7 +1054,7 @@ for num in range(0,len(prepid)):
                                         warning += 1
                                         continue
                                     r_scale = os.popen('more '+filename_rc+' | tr -s \' \' | grep "reweight_scale"').read()
-                                    r_scale = r_scale.split()[0].split('.')[1]
+                                    r_scale = r_scale.split()[0]#.split('.')[1]
                                     if len(r_scale) == 0 or "true" not in str(r_scale).lower():
                                         print "* [ERROR] For NLO MG5_aMC version < 260, one should have .true. = reweight_scale"
                                         error += 1
