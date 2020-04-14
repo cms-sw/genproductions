1f699aa2eedd77329bb8f7a45992955f72e31c00
John Hakala <john_hakala@virginia.edu>
2020-04-14 14:01:08 -0500

    add Hg example cards and script to generate cards for other mass points

---
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/Zprime_Gh/make-masspoint.py
@@ -0,0 +1,32 @@
+def makeMassPoint(mass):
+  from os import mkdir
+  from os.path import join
+  from shutil import copyfile
+
+  templDir = "Zprime_Gh_M1200"
+  outDir = "Zprime_Gh_M%i" % mass
+  mkdir(outDir)
+
+  templates = [join(templDir, card) for card in  [
+                                                        "ZpHgamma_UFO-M1200_proc_card.dat"   ,
+                                                        "ZpHgamma_UFO-M1200_extramodels.dat" ,
+                                                        "ZpHgamma_UFO-M1200_run_card.dat"    
+                                                      ]
+              ]
+  for template in templates:
+    copyfile(template, template.replace("1200", str(mass)))
+  with open(join(outDir, "ZpHgamma_UFO-M%i_customizecards.dat" % mass), "w") as cc:
+    cc.write("set param_card MZp %i\n" % mass)
+    cc.write("set param_card WZp %0.3f" % (mass/float(1e4)))
+
+if __name__ == "__main__":
+  from sys import argv
+  
+  if len(argv) != 2:
+    print "please run this script with one argument, the mass of the Zp->Gh sample to make"
+    exit(1)
+  
+  mass = int(argv[1])
+  makeMassPoint(mass)
+
+
