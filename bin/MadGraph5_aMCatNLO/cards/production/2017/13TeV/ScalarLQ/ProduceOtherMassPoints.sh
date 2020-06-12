83e32dba569d1b032d90d2f3b8bea0373fd2e362
IzaakWN <izaak.neutelings@cern.ch>
2020-06-12 12:44:34 +0200

    remove redundancies between customize and run card: only use run_card for PDF & scale setting

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ScalarLQ/ProduceOtherMassPoints.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ScalarLQ/ProduceOtherMassPoints.sh
@@ -3,8 +3,8 @@
 PROCS="Single" #Pair NonRes"
 MASSES=`seq 500 300 2300`
 LAMBDAS="1.5 2.0 2.5"
-#PDF=315200 # NNPDF31_lo_as_0130
-PDF=247000 # NNPDF23_lo_as_0130_qed
+PDF=315200 # NNPDF31_lo_as_0130 for TuneCP2 (2017, 2018)
+#PDF=247000 # NNPDF23_lo_as_0130_qed for TuneCUETP8M1 (2016)
 
 for proc in $PROCS; do
   proc="${proc}ScalarLQToBTau"
@@ -13,6 +13,7 @@ for proc in $PROCS; do
   for lambda in $LAMBDAS; do
     for mass in $MASSES; do
       [ $lambda = 1.0 -a $mass = 500 ] && continue
+      echo
       echo ">>> Mass = ${mass}, lambda = $lambda"
       
       # SETTINGS
@@ -31,13 +32,18 @@ for proc in $PROCS; do
       # SET PARAMETERS
       SCALES="scale dsqrt_q2fact1 dsqrt_q2fact2"
       for scale in $SCALES; do
-        sed -i -e "s/$scale 500/$scale ${mass}/" $CARDDIR/${SAMPLE}_customizecards.dat
-        sed -i -e "s/500\(\s*= ${scale}\)/${mass}\1/" $CARDDIR/${SAMPLE}_run_card.dat # should be covered by customizecards.dat
+        #sed -i -e "s/$scale 500/$scale ${mass}/" $CARDDIR/${SAMPLE}_customizecards.dat
+        sed -i -e "s/500\(\s*=\s*${scale}\)/${mass}\1/" $CARDDIR/${SAMPLE}_run_card.dat
       done
       sed -i -e "s/FRBLOCK 3 1.0/FRBLOCK 3 ${lambda}/" $CARDDIR/${SAMPLE}_customizecards.dat
       sed -i -e "s/MASS 9000006 500/MASS 9000006 ${mass}/" $CARDDIR/${SAMPLE}_customizecards.dat
-      sed -i -e "s/lhaid [0-9]\+/lhaid ${PDF}/" $CARDDIR/${SAMPLE}_customizecards.dat
+      #sed -i -e "s/lhaid [0-9]\+/lhaid ${PDF}/" $CARDDIR/${SAMPLE}_customizecards.dat
+      sed -i -e "s/315200\(\s*=\s*lhaid\)/${PDF}\1/" $CARDDIR/${SAMPLE}_run_card.dat
       sed -i -e "s/output $SAMPLE0/output $SAMPLE/" $CARDDIR/${SAMPLE}_proc_card.dat
+      
+      diff $SAMPLE0/${SAMPLE0}_proc_card.dat $CARDDIR/${SAMPLE}_proc_card.dat
+      diff $SAMPLE0/${SAMPLE0}_customizecards.dat $CARDDIR/${SAMPLE}_customizecards.dat
+      diff $SAMPLE0/${SAMPLE0}_run_card.dat $CARDDIR/${SAMPLE}_run_card.dat
       
     done
   done
