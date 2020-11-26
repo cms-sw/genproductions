09e348d4e458b35d4fe60def946f6b9989c08c81
grohsjea <alexander.grohsjean@cern.ch>
2020-11-26 21:03:52 +0100

    started cleaning up genproduction

--- Utilities/scripts/PDFSetsChooserTools.py
+++ Utilities/scripts/PDFSetsChooserTools.py
@@ -9,7 +9,7 @@ class PDFSetHelper(object):
 
     def readDefaultPDFsFile(self, is5FlavorScheme):
         base_dir = os.path.dirname(os.path.realpath(__file__)) 
-        meta_data_dir = base_dir.replace("Utilities/python", "MetaData") 
+        meta_data_dir = base_dir.replace("Utilities/scripts", "MetaData") 
 
         self.pdflist_file = meta_data_dir + "/" + ("pdflist_5f_2017.dat" \
             if is5FlavorScheme else "pdflist_4f_2017.dat")
