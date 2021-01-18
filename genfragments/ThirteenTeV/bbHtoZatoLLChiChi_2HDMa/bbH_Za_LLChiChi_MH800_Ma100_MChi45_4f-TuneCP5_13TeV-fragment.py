889963153a2f35a7469d90d8dc8a1d3fbe55b036
Perez Danyer <danyer.perez.adan@desy.de>
2021-01-18 12:46:39 +0100

    Adding gridpack cards and hadronizer fragments for process bbH->Za->LLChiChi

---
+++ genfragments/ThirteenTeV/bbHtoZatoLLChiChi_2HDMa/bbH_Za_LLChiChi_MH800_Ma100_MChi45_4f-TuneCP5_13TeV-fragment.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/eos/user/d/dperezad/Store/Gridpacks/bbHtoZatoLLChiChi_2HDMa/bbH_Za_LLChiChi_MH800_Ma100_MChi45_4f_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings'
+                                    )
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
