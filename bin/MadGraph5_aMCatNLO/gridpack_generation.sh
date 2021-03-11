c8c692a2d833e9970a7049c2b9e29772adbeda76
agrohsje <agrohsje@users.noreply.github.com>
2021-03-11 11:41:26 +0100

    Update gridpack_generation.sh
    bug fix when checking compute_width option

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -85,8 +85,8 @@ make_gridpack () {
         echo "This could be problematic from time to time, so instead use <<set decay wX AUTO>> for width computations. Please take a look at \"compute_widths\" under \"Troubleshooting_and_Suggestions\" section."
         echo "https://twiki.cern.ch/twiki/bin/view/CMS/QuickGuideMadGraph5aMCatNLO#Troubleshooting_and_Suggestions"
         exit 1;
-      fi
       else return 1;
+      fi
     fi
 
     # CMS Connect runs git status inside its own script.
