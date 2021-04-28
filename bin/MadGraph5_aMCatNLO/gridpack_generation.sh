33d9476095fa36a9b846066a7296b1e0a8adfb3a
grohsjea <alexander.grohsjean@cern.ch>
2021-04-28 11:42:56 +0200

    added check of ME weight names

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -79,6 +79,8 @@ make_gridpack () {
       echo $CARDSDIR/${name}_run_card.dat " does not exist!"
       if [ "${BASH_SOURCE[0]}" != "${0}" ]; then return 1; else exit 1; fi
     fi
+    
+    # avoid compute_widths in customizecards 
     if [ -e $CARDSDIR/${name}_customizecards.dat ]; then
         if grep -F "compute_widths" $CARDSDIR/${name}_customizecards.dat ; then
             echo "<<compute_widths X>> is used in your customizecards.dat"
@@ -86,6 +88,22 @@ make_gridpack () {
             echo "https://twiki.cern.ch/twiki/bin/view/CMS/QuickGuideMadGraph5aMCatNLO#Troubleshooting_and_Suggestions"
             exit 1;
         fi
+    fi
+
+    # avoid characters in weight names that potentially corrupt lhe header 
+    if [ -e $CARDSDIR/${name}_reweight_card.dat ]; then
+        for weightname in `grep rwgt_name $CARDSDIR/${name}_reweight_card.dat` ; do
+            if [[ "$weightname" == *"rwgt_name="* ]]; then
+                remove="rwgt_name="
+                weightname=${weightname#*$remove}
+            else
+                continue
+            fi
+            if [[ $weightname == *['!'@#\$%^\&*()\+\-\[\]{}\.]* ]]; then 
+                echo " Please remove problematic characters from weight name: $weightname"  
+                exit 1;    
+            fi
+        done 
     fi
 
     # CMS Connect runs git status inside its own script.
