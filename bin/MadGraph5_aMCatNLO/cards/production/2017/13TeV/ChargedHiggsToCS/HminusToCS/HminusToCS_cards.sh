8daabbc9586cdb7ed72adda5e25f3615219899b9
Sanu Varghese <74061496+sanuvarghese@users.noreply.github.com>
2021-07-22 19:13:52 +0530

    ChargedHiggsToCS

---
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ChargedHiggsToCS/HminusToCS/HminusToCS_cards.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+masses=(80 90 100 110 120 130 140 150 160)
+
+sample=HminusToCS
+postfix=(_run_card.dat _param_card.dat _proc_card.dat _extramodels.dat _madspin_card.dat)
+
+#echo ${masses[*]}
+
+# get length of an array
+tLen=${#postfix[@]}
+
+for mass in ${masses[*]}; do
+    echo generating cards for M = $mass GeV
+
+    for (( i=0; i<${tLen}; i++ )) do
+        if [ ! -d ${sample}_M${mass} ]; then
+            mkdir ${sample}_M${mass}
+        fi
+        sed "s/<MASS>/${mass}/g" ${sample}/${sample}${postfix[$i]} > ${sample}_M$mass/${sample}_M$mass${postfix[$i]}
+    done  
+done
