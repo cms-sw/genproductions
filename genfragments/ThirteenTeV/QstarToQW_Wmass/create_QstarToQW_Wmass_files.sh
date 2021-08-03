8bb5eb772dab7641fab97176e70041aa12ad1e7d
William McCormack <wmccorma@mit.edu>
2021-08-03 16:25:02 -0500

    adding genfragments for QstarToQW 1+2 topology, adjusting Qstar mass and W mass

---
+++ genfragments/ThirteenTeV/QstarToQW_Wmass/create_QstarToQW_Wmass_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in 2000 3000 4000; do
+    for j in 25 80 170 400; do
+	cp -v QstarToQW_M_2000_mW_25_TuneCUETP8M1_13TeV_pythia8_cfi.py $(echo QstarToQW_M_2000_mW_25_TuneCUETP8M1_13TeV_pythia8_cfi.py | sed "s/2000/$i/" | sed "s/25/$j/")
+	cp -v QstarToQW_M_2000_mW_25_Tune4C_13TeV_pythia8_cfi.py $(echo QstarToQW_M_2000_mW_25_Tune4C_13TeV_pythia8_cfi.py | sed "s/2000/$i/" | sed "s/25/$j/")
+    done
+done
+
+for i in QstarToQW_M_*_mW_*_Tune*.py; do
+    ENERGY=$(echo $i | awk -F_ '{ print $3 }')
+    MASS=$(echo $i | awk -F_ '{ print $5 }')
+    sed -i "s/2000/$ENERGY/" $i
+    sed -i "s/25/$MASS/" $i
+done
+
