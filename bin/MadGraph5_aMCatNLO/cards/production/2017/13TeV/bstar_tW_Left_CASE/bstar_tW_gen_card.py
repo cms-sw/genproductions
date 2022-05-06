aaf2f08d1743ce627455d5397f252efe8d93a450
William McCormack <william.patrick.mc.cormack.iii@cern.ch>
2022-05-06 04:42:35 +0200

    adding cards for bstar to tW for CASE analysis

---
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/bstar_tW_Left_CASE/bstar_tW_gen_card.py
@@ -0,0 +1,32 @@
+#! /bin/env python
+
+import os, shutil, subprocess
+
+Ref_dir = "bstar_tW"
+pdgidBStar = '1005'
+pdgidW = '24'
+
+MassesBStar = [2000, 3000, 5000]
+MassesW = [25,80,170,400]
+
+WidthBStar = 0.03
+
+for mbs in MassesBStar:
+        for mw in MassesW:
+                sampleName = Ref_dir+'_MB'+str(mbs)+'_MW'+str(mw)
+                print sampleName
+                if os.path.isdir(sampleName):
+                        shutil.rmtree(sampleName)
+                os.makedirs(sampleName)
+                # Copy cards
+                shutil.copyfile(Ref_dir+'_MB_MW/'+Ref_dir+'_MB_MW_run_card.dat',sampleName+'/'+sampleName+'_run_card.dat')
+                shutil.copyfile(Ref_dir+'_MB_MW/'+Ref_dir+'_MB_MW_proc_card.dat',sampleName+'/'+sampleName+'_proc_card.dat')
+                shutil.copyfile(Ref_dir+'_MB_MW/'+Ref_dir+'_MB_MW_customizecards.dat',sampleName+'/'+sampleName+'_customizecards.dat')
+                shutil.copyfile(Ref_dir+'_MB_MW/'+Ref_dir+'_MB_MW_extramodels.dat',sampleName+'/'+sampleName+'_extramodels.dat')
+                #customization
+                with open("{0}/{0}_proc_card.dat".format(sampleName), "a") as f:
+                        f.write("output "+sampleName)
+                with open("{0}/{0}_customizecards.dat".format(sampleName), "a") as f:
+                        f.write("set param_card mass "+pdgidBStar+" %e\n" % mbs)
+                        f.write("set param_card mass "+pdgidW+" %e\n" % mw)
+                        f.write("set param_card DECAY "+pdgidBStar+" %e\n" % (mbs * WidthBStar))
