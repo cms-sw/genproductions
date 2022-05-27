364c474f8a6c6d60528ef1f670a6f0dc74c7fc74
jshin96 <87310162+jshin96@users.noreply.github.com>
2022-05-27 11:02:33 +0900

    Update Card_Gen.sh

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ChargedHiggsToCB/Card_Gen.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ChargedHiggsToCB/Card_Gen.sh
@@ -13,4 +13,3 @@ do
     cp ChargedHiggsToCB_M160_proc_card.dat ChargedHiggsToCB_M${mass}/ChargedHiggsToCB_M${mass}_proc_card.dat
     sed -i 's|output ChargedHiggsToCB_M160 -nojpeg|output ChargedHiggsToCB_M'"${mass}"' -nojpeg|g' ChargedHiggsToCB_M${mass}/ChargedHiggsToCB_M${mass}_proc_card.dat
 done
- 
