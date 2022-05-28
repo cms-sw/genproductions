58421c3b12ef819ccdbd79236980834a89cdb7ae
Markus Seidel <markus.seidel@cern.ch>
2022-05-28 13:22:47 +0200

    Test multiple CMSSW releases

--- bin/Powheg/test/test_build.sh
+++ bin/Powheg/test/test_build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 
-while getopts r:p: flag
+while getopts r:a:p: flag
 do
     case "${flag}" in
         r) CMSSW_RELEASE=${OPTARG};;
+        a) SCRAM_ARCH=${OPTARG};;
         p) PROC=${OPTARG};;
     esac
 done
@@ -27,6 +28,7 @@ POWHEG_DIR="$(dirname $(dirname $(realpath $0)))"
 BASE_DIR=/tmp/$USER/test_$PROC
 
 echo "CMSSW release: $CMSSW_RELEASE";
+echo "Architecture: $SCRAM_ARCH";
 echo "Test process: $PROC";
 echo "Test config: $INPUT"
 echo "Base directory: $BASE_DIR"
@@ -39,15 +41,21 @@ cd $BASE_DIR
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 /cvmfs/cms.cern.ch/common/scramv1 project CMSSW  $CMSSW_RELEASE
 if [ ! -e $CMSSW_RELEASE ]; then
-    echo "Error: no CMSSW area"
-    exit 1
+    echo "Error: no CMSSW area, wait a little to recover CVMFS issue"
+    sleep 10
+    source /cvmfs/cms.cern.ch/cmsset_default.sh
+    /cvmfs/cms.cern.ch/common/scramv1 project CMSSW  $CMSSW_RELEASE
+    if [ ! -e $CMSSW_RELEASE ]; then
+        echo "Error: no CMSSW area, aborting"
+        exit 1
+    fi
 fi
 cd $CMSSW_RELEASE/src
 eval `/cvmfs/cms.cern.ch/common/scramv1 runtime -sh`
 ln -s $POWHEG_DIR/* .
 # run Powheg
 python run_pwg_condor.py -p f -i $INPUT -m $PROC -d 1
-cp testProd/pwg-stat.dat test/pwg-stat.dat.$PROC
+cp testProd/pwg-stat.dat test/pwg-stat.dat.${PROC}_${SCRAM_ARCH}_${CMSSW_RELEASE}
 
 GRIDPACK=${PROC}_${SCRAM_ARCH}_${CMSSW_RELEASE}_testProd.tgz
 if [ ! -f $GRIDPACK ]; then
