fb4461a414671be34254661d9e020d9aae069d86
Konstantinos Christoforou <konstantinos.christoforou@cern.ch>
2022-08-29 11:19:43 +0300

    cards[ChargedHiggs_hw]: Updates for additional mass points for both charged and neutral Higgs

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ChargedHiggs_hw/ChargedHiggs_hw_mh1_90_NLO_cards.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ChargedHiggs_hw/ChargedHiggs_hw_mh1_90_NLO_cards.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-masses=(175 200 220 250 300 400 500 600 700 800 900 1000)
+masses=(185 200 220 250 300 400 500 600 700 800 900 1000 1250 1500 2000)
 
 sample=ChargedHiggs_hw_mh1_90_NLO
 postfix=(_run_card.dat _customizecards.dat _proc_card.dat _extramodels.dat _madspin_card.dat)
