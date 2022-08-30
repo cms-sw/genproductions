f1a4f957b5105fc7aa5bd7d6dbb379e13b2a264a
menglu21 <1501110102@pku.edu.cn>
2022-08-30 14:41:31 +0200

    modify pdf related scripts for run3

--- Utilities/scripts/getMG5_aMC_PDFInputs.py
+++ Utilities/scripts/getMG5_aMC_PDFInputs.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--format', choices=['systematics', 'central', 'members', 'sets'], 
         required=True, help='Output PDF set list or list of members to store')
-parser.add_argument('-c', '--pdf_choice', choices=['custom', '2016', '2017'],
+parser.add_argument('-c', '--pdf_choice', choices=['custom', '2016', '2017', 'run3'],
         required=True, help="Use 2017 or 2016 defaults or custom choice")
 parser.add_argument('--is5FlavorScheme', action='store_true',
         help='Use PDF set for 5 flavor scheme (vs. 4F if false)')
@@ -14,7 +14,7 @@ parser.add_argument('--isNLO', action='store_true',
 args = parser.parse_args()
 
 helper = PDFSetHelper_MG5_aMC()
-if args.pdf_choice == '2017':
+if args.pdf_choice == 'run3':
     helper.readDefaultPDFsFile(args.is5FlavorScheme)
 else:
     #TODO Implement option for custom PDF list
