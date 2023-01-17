454f2f9610c5730f3c6dd226caf54fe7256289c2
agrohsje <agrohsje@users.noreply.github.com>
2023-01-17 18:26:26 +0100

    Update runcmsgrid_powhegjhugen.sh
    fixed echo for powheg + jhugen

--- bin/Powheg/runcmsgrid_powhegjhugen.sh
+++ bin/Powheg/runcmsgrid_powhegjhugen.sh
@@ -41,7 +41,7 @@ if [ "$use_gridpack_env" = true ]
       else
         scram_arch_version=SCRAM_ARCH_VERSION_REPLACE
     fi
-    echo "%MSG-MG5 SCRAM_ARCH version = $scram_arch_version"
+    echo "%MSG-POWHEG SCRAM_ARCH version = $scram_arch_version"
 
     if [ -n "$6" ]
       then
@@ -49,7 +49,7 @@ if [ "$use_gridpack_env" = true ]
       else
         cmssw_version=CMSSW_VERSION_REPLACE
     fi
-    echo "%MSG-MG5 CMSSW version = $cmssw_version"
+    echo "%MSG-POWHEG CMSSW version = $cmssw_version"
     export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
     source $VO_CMS_SW_DIR/cmsset_default.sh
     export SCRAM_ARCH=${scram_arch_version}
