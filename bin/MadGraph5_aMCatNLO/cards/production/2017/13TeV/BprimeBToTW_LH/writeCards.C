9a8621116e8ddf6b5b2b36cbc5d405c3abeeed33
jmhogan <j.hogan@cern.ch>
2023-03-10 22:31:46 +0100

    card copiers

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/BprimeBToTW_LH/writeCards.C
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/BprimeBToTW_LH/writeCards.C
@@ -11,7 +11,7 @@ void writeCards(double masspoint, std::string path) {
   std::string filename;
   std::stringstream stream;
   stream << std::fixed << std::setprecision(0) << masspoint;
-  filename = path + "PairVLQ_b13b13_Incl_NLO_narrow_M" + stream.str() + "_customizecards.dat";
+  filename = path + "BprimeBToTW_LH_narrowLO_M" + stream.str() + "_customizecards.dat";
   ofstream f;
   f.open(filename);
   f << "set param_card mass 6000007 " << stream.str() << std::endl;
