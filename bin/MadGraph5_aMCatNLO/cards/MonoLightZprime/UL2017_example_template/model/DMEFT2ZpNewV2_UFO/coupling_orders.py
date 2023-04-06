5fbc38786b40aec9fcea74e397cce7336af5d368
Abhishikth Mallampalli <abhishikthmallampalli@gmail.com>
2023-04-06 09:32:20 -0500

    adding model

---
+++ bin/MadGraph5_aMCatNLO/cards/MonoLightZprime/UL2017_example_template/model/DMEFT2ZpNewV2_UFO/coupling_orders.py
@@ -0,0 +1,24 @@
+# This file was automatically created by FeynRules 2.3.22
+# Mathematica version: 11.0.0 for Mac OS X x86 (64-bit) (July 28, 2016)
+# Date: Tue 14 Aug 2018 21:43:14
+
+
+from object_library import all_orders, CouplingOrder
+
+
+dmvector = CouplingOrder(name = 'dmvector',
+                         expansion_order = 99,
+                         hierarchy = 1)
+
+dmzpu = CouplingOrder(name = 'dmzpu',
+                      expansion_order = 99,
+                      hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
