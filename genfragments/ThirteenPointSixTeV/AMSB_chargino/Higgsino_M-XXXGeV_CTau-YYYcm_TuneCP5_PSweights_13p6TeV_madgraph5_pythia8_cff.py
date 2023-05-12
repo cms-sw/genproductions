9fd737417ccb387ee6e4ae06ad60814430422eb4
Breno Orzari <breno.orzari@hotmail.com>
2023-05-12 11:56:22 +0200

    Fixing nQmatch in hadronizer templates

--- genfragments/ThirteenPointSixTeV/AMSB_chargino/Higgsino_M-XXXGeV_CTau-YYYcm_TuneCP5_PSweights_13p6TeV_madgraph5_pythia8_cff.py
+++ genfragments/ThirteenPointSixTeV/AMSB_chargino/Higgsino_M-XXXGeV_CTau-YYYcm_TuneCP5_PSweights_13p6TeV_madgraph5_pythia8_cff.py
@@ -46,7 +46,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
             'JetMatching:coneRadius = 1.',
             'JetMatching:slowJetPower = 1',
             'JetMatching:qCut = %.1f' % (XQCUT * 1.5), #this is the actual merging scale
-            'JetMatching:nQmatch = 5', #4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
+            'JetMatching:nQmatch = 4', #4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
             'JetMatching:nJetMax = 1', #number of partons in born matrix element for highest multiplicity
             'JetMatching:doShowerKt = off', #off for MLM matching, turn on for shower-kT matching
        ),
