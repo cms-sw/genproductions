60ea70746e4b39c8dda758453f5d91be0f2596b2
jmhogan <j.hogan@cern.ch>
2023-06-02 16:50:03 +0200

    fix ebeam, path, weight name for finite width

--- bin/MadGraph5_aMCatNLO/cards/production/13p6TeV/BprimeBToTW_LH/createCards_BprimeBToTW_LH_tenpctLO.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/13p6TeV/BprimeBToTW_LH/createCards_BprimeBToTW_LH_tenpctLO.sh
@@ -1,8 +1,8 @@
 # script to create production cards for 2016 bstar from example card (bstar_tW_Left_M700)
 
-masspoints=(800 1000 1200 1300 1400 1500 1600 1700 1800 2000 2200 2400)
-base_dir="2017/13TeV/BprimeBToTW_LH/"
-template_dir="2017/13TeV/BprimeBToTW_LH/BprimeBToTW_LH_tenpctLO_M1000/"
+masspoints=(600 800 1000 1200 1300 1400 1500 1600 1700 1800 2000 2200 2400)
+base_dir="13p6TeV/BprimeBToTW_LH/"
+template_dir="13p6TeV/BprimeBToTW_LH/BprimeBToTW_LH_tenpctLO_M1000/"
 
 # loop over all masspoints
 for mp in "${masspoints[@]}"
