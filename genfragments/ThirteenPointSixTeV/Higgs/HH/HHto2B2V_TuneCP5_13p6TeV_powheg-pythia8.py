f2fcf68f132840789e6ea5a174f9436d7346578d
Javier Duarte <jduarte@ucsd.edu>
2023-09-04 16:31:09 -0700

    make inclusive

--- genfragments/ThirteenPointSixTeV/Higgs/HH/HHto2B2V_TuneCP5_13p6TeV_powheg-pythia8.py
+++ genfragments/ThirteenPointSixTeV/Higgs/HH/HHto2B2V_TuneCP5_13p6TeV_powheg-pythia8.py
@@ -18,21 +18,17 @@ generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
         processParameters = cms.vstring(
             'POWHEG:nFinal = 2',   # Number of final state particles (BEFORE THE DECAYS) in the LHE other than emitted extra parton
             '23:mMin = 0.05',
-            '23:onMode = off',
-            '23:onIfAny = 1 2 3 4 5', # Add Z->qq,bb decays
             '24:mMin = 0.05',
-            '24:onMode = on',
-            '24:onIfAny = 1 2 3 4', # Add W->qq decays 
             '25:m0 = 125.0',
             '25:onMode = off',
             '25:onIfMatch = 5 -5', # Add H->bb decay
             '25:onIfMatch = 24 -24', # Add H->WW decay
             '25:onIfMatch = 23 23', # Add H->ZZ decay
             'ResonanceDecayFilter:filter = on',
-            'ResonanceDecayFilter:exclusive = on', #on: require exactly the specified number of daughters
-            'ResonanceDecayFilter:udscAsEquivalent = on', #on: treat udsc quarks as equivalent
-            'ResonanceDecayFilter:mothers = 23,24,25',
-            'ResonanceDecayFilter:daughters = 5,5,1,1,1,1',
+            'ResonanceDecayFilter:exclusive = on', #off: require at least the specified number of daughters, on: require exactly the specified number of daughters
+            'ResonanceDecayFilter:mothers = 25', #list of mothers not specified -> count all particles in hard process+resonance decays (better to avoid specifying mothers when including leptons from the lhe in counting, since intermediate resonances are not gauranteed to appear in general
+            'ResonanceDecayFilter:wzAsEquivalent = on',
+            'ResonanceDecayFilter:daughters = 5,5,23,23'
           ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CP5Settings',
