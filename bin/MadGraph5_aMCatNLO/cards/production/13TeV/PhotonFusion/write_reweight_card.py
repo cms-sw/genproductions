691e79e48b06a1793d2988a7ee3388472ed92acc
Michael Pitt <m_u_pitt@yahoo.com>
2024-06-25 09:28:50 +0200

    add WW BSM samples

--- bin/MadGraph5_aMCatNLO/cards/production/13TeV/PhotonFusion/write_reweight_card.py
+++ bin/MadGraph5_aMCatNLO/cards/production/13TeV/PhotonFusion/write_reweight_card.py
@@ -13,12 +13,25 @@ def GetName(x):
 couplings={}
 
 # yy > tautau BSM couplings:
-couplings['ceBRe'] = [np.linspace(-40,40,101), ('smeft','ceBRe33')]
-couplings['ceBIm'] = [np.linspace(-40,40,101), ('smeftcpv','ceBIm33')]
+#couplings['ceBRe'] = [np.linspace(-40,40,101), ('smeft','ceBRe33')]
+#couplings['ceBIm'] = [np.linspace(-40,40,101), ('smeftcpv','ceBIm33')]
 
 # yy > WW BSM couplings:
-#couplings['cw'] = [np.linspace(-10,10,21), ('Dim6','2')]
-#couplings['cb'] = [np.linspace(-20,20,21), ('Dim6','3')]
+couplings['fm0'] = [np.linspace(-300,300,21), ('anoinputs','4')]
+couplings['fm1'] = [np.linspace(-1000,1000,21), ('anoinputs','5')]
+couplings['fm2'] = [np.linspace(-40,40,21), ('anoinputs','6')]
+couplings['fm3'] = [np.linspace(-200,200,21), ('anoinputs','7')]
+couplings['fm4'] = [np.linspace(-150,150,21), ('anoinputs','8')]
+couplings['fm5'] = [np.linspace(-300,300,21), ('anoinputs','9')]
+couplings['fm7'] = [np.linspace(-2000,2000,21), ('anoinputs','11')]
+couplings['ft0'] = [np.linspace(-2.3,2.3,21), ('anoinputs','12')]
+couplings['ft1'] = [np.linspace(-30,30,21), ('anoinputs','13')]
+couplings['ft2'] = [np.linspace(-10,10,21), ('anoinputs','14')]
+couplings['ft3'] = [np.linspace(-10,10,21), ('anoinputs','15')]
+couplings['ft4'] = [np.linspace(-2,2,21), ('anoinputs','16')]
+couplings['ft5'] = [np.linspace(-0.5,0.5,21), ('anoinputs','17')]
+couplings['ft6'] = [np.linspace(-1,1,21), ('anoinputs','18')]
+couplings['ft7'] = [np.linspace(-2,2,21), ('anoinputs','19')]
 
 #couplings['ceBRe33'] = np.linspace(-10,10,3) # use this line for tests
 
@@ -41,7 +54,6 @@ with open(outfile, 'w') as f:
         for coupling in couplings:
           if coupling==sel_coupling: f.write('set param_card %s %s %1.5e\n'%(model_params[0],model_params[1],val))
           else: f.write('set param_card %s %s %1.5e\n'%(couplings[coupling][1][0],couplings[coupling][1][1],0))
-        f.write('set param_card mass 15 1.777\n')
         f.write('\n')
       f.write('\n')
 print('writes: '+outfile+', move this files to cards/PROCESSNAME/PROCESSNAME_reweight_card.dat')
