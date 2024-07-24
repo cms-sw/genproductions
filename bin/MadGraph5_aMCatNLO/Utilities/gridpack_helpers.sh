ea92b71f6a542edbf070cb362042b99d233a9b87
shdutta16 <shdutta@cern.ch>
2024-07-24 07:12:26 +0200

    Reverted changes made to 'gridpack_helpers.sh' and 'gridpack_generation.sh'. Replaced the cards with corresponding templates.

--- bin/MadGraph5_aMCatNLO/Utilities/gridpack_helpers.sh
+++ bin/MadGraph5_aMCatNLO/Utilities/gridpack_helpers.sh
@@ -4,46 +4,23 @@ set_run_card_pdf () {
     is5FlavorScheme=$3
     script_dir=$4
 
-    
     pdfExtraArgs=""
-    echo ""
-    echo "################################################################"
-    echo "is5FlavorScheme = " $is5FlavorScheme
-    echo "################################################################"
-    echo ""
     if [ $is5FlavorScheme -eq 1 ]; then
         pdfExtraArgs+="--is5FlavorScheme "
     fi
 
-    
     if grep -q -e "\$DEFAULT_PDF_SETS" $CARDSDIR/${name}_run_card.dat; then
-	
         local central_set=$(python ${script_dir}/getMG5_aMC_PDFInputs.py -f "central" -c 2017 $pdfExtraArgs)
         echo "INFO: Using default PDF sets for 2017 production"
 
         sed "s/\$DEFAULT_PDF_SETS/${central_set}/g" $CARDSDIR/${name}_run_card.dat > ./Cards/run_card.dat
         sed -i "s/ *\$DEFAULT_PDF_MEMBERS.*=.*//g" ./Cards/run_card.dat
-
-	echo ""
-	echo "################################################################"
-	echo "DEFAULT_PDF_SETS = " $central_set
-	echo "################################################################"
-	echo ""
-	
     elif grep -q -e "\$DEFAULT_PDF_SETS" -e "\$DEFAULT_PDF_MEMBERS" $CARDSDIR/${name}_run_card.dat; then
-	
         local central_set=$(python ${script_dir}/getMG5_aMC_PDFInputs.py -f "central" -c 2016 $pdfExtraArgs)
 
         echo "INFO: Using default PDF sets for 2017 production"
         sed "s/\$DEFAULT_2016_PDF_SETS/${central_set}/g" $CARDSDIR/${name}_run_card.dat > ./Cards/run_card.dat
         sed -i "s/ *\$DEFAULT_PDF_MEMBERS.*=.*//g" ./Cards/run_card.dat
-
-	echo ""
-	echo "################################################################"
-        echo "DEFAULT_PDF_SETS = " $central_set
-        echo "################################################################"
-	echo ""
-	
     else
         cat << EOF
 
