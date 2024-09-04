64fc2938d7668e9890f41134139ac5e0c20e1ce4
Matej Roguljic <matej.roguljic@gmail.com>
2024-09-04 14:40:40 +0200

    fragments for Y->tt had and Y->uu

---
+++ genfragments/ThirteenTeV/B2G/ResonanceDecayFilter_XToYH_HTo2bYTo2q_cff.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        processParameters = cms.vstring(
+            '25:onMode = off', # Turn off all H decays
+            '25:oneChannel = 1 1 100 5 -5', # H->b b~
+            '25:onIfMatch = 5 -5',
+            '35:oneChannel = 1 1 100 1 -1', # Y->u u~
+            '35:onMode = off',
+            '35:onIfMatch = 1 -1'
+          ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings',
+                                    'processParameters')
+    )
+)
