5288b37910491a2346d55c37ebae3f4313b8249c
Luca Della Penna <160840792+ldellape@users.noreply.github.com>
2024-11-08 11:18:29 +0100

    cards PR

--- bin/MadGraph5_aMCatNLO/Utilities/source_condor.sh
+++ bin/MadGraph5_aMCatNLO/Utilities/source_condor.sh
@@ -3,9 +3,6 @@
 # HTCondor python bindings are lost after cmsenv/scram
 #PYTHON_BINDINGS="$(python3 -c 'import htcondor; import os; print(os.path.dirname(htcondor.__path__[0]))' 2>/dev/null)"
 #if [ -z "$PYTHON_BINDINGS" ]; then
-export _condor_SCHEDD_HOST=bigbird17.cern.ch
-export _condor_CREDD_HOST=bigbird17.cern.ch 
-
 if [ -d $CMSSW_BASE/venv ];
 then 
   echo "venv exists"
