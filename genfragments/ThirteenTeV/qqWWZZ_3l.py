4004c35cd315036b6c1a7e3e0b99c3ef4c55cca4
Ana Sculac <73283250+asculac@users.noreply.github.com>
2024-12-18 12:48:44 +0100

    Create qqWWZZ_3l.py

---
+++ genfragments/ThirteenTeV/qqWWZZ_3l.py
@@ -0,0 +1,52 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/eos/home-r/rjiang/wwzz/qq_3l_new/qq_wwzz_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz'), #private gridpack
+    nEvents = cms.untracked.uint32(20000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+#Link to datacards:
+#https://github.com/cms-sw/genproductions/tree/master/bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/exo_diboson/
+
+
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(13000.),
+                         PythiaParameters = cms.PSet(
+              pythia8CommonSettingsBlock,
+              pythia8CP5SettingsBlock,
+              pythia8PSweightsSettingsBlock,
+              pythia8PowhegEmissionVetoSettingsBlock,
+              processParameters = cms.vstring(
+              'POWHEG:nFinal = 4',   ## Number of final state particles
+              '23:mMin = 0.05',
+              '24:mMin = 0.05',
+              '23:onMode = off', # disable all Z decay modes
+              '23:onIfAny = 1 2 3 4 5 6 7 8 11 13 15', # enable only Z->ll and Z->qq
+            'ResonanceDecayFilter:filter = on',
+            'ResonanceDecayFilter:exclusive = on', #off: require at least the specified number of daughters, on: require exactly the specified number of daughters
+             'ResonanceDecayFilter:mothers = 24,23', #? TBC! list of mothers not specified -> count all particles in hard process+resonance decays 
+            'ResonanceDecayFilter:eMuAsEquivalent = on', #on: treat electrons and muons as equivalent
+            'ResonanceDecayFilter:daughters = 11,11,11',
+           ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings',
+                                    'pythia8PowhegEmissionVetoSettings',
+                                    'processParameters'
+                                    )
+        )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
