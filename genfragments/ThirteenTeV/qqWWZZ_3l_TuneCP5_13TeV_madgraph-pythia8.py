6dac54e28a8a911574d2d5cbb456b91f3d8715e8
Ana Sculac <73283250+asculac@users.noreply.github.com>
2025-02-14 13:34:31 +0100

    fix qqWWZZ_3l_13TeV

--- genfragments/ThirteenTeV/qqWWZZ_3l_TuneCP5_13TeV_madgraph-pythia8.py
+++ genfragments/ThirteenTeV/qqWWZZ_3l_TuneCP5_13TeV_madgraph-pythia8.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
-from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
@@ -14,9 +13,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
               pythia8CommonSettingsBlock,
               pythia8CP5SettingsBlock,
               pythia8PSweightsSettingsBlock,
-              pythia8PowhegEmissionVetoSettingsBlock,
               processParameters = cms.vstring(
-              'POWHEG:nFinal = 4',   ## Number of final state particles
               '23:mMin = 0.05',
               '24:mMin = 0.05',
               '23:onMode = off', # disable all Z decay modes
@@ -30,7 +27,6 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CP5Settings',
                                     'pythia8PSweightsSettings',
-                                    'pythia8PowhegEmissionVetoSettings',
                                     'processParameters'
                                     )
         )
